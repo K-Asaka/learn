0a68f98fe18bbffe3ab72ddb898f0d0dcb620316
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-12-19 01:05:08 +0900

    最終的な結果を表示する

--- js-kyohon/04/janken/practice/js/app.js
+++ js-kyohon/04/janken/practice/js/app.js
@@ -1,30 +1,62 @@
-/* 変数定義 ******/
-// ジャンケンの手の番号を設定
-var GU    = 1;
-var CHOKI = 2;
-var PA    = 3;
+function janken() {
+  /* 変数定義 ******/
+  // ジャンケンの手の番号を設定
+  var GU    = 1;
+  var CHOKI = 2;
+  var PA    = 3;
 
-/* 函数定義 ******/
-function getHumHand() {
-  var hum = prompt('半角数字で1〜3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
-  hum = parseInt(hum, 10);
-  if (hum !== GU && hum !== CHOKI && hum !== PA) {
-    return undefined;
+  /* 函数定義 ******/
+  function getHumHand() {
+    var hum = prompt('半角数字で1〜3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
+    hum = parseInt(hum, 10);
+    if (hum !== GU && hum !== CHOKI && hum !== PA) {
+      return undefined;
+    } else {
+      return hum;
+    }
+  }
+
+  // コンピュータの手を決める
+  function getComHand() {
+    return Math.floor(Math.random() * 3) + 1;
+  }
+
+  // コンピュータの手の名前を取得
+  function getHandName(num) {
+    switch (num) {
+      case GU:
+        return 'グー';
+      case CHOKI:
+        return 'チョキ';
+      case PA:
+        return 'パー';
+    }
+  }
+
+  // 結果の判定
+  function getResult(com, hum) {
+    if (hum === com) {
+      return '結果はあいこでした。';
+    } else if ((com === GU && hum === PA) || (com === CHOKI && hum === GU) || (com === PA && hum === CHOKI)) {
+      return '勝ちました。';
+    } else {
+      return '負けました。';
+    }
+  }
+
+  // 最終的な結果のメッセージ
+  function getResultMsg(com, hum) {
+    return getResult(com, hum) + 'コンピュータの出した手は「' + getHandName(com) + '」でした';
+  }
+
+  /* 実行する処理 ******/
+  var hum = getHumHand();
+  if (!hum) {
+    alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
   } else {
-    return hum;
+    var com = getComHand();
+    alert(getResultMsg(com, hum));
   }
 }
 
-// コンピュータの手を決める
-function getComHand() {
-  return Math.floor(Math.random() * 3) + 1;
-}
-
-/* 実行する処理 ******/
-var hum = getHumHand();
-if (!hum) {
-  alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
-} else {
-  var com = getComHand();
-  console.log(com); // この行は動作確認したら消す
-}
+janken();
