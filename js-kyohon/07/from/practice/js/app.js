e4a084fc4c5866e0861b7b5f2cf32ffeaf57ad70
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-12-19 12:11:31 +0900

    制限時間付きの回答フォームにする

--- js-kyohon/07/from/practice/js/app.js
+++ js-kyohon/07/from/practice/js/app.js
@@ -6,6 +6,8 @@ var textarea = document.getElementById('textarea');
 
 // 文字数制限
 var maxTextNum = textarea.getAttribute('maxlength');
+// 残り時間(秒)
+var reminingTimeNum = 10;
 
 /* 要素の追加 ******/
 // 残り文字数を表示する要素の追加
@@ -13,11 +15,25 @@ var textMessage = document.createElement('div');
 var parent = textarea.parentElement;
 parent.insertBefore(textMessage, textarea);
 
+// 残り時間を表示する要素の追加
+var timeMessage = document.createElement('div');
+parent.insertBefore(timeMessage, null);
+
 /* イベント処理 ******/
 // お問い合わせボタンを押したとき
 button.addEventListener('click', function() {
   // フォームを表示
   form.style.display = 'block';
+
+  // タイマー処理で残り時間を表示
+  var timerId = setInterval(function() {
+    timeMessage.innerHTML = '<p>制限時間:' + reminingTimeNum + '秒</p>';
+    if (reminingTimeNum <= 0) {
+      alert('制限時間終了');
+      clearInterval(timerId);
+    }
+    reminingTimeNum--;
+  }, 1000);
 });
 
 // テキストエリアでキーをタイプしたとき
