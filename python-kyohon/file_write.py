0826bca2a432456933e964bf5371c960257b7f6b
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-12-21 15:16:35 +0900

    複数行のデータを書き込む

--- python-kyohon/file_write.py
+++ python-kyohon/file_write.py
@@ -1,5 +1,5 @@
 write_file = open('output.txt', 'w')
-write_file.write('Hello World!')
-write_file.write('Hello Python!')
+write_file.write('Hello World!\n')
+write_file.write('Hello Python!\n')
 write_file.close()
 
