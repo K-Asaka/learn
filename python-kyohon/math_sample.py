2c811fc4d91056bc64778e5dc57f615f9754cec3
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-12-21 18:55:26 +0900

    mathモジュールを使う

---
+++ python-kyohon/math_sample.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.ceil(1.1))
+print(math.floor(1.1))
+print(math.ceil(-1.1))
+print(math.floor(-1.1))
+
+print(math.pow(2, 5))
+print(math.factorial(10))
+print(math.sqrt(2))
+print(math.sqrt(123454321))
+
