063bbb06d3788c3284b38b167b4b81e9162271c9
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-07 17:18:41 +0900

    scope.rbを変更。class...endでのスコープ確認

--- bootupruby/scope.rb
+++ bootupruby/scope.rb
@@ -13,6 +13,16 @@ end
 next_of(2)
 next_of(2)
 prev_of(2)
-p value
-p undefined_variable
+#p value
+#p undefined_variable
+
+class Duration
+  attribute_names = %w[days hours minutes seconds ]
+  p attribute_names
+  attribute_names.each do |name|
+    attr_accessor name
+  end
+end
+duration = Duration.new
+#p attribute_names
 
