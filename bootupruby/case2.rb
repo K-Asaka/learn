58c975e927eebf943a9cddde10f7fc47345274d3
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-07 18:05:26 +0900

    case2.rbを変更。case比較演算子の内容を追加

--- bootupruby/case2.rb
+++ bootupruby/case2.rb
@@ -7,3 +7,13 @@ when 1..9   then '1けた'
 when 10..99 then '2けた'
 end
 
+# case比較演算子
+# 上記コードとほぼ同義
+if 0 === value then
+  puts '0'
+elsif (1..9) === value then
+  puts '1けた'
+elsif (10..99) === value then
+  puts '2けた'
+end
+
