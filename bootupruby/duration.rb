28a70c2d5dc49f8a0832d1bae9d8f8d5e00caa8d
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-08 14:38:09 +0900

    duration.rbを変更。クラスメソッドの定義をselfを使った形に変更

--- bootupruby/duration.rb
+++ bootupruby/duration.rb
@@ -1,7 +1,7 @@
 #! /usr/local/bin/ruby
 # -*- coding: utf-8 -*-
 class Duration
-  def Duration.print(x); p x end
+  def self.print(x); p x end
   def display; puts self end
 end
 duration = Duration.new
