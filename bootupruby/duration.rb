19b5f4ea26d1232306244f0a5520dc2cb461d4f0
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-08 14:49:25 +0900

    duration.rbを変更。インスタンス化の確認

--- bootupruby/duration.rb
+++ bootupruby/duration.rb
@@ -1,10 +1,19 @@
 #! /usr/local/bin/ruby
 # -*- coding: utf-8 -*-
 class Duration
+  def initialize(since, till)
+    @since = since
+    @until = till
+  end
+  attr_accessor :since, :until
   def self.print(x); p x end
   def display; puts self end
 end
-duration = Duration.new
+duration = Duration.new(Time.now, Time.now + 3600)
+p duration.until
+p duration.since
+p duration.since = Time.now
+p duration.since
 duration.display
 Duration.print 1
 
