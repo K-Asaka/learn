ff4c857130e50460fd9de80481da8cac351c5bc6
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-08 18:59:42 +0900

    singularclass.rbを追加。特異クラスの確認

---
+++ bootupruby/singularclass.rb
@@ -0,0 +1,16 @@
+#! /usr/local/bin/ruby
+# -*- coding: utf-8 -*-
+CENTRAL_REPOSITORY = Object.new
+class << CENTRAL_REPOSITORY
+  def register(target)
+    @registered_objects ||= []
+    unless @registered_objects.include? target
+      @registered_objects << target
+    end
+  end
+  def unregister(target)
+    @registered_objects ||= []
+    @registered_objects.delete(target)
+  end
+end
+
