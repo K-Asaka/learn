16795b907b449374291576253cb94a8b64deb867
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-08 14:32:31 +0900

    duration.rbを変更。クラスメソッドの定義を確認

--- bootupruby/duration.rb
+++ bootupruby/duration.rb
@@ -1,8 +1,10 @@
 #! /usr/local/bin/ruby
 # -*- coding: utf-8 -*-
 class Duration
+  def Duration.print(x); p x end
   def display; puts self end
 end
 duration = Duration.new
 duration.display
+Duration.print 1
 
