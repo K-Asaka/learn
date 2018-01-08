6bde04cadf9da164b2dea1f1228ea404135ae4e0
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-01-08 17:41:30 +0900

    singleton.rbを追加。Singletonパターン

---
+++ bootupruby/singleton.rb
@@ -0,0 +1,14 @@
+#! /usr/local/bin/ruby
+# -*- coding: utf-8 -*-
+CENTRAL_REPOSITORY = Object.new
+def CENTRAL_REPOSITORY.register(target)
+  @registered_objects ||= []
+  unless @registered_objects.include? target
+    @registered_objects << target
+  end
+end
+def CENTRAL_REPOSITORY.unregister(target)
+  @registered_objects ||= []
+  @registered_objects.delete(target)
+end
+
