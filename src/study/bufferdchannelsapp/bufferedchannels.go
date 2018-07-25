85986a8a9550d0193af4db50db8b12eb4db53132
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-07-25 21:52:38 +0900

    mod: RangeとCloseを使ったコードを追加した

--- src/study/bufferdchannelsapp/bufferedchannels.go
+++ src/study/bufferdchannelsapp/bufferedchannels.go
@@ -14,10 +14,35 @@ package main
 
 import "fmt"
 
+func fibonacci(n int, c chan int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
 	c := make(chan int, 2) // 2を1に修正するとエラーが発生する。2を3に修正すると正常に実行される
 	c <- 1
 	c <- 2
 	fmt.Println(<-c)
 	fmt.Println(<-c)
+	// 上の例では、2回cの値を読み込む必要がある。これではあまり便利では無い。
+	// Goはこの点を考慮し、rangeによってsliceやmapを操作するのと同じ感覚でバッファリング型のchannelを操作することができる。
+	c1 := make(chan int, 10)
+	go fibonacci(cap(c1), c1)
+	for i := range c1 {
+		fmt.Println(i)
+	}
+	// for i := range c1でこのchannelがクローズを明示されるまで連続してchannelの中のデータを
+	// 読み込むことができる。上のコードでchannelのクローズが明示されているのが確認できる。
+	// 生産者はcloseビルトイン関数によってchannelを閉じる。channelを閉じた後はいかなるデータも送信することはできない。
+	// 消費側は、v, ok := <-chという式でchannelがすでに閉じられているかテストすることができる。
+	// もしokにfalseが返ってきたら、channelはすでにどのようなデータも無く、閉じらてていることになる。
+	// 生産者でchannelが閉じられる。消費側ではない。これは容易にpanicを引き起こす
+	// また、channelはファイルのようなものではない。
+	// 頻繁に閉じる必要はない。何のデータも送ることが無い場合はrangeループを終了させたい場合などで良い。
+
 }
