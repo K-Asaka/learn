6d925643490caf26a3cf672a825fbb0f3f98f083
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2018-09-30 23:13:07 +0900

    add: webpack確認用のファイルを追加

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: `./src/index.js`,
+
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'main.js'
+    },
+    devServer: {
+        contentBase: 'dist',
+        open: true
+    }
+};
