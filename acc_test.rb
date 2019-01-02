4a17056c06db1a4fd0f6a3af83800bedf729051d
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-02 19:43:32 +0900

    [mod] 複数のメソッドをまとめて同じアクセス制限に定義したい場合の記述例

--- acc_test.rb
+++ acc_test.rb
@@ -1,13 +1,13 @@
 class AccTest
+    public      # 引数を指定しなければ、これ以降に定義したメソッドはpublicになる
     def pub
         puts "pub is a public method."
     end
-    public :pub     # pubメソッドをpublicに設定(指定しなくてもよい)
 
+    private     # これ以降に定義したメソッドはprivateになる
     def priv
         puts "priv is a private method."
     end
-    private :priv   # privメソッドをprivateに設定
 end
 
 acc = AccTest.new
