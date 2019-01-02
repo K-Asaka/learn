bfbd97644770cbb5fe598468b76d8b131f50d027
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-02 22:58:45 +0900

    [mod] 同じモジュールを2回以上インクルードした場合の確認

--- method_search.rb
+++ method_search.rb
@@ -10,13 +10,10 @@ module M2
     end
 end
 
-module M3
-    include M2      # M2をインクルードする
-end
-
 class C
     include M1       # M1をインクルードする
-    include M3       # M3をインクルードする
+    include M2       # M2をインクルードする
+    include M1       # M1をインクルードする
 end
 
 p C.ancestors
