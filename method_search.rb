1c5d796b2421c74c5b8d27ef10149ae2c6b41bfd
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-02 22:56:34 +0900

    [mod] 同じクラスに複数のモジュールをインクルードした場合の確認

--- method_search.rb
+++ method_search.rb
@@ -1,15 +1,18 @@
-module M
+module M1
+    def meth
+        "M#meth"
+    end
+end
+
+module M2
     def meth
         "M#meth"
     end
 end
 
 class C
-    include M       # Mをインクルードする
-    def meth
-        "C#meth"
-    end
+    include M1       # M1をインクルードする
+    include M2       # M2をインクルードする
 end
 
-c = C.new
-p c.meth
+p C.ancestors
