e797831dec3f47ca189a91f9f984469564097638
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-02 19:56:52 +0900

    [add] クラスを拡張する例

---
+++ ext_string.rb
@@ -0,0 +1,9 @@
+class String
+    def count_word
+        ary = self.split(/\s+/)     # selfを空白文字区切りで配列に分解する
+        return ary.size             # 分解後の配列の要素数を返す
+    end
+end
+
+str = "Just Another Ruby Newbie"
+p str.count_word
