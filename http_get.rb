9d236582dcf21dac1f2b8bee8ac7c90db603eb6a
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-02 23:33:33 +0900

    [mod] URL文字列を解析して分解した情報を扱う例

--- http_get.rb
+++ http_get.rb
@@ -4,3 +4,9 @@ url = URI.parse("http://www.ruby-lang.org/ja/")
 http = Net::HTTP.start(url.host, url.port)
 doc = http.get(url.path)
 puts doc.body
+
+p url.scheme
+p url.host
+p url.port
+p url.path
+p url.to_s
