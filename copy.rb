a35d57f5679a5ab76af2b72bc3d45f6ea823b78f
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-03 14:55:11 +0900

    [add] 例外処理の後処理を確認

---
+++ copy.rb
@@ -0,0 +1,13 @@
+def copy(from, to)
+    src = File.open(from)          # コピー元ファイルfromを開く
+    begin
+        dst = File.open(to, "w")    # コピー先ファイルtoを開く
+        data = src.read
+        dst.write(data)
+        dst.close
+    ensure
+        src.close
+    end
+end
+
+copy(ARGV[0], ARGV[1])
