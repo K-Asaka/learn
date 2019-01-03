137ddf0d8f3036570fa9e1fedbfd18f974df48ef
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-03 19:17:38 +0900

    [mod] sort_byメソッドを使ってより効率よくソートを行う

--- sort_comp_count.rb
+++ sort_comp_count.rb
@@ -5,12 +5,5 @@ ary = %w(
     that is natural to read and easy to write
 )
 
-call_num = 0                # ブロックの呼び出し回数
-sorted = ary.sort do |a, b|
-    call_num += 1           # ブロックの呼び出し回数を加算する
-    a.length <=> b.length
-end
-
-puts "ソートの結果 #{sorted}"
-puts "配列の要素数 #{ary.length}"
-puts "ブロックの呼び出し回数 #{call_num}"
+sorted = ary.sort_by { |item| item.length }
+p sorted 
