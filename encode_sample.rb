72ac2962fab32106127fe963ee65e175f30ce430
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-04 22:31:22 +0900

    [mod] nkfライブラリ

--- encode_sample.rb
+++ encode_sample.rb
@@ -6,3 +6,10 @@ p utf8_str.encoding
 
 euc_str.encode!("utf-8")    # strはUTF-8になる
 p euc_str.encoding
+
+require "nkf"
+utf8_str = NKF.nkf("-E -w -xm0", euc_str)
+p utf8_str.encoding
+# 入力時文字コードの指定を明示的に行わない場合
+utf8_str = NKF.nkf("-w -xm0", euc_str)
+p utf8_str.encoding
