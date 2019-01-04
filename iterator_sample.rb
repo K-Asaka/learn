0b5cd3ab302a77bfdafd8a975918c1513c4c5ca1
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2019-01-04 09:45:19 +0900

    [add] 数えあげ

---
+++ iterator_sample.rb
@@ -0,0 +1,32 @@
+ary = []
+10.times do |i|
+    ary << i            # iを配列の要素として追加する
+end
+p ary
+
+ary = []
+2.upto(10) do |i|
+    ary << i
+end
+p ary
+
+ary = []
+10.downto(2) do |i|
+    ary << i
+end
+p ary
+
+ary = []
+2.step(10, 3) do |i|
+    ary << i
+end
+p ary
+
+ary = []
+10.step(2, -3) do |i|
+    ary << i
+end
+p ary
+
+ary = 2.step(10).collect{|i| i * 2 }
+p ary
