b6a083a380219bb0a1d3d4f50df00c01a156bf0c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-20 20:32:28 +0900

    [add] リストを定義する

---
+++ 02/list.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 変数にリストを代入する
+tokyo_temps = [15.1, 15.4, 15.2, 15.4, 17.0, 16.9]
+
+# グラフ表示の準備
+import matplotlib.pyplot as plt
+# グラフの表示
+plt.plot(tokyo_temps)
+plt.show()
+
+# インデックスを指定して要素を取り出す
+print(tokyo_temps[0])
+
+# リスト要素の引き算
+print(tokyo_temps[5] - tokyo_temps[0])
+
+# リストの最後の要素を指定する
+print(tokyo_temps[-1] - tokyo_temps[0])
