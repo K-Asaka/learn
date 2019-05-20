3d336c12548fa0d65a2deacdc3c0ecb47a3e4446
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-20 20:23:04 +0900

    [add] 文字列と数値の変換

---
+++ 02/type.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 違うデータ型を足し算した場合のエラー
+#day = 24
+#date = day + "日"
+
+# str()関数を使う
+day = 24
+str_day = str(day)
+date = str_day + "日"
+print(date)
