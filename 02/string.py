7375c05fc60467600f43ca7df4438e6f3751f28d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-20 19:57:01 +0900

    [add] 文字列

---
+++ 02/string.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 文字列を連結する
+a_lylic = "でもね私のエネルギーは"
+a_lylic = a_lylic + "すでにインフィニティだよ。"
+print(a_lylic)
+
+# 複合演算子を使ってみる
+lylic2 = "ずっと笑顔ばかりを選んで"
+lylic2 += "泣き顔見せるのを迷ってた"
+print(lylic2)
+
+# 三連クォーテーション
+lylic3 = """強い人になろうとして
+弱い僕を封じ込めて
+ひとりぼっちになった"""
+print(lylic3)
