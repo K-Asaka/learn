a9dbd76dd69178689083a9699438a8a4e09963d6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-21 22:53:27 +0900

    [mod] キーの存在確認

--- 03/dict.py
+++ 03/dict.py
@@ -16,3 +16,17 @@ print(purple)
 
 del purple["ニックネーム"]
 print(purple)
+
+def convert_number(num):
+    # アラビア数字とローマ数字の対応表をディクショナリに定義
+    roman_nums = {1:"Ⅰ", 2:"Ⅱ", 3:"Ⅲ", 4:"Ⅳ", 5:"Ⅴ",
+                  6:"Ⅵ", 7:"Ⅶ", 8:"Ⅷ", 9:"Ⅸ"}
+    # ディクショナリのキーとして引数の整数が存在していたら
+    # キーに対応する値を戻り値にする
+    if num in roman_nums:
+        return roman_nums[num]
+    else:
+        return "[変換できません]"
+
+print(convert_number(3))
+print(convert_number(15))
