be5741a872b555c7f5d378a1a5c1a7be98b62a0b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-23 20:11:07 +0900

    [add] 数値型を操作する

---
+++ 04/integer.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print(0x1ff)
+print(hex(1023))
+print(int("0x100", 16))
+print(0b1000)
+print(bin(1023))
+print(int("0b1111111111", 2))
+
+print(0o1777)
+print(oct(1023))
+print(int("0o1777", 8))
