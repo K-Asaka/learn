e3b0f3979f3ba02b881e62eff5905a19c2219be6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-24 21:34:14 +0900

    [mod] split()メソッド

--- 04/string.py
+++ 04/string.py
@@ -8,3 +8,19 @@ str_num = "1,000,000"
 num = int(str_num.replace(",", ""))
 print(num)
 
+import matplotlib.pyplot as plt
+
+str_speeds = "38 42 20 40 39"
+str_armor = "80 50 17 50 51"
+speeds = str_speeds.split(" ")
+armors = str_armor.split(" ")
+markers = ["o", "v", "^", "<", ">"]
+
+for idx in range(len(speeds)):
+    x = int(speeds[idx])
+    y = int(armors[idx])
+    plt.scatter(x, y, marker=markers[idx])
+
+plt.show()
+
+# IV号戦車(o) LT-38(v) 八九式中戦車(^) III号突撃砲(<) M3中戦車(>)
