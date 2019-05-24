43a2b6f4ec82c41407a65ac460fd38a79d4c1b63
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-24 22:25:24 +0900

    [mod] 文字列のフォーマット

--- 04/string.py
+++ 04/string.py
@@ -37,3 +37,47 @@ print(str_speeds2.replace(" ", ","))
 speeds2 = str_speeds2.split()
 csep_speeds2 = ",".join(speeds2)
 print(csep_speeds2)
+
+# エスケープシーケンス
+def func():
+    words = "ゆく河の流れは絶えずして\nしかももとの水にあらず"
+    print(words)
+
+func()
+
+# raw文字列
+raw = r"C:\path\to\file"
+print(raw)
+
+print("{} loves Python !".format('Guido'))
+
+linkstr = '<a href="{}">{}</a>'
+for i in [ 'http://python.org',
+           'http://pypy.org',
+           'http://cython.org', ]:
+    print(linkstr.format(i, i.replace('http://', '')))
+
+# 引数の順番を指定した置換
+print("{0} {1} {0}".format('Spam', 'Ham'))
+
+# キーワード引数を指定した置換
+print("{food1} {food2} {food1}".format(food1='Spam', food2='Ham'))
+
+# ディクショナリを指定した置換
+d = {'name': 'Guido', 'birthyear': 1964}
+print("{0[birthyear]} is {0[name]}'s birthyear.".format(d))
+
+import sys
+print("Python version: {0.version}".format(sys))
+
+# 埋め込む文字列のフォーマット指定
+tmp1 = "{0:10} {1:>8}"
+print(tmp1.format('Spam', 300))
+print(tmp1.format('Ham', 200))
+
+print("{:.2%}".format(6381 / 12708))
+print("{:,}".format(10000))
+
+# f文字列
+name = "君"
+print(f"まずは{name}が落ち着け")
