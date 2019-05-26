6911b137d53331ab3fedad14da7a6953067b5b96
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 21:59:10 +0900

    [mod] メソッドの定義と初期化メソッド__init__()

--- 06/myclass.py
+++ 06/myclass.py
@@ -11,3 +11,11 @@ print(i.value)
 i2 = MyClass()
 i2.value = 12
 print(i2.value)
+
+class MyClass2():
+    def __init__(self):
+        self.value = 0
+        print("This is __init__() method !")
+
+i3 = MyClass2()
+print(i3.value)
