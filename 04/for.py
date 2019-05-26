c825671bcb6b4e71ce3c6f1cb186cc3eb1a9af91
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 11:32:10 +0900

    [mod] 2つのシーケンスを使ったループ

--- 04/for.py
+++ 04/for.py
@@ -7,3 +7,6 @@ for i in range(10, 21):
 seq = ["abcde", "fghij", "klm", "nopqrstu", "vwxyz"]
 for cnt, item in enumerate(seq):
     print(cnt, item)
+
+for n, w in zip([1, 2, 3, 4], ['a', 'b', 'c', 'd']):
+    print(n, w)
