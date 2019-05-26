ea160a52a3cefedbe4ab0f75a77c9a66744fb2b8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 17:12:33 +0900

    [mod] readline()メソッドで1行ずつ読み込むよう修正

--- 05/iterator.py
+++ 05/iterator.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 f = open('./05/some.txt', encoding='utf-8')
-body = f.read()
-lines = body.split('\n')
-print('\n'.join(lines[:5]))
+lines = ''
+for i in range(5):
+    lines += f.readline()
+print(lines)
