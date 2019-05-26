ab6c47614be0be7e40a647be3611ae2c29b53490
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 19:34:48 +0900

    [mod] イテレータを使う

--- 05/iterator.py
+++ 05/iterator.py
@@ -6,3 +6,8 @@ lines = ''
 for i in range(5):
     lines += f.readline()
 print(lines)
+
+for c, l in enumerate(open('./05/some.txt', encoding='utf-8')):
+    print(l, end=" ")
+    if c == 4:
+        break
