6e6f2fb4ee88c93bd43c43ee34277211db9c1243
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 21:31:36 +0900

    [mod] デコレータ

--- 05/hofunction.py
+++ 05/hofunction.py
@@ -12,10 +12,25 @@ def logger(func):
         return func(*args)
     return inner
 
+#def accumulate(a, b):
+#    return a + b
+#print(accumulate(1, 2))
+
+#newfunc = logger(accumulate)
+#print(newfunc(1, 2))
+
+# デコレータ
+@logger
 def accumulate(a, b):
     return a + b
 
 print(accumulate(1, 2))
 
-newfunc = logger(accumulate)
-print(newfunc(1, 2))
+from functools import lru_cache
+@lru_cache(maxsize = None)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n - 1) + (n - 2)
+
+print([fib(n) for n in range(16)])
