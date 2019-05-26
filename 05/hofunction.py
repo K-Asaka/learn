c36d1caa4916022b0770c8f5fb6e6f3ede835afa
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-26 21:18:17 +0900

    [mod] 関数を返す関数

--- 05/hofunction.py
+++ 05/hofunction.py
@@ -5,3 +5,17 @@ def execute(func, arg):
     return func(arg)
 
 print(execute(int, "100"))
+
+def logger(func):
+    def inner(*args):
+        print("引数：", args)
+        return func(*args)
+    return inner
+
+def accumulate(a, b):
+    return a + b
+
+print(accumulate(1, 2))
+
+newfunc = logger(accumulate)
+print(newfunc(1, 2))
