e3c10ddab1a86b61ef0cfb98e10ca5fee55ad956
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-27 23:16:13 +0900

    [mod] tracebackモジュール

--- 10/traceback.py
+++ 10/traceback.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 from urllib import request
 request.urlopen("spam://spam.spam/")
+
+# tracebackモジュールを使うと例外が発生したときに出力されるトレースバックを
+# 表示したり、文字列に格納したりできる。
+# import traceback
+# try:
+#     pass
+# except:
+#     traceback.print_exc()       # 例外を表示
+#     ex = traceback.print_exc()  # 例外を文字列として取り出す
