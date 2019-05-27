44bafb404089cf079d7ffbfd902fd7ba69b36592
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-28 00:23:14 +0900

    [mod] 追加時の順番を保持するcollection.OrderedDict

--- 11/urllib.py
+++ 11/urllib.py
@@ -3,3 +3,16 @@
 from urllib import request
 src = request.urlopen('https://www.python.org/').read()
 print(src)
+
+from collections import OrderedDict
+od = OrderedDict()
+od['a'] = 'A'
+od['c'] = 'C'
+od['b'] = 'B'
+print(od)
+
+d = {}
+d['a'] = 'A'
+d['c'] = 'C'
+d['b'] = 'B'
+print(d)
