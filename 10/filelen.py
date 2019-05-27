1c23f61481adb031a5cd7940297071efd6908a64
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-27 22:40:20 +0900

    [mod] with文

--- 10/filelen.py
+++ 10/filelen.py
@@ -3,12 +3,15 @@
 import sys
 
 for fn in sys.argv[1:]:
-    try:
-        f = open(fn)
-    except FileNotFoundError:
-        print("{}というファイルは存在しません".format(fn))
-    else:
-        try:
-            print(fn, len(f.read()))
-        finally:
-            f.close()
+    # try:
+    #     f = open(fn)
+    # except FileNotFoundError:
+    #     print("{}というファイルは存在しません".format(fn))
+    # else:
+    #     try:
+    #         print(fn, len(f.read()))
+    #     finally:
+    #         f.close()
+    with open(fn) as f:
+        for line in f:
+            print(line)
