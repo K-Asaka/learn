5bc7ad9da7c07b95141f64b78cccf23155c4e388
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-28 00:28:00 +0900

    [mod] タプルからディクショナリを作る

--- 11/urllib.py
+++ 11/urllib.py
@@ -16,3 +16,15 @@ d['a'] = 'A'
 d['c'] = 'C'
 d['b'] = 'B'
 print(d)
+
+animals = [('猫', '三毛'), ('犬', 'コーギー'),
+           ('猫', 'シャム'), ('犬', 'ダックス'),
+           ('犬', '黒ラブ')]
+d = {}
+for k, v in animals:
+    if k not in d:
+        d[k] = [v]
+    else:
+        d[k].append(v)
+
+print(d)
