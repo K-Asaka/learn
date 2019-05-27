d4d85605f507c734b46d0a2083337f12ad57178a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-28 00:53:16 +0900

    [mod] 日時の加算

--- 11/datetime.py
+++ 11/datetime.py
@@ -5,3 +5,9 @@ d1 = datetime.date(2016, 6, 28)
 d2 = datetime.date(2015, 6, 28)
 td = d1 - d2
 print(td)
+
+d1 = datetime.date(2016, 4, 14)
+td = datetime.timedelta(days=100)
+d2 = d1 + td
+print(d2)
+
