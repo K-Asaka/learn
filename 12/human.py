0f714ccf0b969f6883cdf37a87a66f807f42f0be
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-31 23:39:26 +0900

    [mod] 出生率のグラフを描く

--- 12/human.py
+++ 12/human.py
@@ -13,3 +13,8 @@ t = plt.plot(range(1944, 2015), p_yearly)
 plt.ylim((0, 130000))
 plt.grid(True)
 plt.show()
+
+tfr = np.loadtxt('./12/total_fertility_rate.csv', delimiter=",", skiprows=1)
+t = plt.plot(range(1960, 2015), tfr, ls=":")
+t = plt.plot([1960, 2015], [2.07, 2.07])
+plt.show()
