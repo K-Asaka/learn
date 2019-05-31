495ed1cd1f9b133d2b9486d76988a5bd5c7c1603
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-05-31 23:24:38 +0900

    [mod] グラフを重ねる

--- 12/matplotlibsamp.py
+++ 12/matplotlibsamp.py
@@ -11,3 +11,13 @@ x = np.random.randn(5000)
 y = np.random.randn(5000)
 t = plt.plot(x, y, 'o', alpha = 0.1)
 plt.show()
+
+x = np.array([1.628, 3.363, 5.145, 7.683, 9.855])
+y = np.array([1.257, 3.672, 5.841, 7.951, 9.775])
+
+a = np.array([x, np.ones(x.size)])
+a = a.T
+m, c = np.linalg.lstsq(a, y)[0]
+t = plt.plot(x, y, 'o')
+t = plt.plot(x, (m * x + c))
+plt.show()
