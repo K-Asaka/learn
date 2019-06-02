40017410dd8ce94e1f3046aad3d533e5d4029547
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 10:25:20 +0900

    [add] 画像フォルダを除外

--- chap3.py
+++ chap3.py
@@ -8,6 +8,8 @@ root.minsize(640, 480)
 # 画像表示
 canvas = tkinter.Canvas(bg="black", width=640, height=480)
 canvas.place(x=0, y=0)
+img = tkinter.PhotoImage(file="img3/chap3-back.png")
+canvas.create_image(0, 0, image=img)
 
 # メインループ
 root.mainloop()
