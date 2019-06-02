fccca2f57784897ce7a06f7ed3e7337a6045b23e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 10:28:47 +0900

    [mod] 画像を画面中央に表示

--- chap3.py
+++ chap3.py
@@ -9,7 +9,7 @@ root.minsize(640, 480)
 canvas = tkinter.Canvas(bg="black", width=640, height=480)
 canvas.place(x=0, y=0)
 img = tkinter.PhotoImage(file="img3/chap3-back.png")
-canvas.create_image(0, 0, image=img)
+canvas.create_image(320, 240, image=img)
 
 # メインループ
 root.mainloop()
