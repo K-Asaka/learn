cb1effdcbe1a67c454f4b03eecaa1c5d23accdd0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 16:31:10 +0900

    [mod] 勇者が仲間を選ぶプログラムを作る

--- chap4.py
+++ chap4.py
@@ -2,57 +2,32 @@ import tkinter
 
 # ウィンドウ作成
 root = tkinter.Tk()
-root.title("勇者求む！")
+root.title("仲間を選ぶ")
 root.minsize(640, 480)
 root.option_add("*font", ["メイリオ", 14])
 
 # 画像読み込み
-img1 = tkinter.PhotoImage(file='img4/chap4-1-1.png')
-img2 = tkinter.PhotoImage(file='img4/chap4-1-2.png')
-img3 = tkinter.PhotoImage(file='img4/chap4-1-3.png')
+king_img = tkinter.PhotoImage(file='img4/chap4-2-1.png')
+monsterA_img = tkinter.PhotoImage(file='img4/chap4-2-2.png')
+monsterB_img = tkinter.PhotoImage(file='img4/chap4-2-3.png')
 
 # キャンバス作成
 canvas = tkinter.Canvas(root, width=640, height=480)
 canvas.place(x=0, y=0)
-canvas.create_image(320, 220, image=img1, tag="illust")
+canvas.create_image(110, 220, image=king_img, tag="illust")
+canvas.create_image(320, 220, image=monsterA_img, tag="illust")
+canvas.create_image(530, 220, image=monsterB_img, tag="illust")
 
 # ラベル配置
-serihu_text = tkinter.Label(text="王様「魔王を倒したら褒美をやるぞ！」")
-serihu_text.place(x=160, y=10)
-sys_text = tkinter.Label(text="褒美はいくらあげますか？", fg="red")
-sys_text.place(x=180, y=380)
+sys_text = tkinter.Label(text="だれを仲間にしますか？（1:王様、2:魔物A、3:魔物B）")
+sys_text.place(x=100, y=20)
 
 # 入力ボックス配置
 entry = tkinter.Entry(width=12)
-entry.place(x=180, y=420)
-gold_text = tkinter.Label(text="ゴールド")
-gold_text.place(x=330, y=420)
+entry.place(x=200, y=350)
 
 # ボタン配置
 button = tkinter.Button(text="決定")
-button.place(x=420, y=420)
-
-# ボタンクリックイベント関数
-def btn_click():
-    gold = float(entry.get())
-    if gold >= 100000:
-        canvas.delete("illust")
-        canvas.create_image(320, 220, image=img3, tag="illust")
-        serihu_text["text"] = "勇者「そんな大金、よっぽど危険なんだ……。\n関わらないでおこう。」"
-    elif gold >= 5000:
-        canvas.delete("illust")
-        canvas.create_image(320, 220, image=img2, tag="illust")
-        serihu_text["text"] = "勇者「よーし、私に任せなさい！」"
-    else:
-        serihu_text["text"] = "志願者は誰も来ませんでした。"
-    sys_text.destroy()
-    button["state"] = "disabled"
-    entry["state"] = "disabled"
-
-# ボタンクリックイベントと関数の関連付け
-button["command"] = btn_click
-
-root.mainloop()
-
+button.place(x=380, y=350)
 
 root.mainloop()
