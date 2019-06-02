efe4b85034b6c0f8373ec169ded6c440b5084d13
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 23:39:34 +0900

    [mod] コンストラクタと属性

--- chap7-class.py
+++ chap7-class.py
@@ -1,7 +1,10 @@
 # クラスの定義
 class CharaTest:
+    def __init__(self):
+        self.name = "俺"
     def fight(self):
-        print("戦うぞ！")
+        print(self.name + "は戦うぞ！")
 
 chara1 = CharaTest()
+print(chara1.name)
 chara1.fight()
