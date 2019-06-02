f3e3c0bdb7d7bcc15d142a4170784d07734c6db5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 23:40:26 +0900

    [mod] コンストラクタに引数を持たせる

--- chap7-class.py
+++ chap7-class.py
@@ -1,10 +1,10 @@
 # クラスの定義
 class CharaTest:
-    def __init__(self):
-        self.name = "俺"
+    def __init__(self, name):
+        self.name = name
     def fight(self):
         print(self.name + "は戦うぞ！")
 
-chara1 = CharaTest()
+chara1 = CharaTest("戦士")
 print(chara1.name)
 chara1.fight()
