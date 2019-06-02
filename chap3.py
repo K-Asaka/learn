01fcfb429d93cd5e012ba2e23d95be73caf82882
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 10:18:08 +0900

    [mod] 画像を表示するパーツを作る

--- chap3.py
+++ chap3.py
@@ -5,5 +5,9 @@ root = tkinter.Tk()
 root.title("リリーに質問")
 root.minsize(640, 480)
 
+# 画像表示
+canvas = tkinter.Canvas(bg="black", width=640, height=480)
+canvas.place(x=0, y=0)
+
 # メインループ
 root.mainloop()
