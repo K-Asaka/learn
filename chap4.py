278326a6922d53b3d85e218f7df81c0e819b7914
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 10:54:06 +0900

    [mod] elseを使う

--- chap4.py
+++ chap4.py
@@ -1,7 +1,6 @@
-num = 3
-if num == 3 :
-    print("numは 3 です")
-if num >= 4 :
-    print("numは 4 以上です")
-if num > 2 :
-    print("numは 2 より大きいです")
+num = 2
+if num == 1 :
+    print("numは 1 です")
+else :
+    print("numは 1 ではありません")
+print("おわり")
