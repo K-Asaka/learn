c6f6ea253bff5a1bfede92ae1f3ba24761e6151a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-06-02 10:33:15 +0900

    [mod] 文字の見た目を調整する

--- chap3.py
+++ chap3.py
@@ -4,6 +4,7 @@ import tkinter
 root = tkinter.Tk()
 root.title("リリーに質問")
 root.minsize(640, 480)
+root.option_add("*font", ["MS　Pゴシック", 22])
 
 # 画像表示
 canvas = tkinter.Canvas(bg="black", width=640, height=480)
@@ -12,7 +13,7 @@ img = tkinter.PhotoImage(file="img3/chap3-back.png")
 canvas.create_image(320, 240, image=img)
 
 # テキスト表示
-question = tkinter.Label(text="知りたいのは何分かな？")
+question = tkinter.Label(text="知りたいのは何分かな？", bg="white")
 question.place(x=100, y=40)
 
 # メインループ
