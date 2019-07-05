f8580aa56b5c03e120736c0b2ca14c62d22b897c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-07-05 16:36:49 +0900

    '円を描く'

---
+++ sample02.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+try:
+    img = cv2.imread('img/sample02.png', cv2.IMREAD_COLOR)
+    if img is None:
+        raise FileNotFoundError('ファイルが見つかりません。')
+    
+    img = cv2.circle(img, (250, 250), 100, (0, 255, 0), 3)
+
+    cv2.imshow('sample02', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+except FileNotFoundError as e:
+    print(e)
