2b79afec3bf2fdefbeaa797dc3ce6e56a71792ec
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-07-05 16:31:31 +0900

    '画像ファイルが見つからない時の処理を追加'

--- sample01.py
+++ sample01.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import cv2
 
-img = cv2.imread('img/sample01.jpg', cv2.IMREAD_COLOR)
-cv2.imshow('sample01', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+try:
+    img = cv2.imread('img/sample012.jpg', cv2.IMREAD_COLOR)
+    if img is None:
+        raise FileNotFoundError('ファイルが見つかりません。')
+    cv2.imshow('sample01', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+except FileNotFoundError as e:
+    print(e)
