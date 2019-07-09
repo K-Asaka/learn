1245458981c6afd0671cc9e033e021a7f2b41e99
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-07-09 09:06:37 +0900

    '透視投影：右側の上と下を内側に縮める'

--- sample20.py
+++ sample20.py
@@ -12,7 +12,7 @@ try:
     # 元画像の座標
     pts1 = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])
     # 変換後の座標
-    pts2 = np.float32([[(0 + 100), 0], [(cols - 100), 0], [0, rows], [cols, rows]])
+    pts2 = np.float32([[0, 0], [cols, 100], [0, rows], [cols, rows - 100]])
 
     # 透視変換行列を求める
     M = cv2.getPerspectiveTransform(pts1, pts2)
