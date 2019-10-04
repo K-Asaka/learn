fd62f92b7c285d591525f3141ac7ab9b3b4c0530
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-04 14:36:58 +0900

    新しいファイルとワークシートを作成する

--- excel/openkaikei.py
+++ excel/openkaikei.py
@@ -14,3 +14,13 @@ in17 = wsk['A4:B8']
 for row in in17:
     print(row[0].value, row[1].value)
 
+new_wbk = px.Workbook()
+wsk17_in = new_wbk.create_sheet("2017収入", 0)
+wsk17_in.append(["項目", "金額"])
+for row in wsk['A4:B8']:
+    wsk17_in.append([row[i].value for i in range(len(row))])
+wsk17_out = new_wbk.create_sheet("2017支出", 1)
+wsk17_out.append(["項目", "金額"])
+for row in wsk['D4:E11']:
+    wsk17_out.append([row[i].value for i in range(len(row))])
+new_wbk.save('new_kaikei.xlsx')
