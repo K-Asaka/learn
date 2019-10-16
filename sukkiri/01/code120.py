38a7cd6d043c2d373c2b258d0e8616e80e5fcc6f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-16 17:12:49 +0900

    割り勘計算プログラムの修正(未完成)

---
+++ sukkiri/01/code120.py
@@ -0,0 +1,8 @@
+# 割り勘計算プログラムの修正(未完成)
+price = input('料金を入力 >>')
+price = int(price)
+number = input('人数を入力 >>')
+number = int(number)
+payment = price / number
+payment = int(payment)
+print('お支払いは' + payment + '円です')
