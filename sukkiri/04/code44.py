3efa659825c78f1d974d0a033cf6d488eb1a0f8b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-17 18:06:16 +0900

    ひつじを数えるのを眠るまで繰り返す

---
+++ sukkiri/04/code44.py
@@ -0,0 +1,10 @@
+# ひつじを数えるのを眠るまで繰り返す
+is_awake = True
+count = 0
+while is_awake == True:
+    count += 1
+    print('ひつじが{}匹'.format(count))
+    key = input('もう眠りそうですか？(y/n) >>')
+    if key == 'y':
+        is_awake = False
+print('おやすみなさい…')
