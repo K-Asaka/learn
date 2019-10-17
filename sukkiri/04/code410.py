9fc4788dbcd6d2efcd0b2a26a79557cbeb27129d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-17 18:19:55 +0900

    目標数に達したら繰り返しを終了する

---
+++ sukkiri/04/code410.py
@@ -0,0 +1,10 @@
+# 目標数に達したら繰り返しを終了する
+ages = [28, 50, 8, 20, 78, 25, 22, 10, 27, 33]
+num = 5
+samples = list()
+for age in ages:
+    if 20 <= age < 30:
+        samples.append(age)
+        if len(samples) == num:
+            break
+print(samples)
