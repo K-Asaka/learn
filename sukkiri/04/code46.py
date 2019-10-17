a51c0ba4c64274b3b27fef4f48b287206e469354
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-17 18:10:25 +0900

    リストの全要素を繰り返し参照する

---
+++ sukkiri/04/code46.py
@@ -0,0 +1,9 @@
+# リストの全要素を繰り返し参照する
+scores = [80, 20, 75, 60]
+count = 0
+while count < len(scores):
+    if scores[count] >= 60:
+        print('合格')
+    else:
+        print('不合格')
+    count += 1
