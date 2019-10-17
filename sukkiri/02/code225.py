6beee0d16dafc4212225ef88ec67bbcfa6b0a13f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-17 16:37:03 +0900

    ディクショナリの中にセットをネスト

---
+++ sukkiri/02/code225.py
@@ -0,0 +1,8 @@
+# ディクショナリの中にセットをネスト
+member_hobbies = {
+    '開発者':{'SNS', '麻雀', '自転車'},
+    '分析者':{'麻雀', '食べ歩き', '数学', '数学', '数学'}
+}
+print(member_hobbies)
+print(member_hobbies['開発者'])
+print(member_hobbies['分析者'])
