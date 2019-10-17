0a49cf19f5d586816fd4cb8ae7b2a31ccae25ca6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-17 18:01:56 +0900

    繰り返し：繰り返しの基本構造。ひつじを数えて眠る

---
+++ sukkiri/04/code41.py
@@ -0,0 +1,10 @@
+# ひつじを数えて眠る
+print('さあ、寝ようかしら')
+count = 0
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+print('おやすみなさい…')
