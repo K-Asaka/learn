0f468921136fb38da30c7befd4b578662c1bc90a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 16:52:33 +0900

    練習問題7-4(2)

--- sukkiri/07/ren74.py
+++ sukkiri/07/ren74.py
@@ -4,3 +4,10 @@ for n in range(3):
     data = int(input('{}個目の整数を入力してください >>'.format(n + 1)))
     nums.append(data)
 print(max(nums))
+
+
+# 練習問題7-4(2)
+pi = 3.141519
+print(round(pi))
+for n in range(4):
+    print(round(pi, n + 1))
