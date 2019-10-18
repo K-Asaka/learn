db31366c6e64a71dff386845bb193b8ece1809b9
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 09:39:45 +0900

    さまざまな機能を担当する関数の定義

---
+++ sukkiri/05/code516.py
@@ -0,0 +1,14 @@
+# さまざまな機能を担当する関数の定義
+def input_scores(name):
+    print('{}さんの試験結果を入力してください'.format(name))
+    network = int(input('ネットワークの得点？ >>'))
+    database = int(input('データベースの得点？ >>'))
+    security = int(input('セキュリティの得点？ >>'))
+    scores = [network, database, security]
+
+def cacl_average(scores):
+    avg = sum(scores) / len(scores)
+    print('平均点は{}です'.format(avg))
+
+def output_result(name, avg):
+    print('{}さんの平均点は{}です'.format(name, avg))
