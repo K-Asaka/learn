b51cc7536c05aa6e80eefbfc178e3dfe84ef58fd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 11:50:51 +0900

    リテラルやクラス名関数を用いたオブジェクトの生成

---
+++ sukkiri/06/code64.py
@@ -0,0 +1,7 @@
+# リテラルやクラス名関数を用いたオブジェクトの生成
+int_value1 = 0          # intオブジェクト(中身0)
+int_value2 = int()      # intオブジェクト(中身0)
+int_value3 = int(9)     # intオブジェクト(中身9)
+list_value1 = []        # 空のリストオブジェクト
+list_value2 = list()    # 空のリストオブジェクト
+list_value3 = list(('開発者', '分析者'))    # 2つの要素を持つリストオブジェクト
