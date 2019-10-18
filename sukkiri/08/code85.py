e2e86dee7034ff4688d7af3b16edc3e0f9b2ee33
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 17:32:57 +0900

    Raspberry Piから温湿度を取得して表示する

---
+++ sukkiri/08/code85.py
@@ -0,0 +1,18 @@
+# Raspberry Piから温湿度を取得して表示する
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+instance = dht11.DHT11(pin=14)
+while True:
+    result = instance.read()
+    if result.is_valid():
+        temperature = result.temperature
+        humidity = result.humidity
+        print('温度:{}'.format(temperature))
+        print('湿度:{}'.format(humidity))
+    time.sleep(1)
