c7850c82abbb050b56f63d53c78dc145113da640
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 10:05:37 +0900

    開発者の基本的なeat関数

---
+++ sukkiri/05/code524.py
@@ -0,0 +1,5 @@
+# 開発者の基本的なeat関数
+def eat(breakfast, lunch='ラーメン', dinner='カレー'):
+    print('朝は{}を食べました。'.format(breakfast))
+    print('昼は{}を食べました。'.format(lunch))
+    print('晩は{}を食べました。'.format(dinner))
