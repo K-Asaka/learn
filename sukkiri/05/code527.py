80e588e5f7dc9450c3ca45099c0a6559d1593b15
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 10:12:30 +0900

    おやつも出力できるeat関数

---
+++ sukkiri/05/code527.py
@@ -0,0 +1,7 @@
+# おやつも出力できるeat関数
+def eat(breakfast, lunch, dinner='カレー', desserts=()):
+    print('朝は{}を食べました。'.format(breakfast))
+    print('昼は{}を食べました。'.format(lunch))
+    print('晩は{}を食べました。'.format(dinner))
+    for d in desserts:
+        print('おやつに{}を食べました'.format(d))
