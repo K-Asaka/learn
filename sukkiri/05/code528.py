1bf438d6cfe1356eabf6cc40b62e2742255ede69
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-18 10:13:44 +0900

    おやつも食べた日のeat関数の呼び出し

---
+++ sukkiri/05/code528.py
@@ -0,0 +1,10 @@
+# おやつも食べた日のeat関数の呼び出し
+
+def eat(breakfast, lunch, dinner='カレー', desserts=()):
+    print('朝は{}を食べました。'.format(breakfast))
+    print('昼は{}を食べました。'.format(lunch))
+    print('晩は{}を食べました。'.format(dinner))
+    for d in desserts:
+        print('おやつに{}を食べました'.format(d))
+
+eat('トースト', 'パスタ', 'カレー', ('アイス', 'チョコ', 'パフェ'))
