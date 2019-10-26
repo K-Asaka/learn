15106700a534ca4917eed97058200b89f659cc50
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-27 00:24:29 +0900

    ハンドラのチェイン

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reflect"
-	"runtime"
 )
 
 // Server Server構造体
@@ -20,16 +18,25 @@ import (
 // 	ErrorLog       *log.Logger
 // }
 
-func hello(w http.ResponseWriter, r *http.Request) {
+// HelloHandler HelloHandler
+type HelloHandler struct{}
+
+func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func log(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Handler called - %T\n", h)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func protect(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("ユーザー権限確認")
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -37,6 +44,7 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.HandleFunc("/hello", log(hello))
+	hello := HelloHandler{}
+	http.Handle("/hello", protect(log(hello)))
 	server.ListenAndServe()
 }
