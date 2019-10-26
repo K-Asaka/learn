15bc02ea90fe459c68340bee2d350ea3f8c61b37
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-26 17:53:36 +0900

    ハンドラを用意しリクエストの処理

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -17,10 +18,17 @@ import (
 // 	ErrorLog       *log.Logger
 // }
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
+	handler := MyHandler{}
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Handler: &handler,
 	}
-	server.ListenAndServeTLS("cert.pem", "key.pem")
+	server.ListenAndServe()
 }
