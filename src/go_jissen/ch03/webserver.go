b6a37d5056ac0a644a97ac3acda222488116d3da
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-29 19:51:15 +0900

    リクエスト内のヘッダの読み取り

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -3,23 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"golang.org/x/net/http2"
 )
 
-// HelloHandler HelloHandler
-type MyHandler struct{}
-
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
 }
 
 func main() {
-	handler := MyHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: &handler,
+		Addr: "127.0.0.1:8080",
 	}
-	http2.ConfigureServer(&server, &http2.Server{})
-	server.ListenAndServeTLS("cert.pem", "key.pem")
+	http.HandleFunc("/headers", headers)
+	server.ListenAndServe()
 }
