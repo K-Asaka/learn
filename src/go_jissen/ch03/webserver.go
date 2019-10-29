9001dc61523f674f604ee7149207a70b538af7d4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-29 21:27:49 +0900

    クライアントを転送するヘッダの書き込み

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -11,11 +11,17 @@ func writeExample(w http.ResponseWriter, r *http.Request) {
 <body><h1>Hello World</h1></body>
 </html>`
 	w.Write([]byte(str))
+	w.Write([]byte(str))
 }
 
 func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(501)
 	fmt.Fprintln(w, "そのようなサービスはありません。ほかを当たってください。")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
 }
 
 func main() {
@@ -24,5 +30,6 @@ func main() {
 	}
 	http.HandleFunc("/write", writeExample)
 	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
 	server.ListenAndServe()
 }
