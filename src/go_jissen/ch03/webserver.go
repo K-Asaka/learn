1218618dc3e071d618daec8167515ca92eda449f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-29 21:05:42 +0900

    ファイルのアップロード

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.PostFormValue("hello"))
-	fmt.Fprintln(w, r.PostForm)
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
 }
 
 func main() {
