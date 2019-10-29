01bfdc68b0a0b77979c1a7b58d54ec5d48bbc46d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-29 20:56:49 +0900

    PostFormValueの確認

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -6,8 +6,8 @@ import (
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.FormValue("hello"))
-	fmt.Fprintln(w, r.Form)
+	fmt.Fprintln(w, r.PostFormValue("hello"))
+	fmt.Fprintln(w, r.PostForm)
 }
 
 func main() {
