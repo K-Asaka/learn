26a855705ac572bf7f0d303943bed9d04e5e0086
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-29 18:16:39 +0900

    HttpRouterを使った実装

--- src/go_jissen/ch03/webserver.go
+++ src/go_jissen/ch03/webserver.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // Server Server構造体
@@ -21,30 +23,18 @@ import (
 // HelloHandler HelloHandler
 type HelloHandler struct{}
 
-func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-func log(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Handler called - %T\n", h)
-		h.ServeHTTP(w, r)
-	})
-}
-
-func protect(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("ユーザー権限確認")
-		h.ServeHTTP(w, r)
-	})
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
 }
 
 func main() {
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
 
-	hello := HelloHandler{}
-	http.Handle("/hello", protect(log(hello)))
 	server.ListenAndServe()
 }
