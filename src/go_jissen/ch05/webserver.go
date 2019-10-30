88c4579f7d5a8342c7adc86aebe005d9fa34b787
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-30 13:05:01 +0900

    同じ名前のテンプレートを切り替えて利用する

--- src/go_jissen/ch05/webserver.go
+++ src/go_jissen/ch05/webserver.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("src/go_jissen/ch05/layout.html")
-	t.ExecuteTemplate(w, "layout", "")
+	rand.Seed(time.Now().Unix())
+	if rand.Intn(10) > 5 {
+		t, _ := template.ParseFiles("src/go_jissen/ch05/layout.html", "src/go_jissen/ch05/red_hello.html")
+		t.ExecuteTemplate(w, "layout", "")
+	} else {
+		t, _ := template.ParseFiles("src/go_jissen/ch05/layout.html", "src/go_jissen/ch05/blue_hello.html")
+		t.ExecuteTemplate(w, "layout", "")
+	}
 }
 
 func main() {
