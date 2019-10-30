8cb0154339e103675226516ce8c9819ff4070693
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-10-30 12:01:49 +0900

    コンテキスト依存処理。テンプレート内でのコンテキスト認識を試す

--- src/go_jissen/ch05/webserver.go
+++ src/go_jissen/ch05/webserver.go
@@ -3,19 +3,12 @@ package main
 import (
 	"html/template"
 	"net/http"
-	"time"
 )
 
-func formatDate(t time.Time) string {
-	layout := "2006-01-02"
-	return t.Format(layout)
-}
-
 func process(w http.ResponseWriter, r *http.Request) {
-	funcMap := template.FuncMap{"fdate": formatDate}
-	t := template.New("tmpl.html").Funcs(funcMap)
-	t1, _ := t.ParseFiles("src/go_jissen/ch05/tmpl.html")
-	t1.Execute(w, time.Now())
+	t, _ := template.ParseFiles("src/go_jissen/ch05/tmpl.html")
+	content := `I asked: <i>"What's up?"</i>`
+	t.Execute(w, content)
 }
 
 func main() {
