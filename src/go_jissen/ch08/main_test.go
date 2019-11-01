45e03520447c1e6f55d11d762a713b15c004a2e0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-01 18:29:28 +0900

    テストの並行実行

--- src/go_jissen/ch08/main_test.go
+++ src/go_jissen/ch08/main_test.go
@@ -28,3 +28,18 @@ func TestLongRunningTest(t *testing.T) {
 	}
 	time.Sleep(10 * time.Second)
 }
+
+func TestParallel_1(t *testing.T) {
+	t.Parallel()
+	time.Sleep(1 * time.Second)
+}
+
+func TestParallel_2(t *testing.T) {
+	t.Parallel()
+	time.Sleep(2 * time.Second)
+}
+
+func TestParallel_3(t *testing.T) {
+	t.Parallel()
+	time.Sleep(3 * time.Second)
+}
