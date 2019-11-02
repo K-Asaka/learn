b53cbb3785c26f70d4ef5e9f7b57b5411c2a1338
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-03 00:18:56 +0900

    WaitGroupの使用法

--- src/go_jissen/ch09/goroutine.go
+++ src/go_jissen/ch09/goroutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -27,30 +28,26 @@ func goPrint1() {
 	go printLetters1()
 }
 
-func printNumbers2() {
+func printNumbers2(wg *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		time.Sleep(10 * time.Microsecond)
 		fmt.Printf("%d ", i)
 	}
+	wg.Done()
 }
 
-func printLetters2() {
+func printLetters2(wg *sync.WaitGroup) {
 	for i := 'A'; i < 'A'+10; i++ {
 		time.Sleep(10 * time.Microsecond)
 		fmt.Printf("%c ", i)
 	}
-}
-
-func print2() {
-	printNumbers2()
-	printLetters2()
-}
-
-func goPrint2() {
-	go printNumbers2()
-	go printLetters2()
+	wg.Done()
 }
 
 func main() {
-
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go printNumbers2(&wg)
+	go printLetters2(&wg)
+	wg.Wait()
 }
