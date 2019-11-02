6b81676e138d110779bba184051e8af3b5a5c855
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-02 23:55:00 +0900

    遅延を追加

--- src/go_jissen/ch09/goroutine.go
+++ src/go_jissen/ch09/goroutine.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func printNumbers1() {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d ", i)
+		time.Sleep(1 * time.Microsecond)
 	}
 }
 
 func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
-		fmt.Printf("%c ", i)
+		time.Sleep(1 * time.Microsecond)
 	}
 }
 
@@ -22,6 +25,25 @@ func print1() {
 func goPrint1() {
 	go printNumbers1()
 	go printLetters1()
+}
+
+func printNumbers2() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(10 * time.Microsecond)
+		fmt.Printf("%d ", i)
+	}
+}
+
+func printLetters2() {
+	for i := 'A'; i < 'A'+10; i++ {
+		time.Sleep(10 * time.Microsecond)
+		fmt.Printf("%c ", i)
+	}
+}
+
+func goPrint2() {
+	go printNumbers2()
+	go printLetters2()
 }
 
 func main() {
