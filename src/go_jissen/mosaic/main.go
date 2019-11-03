d0eab05dfb298b674aa685f3d728641b11f78d65
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-03 20:50:52 +0900

    メモとして並行実行版のモザイク写真生成Webアプリケーションのコード

--- src/go_jissen/mosaic/main.go
+++ src/go_jissen/mosaic/main.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"image/jpeg"
 	"io/ioutil"
 	"math"
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"text/template"
 	"time"
 )
@@ -67,8 +69,9 @@ func tilesDB() map[string][3]float64 {
 
 func nearest(target [3]float64, db *map[string][3]float64) string {
 	var filename string
+	db.mutex.Lock()
 	smallest := 1000000.0
-	for k, v := range *db {
+	for k, v := range db.store {
 		dist := distance(target, v)
 		if dist < smallest {
 			filename, smallest = k, dist
@@ -94,6 +97,80 @@ func cloneTilesDB() map[string][3]float64 {
 		db[k] = v
 	}
 	return db
+}
+
+type DB struct {
+	mutex *sync.Mutex
+	store map[string][3]float64
+}
+
+func cut(original image.Image, db *DB, tileSize, x1, y1, x2, y2 int) <-chan image.Image {
+	c := make(chan image.Image)
+	sp := image.Point{0, 0}
+	go func() {
+		newimage := image.NewNRGBA(image.Rect(x1, y1, x2, y2))
+		for y := y1; y < y2; y = y + tileSize {
+			for x := x1; x < x2; x = x + tileSize {
+				r, g, b, _ := original.At(x, y).RGBA()
+				color := [3]float64{float64(r), float64(g), float64(b)}
+				nearest := db.nearest(color)
+				file, err := os.Open(nearest)
+				if err == nil {
+					img, _, err := image.Decode(file)
+					if err == nil {
+						t := resize(img, tileSize)
+						tile := t.SubImage(t.Bounds())
+						tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+						draw.Draw(newimage, tileBounds, tile, sp, draw.Src)
+					} else {
+						fmt.Println("error:", err)
+					}
+				} else {
+					fmt.Println("error:", nearest)
+				}
+				file.Close()
+			}
+		}
+		c <- newimage.SubImage(newimage.Rect)
+	}()
+	return c
+}
+
+func combine(r image.Rectangle, c1, c2, c3, c4 <-chan image.Image) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		var wg sync.WaitGroup
+		img := image.NewNRGBA(r)
+		copy := func(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
+			draw.Draw(dst, r, src, sp, draw.Src)
+			wg.Done()
+		}
+		wg.Add(4)
+		var s1, s2, s3, s4 image.Image
+		var ok1, ok2, ok3, ok4 bool
+		for {
+			select {
+			case s1, ok1 = <-c1:
+				go copy(img, s1.Bounds(), s1, image.Point{r.Min.X, r.Min.Y})
+			case s2, ok2 = <-c2:
+				go copy(img, s2.Bounds(), s2, image.Point{r.Max.X / 2, r.Min.Y})
+			case s3, ok3 = <-c3:
+				go copy(img, s3.Bounds(), s3, image.Point{r.Min.X, r.Max.Y / 2})
+			case s4, ok4 = <-c4:
+				go copy(img, s4.Bounds(), s4, image.Point{r.Max.X / 2, r.Max.Y / 2})
+			}
+			if ok1 && ok2 && ok3 && ok4 {
+				break
+			}
+		}
+
+		wg.Wait()
+		buf2 := new(bytes.Buffer)
+		jpeg.Encode(buf2, img, nil)
+		c <- base64.StdEncoding.EncodeToString(buf2.Bytes())
+	}()
+	return c
 }
 
 func main() {
