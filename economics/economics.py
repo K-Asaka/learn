f38c2155f788611b93f62181d6faa179b48791af
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-27 19:23:13 +0900

    ジニ係数を計算する

--- economics/economics.py
+++ economics/economics.py
@@ -14,24 +14,31 @@ def giving_money(agents):
         agent2.wealth += 1
         agent1.wealth -= 1
 
+# ジニ係数を計算する関数
+def gini(y):
+    y.sort()
+    n = len(y)
+    nume = 0
 
-n = 30      # エージェント数(人数)
-time = 100    # 取引回数
+    for i in range(n):
+        nume = nume + (i + 1) * y[i]
+        deno = n * sum(y)
+    
+    return ((2 * nume)/deno - (n + 1)/(n)) * (n / (n - 1))
+
+
+n = 300      # エージェント数(人数)
+time = 1000    # 取引回数
 
 agents = [Agent(i, 1) for i in range(n)]
-histories = [[1] * n]       # 資産履歴の初期値
 
 for i in range(time):
+    if i % 200 == 0 or i == time - 1:
+        agent_wealth = [a.wealth for a in agents]
+        G = gini(agent_wealth)
+        print("G = " + str(G) + ", time = " + str(i))
+        plt.hist(agent_wealth, bins=50)
+        plt.show()
     giving_money(agents)
-    histories.append([agent.wealth + 0.1 * random.random() for agent in agents])
 
 print([agent.wealth for agent in agents])
-
-plt.figure(figsize=(10, 5), dpi=150)
-plt.plot(histories)
-plt.show()
-
-# ヒストグラムで表示
-agent_wealth = [a.wealth for a in agents]
-plt.hist(agent_wealth, bins=50)
-plt.show()
