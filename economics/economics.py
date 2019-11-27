525e6a2b1dce22b630c394935d61b0dfaa59edd7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2019-11-27 19:03:19 +0900

    貧富の差の発生をシミュレーションするプログラムをクラスと関数を使って書き直したもの

--- economics/economics.py
+++ economics/economics.py
@@ -1,18 +1,25 @@
 import random
 
+class Agent:
+    def __init__(self, unique_id, initial_wealth):
+        self.unique_id = unique_id      # id番号
+        self.wealth = initial_wealth    # 初期資産
+
+def giving_money(agents):
+    agent1 = random.choice(agents)
+    agent2 = random.choice(agents)
+
+    if agent1.wealth >= 1:
+        agent2.wealth += 1
+        agent1.wealth -= 1
+
+
 n = 10      # エージェント数(人数)
 time = 5    # 取引回数
 
-# 資産の初期値を設定
-money = [1 for i in range(n)]
+agents = [Agent(i, 1) for i in range(n)]
 
 for i in range(time):
-    # ランダムで2人選ぶ
-    agent1 = random.choice(range(n))
-    agent2 = random.choice(range(n))
+    giving_money(agents)
 
-    if money[agent1] >= 1:
-        money[agent2] += 1  # agent2の資産を1増やす
-        money[agent1] -= 1  # agent1の資産を1減らす
-
-print(money)    # 結果の確認
+print([agent.wealth for agent in agents])
