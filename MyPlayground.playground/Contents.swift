c037db636536e93d2718a2ab5790728bd281bd10
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-13 21:24:10 +0900

    変数

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,27 @@ var str = "Hello, playground"
 "Hello, " + "playground"
 
 print("Hello, " + "playground")
+
+// 一人目に対しての質問
+print("あなたは何が好きですか？")
+print("りんごです")
+// 二人目に対しての質問
+print("あなたは何が好きですか？")
+print("みかんです")
+
+var value: String = "Hello, playground"
+// 型を省略した指定例
+// var value = "Hello, playground"
+
+// 変数の宣言
+var st = "あなたは何が好きですか？"
+// 一人目に対しての質問
+print(st)
+print("りんごです")
+// 二人目に対しての質問
+print(st)
+print("みかんです")
+
+// 変数の値を変更
+st = "あなたの好きな食べ物は何ですか？"
+
