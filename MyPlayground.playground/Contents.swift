490a63db5f0f6292fd1fbf92ac25be31b2d01f40
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-13 21:47:27 +0900

    文字列と変数の組み合わせ

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -41,3 +41,29 @@ st = "あなたの好きな食べ物は何ですか？"
 let value2: String = "Hello, playground"
 // value2 = "みかんが好きです"     // エラー
 
+
+// 変数
+var str1 = "魚が1匹います"
+str1 = "魚が2匹います"
+
+var value3 = "りんごを1個、みかんを2個、きゅうりを3本、魚を4匹買いました"
+
+// 個数の書き変え例
+var a = 1                   // りんごの数
+var b = 2                   // みかんの数
+var c = 3                   // きゅうりの数
+var d = 4                   // 魚の数
+let aString = String(a)     // りんごの数を文字列に変換
+let bString = String(b)     // みかんの数を文字列に変換
+let cString = String(c)     // きゅうりの数を文字列に変換
+let dString = String(d)     // 魚の数を文字列に変換
+// 文字列を結合。value4変数には "りんごを1個、みかんを2個、きゅうりを3本、魚を4匹買いました" という文字列が設定される
+var value4 = "りんごを" + aString + "個、みかんを" + bString + "個、きゅうりを" + cString + "本、魚を" + dString + "匹買いました"
+
+// ¥(変数)を利用
+var value5 = "りんごを\(a)個、みかんを\(b)個、きゅうりを\(c)本、魚を\(d)匹買いました"
+
+// 埋め込む文字列が少ないときは結合したほうが読みやすいこともある
+var a1 = 10                                        // りんごの数を宣言
+var aString1 = String(a1)                           // 文字列に変換
+var value6 = "りんごを" + aString1 + "個買いました"    // 文字列を結合する
