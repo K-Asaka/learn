2b1e56c928b52f8c2e57fa5e855587a0af55058d
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-18 01:08:21 +0900

    labelに引き算の結果を出力

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 引き算の計算結果をvalue変数に設定
+        let value: Int = 10 - 1
+        // label.textに値を設定
+        label.text = " \(value)"
     }
 
 
