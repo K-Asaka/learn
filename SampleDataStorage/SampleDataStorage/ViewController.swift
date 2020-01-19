0bd9d3409529b6881ae3d58acc70d3a33285a8a1
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-20 01:33:46 +0900

    シンプルな値の保存処理

--- SampleDataStorage/SampleDataStorage/ViewController.swift
+++ SampleDataStorage/SampleDataStorage/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // UserDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        // textというキーを指定して保存していた値を取り出す
+        if let value = userDefaults.string(forKey: "text") {
+            textField.text = value
+        }
     }
 
-
+    @IBAction func tapActionButton(_ sender: Any) {
+        // UserDefaultsの参照
+        let userDefaults = UserDefaults.standard
+        // textというキーで値を保存する
+        userDefaults.set(textField.text, forKey: "text")
+        // UserDefaultsへの値の保存を明示的に行う
+        userDefaults.synchronize()
+    }
+    
 }
 
