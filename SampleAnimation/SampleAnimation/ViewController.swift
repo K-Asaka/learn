efb53dbb7bf654b4339e8dac16ac2e7e9be487f3
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-22 23:03:26 +0900

    明示的なアニメーションの記述方法

--- SampleAnimation/SampleAnimation/ViewController.swift
+++ SampleAnimation/SampleAnimation/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapStartButton(_ sender: Any) {
+        // ビューの角を丸める処理をアニメーションで表現する
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        // 変更前の値は0
+        animation.fromValue = 0
+        // 変更後の値は20
+        animation.toValue = 20
+        // アニメーションの時間は1秒
+        animation.duration = 1
+        // アニメーションをレイヤーに追加する
+        targetView.layer.add(animation, forKey: "cornerRadius")
+        // 変更後も角丸を維持
+        targetView.layer.cornerRadius = 20
+    }
 }
 
