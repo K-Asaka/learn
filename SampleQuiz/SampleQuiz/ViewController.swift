a81a8c3ddecf9f13238d8c3b70cb33bb902e5176
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-22 22:44:08 +0900

    カスタムサウンドを鳴らす

--- SampleQuiz/SampleQuiz/ViewController.swift
+++ SampleQuiz/SampleQuiz/ViewController.swift
@@ -24,5 +24,18 @@ class ViewController: UIViewController {
                 // ここにはサウンドが鳴り終わったあとに呼ばれる処理を記述する
         }
     }
+    
+    @IBAction func tapCustomSound(_ sender: Any) {
+        // 指定するサウンドファイルは再生時間が30秒以下であること
+        let soundUrl = Bundle.main.url(forResource: "custom", withExtension: "mp3")
+        // サウンドIDを登録するための変数を用意
+        var soundId: SystemSoundID = 0
+        // サウンドファイルを登録してサウンドIDを取得
+        AudioServicesCreateSystemSoundID(soundUrl! as CFURL, &soundId)
+        // 取得したサウンドIDを使用してサウンドを鳴らす
+        AudioServicesPlaySystemSoundWithCompletion(soundId) {
+            // ここにはサウンドが鳴り終わったあとに呼ばれる処理を記述する
+        }
+    }
 }
 
