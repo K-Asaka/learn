da4c0f0d12d1e27544564305101b502f6ccc1088
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-24 12:11:07 +0900

    nextTickメソッドを使ってビューの更新を待ってから確認

--- js/react_async.js
+++ js/react_async.js
@@ -9,5 +9,11 @@ let app = new Vue ({
         Vue.set(this.author, 'company', 'WINGSプロジェクト');
         // <div id="app">配下にcompanyプロパティの内容が含まれているか
         console.log(this.$el.textContent.includes(this.author.company));
+
+        let that = this;
+        this.$nextTick().then(function() {
+            // ビューへの反映を待ってから確認
+            console.log(that.$el.textContent.includes(that.author.company));
+        })
     }
 });
