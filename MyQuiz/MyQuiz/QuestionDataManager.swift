1f896616db161076e13ac6031a2df6ceb40d93cb
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-25 11:06:50 +0900

    開始画面の実装クラス

---
+++ MyQuiz/MyQuiz/QuestionDataManager.swift
@@ -0,0 +1,105 @@
+//
+//  QuestionDataManager.swift
+//  MyQuiz
+//
+//  Created by Katsuhiro Asaka on 2020/01/24.
+//  Copyright © 2020 Katsuhiro Asaka. All rights reserved.
+//
+
+import Foundation
+
+// 1つの問題に関する情報を格納するデータクラス
+class QuestionData {
+    
+    var question: String
+    var answer1: String
+    var answer2: String
+    var answer3: String
+    var answer4: String
+    var correctAnswerNumber: Int
+    var userChoiceAnswerNumber: Int?
+    var questionNo: Int = 0
+    
+    // クラスが生成されたときの処理
+    init(questionSourceDataArray: [String]) {
+        question = questionSourceDataArray[0]
+        answer1 = questionSourceDataArray[1]
+        answer2 = questionSourceDataArray[2]
+        answer3 = questionSourceDataArray[3]
+        answer4 = questionSourceDataArray[4]
+        correctAnswerNumber = Int(questionSourceDataArray[5])!
+    }
+    
+    // ユーザーが選択した答えが正解かどうか判定する
+    func isCorrect() -> Bool {
+        // 答えが一致しているかどうか判定する
+        if correctAnswerNumber == userChoiceAnswerNumber {
+            // 正解
+            return true
+        }
+        // 不正解
+        return false
+    }
+}
+
+// クイズデータ全般の管理と成績を管理するクラス
+class QuestionDataManager {
+    // シングルトンのオブジェクトを生成
+    static let sharedInstance = QuestionDataManager()
+    
+    // 問題を格納するための配列
+    var questionDataArray = [QuestionData]()
+    
+    // 現在の問題のインデックス
+    var nowQuestionIndex: Int = 0
+    
+    // 初期化処理
+    private init() {
+        // シングルトンであることを保証するためにprivateで宣言しておく
+    }
+    
+    // 問題文の読み込み処理
+    func loadQuestion() {
+        // 格納済みの問題文があればいったん削除しておく
+        questionDataArray.removeAll()
+        
+        // 現在の問題のインデックスを初期化
+        nowQuestionIndex = 0
+        
+        // csvファイルパスを取得
+        guard let csvFilePath = Bundle.main.path(forResource: "question", ofType: "csv") else {
+            // csvファイルなし
+            print("csvファイルが存在しません")
+            return
+        }
+        
+        // csvファイル読み込み
+        do {
+            let csvStringData = try String(contentsOfFile: csvFilePath, encoding: String.Encoding.utf8)
+            // csvデータを1行ずつ読み込む
+            csvStringData.enumerateLines(invoking: { (line, stop) in
+                // カンマ区切りで分割
+                let questionSourceDataArray = line.components(separatedBy: ",")
+                // 問題データを格納するオブジェクトを作成
+                let questionData = QuestionData(questionSourceDataArray: questionSourceDataArray)
+                // 問題を追加
+                self.questionDataArray.append(questionData)
+                // 問題番号を設定
+                questionData.questionNo = self.questionDataArray.count
+            })
+        } catch let error {
+            print("csvファイル読み込みエラーが発生しました:\(error)")
+            return
+        }
+    }
+    
+    // 次の問題を取り出す
+    func nextQuestion() -> QuestionData? {
+        if nowQuestionIndex < questionDataArray.count {
+            let nextQuestion = questionDataArray[nowQuestionIndex]
+            nowQuestionIndex += 1
+            return nextQuestion
+        }
+        return nil
+    }
+}
---
+++ MyQuiz/MyQuiz/StartViewController.swift
@@ -0,0 +1,43 @@
+//
+//  StartViewController.swift
+//  MyQuiz
+//
+//  Created by Katsuhiro Asaka on 2020/01/25.
+//  Copyright © 2020 Katsuhiro Asaka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // 次の画面に遷移する前に呼び出される準備処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 問題文の読み込み
+        QuestionDataManager.sharedInstance.loadQuestion()
+        
+        // 遷移先画面の呼び出し
+        guard let nextViewController = segue.destination as? QuestionViewController else {
+            // 取得できずに終了
+            return
+        }
+        
+        // 問題文の取り出し
+        guard let questionData = QuestionDataManager.sharedInstance.nextQuestion() else {
+            // 取得できずに終了
+            return
+        }
+        
+        // 問題文のセット
+        nextViewController.questionData = questionData
+    }
+    
+    // タイトルに戻ってくる時に呼び出される処理
+    @IBAction func goToTitle(_ segue: UIStoryboardSegue) {
+        
+    }
+}
