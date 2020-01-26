5a54da93d673d169ec99c7c7a4a9632f37e49dc2
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-26 21:59:40 +0900

    MySearchAppプロジェクトの作成

---
+++ MySearchApp/MySearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySearchApp
+//
+//  Created by Katsuhiro Asaka on 2020/01/26.
+//  Copyright © 2020 Katsuhiro Asaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
