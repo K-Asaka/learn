5944cd7ca9157e463d0c0a8e4d8a2b4a0722ed5a
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-01-26 21:03:18 +0900

    URLSessionクラスの使い方

--- MyPlaygroundURLSession.playground/Contents.swift
+++ MyPlaygroundURLSession.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// セッションの取り出し
+let session = URLSession.shared
+
+// URLオブジェクトを生成
+if let url = URL(string: "https://www.yahoo.co.jp") {
+    // リクエストオブジェクトを生成
+    let request = URLRequest(url: url)
+    
+    // 処理タスクを生成
+    let task = session.dataTask(with: request, completionHandler: {
+        (data: Data?, response: URLResponse?, error: Error?) in
+        
+        // データ取得後に呼ばれる処理はここに記載する
+        guard let data = data else {
+            print("データなし")
+            return
+        }
+        
+        // Data型の変数をString型に変換してprintで出力
+        if let value = String(data: data, encoding: String.Encoding.utf8) {
+            print(value)
+        }
+    })
+    
+    // 処理開始
+    task.resume()
+}
