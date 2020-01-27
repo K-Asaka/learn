1acb6b86322f818e66f49630162924a95e359727
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-01-27 18:53:24 +0900

    パスワード付きPDFファイルについてのコメントを追記

--- pdf/sample1.py
+++ pdf/sample1.py
@@ -14,6 +14,7 @@ device = TextConverter(rmgr, outfp, laparams=lprms)
 
 iprtr = PDFPageInterpreter(rmgr, device)
 
+# パスワード付きPDFの場合はget_pages()にpassword引数でパスワードを指定する
 for page in PDFPage.get_pages(fp):
     iprtr.process_page(page)
 
