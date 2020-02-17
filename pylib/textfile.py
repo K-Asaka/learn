8792c6a9c70de2b48ac7184b62a045866d84ed18
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-02-17 13:18:11 +0900

    UTF-8で文字列を保存し読み込む

--- pylib/textfile.py
+++ pylib/textfile.py
@@ -1,4 +1,4 @@
-with open('txtfiles/sample2.txt', 'a') as f:
+with open('txtfiles/sample2.txt', 'a', encoding='utf-8') as f:
     f.write('書き込みもできます')
 
 with open('txtfiles/sample2.txt', encoding='utf-8') as f:
