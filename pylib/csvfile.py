b5b69eb136c86eccba422f5b2d7d15888eef72d2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2020-02-17 13:23:07 +0900

    list関数を使い2次元リストに変換して出力する

--- pylib/csvfile.py
+++ pylib/csvfile.py
@@ -2,5 +2,5 @@ import csv
 
 with open('txtfiles/sample3.csv', encoding='utf-8') as f:
     reader = csv.reader(f)
-    for row in reader:
-        print(row[0])
+    csvlist = list(reader)
+    print(csvlist)
