689e56ad0c3e3d7b87b4f5809a171432ff84e359
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-02-21 00:51:06 +0900

    配列の既存の要素を書き換えても検出されない例

---
+++ js/for_change.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        list: [ '赤パジャマ', '青パジャマ', '黃パジャマ' ]
+    },
+    methods: {
+        // ボタンクリック時に2番目の要素を変更
+        onclick: function () {
+            this.list[1] = '茶パジャマ';
+        }
+    }
+});
