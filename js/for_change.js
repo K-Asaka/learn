7029a4ac62f365a0dbd3f1735464e3db1229eb07
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-02-21 00:52:37 +0900

    配列への変更をVue.setメソッドを使ってVue.jsに伝達する

--- js/for_change.js
+++ js/for_change.js
@@ -6,7 +6,7 @@ new Vue({
     methods: {
         // ボタンクリック時に2番目の要素を変更
         onclick: function () {
-            this.list[1] = '茶パジャマ';
+            Vue.set(this.list, 1, '茶パジャマ');
         }
     }
 });
