214f2b3d305f755aaf6ddd4d4c73acf091f1565e
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-02-24 14:42:05 +0900

    propsや$emitを利用しない親子間通信

---
+++ js/ev_parent.js
@@ -0,0 +1,24 @@
+// my-childコンポーネントを定義
+Vue.component('my-child', {
+    data: function () {
+        return {
+            message: ''
+        }
+    },
+    template: `<p>子：{{ message }}</p>`,
+    // マウント時に親のmessageを設定
+    mounted: function () {
+        this.$parent.message = '子から設定';
+    }
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        message: ''
+    },
+    // マウント時に子のmessageを設定
+    mounted: function () {
+        this.$refs.child.message = '親から設定';
+    }
+});
