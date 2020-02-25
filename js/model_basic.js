05c47c18124d6ba2841365c8c5814a3a32ef9eea
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-02-25 23:55:32 +0900

    別解：算出プロパティによる出し入れ

--- js/model_basic.js
+++ js/model_basic.js
@@ -2,8 +2,21 @@ Vue.component('my-input', {
     props: [ 'value' ],
     template: `<label>
         名前：
-        <input type="text" v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
-    </label>`
+        <input type="text" v-model="internalValue">
+    </label>`,
+    // valueプロパティを操作するための算出プロパティを準備
+    computed: {
+        internalValue: {
+            get() {
+                return this.value;
+            },
+            set(newValue) {
+                if (this.value !== newValue) {
+                    this.$emit('input', newValue);
+                }
+            }
+        }
+    }
 });
 
 new Vue({
