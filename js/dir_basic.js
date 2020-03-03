590666146b3c7a8fa0937e8cb44cf85919921101
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-03-03 23:47:42 +0900

    ローカルディレクティブの例

--- js/dir_basic.js
+++ js/dir_basic.js
@@ -1,12 +1,19 @@
 // ディレクティブを定義
-Vue.directive('highlight', {
-    bind: function (el, binding, vnode, oldVnode) {
-        el.style.backgroundColor = binding.value;
-    }
-});
+// Vue.directive('highlight', {
+//     bind: function (el, binding, vnode, oldVnode) {
+//         el.style.backgroundColor = binding.value;
+//     }
+// });
 
 new Vue({
     el: '#app',
+    directives: {
+        highlight: {
+            bind: function (el, binding, vnode, oldVnode) {
+                el.style.backgroundColor = binding.value;
+            }
+        }
+    },
     data: {
         color: 'yellow'
     }
