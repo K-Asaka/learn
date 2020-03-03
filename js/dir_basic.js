bac371f42f38eefb095fdf3875f42551aad21847
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-03-03 23:21:26 +0900

    ディレクティブの基本

---
+++ js/dir_basic.js
@@ -0,0 +1,13 @@
+// ディレクティブを定義
+Vue.directive('highlight', {
+    bind: function (el, binding, vnode, oldVnode) {
+        el.style.backgroundColor = binding.value;
+    }
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        color: 'yellow'
+    }
+});
