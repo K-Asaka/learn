bb4ac5086bf066db3446a28f336d642ed332826b
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-03-09 23:53:44 +0900

    bindとupdateをまとめて定義する

--- js/dir_update.js
+++ js/dir_update.js
@@ -1,12 +1,17 @@
-Vue.directive('highlight', {
-    // 紐づいた時の処理（初回のみ）
-    bind: function (el, binding, vnode, oldVnode) {
-        el.style.backgroundColor = binding.value;
-    },
-    // 上位コンポーネントが変化した時
-    update: function (el, binding, vnode, oldVnode) {
-        el.style.backgroundColor = binding.value;
-    }
+// Vue.directive('highlight', {
+//     // 紐づいた時の処理（初回のみ）
+//     bind: function (el, binding, vnode, oldVnode) {
+//         el.style.backgroundColor = binding.value;
+//     },
+//     // 上位コンポーネントが変化した時
+//     update: function (el, binding, vnode, oldVnode) {
+//         el.style.backgroundColor = binding.value;
+//     }
+// });
+
+// bind/updateをまとめて定義
+Vue.directive('highlight', function (el, binding, vnode, oldVnode) {
+    el.style.backgroundColor = binding.value;
 });
 
 new Vue({
