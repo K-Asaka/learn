c44e9a5a8f89081e13085b1272561aea10ed1bb5
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-03-22 10:35:23 +0900

    モジュールの定義と利用

---
+++ module_basic/index.js
@@ -0,0 +1,6 @@
+import { Article, getTriangle } from './App.js';
+
+console.log(getTriangle(10, 5));        // 結果: 25
+
+let a = new Article();
+console.log(a.getAppTitle());           // 結果: Vue.jsアプリ
