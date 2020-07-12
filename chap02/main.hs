7455e78445828cbe427a0e27e52126e176f527b3
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-07-12 19:25:01 +0900

    I/Oアクションの実行結果を変数に束縛する

---
+++ chap02/main.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+import System.Environment
+
+main :: IO ()
+main = do
+    let title = "Current User:"
+    user <- getEnv "USER" -- Windowsの場合はUSERNAME
+    putStrLn title
+    putStrLn user
+
