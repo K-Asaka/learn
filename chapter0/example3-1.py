b80adde115d9202b00dc43c6a5334b5daf1b0529
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-02 01:04:40 +0900

    練習3-1

---
+++ chapter0/example3-1.py
@@ -0,0 +1,17 @@
+num = int(input("数値を入力>> "))
+
+if num % 2 == 0:
+    print('偶数')
+else:
+    print('奇数')
+
+data = input('入力>> ')
+
+if data == 'こんにちは':
+    print('ようこそ！')
+elif data == '景気は？':
+    print('ぼちぼちです')
+elif data == 'さようなら':
+    print('お元気で！');
+else:
+    print('どうしました？')
