3fdd3d42316372266c6a37bd45c11e16fe3ab90c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 22:33:35 +0900

    SNS2列とsales列の散布図を作成

--- chapter6/cod01.py
+++ chapter6/cod01.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('datafile/cinema.csv')
 print(df.head(3))
@@ -8,3 +9,7 @@ print(df.isnull().any(axis = 0))
 df2 = df.fillna(df.mean())
 # 穴埋めできたか確認
 print(df2.isnull().any(axis = 0))
+
+# SNS2とsalesの散布図の作成
+df2.plot(kind = 'scatter', x = 'SNS2', y = 'sales')
+plt.show()
