0d2377911b5f69764fc1393b5596f37ea43e1992
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 00:31:03 +0900

    練習6-5

--- chapter6/example01.py
+++ chapter6/example01.py
@@ -9,3 +9,8 @@ print(df.head(5))
 # 練習6-4
 print(df.isnull().sum())
 df2 = df.fillna(df.median())
+
+# 練習6-5
+import matplotlib.pyplot as plt
+df2.plot(kind = 'scatter', x = 'x0', y = 'target')
+plt.show()
