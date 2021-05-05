6b84ae650d9cd598165ff56ccb6f352ece620ed3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 17:46:39 +0900

    欠損値の確認

--- chapter5/cod01.py
+++ chapter5/cod01.py
@@ -9,3 +9,19 @@ print(syurui[0])
 
 # value_countsメソッドでデータの出現回数をカウント
 print(df['種類'].value_counts())
+
+# tailメソッドでデータフレームの末尾3行を表示
+print(df.tail(3))
+
+# isnullメソッドで欠損値の有無を調べる
+print(df.isnull())
+
+# 列単位で欠損値が存在するか調べる
+print(df.isnull().any(axis = 0))
+
+# sumメソッドで各列の合計値を求める
+print(df.sum())
+
+# 各列に欠損値がいくつあるか集計
+tmp = df.isnull()
+print(tmp.sum())
