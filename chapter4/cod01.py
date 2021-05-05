89296be1766f4a43214a80a269627dc3daa9c0b2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 14:26:42 +0900

    インデックスや列名の変更と参照方法の確認

--- chapter4/cod01.py
+++ chapter4/cod01.py
@@ -5,9 +5,23 @@ data = {
     'Bの労働時間' : [161, 175]
 }
 
+# データフレームの作成
 df = pd.DataFrame(data)
 print(df)
 
+# データフレームのデータ型を確認
 print(type(df))
+# 行数や列数を調べる
 print(df.shape)
+
+# インデクスの変更
+df.index = ['4月', '5月']
+print(df)
+
+# 列名の変更
+df.columns = ['Aの労働(h)', 'Bの労働(h)']
+print(df)
+
+print(df.index)
+print(df.columns)
 
