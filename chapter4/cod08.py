4f666a09a548b1e3a19ea03f21638b7eac4827ea
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 14:47:33 +0900

    特定の列の参照方法の確認

--- chapter4/cod08.py
+++ chapter4/cod08.py
@@ -4,3 +4,18 @@ import pandas as pd
 df = pd.read_csv('datafile/KvsT.csv')
 # 先頭3行だけ表示
 print(df.head(3))
+
+# 指定した列だけを参照する
+print(df['身長'])
+
+# 複数の列を一度に参照する
+# 抜き出したい列名の文字列リストを作成
+col = ['身長', '体重']
+# 身長列と体重列のみを抜き出す
+print(df[col])
+
+# 1列だけ抜き出したデータの型
+print(type(df['派閥']))
+
+# 1次元のデータを扱うSeries型
+print(df['派閥'])
