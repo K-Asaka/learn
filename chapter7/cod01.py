89427d404ac81c403bded33b486a26d7f54c8c6f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 00:45:37 +0900

    CSVファイルの読み込み

---
+++ chapter7/cod01.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('datafile/Survived.csv')
+print(df.head(2))
