f36b32e2849151de1f69b7aa7c7a76512ee35ee3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 22:29:45 +0900

    欠損値の処理

--- chapter6/cod01.py
+++ chapter6/cod01.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('datafile/cinema.csv')
 print(df.head(3))
+
+print(df.isnull().any(axis = 0))
+# 欠損値を平均で補完して、df2に代入
+df2 = df.fillna(df.mean())
+# 穴埋めできたか確認
+print(df2.isnull().any(axis = 0))
