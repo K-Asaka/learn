f25098835b3371049aee911d200ec6059690ea1b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 23:07:05 +0900

    スライス構文で特徴量と正解データを取り出す

--- chapter6/cod01.py
+++ chapter6/cod01.py
@@ -23,9 +23,9 @@ no = df2[(df2['SNS2'] > 1000) & (df2['sales'] < 8500)].index
 df3 = df2.drop(no, axis = 0)
 
 # 特徴量の列の候補
-col = ['SNS1', 'SNS2', 'actor', 'original']
-x = df3[col]        # 特徴量の取り出し
-t = df3['sales']    # 正解データの取り出し
+#col = ['SNS1', 'SNS2', 'actor', 'original']
+#x = df3[col]        # 特徴量の取り出し
+#t = df3['sales']    # 正解データの取り出し
 
 # インデックスが2、列がSNS1のマスの値のみ参照
 print(df3.loc[2, 'SNS1'])
@@ -38,3 +38,10 @@ print(df3.loc[index, col])
 # スライス構文で連続した要素を参照する
 sample = [10, 20, 30, 40]   # リストの作成
 print(sample[1:3])          # 添え字が1以上3未満の要素を取得
+
+# 0行目以上2行目以下、actor列より左の列(actor列含む)
+print(df3.loc[0:3, :'actor'])
+
+# スライス構文で特徴量と正解データを取り出す
+x = df3.loc[ : , 'SNS1':'original'] # 特徴量の取り出し
+t = df3['sales']                    # 正解ラベルの取り出し
