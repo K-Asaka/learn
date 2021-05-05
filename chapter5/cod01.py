1ac50a96ad57aed25eee7f273d19256750ed697e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 17:41:35 +0900

    value_countsメソッドでデータの出現回数をカウント

--- chapter5/cod01.py
+++ chapter5/cod01.py
@@ -3,6 +3,9 @@ import pandas as pd
 df = pd.read_csv('datafile/iris.csv')
 print(df.head(3))
 
+# array型の特定要素を参照
 syurui = df['種類'].unique()
 print(syurui[0])
 
+# value_countsメソッドでデータの出現回数をカウント
+print(df['種類'].value_counts())
