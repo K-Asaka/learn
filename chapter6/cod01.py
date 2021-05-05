d1aed9284743b407183798365e531a831cb96c98
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-05 22:44:12 +0900

    外れ値を削除する

--- chapter6/cod01.py
+++ chapter6/cod01.py
@@ -11,10 +11,13 @@ df2 = df.fillna(df.mean())
 print(df2.isnull().any(axis = 0))
 
 # SNS2とsalesの散布図の作成
-for name in df.columns:     # for name in df: でも可
-    # X軸がcinema_id列とsales列の散布図は作っても意味がないので外す
-    if name == 'cinema_id' or name == 'sales':
-        continue
+# for name in df.columns:     # for name in df: でも可
+#     # X軸がcinema_id列とsales列の散布図は作っても意味がないので外す
+#     if name == 'cinema_id' or name == 'sales':
+#         continue
 
-    df2.plot(kind = 'scatter', x = name, y = 'sales')
-plt.show()
+#     df2.plot(kind = 'scatter', x = name, y = 'sales')
+# plt.show()
+
+no = df2[(df2['SNS2'] > 1000) & (df2['sales'] < 8500)].index
+df3 = df2.drop(no, axis = 0)
