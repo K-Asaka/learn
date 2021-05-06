a7742c854de625992cf4455b874692aaa1197c1a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 10:40:58 +0900

    練習7-5

--- chapter7/example01.py
+++ chapter7/example01.py
@@ -12,3 +12,9 @@ print(df.groupby('class').mean()['score'])
 
 # 練習7-4
 print(pd.pivot_table(df, index = 'class', columns = 'sex', values = 'score'))
+
+# 練習7-5
+dummy = pd.get_dummies(df['dept_id'], drop_first = True)
+df2 = pd.concat([df, dummy], axis = 1)
+df2 = df2.drop('dept_id', axis = 1)
+print(df2)
