e7805567b3c48d6e877fd097fd507664aba3f7ca
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 11:03:24 +0900

    訓練データ&検証データとテストデータに分割する

--- chapter8/cod01.py
+++ chapter8/cod01.py
@@ -13,3 +13,5 @@ df2 = pd.concat([df, crime], axis = 1)
 df2 = df2.drop(['CRIME'], axis = 1)
 print(df2.head(2))
 
+# 訓練データ&検証データとテストデータに分割する
+train_val, test = train_test_split(df2, test_size = 0.2, random_state = 0)
