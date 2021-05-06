e3c8b98219c563b66bde6ac11c4a292595e02b2e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 11:02:33 +0900

    CRIME列をダミー変数化

--- chapter8/cod01.py
+++ chapter8/cod01.py
@@ -5,3 +5,11 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('datafile/Boston.csv')
 print(df.head(2))
+
+# ダミー変数化
+print(df['CRIME'].value_counts())
+crime = pd.get_dummies(df['CRIME'], drop_first = True)
+df2 = pd.concat([df, crime], axis = 1)
+df2 = df2.drop(['CRIME'], axis = 1)
+print(df2.head(2))
+
