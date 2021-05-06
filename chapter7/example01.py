0050477e2b2af7bf0c9501959e1e7e26dc400c81
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-06 10:35:56 +0900

    練習7-4

--- chapter7/example01.py
+++ chapter7/example01.py
@@ -6,3 +6,9 @@ print(df.head(3))
 
 # 練習7-2
 print(df['sex'].mean())
+
+# 練習7-3
+print(df.groupby('class').mean()['score'])
+
+# 練習7-4
+print(pd.pivot_table(df, index = 'class', columns = 'sex', values = 'score'))
