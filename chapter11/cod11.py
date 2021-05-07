72262fb2c43716b271d04c38c6becfdbe6ff93c2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-07 13:06:58 +0900

    回帰木を用いた学習と特徴量の重要度の参照

--- chapter11/cod11.py
+++ chapter11/cod11.py
@@ -1,9 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('Boston.csv')
+df = pd.read_csv('datafile/Boston.csv')
 df = df.fillna(df.mean())
 x = df.loc[ : , 'ZN':'LSTAT']
 t = df['PRICE']
 
 x_train, x_test, y_train, y_test = train_test_split(x, t, test_size = 0.3, random_state = 0)
+
+from sklearn.tree import DecisionTreeRegressor
+
+# 木の深さの最大を10と設定
+model = DecisionTreeRegressor(max_depth = 10, random_state = 0)
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
+
+print(pd.Series(model.feature_importances_, index = x.columns))
