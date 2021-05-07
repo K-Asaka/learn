3dfd5674907fa346065b2824f1f36faf11fcf5a1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-07 09:06:58 +0900

    内部結合を行う

--- chapter10/cod01.py
+++ chapter10/cod01.py
@@ -3,9 +3,14 @@ import pandas as pd
 df = pd.read_csv('datafile/bike.tsv', sep = '\t')
 print(df.head(3))
 
-df2 = pd.read_csv('datafile/weather.csv', encoding = 'shift-jis')
+df2 = pd.read_csv('datafile/weather.csv')
 print(df2.head(3))
+
+weather = pd.read_csv('datafile/weather.csv', encoding = 'shift-jis')
 
 temp = pd.read_json('datafile/temp.json')
 print(temp.head(2))
 print(temp.T)
+
+df2 = df.merge(weather, how = 'inner', on = 'weather_id')
+print(df2.head(2))
