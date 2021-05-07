b6d090a169b648c158e511c1fc9d9a04382cee99
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-07 09:00:34 +0900

    文字コードを指定してweather.csvを読み込む

--- chapter10/cod01.py
+++ chapter10/cod01.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.read_csv('datafile/bike.tsv', sep = '\t')
 print(df.head(3))
 
-df2 = pd.read_csv('datafile/weather.csv')
+df2 = pd.read_csv('datafile/weather.csv', encoding = 'shift-jis')
 print(df2.head(3))
