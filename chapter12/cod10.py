283a84eb89173529539dbae751a067cbd9327324
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-08 11:25:16 +0900

    文字データの列を数値に変換する

--- chapter12/cod10.py
+++ chapter12/cod10.py
@@ -28,3 +28,14 @@ df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 43
 
 jo2 = df['Survived'] == 1
 df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 35
+
+# 特徴量として利用する列のリスト
+col = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+
+x = df[col]
+t = df['Survived']
+
+# Sex列は文字の列なのでダミー変数化
+dummy = pd.get_dummies(df['Sex'], drop_first = True)
+x = pd.concat([x, dummy], axis = 1)
+print(x.head(2))
