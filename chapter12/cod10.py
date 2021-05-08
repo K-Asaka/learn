fb23fdb056eb7bc86d35061d52657a67b29037d0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-08 11:19:21 +0900

    欠損値を穴埋めする

--- chapter12/cod10.py
+++ chapter12/cod10.py
@@ -4,3 +4,27 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('datafile/Survived.csv')
 print(df.head(2))
+
+jo1 = df['Pclass'] == 1
+jo2 = df['Survived'] == 0
+jo3 = df['Age'].isnull()
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 43
+
+jo2 = df['Pclass'] == 1
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 35
+
+jo1 = df['Pclass'] == 2
+jo2 = df['Survived'] == 0
+jo3 = df['Age'].isnull()
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 26
+
+jo2 = df['Survived'] == 1
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 20
+
+jo1 = df['Pclass'] == 3
+jo2 = df['Survived'] == 0
+jo3 = df['Age'].isnull()
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 43
+
+jo2 = df['Survived'] == 1
+df.loc[(jo1) & (jo2) & (jo3), 'Age'] = 35
