8980acd9f286a23ac7b7b318fb22cef11433b4ac
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-09 14:44:59 +0900

    予測結果と実際の誤差を検証する

--- chapter13/cod01.py
+++ chapter13/cod01.py
@@ -17,3 +17,18 @@ print(mse)
 
 import math
 print(math.sqrt(mse))   # RMSEの計算
+
+from sklearn.metrics import mean_absolute_error
+yosoku = [2, 3, 5, 7, 11, 13]   # 予測結果をリストで作成
+target = [3, 5, 8, 11, 16, 19]  # 実際の結果をリストで作成
+
+mse = mean_squared_error(yosoku, target)
+print('rmse:{}'.format(math.sqrt(mse)))
+print('mae:{}'.format(mean_absolute_error(yosoku, target)))
+
+print('外れ値の混入')
+yosoku = [2, 3, 5, 7, 11, 13, 46]   # 実際には23だけど46と予測
+target = [3, 5, 8, 11, 16, 19, 23]
+mse = mean_squared_error(yosoku, target)
+print('rmse:{}'.format(math.sqrt(mse)))
+print('mae:{}'.format(mean_absolute_error(yosoku, target)))
