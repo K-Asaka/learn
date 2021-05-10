5854f3547571dfb23856458f935a7b66c9e30520
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-10 10:52:55 +0900

    練習15-5

--- chapter15/example01.py
+++ chapter15/example01.py
@@ -13,3 +13,17 @@ df = df.fillna(df.mean())
 dummy = pd.get_dummies(df['Sex'], drop_first = True)
 df = pd.concat([df, dummy], axis = 1)
 df = df.drop('Sex', axis = 1)
+
+# 練習15-5
+from sklearn.covariance import MinCovDet
+import matplotlib.pyplot as plt
+
+mcd = MinCovDet()
+mcd.fit(df)
+maha_dis = mcd.mahalanobis(df)
+tmp = pd.Series(maha_dis)
+tmp.plot(kind = 'box')
+plt.show()
+
+num = tmp[tmp > 10000].index
+df = df.drop(num)
