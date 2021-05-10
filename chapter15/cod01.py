b35a4f5eaa3645bc961bb862f3f0d3c82ffcf8cc
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-10 10:35:24 +0900

    クラスタ数2～30でSSEを調べ、折れ線グラフを描画する

--- chapter15/cod01.py
+++ chapter15/cod01.py
@@ -31,3 +31,19 @@ import matplotlib.pyplot as plt
 cluster_mean = sc_df.groupby('cluster').mean()
 cluster_mean.plot(kind = 'bar')
 plt.show()
+
+sse_list = []
+# クラスタ数2～30でSSE(クラスタ内誤差平方和)を調べる
+for n in range(2, 31):
+    model = KMeans(n_clusters = n, random_state = 0)
+    model.fit(sc_df)
+    sse = model.inertia_    # SSEの計算
+    sse_list.append(sse)
+
+print(sse_list)
+
+se = pd.Series(sse_list)
+num = range(2, 31)      # range関数で2～30の整数列を作る
+se.index = num          # シリーズのインデックスを変更
+se.plot(kind = 'line')
+plt.show()
