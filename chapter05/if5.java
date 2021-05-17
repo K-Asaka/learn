10ded334eb9f485cab6e553a8855a811c95c3b87
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 22:10:51 +0900

    if～else文を記述

---
+++ chapter05/if5.java
@@ -0,0 +1,10 @@
+public class if5 {
+    public static void main(String[] args) {
+        int brightness = 5;
+        if (brightness < 4) {
+            System.out.println("ライトON");
+        } else {
+            System.out.println("ライトOFF");
+        }
+    }
+}
