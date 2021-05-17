1ee683f44840a07fc3805aabf67a3445ddbb1e01
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:07:10 +0900

    切り捨て、切り上げ、四捨五入

---
+++ chapter04/Math2.java
@@ -0,0 +1,8 @@
+public class Math2 {
+    public static void main(String[] args) {
+        double d1 = 19.5;
+        System.out.println("ceil: " + Math.ceil(d1));
+        System.out.println("floor: " + Math.floor(d1));
+        System.out.println("round: " + Math.round(d1));
+    }
+}
