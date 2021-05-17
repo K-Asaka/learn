b6576821c2234c7fff840847c7c8b506fe468b12
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:13:06 +0900

    Mathクラスのpowメソッドを使って半径の2乗を求める例

---
+++ chapter04/Menseki2.java
@@ -0,0 +1,8 @@
+public class Menseki2 {
+    public static void main(String[] args) {
+        double hankei = 10.0;
+        double menseki = Math.PI * Math.pow(hankei, 2);
+        System.out.println("半径: " + hankei);
+        System.out.println("面積: " + menseki);
+    }
+}
