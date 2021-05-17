c9005caf6a92f06406f76b8423b3f3236784c7e9
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:01:16 +0900

    掛け算と割り算

---
+++ chapter03/Calc1.java
@@ -0,0 +1,7 @@
+public class Calc1 {
+    public static void main(String[] args) {
+        System.out.println(4 * 5);
+        System.out.println(100 / 5);
+        System.out.println(10 / 3);
+    }
+}
