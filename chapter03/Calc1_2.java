5dbb4b4d8964c6520f85854eab3bdd6ac80baba6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:04:10 +0900

    小数点以下の表示

---
+++ chapter03/Calc1_2.java
@@ -0,0 +1,7 @@
+public class Calc1_2 {
+    public static void main(String[] args) {
+        System.out.println(4 * 5);
+        System.out.println(100 / 5);
+        System.out.println(10.0 / 3);
+    }
+}
