45629eabbd791b76e8c6c89dc766e1a515ee062f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:05:09 +0900

    割り算の余りを求める

---
+++ chapter03/Calc2.java
@@ -0,0 +1,7 @@
+public class Calc2 {
+    public static void main(String[] args) {
+        System.out.println(10 % 5);
+        System.out.println(20 % 6);
+        System.out.println(16 % 5);
+    }
+}
