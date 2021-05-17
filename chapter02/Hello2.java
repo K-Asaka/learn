625b23e166efabbf2eaf35416f0a7429bda53136
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 17:47:40 +0900

    文を追加する

---
+++ chapter02/Hello2.java
@@ -0,0 +1,9 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("こんにちはJava");
+        System.out.println(100);
+        System.out.print("東京");
+        System.out.print("オリンピックは");
+        System.out.println("2020年");
+    }
+}
