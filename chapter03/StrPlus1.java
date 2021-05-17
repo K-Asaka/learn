6686a108d1c38e31b9c095401bc0e81651bf320b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:06:20 +0900

    文字列の連結

---
+++ chapter03/StrPlus1.java
@@ -0,0 +1,7 @@
+public class StrPlus1 {
+    public static void main(String[] args) {
+        System.out.println("はじめての" + "Java");
+        System.out.println("19" + "59");
+        System.out.println(2020 + "年");
+    }
+}
