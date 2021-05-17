25f541fe433ea02534059fcaad337c1905344d92
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 00:12:32 +0900

    例外

---
+++ chapter05/Exception1.java
@@ -0,0 +1,8 @@
+public class Exception1 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = num1 / 0;
+        System.out.println(num2);
+        System.out.println("プログラムの最後です");
+    }
+}
