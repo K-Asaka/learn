0a8a3209b4e06253bfdc3be55eb9d6883c882e2d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 17:50:08 +0900

    コメントを使う

--- chapter02/Hello2.java
+++ chapter02/Hello2.java
@@ -1,5 +1,9 @@
 public class Hello2 {
+    /*
+        テスト用のプログラム
+    */
     public static void main(String[] args) {
+        // 以下では数値の計算結果を表示
         System.out.println(200 + 40);
         System.out.println(200 - 40);
     }
