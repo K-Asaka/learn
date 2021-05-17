af673716acbcb7b257141725100fe107d7d4e2b0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:24:18 +0900

    変数に計算を行って結果を同じ変数に代入する

---
+++ chapter03/Dainyu1.java
@@ -0,0 +1,15 @@
+public class Dainyu1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        num1 += 5;
+        System.out.println(num1);
+        num1 -= 3;
+        System.out.println(num1);
+        num1 *= 4;
+        System.out.println(num1);
+        num1 /= 2;
+        System.out.println(num1);
+        num1 %= 5;
+        System.out.println(num1);
+    }
+}
