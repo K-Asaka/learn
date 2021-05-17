dcd3aec5e9622c11be9b1047f01126aa3cd89b7b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:18:45 +0900

    整数の値を1増やす

---
+++ chapter03/Inc1.java
@@ -0,0 +1,9 @@
+public class Inc1 {
+    public static void main(String[] args) {
+        int year = 2000;
+        year = year + 1;
+        System.out.println(year);
+        year = year + 1;
+        System.out.println(year);
+    }
+}
