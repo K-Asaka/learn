11c5b45d75325b9b7c8f556ba134beccc2d68233
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:33:11 +0900

    自動で行われるキャスト

---
+++ chapter03/Cast1.java
@@ -0,0 +1,12 @@
+public class Cast1 {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        int i1 = 2;
+        int i2 = 5;
+        double d1 = 5.5;
+        i1 = b1;
+        d1 = i2;
+        System.out.println("i1: " + i1);
+        System.out.println("d1: " + d1);
+    }
+}
