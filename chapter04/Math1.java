5562ccdaa2b04a4fd60846458c19319620e0cf03
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:05:41 +0900

    double型データの最大値をMahクラスのメソッドで取得する

---
+++ chapter04/Math1.java
@@ -0,0 +1,8 @@
+public class Math1 {
+    public static void main(String[] args) {
+        double d1 = 19.5;
+        double d2 = 24.4;
+        double max = Math.max(d1, d2);
+        System.out.println(max);
+    }
+}
