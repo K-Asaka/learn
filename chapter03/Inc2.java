d613bb5c5888c2f1866f3eb64f93dfb5ef98b759
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:19:01 +0900

    ++演算子を使う

---
+++ chapter03/Inc2.java
@@ -0,0 +1,9 @@
+public class Inc2 {
+    public static void main(String[] args) {
+        int year = 2000;
+        year++;
+        System.out.println(year);
+        year++;
+        System.out.println(year);
+    }
+}
