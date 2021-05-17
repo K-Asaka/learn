824df6fb567a11d14cffdf63941209184bd26ecf
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:15:28 +0900

    変数を使用した計算例

---
+++ chapter03/DollarToYen.java
@@ -0,0 +1,8 @@
+public class DollarToYen {
+    public static void main(String[] args) {
+        double rate = 105.1;
+        System.out.println(4 * rate);
+        System.out.println(10 * rate);
+        System.out.println(8 * rate);
+    }
+}
