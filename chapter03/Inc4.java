f6971a90ff86c463a352a69acf752971957afc61
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:21:48 +0900

    ++演算子の位置(変数の前に記述)

---
+++ chapter03/Inc4.java
@@ -0,0 +1,9 @@
+public class Inc4 {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 3;
+        num2 = ++num1;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+}
