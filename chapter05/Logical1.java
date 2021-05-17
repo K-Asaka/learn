31c0ce47b790c3772ec157a28fc79fe000320201
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 22:52:17 +0900

    論理演算子の結果を確認する

---
+++ chapter05/Logical1.java
@@ -0,0 +1,9 @@
+public class Logical1 {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("!b1: " + !b1);
+        System.out.println("b1 && b2: " + (b1 && b2));
+        System.out.println("b1 || b2: " + (b1 || b2));
+    }
+}
