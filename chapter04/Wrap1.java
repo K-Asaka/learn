1f2854e38d62ed0f05945568858c2c7dcb312c90
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:19:12 +0900

    Integerクラスを使用する

---
+++ chapter04/Wrap1.java
@@ -0,0 +1,7 @@
+public class Wrap1 {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf(3);
+        System.out.println(i1.intValue());
+        System.out.println(i1.doubleValue());
+    }
+}
