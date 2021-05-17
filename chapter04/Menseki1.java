38fd5618349b1e8b0ea5e4887c1f0643547deb8c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:11:10 +0900

    円の面積を求める

---
+++ chapter04/Menseki1.java
@@ -0,0 +1,8 @@
+public class Menseki1 {
+    public static void main(String[] args) {
+        double hankei = 10.0;
+        double menseki = Math.PI * hankei * hankei;
+        System.out.println("半径: " + hankei);
+        System.out.println("面積: " + menseki);
+    }
+}
