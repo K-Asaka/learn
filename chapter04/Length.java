ec883e681ed4ee589dd3564a50f3f54fcee6041e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:44:39 +0900

    文字列の長さを調べるlengthメソッド

---
+++ chapter04/Length.java
@@ -0,0 +1,7 @@
+public class Length {
+    public static void main(String[] args) {
+        String s1 = "こんにちは";
+        int len = s1.length();
+        System.out.println("s1の長さ: " + len);
+    }
+}
