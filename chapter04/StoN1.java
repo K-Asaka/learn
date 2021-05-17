83363f396ec193e297c2ea7efa5fc0c31224fcd5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:20:32 +0900

    文字列を数値に変換する

---
+++ chapter04/StoN1.java
@@ -0,0 +1,10 @@
+public class StoN1 {
+    public static void main(String[] args) {
+        String s1 = "456";
+        String s2 = "3.14";
+        System.out.println(s1 + s2);
+        int i1 = Integer.parseInt(s1);
+        double d1 = Double.parseDouble(s2);
+        System.out.println(i1 + d1);
+    }
+}
