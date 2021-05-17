f71e931681978e6fc7d7b626a0293677b51ba206
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:50:32 +0900

    文字列から指定した範囲の文字列を取り出す

---
+++ chapter04/Substring1.java
@@ -0,0 +1,7 @@
+public class Substring1 {
+    public static void main(String[] args) {
+        String s1 = "0123456789";
+        String s2 = s1.substring(3, 6);
+        System.out.println(s2);
+    }
+}
