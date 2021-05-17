0755dd4590c2e67ca9828eec71f6a944e9166157
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:43:05 +0900

    +演算子で変数内の文字列を連結する

---
+++ chapter04/StrPlus.java
@@ -0,0 +1,9 @@
+public class StrPlus {
+    public static void main(String[] args) {
+        String s1 = "こんにちは";
+        String s2 = "Java";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+        System.out.println("ようこそ" + s2);
+    }
+}
