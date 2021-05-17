d13c3b93fc461cf8b625d6c50c14ab5e6dd29057
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 18:56:57 +0900

    コンストラクタを使用しないでメソッドでインスタンスを生成する

---
+++ chapter04/Date3.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+public class Date3 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today.toString());
+    }
+}
