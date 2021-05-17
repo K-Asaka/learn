fa757821b79a37b167f1ff3ad28730b862401196
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:14:20 +0900

    Randomクラスを使った乱数

---
+++ chapter04/Random2.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Random2 {
+    public static void main(String[] args) {
+        Random r1 = new Random();
+        int randomNum = r1.nextInt(10);
+        System.out.println(randomNum);
+    }
+}
