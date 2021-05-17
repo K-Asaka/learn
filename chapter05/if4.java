174b19558cdd35d83b7ceb9ca8723b7a21ade020
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 22:09:37 +0900

    値が等しいかどうかを判断する

---
+++ chapter05/if4.java
@@ -0,0 +1,10 @@
+public class if4 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        boolean b1, b2;
+
+        b1 = 5 == num1;
+        b2 = 5 != num1;
+        System.out.println(b1 + " " + b2);
+    }
+}
