2a87712373163c52075a64978d0b1c636a6287c6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:32:02 +0900

    2つの値を比較する関係演算子

---
+++ chapter05/if2.java
@@ -0,0 +1,8 @@
+public class if2 {
+    public static void main(String[] args) {
+        int brightness = 3;
+        if (brightness < 4) {
+            System.out.println("ライトON");
+        }
+    }
+}
