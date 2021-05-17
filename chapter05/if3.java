305590c85391fa61078b066f3f2a33148e1007a4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 22:07:00 +0900

    値の大小を比較する

---
+++ chapter05/if3.java
@@ -0,0 +1,12 @@
+public class if3 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        boolean b1, b2, b3, b4;
+
+        b1 = 5 > num1;
+        b2 = 5 >= num1;
+        b3 = 4 < num1;
+        b4 = 2 <= num1;
+        System.out.println(b1 + " " + b2 + " " + b3 + " " + b4);
+    }
+}
