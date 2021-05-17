a81c6fe3e004b7e29a507c3bdeec2ac444d65af2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 19:21:58 +0900

    数値を文字列に変換する

---
+++ chapter04/NtoS1.java
@@ -0,0 +1,10 @@
+public class NtoS1 {
+    public static void main(String[] args) {
+        int i1 = 15;
+        double d1 = 9.54;
+        System.out.println(i1 + d1);
+        String s1 = String.valueOf(i1);
+        String s2 = Double.toString(d1);
+        System.out.println(s1 + s2);
+    }
+}
