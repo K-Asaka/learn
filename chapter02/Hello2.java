a98b659b001da7dbceaca8e3702fad5904716a2a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-17 17:49:11 +0900

    足し算と引き算

--- chapter02/Hello2.java
+++ chapter02/Hello2.java
@@ -1,9 +1,6 @@
 public class Hello2 {
     public static void main(String[] args) {
-        System.out.println("こんにちはJava");
-        System.out.println(100);
-        System.out.print("東京");
-        System.out.print("オリンピックは");
-        System.out.println("2020年");
+        System.out.println(200 + 40);
+        System.out.println(200 - 40);
     }
 }
