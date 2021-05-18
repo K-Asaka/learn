76ade10a5de280e3152f5ab796d3acf3fbe4a5f2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 10:00:41 +0900

    フィールドとローカル変数

---
+++ chapter08/Field1.java
@@ -0,0 +1,9 @@
+public class Field1 {
+    static String msg1 = "Hello";
+    static int num1 = 100;
+    public static void main(String[] args) {
+        int num1 = 200;
+        System.out.println(msg1);
+        System.out.println(num1);
+    }
+}
