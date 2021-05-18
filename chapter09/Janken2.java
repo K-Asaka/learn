67e5803bb0cded439046b1858b6d3c83e2c760d8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 11:18:16 +0900

    引き算の結果を使って勝敗判定をする

---
+++ chapter09/Janken2.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Janken2 {
+    public static void main(String[] args) {
+        System.out.print("じゃんけん： 0:グー、1:チョキ、2:パー ?");
+        Random r = new Random();
+        int pc = r.nextInt(3);
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        scan.close();
+        int you = Integer.parseInt(s);
+        String[] janken = {"グー", "チョキ", "パー"};
+        System.out.println("あなた:" + janken[you] + "、コンピュータ:" + janken[pc]);
+        
+        int winlose = you - pc;
+
+        if (winlose == 0) {
+            System.out.println("あいこ");
+        } else if (winlose == 1 || winlose == -2) {
+            System.out.println("負け");
+        } else {
+            System.out.println("勝ち！");
+        }
+    }
+}
