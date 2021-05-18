167139bdcdd7ab814db9306dc818eee03596acf7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:12:44 +0900

    平成年と西暦の対応表を表示する

---
+++ chapter06/HeiseiToSeireki1.java
@@ -0,0 +1,7 @@
+public class HeiseiToSeireki1 {
+    public static void main(String[] args) {
+        for (int heisei = 1; heisei <= 31; heisei++) {
+            System.out.println("平成" + heisei + "年 - " + "西暦" + (heisei + 1988) + "年");
+        }
+    }
+}
