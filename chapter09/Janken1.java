7106acfe3bb7e6e0f9ced79b3b5111e12afaad16
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 11:09:46 +0900

    ユーザー入力の処理

--- chapter09/Janken1.java
+++ chapter09/Janken1.java
@@ -1,10 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Janken1 {
     public static void main(String[] args) {
         System.out.println("じゃんけん： 0:グー、1:チョキ、2:パー ?");
         Random r = new Random();
         int pc = r.nextInt(3);
-        System.out.println("コンピューター:" + pc);
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int you = Integer.parseInt(s);
+        scan.close();
+        System.out.println("あなた:" + you + "、コンピュータ:" + pc);
     }
 }
