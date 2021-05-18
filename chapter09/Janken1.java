fc8921c58fd905b32422f4bbb64f5feb2cc6cbdb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 11:08:05 +0900

    コンピュータの手の決定

--- chapter09/Janken1.java
+++ chapter09/Janken1.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class Janken1 {
     public static void main(String[] args) {
         System.out.println("じゃんけん： 0:グー、1:チョキ、2:パー ?");
+        Random r = new Random();
+        int pc = r.nextInt(3);
+        System.out.println("コンピューター:" + pc);
     }
 }
