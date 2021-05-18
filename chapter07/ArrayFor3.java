8fd6a77240266d896bf100b2889faa3c18fbc733
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:42:02 +0900

    for文で配列の要素を順に処理する

---
+++ chapter07/ArrayFor3.java
@@ -0,0 +1,9 @@
+public class ArrayFor3 {
+    public static void main(String[] args) {
+        int[] heiseis = {1, 5, 20, 29};
+        for (int i = 0; i < heiseis.length; i++) {
+            int seireki = heiseis[i] + 1988;
+            System.out.println("平成" + heiseis[i] + "年 -> " + "西暦" + seireki + "年");
+        }
+    }
+}
