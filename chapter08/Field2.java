a9e483a260b289db66eb5eb88fecfce1b235f7c3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 10:02:35 +0900

    変数の型に応じたフィールドの初期値を確認する

---
+++ chapter08/Field2.java
@@ -0,0 +1,12 @@
+public class Field2 {
+    static String str1;
+    static int intNum1;
+    static double doubleNum1;
+    static boolean bool1;
+    public static void main(String[] args) {
+        System.out.println("String: " + str1);
+        System.out.println("int: " + intNum1);
+        System.out.println("double: " + doubleNum1);
+        System.out.println("boolean: " + bool1);
+    }
+}
