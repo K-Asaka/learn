84a75064ee92283d5046aef94541f1e796e341e8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:32:20 +0900

    配列の要素にアクセスする

---
+++ chapter07/Array1.java
@@ -0,0 +1,13 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] ages = new int[5];
+        ages[0] = 10;
+        ages[1] = 15;
+        ages[2] = 20;
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+    }
+}
