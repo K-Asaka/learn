30279fb0e3e3c8dc80bb3507a443a28eaf49000a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:35:58 +0900

    配列のすべての要素に順にアクセスする

---
+++ chapter07/ArrayFor2.java
@@ -0,0 +1,8 @@
+public class ArrayFor2 {
+    public static void main(String[] args) {
+        int[] ages = {10, 15, 20};
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
+    }
+}
