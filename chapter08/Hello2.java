71282689d20127a537e1c1d223eb4a2d28f64514
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:53:47 +0900

    引数のあるメソッド

---
+++ chapter08/Hello2.java
@@ -0,0 +1,9 @@
+public class Hello2 {
+    static void hello2(String name) {
+        System.out.println("こんにちは" + name + "さん");
+    }
+    public static void main(String[] args) {
+        hello2("田中");
+        hello2("山田");
+    }
+}
