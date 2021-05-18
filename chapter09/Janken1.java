5ebdd436644f8fe9e589bbc186866c449de05932
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 11:15:11 +0900

    勝負の判定結果を表示する

--- chapter09/Janken1.java
+++ chapter09/Janken1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Janken1 {
     public static void main(String[] args) {
-        System.out.println("じゃんけん： 0:グー、1:チョキ、2:パー ?");
+        System.out.print("じゃんけん： 0:グー、1:チョキ、2:パー ?");
         Random r = new Random();
         int pc = r.nextInt(3);
         Scanner scan = new Scanner(System.in);
@@ -12,5 +12,30 @@ public class Janken1 {
         int you = Integer.parseInt(s);
         String[] janken = {"グー", "チョキ", "パー"};
         System.out.println("あなた:" + janken[you] + "、コンピュータ:" + janken[pc]);
+        if (you == 0) {
+            if (pc == 0) {
+                System.out.println("あいこ");
+            } else if (pc == 1) {
+                System.out.println("勝ち！");
+            } else {
+                System.out.println("負け");
+            }
+        } else if (you == 1) {
+            if (pc == 0) {
+                System.out.println("負け");
+            } else if (pc == 1) {
+                System.out.println("あいこ");
+            } else {
+                System.out.println("勝ち！");
+            }
+        } else {
+            if (pc == 0) {
+                System.out.println("勝ち！");
+            } else if (pc == 1) {
+                System.out.println("負け");
+            } else {
+                System.out.println("あいこ");
+            }
+        }
     }
 }
