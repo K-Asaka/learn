05c380de42e66fd0a04360b3f2a41c19c877bd02
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:34:49 +0900

    配列のすべての要素に順にアクセスする(for文を使わない場合)

---
+++ chapter07/ArrayFor1.java
@@ -0,0 +1,8 @@
+public class ArrayFor1 {
+    public static void main(String[] args) {
+        int[] ages = {10, 15, 20};
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
