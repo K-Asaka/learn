8f68e5a270ce36291da65b88988fe40a6992889b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-18 09:43:09 +0900

    lengthを使うと要素の数が増えても変更が簡単

--- chapter07/ArrayFor3.java
+++ chapter07/ArrayFor3.java
@@ -1,6 +1,6 @@
 public class ArrayFor3 {
     public static void main(String[] args) {
-        int[] heiseis = {1, 5, 20, 29};
+        int[] heiseis = {1, 5, 20, 29, 4, 9};
         for (int i = 0; i < heiseis.length; i++) {
             int seireki = heiseis[i] + 1988;
             System.out.println("平成" + heiseis[i] + "年 -> " + "西暦" + seireki + "年");
