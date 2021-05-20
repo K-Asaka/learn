471264f9596d204b00318f25e0e3832c4a7ac149
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-21 00:02:36 +0900

    コメントを使う

---
+++ Sample013.java
@@ -0,0 +1,20 @@
+public class Sample013 {
+    public static void main(String[] args) {
+        
+        // この行はコメントとして扱われます
+
+        /*
+        この行の間は
+        コメントとして扱われます。
+        */
+
+        //System.out.println("このメッセージは表示されません");
+
+        /*
+        System.out.println("このメッセージは表示されません");
+        System.out.println("このメッセージは表示されません");
+        */
+
+        System.out.println("このメッセージは表示されます");
+    }
+}
