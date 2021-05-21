8b5d322657b7a6e8f4d33508e6ce1098b694111f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-22 01:27:21 +0900

    演算を行う

---
+++ Sample017.java
@@ -0,0 +1,14 @@
+public class Sample017 {
+    public static void main(String[] args) {
+        // 変数を宣言し初期値を設定する
+        int a = 20;
+        int b = 4;
+
+        // 演算を行う
+        System.out.println("a + b は" + (a + b) + " です");
+        System.out.println("a - b は" + (a - b) + " です");
+        System.out.println("a * b は" + (a * b) + " です");
+        System.out.println("a / b は" + (a / b) + " です");
+        System.out.println("a % b は" + (a % b) + " です");
+    }
+}
