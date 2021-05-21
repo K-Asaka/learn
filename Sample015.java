60a857fc850cf128f8f9ba762b8714769086ac94
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-21 23:33:54 +0900

    変数を使う

---
+++ Sample015.java
@@ -0,0 +1,20 @@
+public class Sample015 {
+    public static void main(String[] args) {
+        // 変数を宣言する
+        int hensu1;
+        int hensu2;
+
+        // 変数の宣言と初期化を同時に行う
+        int hensu3 = 610;
+
+        // 変数に値を代入して初期化する
+        hensu1 = 715;
+
+        // 変数の値を変数に代入して初期化する
+        hensu2 = hensu1;
+
+        System.out.println("hensu1の値は" + hensu1 + "です");
+        System.out.println("hensu2の値は" + hensu2 + "です");
+        System.out.println("hensu3の値は" + hensu3 + "です");        
+    }
+}
