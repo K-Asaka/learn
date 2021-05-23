e525ee1d80213cb97d0842e14b537d6244859808
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-23 22:45:31 +0900

    関係演算子を使う

---
+++ Sample019.java
@@ -0,0 +1,15 @@
+public class Sample019 {
+    public static void main(String[] args) {
+        // 変数を宣言し初期値を設定する
+        int a = 15;
+        int b = 7;
+
+        // 演算を行う
+        System.out.println("a >  b は " + (a > b) + " です");
+        System.out.println("a >= b は " + (a >= b) + " です");
+        System.out.println("a <  b は " + (a < b) + " です");
+        System.out.println("a <= b は " + (a <= b) + " です");
+        System.out.println("a == b は " + (a == b) + " です");
+        System.out.println("a != b は " + (a != b) + " です");
+    }
+}
