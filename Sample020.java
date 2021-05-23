60c6401c56958f31c3fbb3ecebed10711a91caed
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-23 23:07:12 +0900

    エスケープシーケンスを使う

---
+++ Sample020.java
@@ -0,0 +1,8 @@
+public class Sample020 {
+    public static void main(String[] args) {
+        // エスケープシーケンスを画面に表示する
+        System.out.println("\'アポストロフィを表示\'");
+        System.out.println("\"ダブルクォーテーションを表示\"");
+        System.out.println("円記号を表示 \\1000");
+    }
+}
