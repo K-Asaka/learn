8244cdcd599d36a3d10574a3014273b77e5d03de
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-23 22:14:35 +0900

    代入演算子を使う

---
+++ Sample018.java
@@ -0,0 +1,19 @@
+public class Sample018 {
+    public static void main(String[] args) {
+        // 変数を宣言し初期値を設定する
+        // この「=」が基本的な代入演算子
+        int a = 20;
+        int b = 20;
+        int c = 20;
+        int d = 20;
+        int e = 20;
+        int f = 4;
+
+        // 演算を行う
+        System.out.println("a += f は" + (a += f) + " です");
+        System.out.println("a -= f は" + (a -= f) + " です");
+        System.out.println("a *= f は" + (a *= f) + " です");
+        System.out.println("a /= f は" + (a /= f) + " です");
+        System.out.println("a %= f は" + (a %= f) + " です");
+    }
+}
