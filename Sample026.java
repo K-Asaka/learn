80c2026d5f7412e6100ad774594aec9f4a55885f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-24 22:54:37 +0900

    whileループ文を使う

---
+++ Sample026.java
@@ -0,0 +1,10 @@
+public class Sample026 {
+    public static void main(String[] args) {
+        int i = 1000;
+
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
