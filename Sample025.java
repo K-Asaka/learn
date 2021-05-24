4860ec96f41c75f00944246ba0c2b6c0622a1d8d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-24 22:53:37 +0900

    doループ文を使う

---
+++ Sample025.java
@@ -0,0 +1,10 @@
+public class Sample025 {
+    public static void main(String[] args) {
+        int i = 1000;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 100); // ここで条件判断が行われる
+    }
+}
