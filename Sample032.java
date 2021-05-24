4409674d4a4bd96358de0c237e83cddfa43a73c3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-24 23:16:16 +0900

    乱数を使う

---
+++ Sample032.java
@@ -0,0 +1,10 @@
+public class Sample032 {
+    public static void main(String[] args) {
+        double a;
+
+        for (int i = 1; i <= 10; i++) {
+            a = Math.random() * 10;
+            System.out.println((int)a);
+        }
+    }
+}
