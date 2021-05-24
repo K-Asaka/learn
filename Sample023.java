c32288c03858a4a889c112b32ab6cef92ffd0519
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-24 22:49:48 +0900

    switch文を使う

---
+++ Sample023.java
@@ -0,0 +1,17 @@
+public class Sample023 {
+    public static void main(String[] args) {
+        int a = 20;
+
+        switch (a) {
+            case 10:
+                System.out.println("aは10です");
+                break;
+            case 20:
+                System.out.println("aは20です");
+                break;
+            default:
+                System.out.println("aは10でも20でもありません");
+                break;
+        }
+    }
+}
