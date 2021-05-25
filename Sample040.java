aca012c1fde1ed400645d984cc37d9e6ec7f1697
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-25 23:40:37 +0900

    String型文字列を使う

---
+++ Sample040.java
@@ -0,0 +1,10 @@
+public class Sample040 {
+    public static void main(String[] args) {
+        String s1 = "Hello Java String1!";
+        System.out.println(s1);
+
+        String s2;
+        s2 = "Hello Java String2!";
+        System.out.println(s2);
+    }
+}
