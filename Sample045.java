6018c1d0b5a52b6ecffdc9ae67028e536f64c446
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 22:34:14 +0900

    String型文字列を置換する

---
+++ Sample045.java
@@ -0,0 +1,7 @@
+public class Sample045 {
+    public static void main(String[] args) {
+        String s = "Hello Java String!";
+        s = s.replace('a', 'A');
+        System.out.println(s);
+    }
+}
