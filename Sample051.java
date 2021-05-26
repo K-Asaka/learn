4a8c373c356ccd7a0cf7065064a4e2fd6219a1e1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 23:13:11 +0900

    StringBuffer型文字列を使う

---
+++ Sample051.java
@@ -0,0 +1,7 @@
+public class Sample051 {
+    public static void main(String[] args) {
+        StringBuffer sb = 
+            new StringBuffer("Hello Java StringBuffer!");
+        System.out.println(sb);        
+    }
+}
