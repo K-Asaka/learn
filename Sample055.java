c5cd0290e0e600d72b00f35d58edabc1730f522e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 23:34:05 +0900

    StringBuffer型文字列へ追加を行う

---
+++ Sample055.java
@@ -0,0 +1,7 @@
+public class Sample055 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello Java ");
+        sb.append("StringBuffer!");
+        System.out.println(sb);
+    }
+}
