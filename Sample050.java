b8ebf65569b2c808669435f575e23dbc64c13e8d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 23:10:47 +0900

    String型文字列をコピーする

---
+++ Sample050.java
@@ -0,0 +1,10 @@
+public class Sample050 {
+    public static void main(String[] args) {
+        String s1 = "Hello Java String!";
+        String s2 = s1.toString();
+        System.out.println(s2);
+
+        String s3 = s1;
+        System.out.println(s3);
+    }
+}
