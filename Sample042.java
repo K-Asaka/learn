c147570a851ce26ee593ce9972e71126f272fe79
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 22:28:22 +0900

    String型文字列の長さを取得する

---
+++ Sample042.java
@@ -0,0 +1,8 @@
+public class Sample042 {
+    public static void main(String[] args) {
+        String s = "Hello Jaav String!";
+        int len = s.length();
+        System.out.print(s + "の長さは");
+        System.out.println(len + "文字です");
+    }
+}
