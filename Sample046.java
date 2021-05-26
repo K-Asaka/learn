058db7650fa6c48fd3abdcec50052776ff1fa099
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-26 22:35:22 +0900

    String型文字列を小文字、大文字に変換する

---
+++ Sample046.java
@@ -0,0 +1,7 @@
+public class Sample046 {
+    public static void main(String[] args) {
+        String s = "Hello Java String!";
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+    }
+}
