0ee71cfa4718cc65128307750d3152d3a91feac7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 11:57:14 +0900

    コンストラクタを理解する

--- Sample089.java
+++ Sample089.java
@@ -1,21 +1,21 @@
-class airplane {
-    private double fuel;
-    private String iro;
-    public void dataset(double num, String str) {
-        fuel = num;
-        iro = str;
-    }
-    public void dataset(double num) {
-        fuel = num;
-    }
-    public void dataset(String str) {
-        iro = str;
-    }
-    public void display() {
-        System.out.print("飛行機の燃料は" + fuel + "です");
-        System.out.println("色は" + iro + "です");
-    }
-}
+// class airplane {
+//     private double fuel;
+//     private String iro;
+//     public void dataset(double num, String str) {
+//         fuel = num;
+//         iro = str;
+//     }
+//     public void dataset(double num) {
+//         fuel = num;
+//     }
+//     public void dataset(String str) {
+//         iro = str;
+//     }
+//     public void display() {
+//         System.out.print("飛行機の燃料は" + fuel + "です");
+//         System.out.println("色は" + iro + "です");
+//     }
+// }
 
 public class Sample089 {
     public static void main(String[] args) {
---
+++ Sample090.java
@@ -0,0 +1,25 @@
+class airplane {
+    private double fuel;
+    private String iro;
+
+    // コンストラクタを宣言する
+    public airplane() {
+        fuel = 0;
+        iro = "未指定";
+    }
+
+    public void display() {
+        System.out.print("飛行機の燃料は" + fuel + "です");
+        System.out.println("色は" + iro + "です");
+    }
+}
+
+public class Sample090 {
+    public static void main(String[] args) {
+        // コンストラクタが呼び出される
+        airplane plane = new airplane();
+        
+        plane.display();
+        
+    }
+}
