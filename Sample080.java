c75f22977209cd1b208f598d022c9ca093f1f00f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 11:04:35 +0900

    フィールドにアクセスする

---
+++ Sample080.java
@@ -0,0 +1,14 @@
+class airplane {
+    double fuel;
+    String iro;
+}
+
+public class Sample080 {
+    public static void main(String[] args) {
+        airplane plane = new airplane();
+        plane.fuel = 100;
+        plane.iro = "赤";
+        System.out.println("飛行機の燃料は" + plane.fuel + "です");        
+        System.out.println("飛行機の色は" + plane.iro + "です");
+    }
+}
