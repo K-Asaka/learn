ddeda1f2519c9c00bb7781b61dc1917a156a6d59
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 12:00:10 +0900

    コンストラクタをオーバーロードする

--- Sample090.java
+++ Sample090.java
@@ -1,18 +1,18 @@
-class airplane {
-    private double fuel;
-    private String iro;
+// class airplane {
+//     private double fuel;
+//     private String iro;
 
-    // コンストラクタを宣言する
-    public airplane() {
-        fuel = 0;
-        iro = "未指定";
-    }
+//     // コンストラクタを宣言する
+//     public airplane() {
+//         fuel = 0;
+//         iro = "未指定";
+//     }
 
-    public void display() {
-        System.out.print("飛行機の燃料は" + fuel + "です");
-        System.out.println("色は" + iro + "です");
-    }
-}
+//     public void display() {
+//         System.out.print("飛行機の燃料は" + fuel + "です");
+//         System.out.println("色は" + iro + "です");
+//     }
+// }
 
 public class Sample090 {
     public static void main(String[] args) {
---
+++ Sample091.java
@@ -0,0 +1,33 @@
+class airplane {
+    private double fuel;
+    private String iro;
+
+    // 引数のないコンストラクタを宣言
+    public airplane() {
+        fuel = 0;
+        iro = "未指定";
+    }
+
+    // 引数のあるコンストラクタを宣言
+    public airplane(double num, String str) {
+        fuel = num;
+        iro = str;
+    }
+    
+    public void display() {
+        System.out.print("飛行機の燃料は" + fuel + "です");
+        System.out.println("色は" + iro + "です");
+    }
+}
+
+public class Sample091 {
+    public static void main(String[] args) {
+        // 引数のないコンストラクタが呼び出される
+        airplane plane1 = new airplane();
+        plane1.display();
+
+        // 引数のあるコンストラクタが呼び出される
+        airplane plane2 = new airplane(100, "赤");
+        plane2.display();
+    }
+}
