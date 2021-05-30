23f61c564cd5f963004898ddbbdfdb1305dd66d1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 10:48:57 +0900

    クラスを宣言する

---
+++ Sample076.java
@@ -0,0 +1,11 @@
+class display {
+    public display() {
+        System.out.println("HelloWorld");
+    }
+}
+
+public class Sample076 {
+    public static void main(String[] args) {
+        display disp = new display();
+    }
+}
