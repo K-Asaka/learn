8fdec000e5461a2c91d04dc8a09de636ff0f8528
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 11:49:25 +0900

    クラス外からアクセスできるメンバを作成する

--- Sample088.java
+++ Sample088.java
@@ -1,19 +1,19 @@
-// class airplane {
-//     private double fuel;
-//     private String iro;
-//     void dataset(double num, String str) {
-//         fuel = num;
-//         iro = str;
-//     }
-//     double getfuel() {
-//         return fuel;
-//     }
-//     String getiro() {
-//         return iro;
-//     }
-// }
+class airplane {
+    private double fuel;
+    private String iro;
+    public void dataset(double num, String str) {
+        fuel = num;
+        iro = str;
+    }
+    public double getfuel() {
+        return fuel;
+    }
+    public String getiro() {
+        return iro;
+    }
+}
 
-public class Sample087_2 {
+class Sample088 {
     public static void main(String[] args) {
         airplane plane = new airplane();
         plane.dataset(100, "赤");
