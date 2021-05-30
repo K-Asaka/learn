05620b21eacfef1513ea63c4f48d909bbb85bc4f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-30 10:22:56 +0900

    例外を理解する

---
+++ Sample068.java
@@ -0,0 +1,12 @@
+public class Sample068 {
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        System.out.print("配列aの5番目の要素に");
+        System.out.println("値を代入します");
+
+        a[4] = 10;
+
+        System.out.print("配列aの5番目の要素は");
+        System.out.println(a[4] + "です");
+    }
+}
