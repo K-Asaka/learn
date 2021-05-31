d791cd4cdfa02fc52d1ae5eb8ff2bb606ba20d13
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-31 23:51:52 +0900

    メソッドからオブジェクトを返す

---
+++ Sample098.java
@@ -0,0 +1,22 @@
+class ObjectClass {
+    public DataClass getObject() {
+        return new DataClass();
+    }
+}
+
+class DataClass {
+    public int num1 = 1;
+    public int num2 = 2;
+    public int num3 = 3;
+}
+
+class Sample098 {
+    public static void main(String[] args) {
+        ObjectClass obj = new ObjectClass();
+        // メソッドからオブジェクトが返されている
+        DataClass data = obj.getObject();
+        System.out.print("計算結果は");
+        System.out.print((data.num1 + data.num2 + data.num3));
+        System.out.println("です");
+    }
+}
