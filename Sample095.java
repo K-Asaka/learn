5578434c26de44cb511048bc3df087e3058a3297
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-05-31 23:40:46 +0900

    クラス変数を使う

---
+++ Sample095.java
@@ -0,0 +1,14 @@
+class hensu {
+    // クラス変数を宣言
+    public static int num = 0;
+}
+
+public class Sample095 {
+    public static void main(String[] args) {
+        hensu hen1 = new hensu();
+        hensu hen2 = new hensu();
+        hen1.num = hen1.num + 1;
+        hen2.num = hen2.num + 2;
+        System.out.println("クラス変数の値は" + hen1.num + "です");
+    }
+}
