0bb68a379cc7d030beaa1b94ec6512231fd0e06f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-06-01 00:57:53 +0900

    継承を使ってクラスを作成する

--- Sample094.java
+++ Sample094.java
@@ -1,25 +1,25 @@
-class airplane {
-    private double fuel;
-    private String iro;
+// class airplane {
+//     private double fuel;
+//     private String iro;
 
-    // private修飾子を指定したコンストラクタ
-    private airplane() {
-        fuel = 0;
-        iro = "未指定";
-    }
+//     // private修飾子を指定したコンストラクタ
+//     private airplane() {
+//         fuel = 0;
+//         iro = "未指定";
+//     }
 
-    // public修飾子をを指定したコンストラクタ
-    public airplane(double num) {
-        // private修飾子を指定したコンストラクタを呼び出す
-        this();
-        fuel = num;
-    }
+//     // public修飾子をを指定したコンストラクタ
+//     public airplane(double num) {
+//         // private修飾子を指定したコンストラクタを呼び出す
+//         this();
+//         fuel = num;
+//     }
 
-    public void display() {
-        System.out.print("飛行機の燃料は" + fuel + "です");
-        System.out.println("色は" + iro + "です");
-    }
-}
+//     public void display() {
+//         System.out.print("飛行機の燃料は" + fuel + "です");
+//         System.out.println("色は" + iro + "です");
+//     }
+// }
 
 public class Sample094 {
     public static void main(String[] args) {
