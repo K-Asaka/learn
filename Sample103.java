ba6ae2e76ff92cd237b84cb64b33ef7ee9a63ea0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-06-03 22:32:39 +0900

    スーパークラスのフィールドにアクセスする

--- Sample103.java
+++ Sample103.java
@@ -1,31 +1,31 @@
-// 飛行機クラス(スーパークラス)
-class airplane {
-    private double fuel;
-    private String iro;
+// // 飛行機クラス(スーパークラス)
+// class airplane {
+//     private double fuel;
+//     private String iro;
 
-    public void setdata(double num, String str) {
-        fuel = num;
-        iro = str;
-    }
+//     public void setdata(double num, String str) {
+//         fuel = num;
+//         iro = str;
+//     }
 
-    public void display() {
-        System.out.print("飛行機の燃料は" + fuel + "です");
-        System.out.println("色は" + iro + "です");
-    }
-}
+//     public void display() {
+//         System.out.print("飛行機の燃料は" + fuel + "です");
+//         System.out.println("色は" + iro + "です");
+//     }
+// }
 
-// ジャンボジェット機クラス(サブクラス)
-class jumbojet extends airplane {
-    private int runway;
+// // ジャンボジェット機クラス(サブクラス)
+// class jumbojet extends airplane {
+//     private int runway;
 
-    public void setrunway(int way) {
-        runway = way;
-    }
+//     public void setrunway(int way) {
+//         runway = way;
+//     }
 
-    public void displayrunway() {
-        System.out.println("滑走路は" + runway + "番です");
-    }
-}
+//     public void displayrunway() {
+//         System.out.println("滑走路は" + runway + "番です");
+//     }
+// }
 public class Sample103 {
     public static void main(String[] args) {
         jumbojet jumbo = new jumbojet();
---
+++ Sample104.java
@@ -0,0 +1,34 @@
+// 飛行機クラス(スーパークラス)
+class airplane {
+    protected double fuel;
+    protected String iro;
+
+    public void setdata(double num, String str) {
+        fuel = num;
+        iro = str;
+    }
+}
+
+// ジャンボジェット機クラス(サブクラス)
+class jumbojet extends airplane {
+    private int runway;
+
+    public void setrunway(int way) {
+        runway = way;
+    }
+
+    public void display() {
+        System.out.print("飛行機の燃料は" + fuel + "です ");
+        System.out.print("色は" + iro + "です ");
+        System.out.println("滑走路は" + runway + "番です");
+    }
+}
+
+public class Sample104 {
+    public static void main(String[] args) {
+        jumbojet jumbo = new jumbojet();
+        jumbo.setdata(100, "赤");
+        jumbo.setrunway(1);
+        jumbo.display();
+    }
+}
