da37ed21eedf3510f709e5eddab25a297ee88fc4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-06-05 10:42:26 +0900

    全メモリ容量を取得する

---
+++ Sample115.java
@@ -0,0 +1,6 @@
+public class Sample115 {
+    public static void main(String[] args) {
+        System.out.print("全メモリ容量:");
+        System.out.println(Runtime.getRuntime().totalMemory());
+    }
+}
