8e7dce7905e892b4b5ea823455de17a560d15910
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-06-06 22:31:00 +0900

    文字列の表示位置を変更する

---
+++ Sample125.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+public class Sample125 extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("表示位置1", 10, 40);
+        g.drawString("表示位置2", 120, 60);
+        g.drawString("表示位置3", 210, 80);
+    }
+}
