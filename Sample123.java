8c194627a8f3bdb3de9865a14b25c2391067214a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-06-06 22:08:21 +0900

    アプレットを実行する

---
+++ Sample123.java
@@ -0,0 +1,10 @@
+/* <applet code="Sample123.class"
+    width="250" height="100"></applet> */
+    
+import java.applet.*;
+import java.awt.*;
+public class Sample123 extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello Java AWT!", 10, 50);
+    }
+}
