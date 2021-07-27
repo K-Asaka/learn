2e159fef26ad3c13138c267c80cee42371d9fc22
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-27 19:38:33 +0900

    エスケープシーケンスを用いた例

--- Chapter02/src/chap02/Code02_02.java
+++ Chapter02/src/chap02/Code02_02.java
@@ -2,6 +2,6 @@ package chap02;
 
 public class Code02_02 {
 	public static void main(String[] args) {
-		System.out.println("私の好きな記号は二重引用符(")です");
+		//System.out.println("私の好きな記号は二重引用符(")です");
 	}
 }
