c2be1b15e354cb2b0fcb07f661a3a968c087e088
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-27 19:44:25 +0900

    命令実行の文(画面に表示する)

---
+++ Chapter02/src/chap02/Code02_10.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class Code02_10 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		String message;
+		message = name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
