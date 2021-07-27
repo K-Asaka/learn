243087d9ade4edf47ce20c414c36ca03d15926c0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-27 19:34:21 +0900

    練習1-3

---
+++ Chapter01/src/chap01/Rensyu01_03.java
@@ -0,0 +1,12 @@
+package chap01;
+
+public class Rensyu01_03 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char favoriteCharacter= '駆';
+		double pi = 3.14;
+		// float pi = 3.14f;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+}
