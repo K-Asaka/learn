f567214a627d7750c2f39872d69f10aa33bb3ceb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-27 19:45:23 +0900

    改行なし画面出力の命令

---
+++ Chapter02/src/chap02/Code02_11.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class Code02_11 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
+	}
+}
