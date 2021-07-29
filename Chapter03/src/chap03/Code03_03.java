965fb214eb1b2adcd807acf51abef6eb1aafe323
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 08:29:05 +0900

    波カッコを省略した記述

---
+++ Chapter03/src/chap03/Code03_03.java
@@ -0,0 +1,12 @@
+package chap03;
+
+public class Code03_03 {
+	public static void main(String[] args) {
+		boolean tenki = true;	// ここでtrueかfalseを代入
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else
+			System.out.println("DVDを見ます");
+	}
+}
