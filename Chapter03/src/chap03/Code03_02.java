6992b63f1dafecd8abf964d0965c46e749455bb3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 08:27:32 +0900

    トイレの空きを待つ繰り返し処理

---
+++ Chapter03/src/chap03/Code03_02.java
@@ -0,0 +1,11 @@
+package chap03;
+
+public class Code03_02 {
+	public static void main(String[] args) {
+		boolean doorClose = true;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+}
