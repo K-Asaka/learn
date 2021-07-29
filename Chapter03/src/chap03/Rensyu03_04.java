98887a7697e2e341688bc01b98e4bc79e79acf6c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 08:51:30 +0900

    練習3-4

---
+++ Chapter03/src/chap03/Rensyu03_04.java
@@ -0,0 +1,14 @@
+package chap03;
+
+public class Rensyu03_04 {
+	public static void main(String[] args) {
+		boolean tenki = false;	// ここでtrueかfalseを代入
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
