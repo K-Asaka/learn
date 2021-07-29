3508a2af59371ac7dbe2d24e8cbca8e51b4a2339
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-29 11:32:26 +0900

    String型をint型に変換する命令

---
+++ Chapter02/src/chap02/Code02_13.java
@@ -0,0 +1,9 @@
+package chap02;
+
+public class Code02_13 {
+	public static void main(String[] args) {
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+	}
+}
