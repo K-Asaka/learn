240ce417c9eef998e53ea047eb6e9335999aa140
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:43:29 +0900

    引数を使わずに値を渡せないのか？(エラー)

---
+++ Chapter05/src/chap05/Code05_06.java
@@ -0,0 +1,14 @@
+package chap05;
+
+public class Code05_06 {
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 10;
+		add();
+	}
+	
+	public static void add() {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+}
