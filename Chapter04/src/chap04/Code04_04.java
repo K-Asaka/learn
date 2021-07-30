7fe3cf3b5866064c93d1004fa1aa4df797776991
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:03:46 +0900

    配列の長さを調べる

---
+++ Chapter04/src/chap04/Code04_04.java
@@ -0,0 +1,9 @@
+package chap04;
+
+public class Code04_04 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数: " + num);
+	}
+}
