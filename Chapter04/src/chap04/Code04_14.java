e0ba8776abebbd293075f01534f993764b5388ca
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:17:37 +0900

    拡張for文

---
+++ Chapter04/src/chap04/Code04_14.java
@@ -0,0 +1,10 @@
+package chap04;
+
+public class Code04_14 {
+	public static void main(String[] args) {
+		int[] scores = { 20, 30, 40, 50, 80 };
+		for (int value : scores) {
+			System.out.println(value);
+		}
+	}
+}
