071d18516410afaefe48404618ebd0be430bb2f8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:18:47 +0900

    実行結果は？

---
+++ Chapter04/src/chap04/Code04_15.java
@@ -0,0 +1,11 @@
+package chap04;
+
+public class Code04_15 {
+	public static void main(String[] args) {
+		int[] arrayA = { 1, 2, 3 };
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+	}
+}
