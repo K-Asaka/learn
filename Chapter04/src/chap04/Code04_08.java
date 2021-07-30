135401c2c1260958d2cc8f367a77e0b18af0b541
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:08:17 +0900

    配列を用いた点数管理プログラム(修正)

--- Chapter04/src/chap04/Code04_08.java
+++ Chapter04/src/chap04/Code04_08.java
@@ -3,8 +3,8 @@ package chap04;
 public class Code04_08 {
 	public static void main(String[] args) {
 		int[] scores = { 20, 30, 40, 50, 80 };
-		int sum = scores[1] + scores[2]
-				+ scores[3] + scores[4] + scores[5];
+		int sum = scores[0] + scores[1] + scores[2]
+				+ scores[3] + scores[4];
 		int avg = sum / scores.length;
 		System.out.println("合計点:" + sum);
 		System.out.println("平均点:" + avg);
