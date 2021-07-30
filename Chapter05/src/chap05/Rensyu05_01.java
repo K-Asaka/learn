3c8c64e6b7a34da0f99749fee7092bee483c08f9
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 19:19:59 +0900

    練習5-1

---
+++ Chapter05/src/chap05/Rensyu05_01.java
@@ -0,0 +1,19 @@
+package chap05;
+
+public class Rensyu05_01 {
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+
+	public static void introduceOneself() {
+		String name = "Minato";
+		int age = 22;
+		double height = 169.9;
+		char eto = '辰';
+		
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("歳は" + age + "です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("十二支ｊは" + eto + "です");
+	}
+}
