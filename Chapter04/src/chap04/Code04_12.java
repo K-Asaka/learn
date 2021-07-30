0198db119f872fe20a84c9dd0b84e67bd88d7cfd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:16:17 +0900

    DNAの記号をランダムに表示する(配列版)

--- Chapter04/src/chap04/Code04_12.java
+++ Chapter04/src/chap04/Code04_12.java
@@ -3,6 +3,7 @@ package chap04;
 public class Code04_12 {
 	public static void main(String[] args) {
 		int[] seq = new int[10];
+		char[] base = { 'A', 'T', 'G', 'C' };
 		
 		// 塩基配列をランダムに生成
 		for (int i = 0; i < seq.length; i++) {
@@ -11,20 +12,7 @@ public class Code04_12 {
 		
 		// 生成した塩基配列の記号を表示
 		for (int i = 0; i < seq.length; i++) {
-			switch (seq[i]) {
-			case 0:
-				System.out.print("A ");
-				break;
-			case 1:
-				System.out.print("T ");
-				break;
-			case 2:
-				System.out.print("G ");
-				break;
-			case 3:
-				System.out.print("C ");
-				break;
-			}
+			System.out.print(base[seq[i]] + " ");
 		}
 	}
 }
