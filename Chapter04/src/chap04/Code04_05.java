fe1b727fee4937981e2b4ff7180d1ac7d393e87a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-30 09:04:26 +0900

    配列の要素に値を代入

---
+++ Chapter04/src/chap04/Code04_05.java
@@ -0,0 +1,10 @@
+package chap04;
+
+public class Code04_05 {
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[5];
+		scores[1] = 30;
+		System.out.println(scores[1]);
+	}
+}
