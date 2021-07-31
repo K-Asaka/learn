35df3b65cce1e439476e819923583936dbeea6e2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-31 14:32:45 +0900

    練習6-1

---
+++ Chapter06/src/chap06/Rensyu06_01.java
@@ -0,0 +1,12 @@
+package chap06;
+
+import chap06.comment.Zenhan;
+
+public class Rensyu06_01 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		chap06.comment.Kouhan.callDeae();
+		chap06.comment.Kouhan.showMondokoro();
+	}
+}
