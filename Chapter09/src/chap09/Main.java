83241ffbaadfa91882fd484b94153624fa1784c5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-31 16:08:37 +0900

    newで引数を渡す

--- Chapter09/src/chap09/Main.java
+++ Chapter09/src/chap09/Main.java
@@ -2,7 +2,8 @@ package chap09;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h = new Hero();
+		Hero h = new Hero("ミナト");
 		System.out.println(h.hp);
+		System.out.println(h.name);
 	}
 }
