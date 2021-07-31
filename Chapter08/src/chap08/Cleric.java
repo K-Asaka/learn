facfa1837e6539ba87f0dd411e400e9683c74b47
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-07-31 15:29:39 +0900

    練習8-4

--- Chapter08/src/chap08/Cleric.java
+++ Chapter08/src/chap08/Cleric.java
@@ -1,5 +1,7 @@
 package chap08;
 
+import java.util.*;
+
 public class Cleric {
 	String name;
 	int hp = 50;
@@ -12,5 +14,19 @@ public class Cleric {
 		this.hp = this.MAX_HP;
 		this.mp -= 5;
 		System.out.println("HPが最大まで回復した");
+	}
+	
+	public int pray(int sec) {
+		System.out.println(this.name + "は" + sec + "秒間天に祈った！");
+		
+		// 論理上の回復量を乱数を用いて決定する
+		int recover = new Random().nextInt(3) + sec;
+		
+		// 実際の回復量を計算する
+		int recoverActual = Math.min(this.MAX_HP - this.mp, recover);
+		
+		this.mp += recoverActual;
+		System.out.println("MPが" + recoverActual + "回復した");
+		return recoverActual;
 	}
 }
