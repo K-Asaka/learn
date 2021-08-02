36ce314bf8ffeaa7b9e1f0c051649fe7da34e083
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-02 18:04:56 +0900

    オーバーライドしたつもりのHeroクラス

--- Chapter11/src/chap11/Hero.java
+++ Chapter11/src/chap11/Hero.java
@@ -1,4 +1,9 @@
 package chap11;
 
 public class Hero extends Character {
+	public void atack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		System.out.println("敵に10ポイントのダメージをあたえた！");
+		m.hp -= 10;
+	}
 }
