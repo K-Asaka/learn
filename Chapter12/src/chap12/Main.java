a6879fd52a25b178c01e7e5c139f2e565416ec32
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 14:35:01 +0900

    Wizardにfireballを使わせる(エラー)

--- Chapter12/src/chap12/Main.java
+++ Chapter12/src/chap12/Main.java
@@ -3,9 +3,10 @@ package chap12;
 public class Main {
 	public static void main(String[] args) {
 		Wizard w = new Wizard();
+		Character c = w;
 		Matango m = new Matango();
-		w.name = "アサカ";
-		w.attack(m);
-		w.fireball(m);
+		c.name = "アサカ";
+		c.attack(m);
+		c.fireball(m);
 	}
 }
