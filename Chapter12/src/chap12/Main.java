f6cf986258a65f72b0828d76af5946946b0f87f3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 14:34:04 +0900

    Wizardクラスを通常の方法でインスタンス化

--- Chapter12/src/chap12/Main.java
+++ Chapter12/src/chap12/Main.java
@@ -2,6 +2,10 @@ package chap12;
 
 public class Main {
 	public static void main(String[] args) {
-		Life lf = new Wizard();
+		Wizard w = new Wizard();
+		Matango m = new Matango();
+		w.name = "アサカ";
+		w.attack(m);
+		w.fireball(m);
 	}
 }
