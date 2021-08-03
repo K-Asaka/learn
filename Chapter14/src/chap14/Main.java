ef49d26b8c6925430a9811975eeed68ab279b0ca
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 17:12:57 +0900

    newせずとも静的フィールドは利用可能

--- Chapter14/src/chap14/Main.java
+++ Chapter14/src/chap14/Main.java
@@ -2,12 +2,8 @@ package chap14;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h1 = new Hero();
-		Hero h2 = new Hero();
+		// 1人も勇者を生み出していない状況で…
 		Hero.money = 100;
 		System.out.println(Hero.money);
-		System.out.println(h1.money);
-		h1.money = 300;
-		System.out.println(h2.money);
 	}
 }
