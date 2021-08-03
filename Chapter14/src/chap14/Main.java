f28b50e959bff4667cab9b0542d93e869cd8b776
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 17:12:08 +0900

    各インスタンスから静的フィールドを利用可能

--- Chapter14/src/chap14/Main.java
+++ Chapter14/src/chap14/Main.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Hero h1 = new Hero();
 		Hero h2 = new Hero();
-		System.out.println(h1.hp);
+		Hero.money = 100;
 		System.out.println(Hero.money);
+		System.out.println(h1.money);
+		h1.money = 300;
+		System.out.println(h2.money);
 	}
 }
