2d0ea56f752d93f368105c7fc3139c18027b90df
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 15:07:04 +0900

    練習12-3

--- Chapter12/src/chap12/Main.java
+++ Chapter12/src/chap12/Main.java
@@ -2,9 +2,11 @@ package chap12;
 
 public class Main {
 	public static void main(String[] args) {
-		X obj = new A();
-		obj.a();
-		Y y1 = new A(); Y y2 = new B();
-		y1.a(); y2.a();
+		Y[] array = new Y[2];
+		array[0] = new A();
+		array[1] = new B();
+		for (Y y : array) {
+			y.b();
+		}
 	}
 }
