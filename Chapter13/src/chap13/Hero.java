9cba7813039638a8963d6386bba87df2c065d187
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 16:20:04 +0900

    HeroクラスにgetNameメソッドを追加

--- Chapter13/src/chap13/Hero.java
+++ Chapter13/src/chap13/Hero.java
@@ -2,8 +2,12 @@ package chap13;
 
 public class Hero {
 	private int hp;
-	String name;
-	Sword sword;
+	private String name;
+	private Sword sword;
+	
+	public String getName() {
+		return this.name;
+	}
 	
 	public void bye() {
 		System.out.println("勇者は別れを告げた");
