842ddd2256459c1a82740b4b15c21c4eea98a4d0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-03 17:59:56 +0900

    replaceAllメソッドを使った文字列の置換

---
+++ Chapter15/src/chap15/Code15_08.java
@@ -0,0 +1,9 @@
+package chap15;
+
+public class Code15_08 {
+	public static void main(String[] args) {
+		String s = "abc,def:ghi";
+		String w = s.replaceAll("[beh]", "X");
+		System.out.println(w);
+	}
+}
