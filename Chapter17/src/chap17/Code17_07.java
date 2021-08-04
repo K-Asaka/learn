e6bc5c24ca4d8efdcbe61cd1e0f6cbde09901191
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 17:24:54 +0900

    tryブロック内でnewすると…(エラー)

---
+++ Chapter17/src/chap17/Code17_07.java
@@ -0,0 +1,17 @@
+package chap17;
+
+import java.io.*;
+
+public class Code17_07 {
+	public static void main(String[] args) {
+		FileWriter fw;
+		try {
+			fw = new FileWriter("A:\\data.txt");
+			fw.write("hello!");
+		} catch (IOException e) {
+			System.out.println("エラーです");
+		} finally {
+			fw.close();
+		}
+	}
+}
