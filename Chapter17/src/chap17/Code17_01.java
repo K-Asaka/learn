48dfa4909381bba8b4902e098a3bd114635ca1b2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 17:11:12 +0900

    try-catch文でException系例外の発生に備える

--- Chapter17/src/chap17/Code17_01.java
+++ Chapter17/src/chap17/Code17_01.java
@@ -4,7 +4,10 @@ import java.io.*;
 
 public class Code17_01 {
 	public static void main(String[] args) {
-		FileWriter fw = new FileWriter("data.txt");
-		
+		try {
+			FileWriter fw = new FileWriter("A:\\data.txt");
+		} catch (IOException e) {
+			System.out.println("エラーが発生しました。");
+		}
 	}
 }
