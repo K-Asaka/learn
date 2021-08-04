1f45966a6c003caa999a19b500021d3e6186349e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 18:34:09 +0900

    練習17-4

---
+++ Chapter17/src/chap17/Rensyu17_04.java
@@ -0,0 +1,10 @@
+package chap17;
+
+import java.io.IOException;
+
+public class Rensyu17_04 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("プログラムが起動しました");
+		throw new IOException();
+	}
+}
