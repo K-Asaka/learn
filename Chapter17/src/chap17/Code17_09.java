d410b7c7c12ce2b05222a0c1cc15b6f98bd6ab9f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 17:28:34 +0900

    ファイルを開いたときだけ後片付けする

---
+++ Chapter17/src/chap17/Code17_09.java
@@ -0,0 +1,23 @@
+package chap17;
+
+import java.io.*;
+
+public class Code17_09 {
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("A:\\data.txt");
+			fw.write("hello!");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					;
+				}
+			}
+		}
+	}
+}
