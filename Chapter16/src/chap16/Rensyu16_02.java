8cfd808a6b78740b7354c480e46330f280ba56d5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 16:53:20 +0900

    練習16-3

--- Chapter16/src/chap16/Rensyu16_02.java
+++ Chapter16/src/chap16/Rensyu16_02.java
@@ -1,18 +1,19 @@
 package chap16;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Rensyu16_02 {
 	public static void main(String[] args) {
 		Hero2 h1 = new Hero2("斎藤");
 		Hero2 h2 = new Hero2("鈴木");
-		List<Hero2> heroes = new ArrayList<Hero2>();
-		heroes.add(h1);
-		heroes.add(h2);
+		Map<Hero2, Integer> heroes = new HashMap<Hero2, Integer>();
+		heroes.put(h1, 3);
+		heroes.put(h2, 7);
 		
-		for (Hero2 h : heroes) {
-			System.out.println(h.getName());
+		for (Hero2 key : heroes.keySet()) {
+			int value = heroes.get(key);
+			System.out.println(key.getName() + "が倒した敵=" +value);
 		}
 	}
 }
