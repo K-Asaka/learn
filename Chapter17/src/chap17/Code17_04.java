a057d39b60af75eed4b05da0edd4223cad2f7bbf
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 17:15:33 +0900

    try-catchの後でcloseすると…(エラー)

---
+++ Chapter17/src/chap17/Code17_04.java
@@ -0,0 +1,17 @@
+package chap17;
+
+import java.io.*;
+
+public class Code17_04 {
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("A:\\data.txt");
+			fw.write("hello!");
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("エラーです");
+		}
+		fw.close();
+	}
+}
