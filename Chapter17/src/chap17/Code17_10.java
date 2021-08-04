a1b73e89e2f91862996ba233366afe4c20af9a66
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-08-04 17:30:56 +0900

    try-with-resources文の利用

---
+++ Chapter17/src/chap17/Code17_10.java
@@ -0,0 +1,13 @@
+package chap17;
+
+import java.io.*;
+
+public class Code17_10 {
+	public static void main(String[] args) {
+		try (FileWriter fw = new FileWriter("A:\\data.txt");) {
+			fw.write("hello!");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		}
+	}
+}
