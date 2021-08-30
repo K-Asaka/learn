a599bd957717dc954ae96b54d7ed008105353007
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-08-30 15:24:19 +0900

    ToDoアプリケーションへの実装

--- todo/app.js
+++ todo/app.js
@@ -8,11 +8,35 @@ Vue.createApp({
             hideDoneTodo: false,
             searchWord: '',
             order: 'desc',
+            categoryName: '',
         }
     },
     computed: {
         canCreateTodo: function() {
             return this.todoTitle !== ''
+        },
+        canCreateCategory: function() {
+            return this.categoryName !== ''
+        },
+    },
+    methods: {
+        createTodo: function() {
+            if (!this.canCreateTodo) {
+                return
+            }
+
+            // ToDoタスクを追加する処理
+            this.todoTitle = ''
+            this.todoDescription = ''
+            this.todoCategories = []
+        },
+        createCategory: function() {
+            if (!this.canCreateCategory) {
+                return
+            }
+
+            // カテゴリを追加する処理
+            this.categoryName = ''
         },
     },
 }).mount('#app')
