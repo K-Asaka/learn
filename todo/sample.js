c77dc3633b131b4b3e460ee9dbce0d20d7215333
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-08-31 14:34:05 +0900

    propsの渡し方

--- todo/sample.js
+++ todo/sample.js
@@ -5,12 +5,23 @@ const myTitle = {
             validator: function(value) {
                 return value.length > 0
             },
+            required: true,
+        },
+    },
+    computed: {
+        upperCaseName: function() {
+            return this.name.toUpperCase()
         },
     },
     template: '#title-template',
 }
 
 Vue.createApp({
+    data: function() {
+        return {
+            authorName: 'yamada',
+        }
+    },
     components: {
         'my-title': myTitle,
     },
