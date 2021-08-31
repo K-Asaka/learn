1726d80b273a28c4e93e5260b36b87d14aaa23f3
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-08-31 16:29:41 +0900

    transitionの使い方

--- todo/sample.js
+++ todo/sample.js
@@ -1,35 +1,7 @@
-const updateAuthor = {
-    template: '#update-author-template',
-    props: {
-        name: {
-            type: String,
-            required: true,
-        },
-        age: {
-            type: Number,
-            required: true,
-        },
-    },
-    methods: {
-        onClickAge: function() {
-            this.$emit('update:age', this.age + 1)
-        },
-        onInput: function($event) {
-            this.$emit('update:name', $event.target.value)
-        },
-    },
-}
-
 Vue.createApp({
     data: function() {
         return {
-            author: {
-                name: 'Yamada',
-                age: 40,
-            },
+            isShow: false,
         }
-    },
-    components: {
-        'update-author': updateAuthor,
     },
 }).mount('#app')
