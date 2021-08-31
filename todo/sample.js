920e26887f94dbb608e75141c14dbe2fe28527e4
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-08-31 18:26:15 +0900

    transition-group

--- todo/sample.js
+++ todo/sample.js
@@ -1,35 +1,36 @@
 Vue.createApp({
     data: function() {
         return {
-            isShow: false,
+            filterDone: false,
+            items: [
+                {
+                    title: 'タイトル - 1',
+                    done: false,
+                },
+                {
+                    title: 'タイトル - 2',
+                    done: true,
+                },
+                {
+                    title: 'タイトル - 3',
+                    done: true,
+                },
+                {
+                    title: 'タイトル - 4',
+                    done: false,
+                },
+            ],
         }
     },
-    methods: {
-        beforeEnter: function(el) {
-            //　要素が出現・表示される前の状態をここで定義します
-        },
-        enter: function(el, done) {
-            // elに出現・表示されるアニメーションを実行します
-            // アニメーションが完了したらdoneコールバックを呼び出します
-        },
-        afterEnter: function(el) {
-            // 要素が出現・表示された後の状態を定義します
-        },
-        enterCancelled: function(el) {
-            // 要素が出現・表示するアニメーションをキャンセルされたときの状態を定義します
-        },
-        beforeLeave: function(el) {
-            // 要素が消滅・非表示される前の状態をここで定義します
-        },
-        leave: function(el, done) {
-            // elに消滅・非表示されるアニメーションを実行します
-            // アニメーションが完了したらdoneコールバックを呼び出します
-        },
-        afterLeave: function(el) {
-            // 要素が消滅・非表示されてアニメーション終了時に実行されます
-        },
-        leaveCancelled: function(el) {
-            // 要素が消滅・非表示するアニメーションをキャンセルされたときの状態を定義します
+    computed: {
+        filteredItems: function() {
+            if (this.filterDone) {
+                return this.items.filter(function(item) {
+                    return item.done
+                })
+            }
+
+            return this.items
         },
     },
 }).mount('#app')
