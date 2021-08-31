ad38a9661a20c5593c1b613a4c6ad27a10e5e5da
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-08-31 17:06:49 +0900

    v-onの紐付けと各タイミングにおける要素の定義

--- todo/sample.js
+++ todo/sample.js
@@ -4,4 +4,32 @@ Vue.createApp({
             isShow: false,
         }
     },
+    methods: {
+        beforeEnter: function(el) {
+            //　要素が出現・表示される前の状態をここで定義します
+        },
+        enter: function(el, done) {
+            // elに出現・表示されるアニメーションを実行します
+            // アニメーションが完了したらdoneコールバックを呼び出します
+        },
+        afterEnter: function(el) {
+            // 要素が出現・表示された後の状態を定義します
+        },
+        enterCancelled: function(el) {
+            // 要素が出現・表示するアニメーションをキャンセルされたときの状態を定義します
+        },
+        beforeLeave: function(el) {
+            // 要素が消滅・非表示される前の状態をここで定義します
+        },
+        leave: function(el, done) {
+            // elに消滅・非表示されるアニメーションを実行します
+            // アニメーションが完了したらdoneコールバックを呼び出します
+        },
+        afterLeave: function(el) {
+            // 要素が消滅・非表示されてアニメーション終了時に実行されます
+        },
+        leaveCancelled: function(el) {
+            // 要素が消滅・非表示するアニメーションをキャンセルされたときの状態を定義します
+        },
+    },
 }).mount('#app')
