c1ec4d50ebcbbe96f57fa24434dc3ceea8ac4b4b
K-Asaka <asaka@chiba-fjb.ac.jp>
2021-09-01 09:02:09 +0900

    クリックイベントを追加する

--- modal/app.js
+++ modal/app.js
@@ -14,7 +14,40 @@ Vue.createApp({
                     id: 3,
                     src: "https://placehold.jp/b32020/ffffff/300x300.png"
                 }
-            ]
+            ],
+            selectedThumbnailId: undefined,
+            isVisible: false,
+            thumbnailHeight: 0,
+            isThumbnailLoaded: false
+        }
+    },
+    watch: {
+        selectedThumbnailId: function() {
+            this.isThumbnailLoaded = false
+        }
+    },
+    computed: {
+        currentThumbnail: function() {
+            const self = this
+            return _.find(self.thumbnails, function(thumb) {
+                return thumb.id === self.selectedThumbnailId
+            })
+        },
+        containerStyle: function() {
+            return {
+                height: this.thumbnailHeight + "px"
+            }
+        }
+    },
+    methods: {
+        openModal: function(thumb) {
+            this.isVisible = true
+            this.selectedThumbnailId = thumb.id
+        },
+        onLoad: function(event) {
+            this.thumbnailHeight = 
+                event.target.naturalHeight > 300 ? 300 : event.target.naturalHeight
+            this.isThumbnailLoaded = true
         }
     }
 }).mount("#app")
