ca60e421f7bf34cb481fc0c78416f0467d63e037
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-09-15 09:22:25 +0900

    ソート結果を確認する

--- bitonic-sorter/src/utils.rs
+++ bitonic-sorter/src/utils.rs
@@ -12,3 +12,20 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // コレクションに格納する
     rng.sample_iter(&Standard).take(n).collect()
 }
+
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    // windows(2)は元のイテレータから1要素刻みで2要素ずつ値を取り出す
+    // 新しいイテレータを返す。たとえば元が[1, 2, 3, 4]なら
+    // [1, 2]、[2, 3]、[3, 4]を順に返す
+    //
+    // all(..)はいてラータから値(例：[1, 2])を取り出し、クロージャに渡す
+    // クロージャがfalseを返したら、そこで処理を打ち切りfalseを返す
+    // クロージャがtrueを返している間は、イテレータから次の値を取り出し
+    // クロージャへ与え続ける。イテレータの値が尽きるまで(Noneになるまで)
+    // クロージャが一度もfalseを返さなかったら、all(..)はtrueを返す
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
+}
