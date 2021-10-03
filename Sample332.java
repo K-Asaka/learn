4252e32e9fb5f234afc9d78d25d5e21fd7817da1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-10-03 23:36:24 +0900

    絶対位置にカーソルを移動する

---
+++ Sample332.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.sql.*;
+public class Sample332 {
+    public static void main(String[] args) {
+        try {
+            String shohincd;
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            String url = "jdbc:odbc:URIAGE";
+            String user = "";
+            String pass = "";
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stmt = con.createStatement(
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_READ_ONLY
+            );
+            String query = "SELECT * FROM SHOHINM";
+            ResultSet rs = stmt.executeQuery(query);
+            // 検索結果の取り出し
+            while (rs.next()) {
+                shohincd = rs.getString("SHOHINCD");
+                System.out.println(shohincd);
+            }
+            System.out.println("---------");
+
+            // 絶対位置にカーソルを移動する
+            System.out.print("先頭から2番目に移動:");
+            rs.absolute(2);
+            shohincd = rs.getString("SHOHINCD");
+            System.out.println(shohincd);
+            System.out.print("先頭から6番目に移動:");
+            rs.absolute(6);
+            shohincd = rs.getString("SHOHINCD");
+            System.out.println(shohincd);
+            System.out.print("末尾から3番目に移動:");
+            rs.absolute(-3);
+            shohincd = rs.getString("SHOHINCD");
+            System.out.println(shohincd);
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("例外" + e + "が発生しました");
+        }
+    }
+}
