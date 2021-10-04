07ebed9e4ae49b5d3918f9dcd6b1c22ca2362fc8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-10-05 00:03:29 +0900

    列を選択してレコードを検索する

---
+++ Sample338.java
@@ -0,0 +1,33 @@
+import java.net.*;
+import java.sql.*;
+public class Sample338 {
+    public static void main(String[] args) {
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            String url = "jdbc:odbc:URIAGE";
+            String user = "";
+            String pass = "";
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stmt = con.createStatement();
+            // SQL文
+            String query = "SELECT SHOHINCD,SHOHINMEI,TANKA FROM SHOHINM";
+
+            ResultSet rs = stmt.executeQuery(query);
+            System.out.println("SHOHINCD SHOHINMEI  TANKA");
+            System.out.println("-------------------------");
+            while (rs.next()) {
+                String shohincd = rs.getString("SHOHINCD");
+                String shohinmei = rs.getString("SHOHINMEI");
+                int tanka = rs.getInt("TANKA");
+                System.out.print(shohincd + "     ");
+                System.out.print(shohinmei + " ");
+                System.out.println(tanka);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("例外" + e + "が発生しました");
+        }
+    }
+}
