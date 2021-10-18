5014fd47553fdfa5d759edd1243dd21b1bdf988f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-10-18 09:10:13 +0900

    ユニット

--- primitive/src/main.rs
+++ primitive/src/main.rs
@@ -1,3 +1,8 @@
+// 戻り値の型を省略。コンパイラは戻り値がユニット型だと解釈する
+fn hello() {
+    println!("Hello");    
+}
+
 fn main() {
     // 整数リテラル
     let n = 42;
@@ -6,4 +11,12 @@ fn main() {
     // 文字リテラル
     let c = 'R';
     println!("{}", c);
+
+    // 関数を呼び出し、(ないはずの)戻り値に変数retを束縛する
+    let ret = hello();
+    // アサーションでretの値がユニット値と等しいことを検査する
+    assert_eq!(ret, ());
+    // size_of::<型>()は、その型の値がメモリ上で占める大きさをバイト数で返す
+    println!("{}", std::mem::size_of::<()>());
+    assert_eq!(std::mem::size_of::<()>(), 0);   // 0バイト
 }
