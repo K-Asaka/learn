9dc7054039247f8ea6dbdab23b02e0c58eac32ca
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2021-12-16 09:15:27 +0900

    参照

--- primitive/src/main.rs
+++ primitive/src/main.rs
@@ -1,9 +1,27 @@
 mod integer;
 mod overflow;
+mod float;
+mod character;
+
 
 // 戻り値の型を省略。コンパイラは戻り値がユニット型だと解釈する
 fn hello() {
     println!("Hello");    
+}
+
+// 関数f1は呼び出し元の値のコピーを引数nに束縛し、1に変更する
+fn f1(mut n: u32) {
+    n = 1;
+    println!("f1:       n = {}", n);
+}
+
+// 関数f2は呼び出し元の値を指すポインタを受け取り、ポインタが指す場所に2を格納する
+fn f2(n_ptr: &mut u32) {
+    println!("f2:   n_ptr = {:p}", n_ptr);
+
+    // *を付けると参照先にアクセスできる。これを参照外し(dereference)と呼ぶ
+    *n_ptr = 2;
+    println!("n:  *n_ptr = {}", *n_ptr);
 }
 
 fn main() {
@@ -42,4 +60,28 @@ fn main() {
     overflow::overflow();
     float::float();
     character::character();
+
+
+    let mut n = 0;
+    println!("main:     n = {}", n);
+
+    f1(n);
+    println!("main:     n = {}", n);
+
+    // `&mut n`でnの値を指す可変のポインタを作成する
+    f2(&mut n);
+    println!("main:     n = {}", n);
+
+    let c1 = 'A';       // char型
+    let c1_ptr = &c1;       // &char型。イミュータブルな参照(不変の参照)
+    assert_eq!(*c1_ptr, 'A');
+
+    let mut n1 = 0; // i32型
+    let n1_ptr = &mut n1;   // &mut i32型。ミュータブルな参照(可変の参照)
+    assert_eq!(*n1_ptr, 0);
+
+    // 可変の参照では参照先の値を変更できる
+    *n1_ptr = 1_000;
+    assert_eq!(*n1_ptr, 1_000);
+
 }
