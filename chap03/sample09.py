97beec2b43751a5f704b36cc6bc5da19ca42033c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-26 00:50:14 +0900

    追加

--- chap03/sample09.py
+++ chap03/sample09.py
@@ -14,6 +14,15 @@ def en_del13():
 def en_del0end():
     en.delete(0, tk.END)
 
+def en_ins0():
+    en.insert(0, "先頭に追加")
+
+def en_ins3():
+    en.insert(3, "3の位置に追加")
+
+def en_insend():
+    en.insert(tk.END, "最後に追加")
+
 root = tk.Tk()
 # テキストボックスの作成
 en = tk.Entry()
@@ -21,7 +30,10 @@ bt = tk.Button(text="ボタン", command=print_txtval)
 bt2 = tk.Button(text="delete(2)", command=en_del2)
 bt3 = tk.Button(text="delete(1, 3)", command=en_del13)
 bt4 = tk.Button(text="delete(0, tkinter.END)", command=en_del0end)
-[widget.pack() for widget in (en, bt, bt2, bt3, bt4)]
+bt5 = tk.Button(text="insert(0,...)", command=en_ins0)
+bt6 = tk.Button(text="insert(3,...)", command=en_ins3)
+bt7 = tk.Button(text="insert(END,...)", command=en_insend)
+[widget.pack() for widget in (en, bt, bt2, bt3, bt4, bt5, bt6, bt7)]
 # フォーカスを当てる
 en.focus_set()
 root.mainloop()
