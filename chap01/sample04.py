874dcddb8645d07d20190323a9b19348fdc7e8b5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-25 19:37:27 +0900

    アクションの組み込み

--- chap01/sample04.py
+++ chap01/sample04.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
+# ボタンが押された時に呼び出される
+def action_btn_press():
+    print("ボタンが押されました")
+
 root = tk.Tk()
-root.title("部品(widget)の作成")
+root.title("アクションの組み込み")
 root.geometry("350x150")
 # 部品(widget)の作成
 lb = tk.Label(text="ラベル")
-bt = tk.Button(text="ボタン")
+# commandオプションに関数名を指定
+bt = tk.Button(text="ボタン", command=action_btn_press)
 # 配置
 lb.pack()
 bt.pack()
