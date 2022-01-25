ea7d198e076f66c14aa8b679a906e9632c992900
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-26 00:21:05 +0900

    フォント

---
+++ chap03/sample01.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+root = tk.Tk()
+lb = tk.Label(text="ＭＳ ゴシック,サイズ20,太字でない,斜体,下線なし,取消線あり", font=("ＭＳ ゴシック",20,"normal","italic","normal","overstrike"))
+lb.pack()
+root.mainloop()
