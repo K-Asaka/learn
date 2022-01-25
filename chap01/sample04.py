0d833d4a967c5f8bf59f9bad9cbf8f9a50e7ae98
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-25 19:34:43 +0900

    部品(widget)の作成

---
+++ chap01/sample04.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("部品(widget)の作成")
+root.geometry("350x150")
+# 部品(widget)の作成
+lb = tk.Label(text="ラベル")
+bt = tk.Button(text="ボタン")
+root.mainloop()
