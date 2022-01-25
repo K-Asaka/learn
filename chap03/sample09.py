777c2933a6b2badf452014bcbdd9eeee971377dd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-26 00:46:41 +0900

    削除

--- chap03/sample09.py
+++ chap03/sample09.py
@@ -5,11 +5,23 @@ def print_txtval():
     val_en = en.get()
     print(val_en)
 
+def en_del2():
+    en.delete(2)
+
+def en_del13():
+    en.delete(1, 3)
+
+def en_del0end():
+    en.delete(0, tk.END)
+
 root = tk.Tk()
 # テキストボックスの作成
 en = tk.Entry()
 bt = tk.Button(text="ボタン", command=print_txtval)
-[widget.pack() for widget in (en, bt)]
+bt2 = tk.Button(text="delete(2)", command=en_del2)
+bt3 = tk.Button(text="delete(1, 3)", command=en_del13)
+bt4 = tk.Button(text="delete(0, tkinter.END)", command=en_del0end)
+[widget.pack() for widget in (en, bt, bt2, bt3, bt4)]
 # フォーカスを当てる
 en.focus_set()
 root.mainloop()
