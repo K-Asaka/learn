b2b30a49a28eea2c20de8edd5148fdac1a9bcb53
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-27 00:40:18 +0900

    アイテムの選択/解除

--- chap03/sample26.py
+++ chap03/sample26.py
@@ -1,17 +1,19 @@
 import tkinter as tk
 
-def get_listitem():
-    # 選択中アイテムのインデックス
-    print(listbox.curselection())
+def set_allitem():
+    # 0, tkinter.ENDで全てのアイテムが選択される
+    listbox.select_set(0, tk.END)
+def clear_item():
+    # 引数の渡し方は.select_set()と同じ
+    listbox.select_clear(0, tk.END)
 
 root = tk.Tk()
 root.geometry("280x150")
-root.title("get listitem")
+root.title("listbox set clear")
 strvar = tk.StringVar()
 strvar.set(["北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州沖縄"])
-# 1行で書くことも出来る strvar = tk.StringVar(value=["北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州沖縄"])
-# アイテムの定義にStringVarを使用、また複数行選択可にする
 listbox = tk.Listbox(root, listvariable=strvar, selectmode="multiple", height=5)
-button = tk.Button(text="選択値を出力", command=get_listitem)
-[widget.pack() for widget in (listbox, button)]
+button1 = tk.Button(text="全選択", command=set_allitem)
+button2 = tk.Button(text="全解除", command=clear_item)
+[widget.pack() for widget in (listbox, button1, button2)]
 root.mainloop()
