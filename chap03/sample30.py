1ee3291f6535a4cb22045f518b78af88b9f271d1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-27 01:01:02 +0900

    入力/選択

--- chap03/sample30.py
+++ chap03/sample30.py
@@ -3,11 +3,15 @@ import tkinter.ttk as ttk
 
 def get_comboitem(event):
     print(combobox.get())
+    # 選択は.current(index)でする
+    combobox.current(3)
 
 root = tk.Tk()
 root.geometry("280x150")
 root.title("combobox event")
 combobox = ttk.Combobox(root, value=("北海道", "東北", "関東", "中部", "近畿", "中国", "四国", "九州沖縄"))
+# 入力は.set(value)でする
+combobox.set("あらかじめ入力")
 # コンボボックス選択のイベントは"<<ComboboxSelected>>"を渡す
 combobox.bind("<<ComboboxSelected>>", get_comboitem)
 # 入力時のEnter用
