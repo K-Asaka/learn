eb4a0965a138f00dd596ea77a0fd8a73cbe13c32
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-28 08:51:08 +0900

    各種マウスイベント

---
+++ chap05/sample04.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+def callback_func(event):
+    # カーソル位置の取得
+    label["text"] = "x:{0},y:{1}".format(event.x, event.y)
+
+root = tk.Tk()
+root.geometry("250x180")
+root.title("mouse x, y")
+label = tk.Label(root, text="mouse", relief="groove")
+# マウスボタンのイベント
+root.bind("<Button>", callback_func)
+label.pack()
+root.mainloop()
