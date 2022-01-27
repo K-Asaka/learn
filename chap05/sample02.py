940833ccd7637a10607ba78ecc98dba9e7e642de
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-28 08:44:15 +0900

    文字列比較による分岐

--- chap05/sample02.py
+++ chap05/sample02.py
@@ -4,6 +4,11 @@ def callback_func(event):
     # 識別子の取得
     label["text"] = event.keysym
 
+    ks = event.keysym
+    # 文字列比較による分岐
+    if ks == "Control_L" or ks == "Control_R":
+        label["text"] = "pressed control"
+
 root = tk.Tk()
 root.geometry("300x150")
 root.title("keysym")
