a1c6a662b54fc82833b321c5d4bf39df266dd91b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-27 09:37:27 +0900

    メニューを入れ子にする

--- chap03/sample33.py
+++ chap03/sample33.py
@@ -1,36 +1,28 @@
 import tkinter as tk
 
+def file_new():
+    label["text"] = "File new"
+
 def file_open():
     label["text"] = "File open"
 
 def file_save():
     label["text"] = "File save"
 
-def edit_copy():
-    label["text"] = "Edit copy"
-
-def edit_paste():
-    label["text"] = "Edit paste"
-
 root = tk.Tk()
-root.geometry("200x100")
-root.title("menu")
-# 1.メニューバーを作成(マスターはウィンドウ)
+root.geometry("300x100")
+root.title("menu cascade")
 menubar = tk.Menu(root)
-# 2.メニューを作成(マスターはメニューバー)、tearoff=Falseにして切り取らせないようにする
 menu1 = tk.Menu(menubar, tearoff=False)
-# 3.メニューにアイテムを追加
-menu1.add_command(label="open", command=file_open)
-# セパレーター
-menu1.add_separator()
-menu1.add_command(label="save", command=file_save)
-# 4.メニューバーにメニューをカスケード
 menubar.add_cascade(label="file", menu=menu1)
-menu2 = tk.Menu(menubar, tearoff=False)
-menu2.add_command(label="copy", command=edit_copy)
-menu2.add_command(label="paste", command=edit_paste)
-menubar.add_cascade(label="edit", menu=menu2)
-# 5.ウィンドウにメニューバーを追加
+# メニューを作成(マスターは親メニュー)
+menu11 = tk.Menu(menu1, tearoff=False)
+# メニューにアイテムを追加
+menu11.add_command(label="new", command=file_new)
+menu11.add_command(label="open", command=file_open)
+# 親メニューにメニューをカスケード
+menu1.add_cascade(label="new/open", menu=menu11)
+menu1.add_command(label="save", command=file_save)
 root["menu"] = menubar
 label = tk.Label(root)
 label.pack()
