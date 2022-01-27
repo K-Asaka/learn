e3bcffa06eddeef0387b8b1facea238d0a494781
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-01-27 10:14:18 +0900

    読み込みモード

--- chap03/sample34.py
+++ chap03/sample34.py
@@ -1,18 +1,25 @@
 import tkinter as tk
+import tkinter.scrolledtext as tksc
 # tkinter内のfiledialogクラスをインポート
 from tkinter import filedialog
 
 def get_filepath():
     # .txt, .py, 全てのファイルの場合
     filetype_list = [("text file", "*.txt"), ("py file", "*.py"), ("all file", "*")]
-    # ファイルパスの取得
-    filepath = filedialog.askopenfilename(initialdir="", filetypes=filetype_list, title="select file")
-    message["text"] = filepath
+    # ファイルオブジェクト(読み込みモード)
+    # ファイルの選択が出来ない場合はファイル権限を替えてみましょう
+    fileobj = filedialog.askopenfile(initialdir="", filetypes=filetype_list, title="select file")
+    if fileobj is not None:
+        message["text"] = str(fileobj)
+        content = fileobj.read()
+        scrolledtext.insert(tk.END, content)
+        fileobj.close()
 
 root = tk.Tk()
-root.geometry("650x100")
-root.title("filedialog")
-message = tk.Message(root, text="file path", width=600)
+root.geometry("550x250")
+root.title("filedialog_read")
+message = tk.Message(root, text="file path", width=550)
 button = tk.Button(text="get_filepath", command=get_filepath)
-[widget.pack() for widget in (message, button)]
+scrolledtext = tksc.ScrolledText(root, height=10, width=70)
+[widget.pack() for widget in (message, button, scrolledtext)]
 root.mainloop()
