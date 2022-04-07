e4e35d47061506e47660a61fcdfaab7214910c06
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 18:54:38 +0900

    switch文による多分岐処理

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,10 +10,49 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            var num = 15;
-            if (num % 3 == 0 || num % 5 == 0)
+            Console.WriteLine("ご希望の時間帯の番号を選択してください。");
+            Console.WriteLine(" 1: 10時から12時");
+            Console.WriteLine(" 2: 13時から15時");
+            Console.WriteLine(" 3: 15時から18時");
+            var line = Console.ReadLine();
+            var value = int.Parse(line);
+            
+            switch (value)
             {
-                    Console.WriteLine("numは3か5で割り切れます");
+                case 1:
+                    Console.WriteLine("10時から12時がご希望ですね。");
+                    break;
+                case 2:
+                    Console.WriteLine("13時から15時がご希望ですね。");
+                    break;
+                case 3:
+                    Console.WriteLine("15時から18時がご希望ですね。");
+                    break;
+                default:
+                    Console.WriteLine("入力した値に誤りがあります。");
+                    break;
+            }
+
+
+            var word = Console.ReadLine();
+            var term = "";
+
+            switch (word)
+            {
+                case "API":
+                    term = "Application Programming Interface";
+                    break;
+                case "RDB":
+                    term = "Relational Database";
+                    break;
+                case "UI":
+                    term = "User Interfadce";
+                    break;
+            }
+
+            if (term != "")
+            {
+                Console.WriteLine("{0}は{1}の略です。", word, term);
             }
         }
     }
