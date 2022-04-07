ab79c6012b2ba121d46e919023e36084da44dd72
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 13:43:32 +0900

    同じ型による算術演算

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,11 +10,18 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            bool exists = true;
-            var married = false;
+            var price1 = 1100;
+            var price2 = 800;
+            var price3 = 1250;
+            var total = price1 + price2 + price3;
+            Console.WriteLine("合計：{0}円", total);
 
-            Console.WriteLine(exists);
-            Console.WriteLine(married);
+            var member = 4;
+            var perPerson = total / member;
+            Console.WriteLine("1人当たり：{0}円", perPerson);
+
+            var remainder = total % member;
+            Console.WriteLine("余り：{0}", remainder);
         }
     }
 }
