c913894c018c25d4d582c920c3df65726bd4a2cc
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 13:33:11 +0900

    浮動小数点型

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,32 +10,10 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            int age;
-            age = 23;
-            Console.WriteLine(age);
-            Console.WriteLine("年齢：{0}", age);
-            Console.WriteLine("年齢は{0}歳です", age);
-            int number = 8;
-            Console.WriteLine("{0}歳の人は{1}人います", age, number);
-
-            string name = "山田";
-            Console.WriteLine("{0}さん、こんにちは", name);
-            name = "佐藤";
-            Console.WriteLine("{0}さん、お久しぶりです", name);
-
-            int height = 8;
-            int width = 5;
-            int area;
-            area = height * width;
-            Console.WriteLine("面積：{0}㎡", area);
-
-            var age2 = 19;
-            var name2 = "近藤";
-            Console.WriteLine("age2：{0}, name2：{1}", age2, name2);
-
-            Console.WriteLine("名前を入力してください。");
-            var name3 = Console.ReadLine();
-            Console.WriteLine("{0}さん、おはようございます", name3);
+            var squareRoot = 1.41421356;
+            var sideLength = 10.0;
+            var diagonal = sideLength * squareRoot;
+            Console.WriteLine("対角線の長さ：{0}メートル", diagonal);
         }
     }
 }
