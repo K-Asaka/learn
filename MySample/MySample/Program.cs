7b433a5cfaf1a5af840329ba9d20513e6b99f992
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 18:58:39 +0900

    else if構文を使った多分岐処理

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,49 +10,23 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ご希望の時間帯の番号を選択してください。");
-            Console.WriteLine(" 1: 10時から12時");
-            Console.WriteLine(" 2: 13時から15時");
-            Console.WriteLine(" 3: 15時から18時");
-            var line = Console.ReadLine();
-            var value = int.Parse(line);
-            
-            switch (value)
+            var temperature = 29.5;
+
+            if (temperature >= 35.0)
             {
-                case 1:
-                    Console.WriteLine("10時から12時がご希望ですね。");
-                    break;
-                case 2:
-                    Console.WriteLine("13時から15時がご希望ですね。");
-                    break;
-                case 3:
-                    Console.WriteLine("15時から18時がご希望ですね。");
-                    break;
-                default:
-                    Console.WriteLine("入力した値に誤りがあります。");
-                    break;
+                Console.WriteLine("猛暑日です");
             }
-
-
-            var word = Console.ReadLine();
-            var term = "";
-
-            switch (word)
+            else if (temperature >= 30.0)
             {
-                case "API":
-                    term = "Application Programming Interface";
-                    break;
-                case "RDB":
-                    term = "Relational Database";
-                    break;
-                case "UI":
-                    term = "User Interfadce";
-                    break;
+                Console.WriteLine("真夏日です");
             }
-
-            if (term != "")
+            else if (temperature >= 25.0)
             {
-                Console.WriteLine("{0}は{1}の略です。", word, term);
+                Console.WriteLine("夏日です");
+            }
+            else
+            {
+                Console.WriteLine("いずれでもありません");
             }
         }
     }
