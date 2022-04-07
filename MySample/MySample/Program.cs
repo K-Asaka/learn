d968a002f348179ef0f404ff427480974c1c9d7c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 14:13:39 +0900

    +演算子による文字列の連結

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,11 +10,20 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            var total = 100;
-            var line = Console.ReadLine();
-            var count = int.Parse(line);
-            var num = total / count;
-            Console.WriteLine(num);
+            var name = "渡辺";
+            var message = name + "さん、おはようございます。";
+            Console.WriteLine(message);
+
+            var str = "これは正しい文字列です。" +
+                        "+演算子で連結させています。";
+            Console.WriteLine(str);
+
+            var s1 = "おはよう。";
+            var s2 = "こんにちは。";
+            var s3 = "こんばんは。";
+            var s4 = "またあした。";
+            var str2 = s1 + s2 + s3 + s4;
+            Console.WriteLine(str2);
         }
     }
 }
