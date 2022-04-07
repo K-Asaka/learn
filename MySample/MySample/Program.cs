1898fe2b38fe5dd3056ccdd862c3a7a4f6a1a6ba
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-07 13:44:52 +0900

    異なる型での算術演算

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,18 +10,12 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            var price1 = 1100;
-            var price2 = 800;
-            var price3 = 1250;
-            var total = price1 + price2 + price3;
-            Console.WriteLine("合計：{0}円", total);
+            var total = 998;
+            var discount = total * 0.1;
+            var payment = total - discount;
 
-            var member = 4;
-            var perPerson = total / member;
-            Console.WriteLine("1人当たり：{0}円", perPerson);
-
-            var remainder = total % member;
-            Console.WriteLine("余り：{0}", remainder);
+            Console.WriteLine("割引額：{0}円", discount);
+            Console.WriteLine("支払額：{0}円", payment);
         }
     }
 }
