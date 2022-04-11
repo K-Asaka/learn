9da9e8643771c1a31a6c0355de5c1ada28c6e590
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-11 12:50:22 +0900

    プロパティへの値の代入

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,6 +10,21 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.Title = "吾輩は猫である";
+            book.Author = "夏目漱石";
+            book.Pages = 610;
+            book.Rating = 4;
+
+            var title = book.Title;
+            var author = book.Author;
+            var pages = book.Pages;
+            var rating = book.Rating;
+
+            Console.WriteLine($"書籍名: {title}");
+            Console.WriteLine($"著者名: {author}");
+            Console.WriteLine($"ページ数: {pages}");
+            Console.WriteLine($"評価: {rating}");
         }
     }
 
