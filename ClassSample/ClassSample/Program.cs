d8c6f8494a2eede92ebf832c8ad4e59bbc53d1ff
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 08:42:26 +0900

    静的メソッドと静的プロパティを定義してみる

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,25 +10,37 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var array = new int[] { 5, 8, 4, 9, 5, -3, 6 };
-            var foundIndex = -1;
-            for (var i = 0; i < array.Length; i++)
-            {
-                if (array[i] < 0)
-                {
-                    foundIndex = i;
-                    break;
-                }
-            }
+            var book1 = new Book { Title = "伊豆の踊り子", Author = "川端康成" };
+            book1.PrintTitle();
+            var book2 = new Book { Title = "走れメロス", Author = "太宰治" };
+            book2.PrintTitle();
+            var book3 = new Book { Title = "銀河鉄道の夜", Author = "宮沢賢治" };
+            book3.PrintTitle();
+            Book.ClearCount();
+            Console.WriteLine(Book.Count);
+        }
+    }
 
-            if (foundIndex >= 0)
-            {
-                Console.WriteLine($"配列最初のマイナス値は {array[foundIndex]}です。");
-            }
-            else
-            {
-                Console.WriteLine("配列にマイナス値はありませんでした。");
-            }
+    class Book
+    {
+        // 静的プロパティ
+        public static int Count { get; set; }
+
+        // 静的メソッド
+        public static void ClearCount()
+        {
+            Count = 0;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public void PrintTitle()
+        {
+            Console.WriteLine("書籍名: {0}", Title);
+            Count++;
+
+            Console.WriteLine(Count);
         }
     }
 }
