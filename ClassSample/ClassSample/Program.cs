cacf19cfae8f9d8784522227f7089b241c56041e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 08:53:09 +0900

    コマンドライン引数

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,30 +10,11 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var scores = new int[] { 55, 60, 45, 70, 85, 93, 68 };
-            var total = ArrayUtils.Total(scores);
-            var average = ArrayUtils.Average(scores);
-            Console.WriteLine($"合計:{total}, 平均:{average}");
-        }
-    }
-    static class ArrayUtils
-    {
-        // 配列内の数値の合計を求める
-        public static int Total(int[] numbers)
-        {
-            var total = 0;
-            foreach (var n in numbers)
+            for (var i = 0; i < args.Length; i++)
             {
-                total += n;
+                var s = args[i];
+                Console.WriteLine($"{i}番目の引数は、{s}です");
             }
-            return total;
-        }
-
-        // 配列内の数値の平均を求める
-        public static double Average(int[] numbers)
-        {
-            var total = Total(numbers);
-            return (double)total / numbers.Length;
         }
     }
 }
