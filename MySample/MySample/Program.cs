d10e8e13a780f25073824a226f29aaa92e8f07f8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-11 12:15:26 +0900

    for文で2次元配列の要素にアクセスする

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,14 +10,21 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            var scores = new int[] { 65, 54, 78, 96, 81 };
-            var total = 0;
-            foreach (var p in scores)
+            var array2d = new int[,]
             {
-                total += p;
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+                { 10, 11, 12 }
+            };
+            for (var row = 0; row < 4; row++)
+            {
+                for (var col = 0; col < 3; col++)
+                {
+                    Console.Write("{0} ", array2d[row, col]);
+                }
+                Console.WriteLine();
             }
-            var average = (double)total / scores.Length;
-            Console.WriteLine("平均点: {0}", average);
         }
     }
 }
