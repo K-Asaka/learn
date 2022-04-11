9a904ba3de1da4e496855eb9ed0f776ed36f65a6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-11 12:46:19 +0900

    クラスの定義

---
+++ ClassSample/ClassSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
+    }
+}
