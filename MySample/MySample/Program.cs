26f7b5a7782d3dc091cd2efe31337339ea2e8645
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-11 11:45:42 +0900

    配列を使った例

--- MySample/MySample/Program.cs
+++ MySample/MySample/Program.cs
@@ -10,12 +10,8 @@ namespace MySample
     {
         static void Main(string[] args)
         {
-            int score1 = 65;
-            int score2 = 54;
-            int score3 = 78;
-            int score4 = 96;
-            int score5 = 81;
-            var total = score1 + score2 + score3 + score4 + score5;
+            var scores = new int[] { 65, 54, 78, 96, 81 };
+            var total = scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
             var average = (double)total / 5;
             Console.WriteLine("平均点: {0}", average);
         }
