d5ef1c7d20d3b244534c8a2e4237fc6058572203
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:19:16 +0900

    部分文字列を取り出す

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,9 +10,12 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var str = "ようこそ、C#の世界へ";
-            var length = str.Length;
-            Console.WriteLine($"{length}文字です");
+            var str = "オブジェクト指向";
+            var sub1 = str.Substring(0, 6);
+            var sub2 = str.Substring(6, 2);
+            Console.WriteLine(str);
+            Console.WriteLine(sub1);
+            Console.WriteLine(sub2);
         }
     }
 }
