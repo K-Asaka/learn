8c2c8a48d3790c1f2818057b28ff4ca456ce50f7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 10:06:05 +0900

    配列を返すメソッド

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,11 +10,46 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines(@"C:\temp\祇園精舎.txt");
-            foreach (var line in lines)
+            var nums = ArrayUtils.GetArray(5);
+            // nums配列内の要素の合計を求める
+            var total = 0;
+            foreach (var x in nums)
             {
-                Console.WriteLine(line);
+                total += x;
             }
+            Console.WriteLine($"合計: {total}");
+        }
+    }
+
+    static class ArrayUtils
+    {
+        // 配列内の数値の合計を求める
+        public static int Total(int[] numbers)
+        {
+            var total = 0;
+            foreach (var n in numbers)
+            {
+                total += n;
+            }
+            return total;
+        }
+
+        // 配列内の数値の平均を求める
+        public static double Average(int[] numbers)
+        {
+            var total = Total(numbers);
+            return (double)total / numbers.Length;
+        }
+
+        public static int[] GetArray(int count)
+        {
+            var array = new int[count];
+            for (var i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine();
+                array[i] = int.Parse(line);
+            }
+            return array;
         }
     }
 }
