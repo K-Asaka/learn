1bf3125a7fc592b6cefb1fd5f9d3dce123eb0716
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 10:58:51 +0900

    コンストラクターの定義方法

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,67 +10,29 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            // 1⃣配列の要素を0、10、20、30、40……に設定する
-            var array = new int[10];
-            ArrayUtils.SetValue(array, 0, 10);
-            // 2⃣配列のすべての要素を1に設定する
-            var array2 = new int[10];
-            ArrayUtils.SetValue(array2, 1);
-            // 3⃣配列のすべての要素を0に設定する
-            var array3 = new int[10];
-            ArrayUtils.SetValue(array3);
-
-            foreach (var v in array)
-            {
-                Console.WriteLine(v);
-            }
-            foreach (var v in array2)
-            {
-                Console.WriteLine(v);
-            }
-            foreach (var v in array3)
-            {
-                Console.WriteLine(v);
-            }
+            var mypet = new VirtualPet();
+            Console.WriteLine($"Name: {mypet.Name}");
+            Console.WriteLine($"Mood: {mypet.Mood}");
+            Console.WriteLine($"Energy: {mypet.Energy}");
         }
     }
-    static class ArrayUtils
+    class VirtualPet
     {
-        public static void SetValue(int[] array, int value = 0, int inc = 0)
-        {
-            for (var i = 0; i < array.Length; i++)
-            {
-                array[i] = value;
-                value += inc;
-            }
-        }
-        // 配列内の数値の合計を求める
-        public static int Total(int[] numbers)
-        {
-            var total = 0;
-            foreach (var n in numbers)
-            {
-                total += n;
-            }
-            return total;
-        }
+        // ペットの名前
+        public string Name { get; set; }
 
-        // 配列内の数値の平均を求める
-        public static double Average(int[] numbers)
-        {
-            var total = Total(numbers);
-            return (double)total / numbers.Length;
-        }
+        // ペットの機嫌を示す(値が大きいと機嫌が良い)
+        public int Mood { get; set; }
 
-        public static int[] GetArray(int count)
+        // ペットの元気度を示す(エネルギーの値で元気かどうかを判断)
+        public int Energy { get; set; }
+
+        // コンストラクターの定義
+        public VirtualPet()
         {
-            var array = new int[count];
-            for (var i = 0; i < count; i++)
-            {
-                var line = Console.ReadLine();
-                array[i] = int.Parse(line);
-            }
-            return array;
+            Name = "エイミー";
+            Mood = 5;
+            Energy = 100;
         }
     }
 }
