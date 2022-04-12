949b83f7619ea430cdd896942a2f8384a9172126
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:59:21 +0900

    テキストファイルを作成する

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,17 +10,14 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var date = new DateTime(2019, 4, 3, 19, 8, 53);
-            var s5 = date.ToString("yyyy年MM月dd日(ddd) HH時mm分ss秒");
-            var s6 = date.ToString("yy年M月d日(ddd) H時m分s秒");
-            var s7 = date.ToString("yy年M月d日(ddd) tth時m分s秒");
-            var s8 = date.ToString("yyyy/MM/dd HH:mm");
-            var s9 = date.ToString("yy/M/d H:m");
-            Console.WriteLine(s5);
-            Console.WriteLine(s6);
-            Console.WriteLine(s7);
-            Console.WriteLine(s8);
-            Console.WriteLine(s9);
+            var lines = new string[]
+            {
+                "祇園精舎の鐘の声、諸行無常の響きあり。",
+                "沙羅双樹の花の色、盛者必衰の理をあらはす。",
+                "奢れる人も久しからず、ただ春の夜の夢のごとし。",
+                "猛き者もつひにはほろびぬ、ひとへに風の前の塵に同じ。"
+            };
+            File.WriteAllLines("C:\\temp\\祇園精舎.txt", lines);
         }
     }
 }
