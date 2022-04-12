0a4707e651f06b724d705609e2a6226a745578d0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:50:50 +0900

    N日前を得る

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,8 +10,8 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var date1 = new DateTime(2020, 9, 10);
-            var date2 = date1.AddDays(10);
+            var date1 = new DateTime(2020, 9, 3);
+            var date2 = date1.AddDays(-7);
             Console.WriteLine($"{date2.Year}年{date2.Month}月{date2.Day}日");
         }
     }
