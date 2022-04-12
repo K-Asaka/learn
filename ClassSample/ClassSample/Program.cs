d9344de28686ae1d74bf21645202f71154ede81b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:15:07 +0900

    usingディレクティブがない場合

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -1,35 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassSample
+﻿namespace ClassSample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var mapLocation = new MyApp.Map.Location();
-            var canvasLocation = new MyApp.Drawing.Location();
+            System.Console.WriteLine("Hello! C# world.");
         }
-    }
-}
-
-namespace MyApp.Map
-{
-    class Location
-    {
-        public double Latitude { get; set; }    // 緯度
-        public double Longitude { get; set; }   // 経度
-    }
-}
-
-namespace MyApp.Drawing
-{
-    class Location
-    {
-        public int X { get; set; }  // X座標
-        public int Y { get; set; }  // Y座標
     }
 }
