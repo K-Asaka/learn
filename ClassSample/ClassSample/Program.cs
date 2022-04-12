1b45529e0764fe6c9bf1ae0a6d00e2a7e5a6cf80
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 08:54:29 +0900

    列挙型の定義方法

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,15 +10,57 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var week = DayOfWeek.Sunday; 
-            if (week == DayOfWeek.Friday)
+            var card = new Card(CardSuit.Heart, 8);
+            card.Print();
+            if (card.Suit == CardSuit.Diamond)
             {
-                Console.WriteLine("金曜日です");
+                Console.WriteLine("ダイヤです");
             }
             else
             {
-                Console.WriteLine("金曜日ではありません");
+                Console.WriteLine("ダイヤではありません");
             }
+        }
+    }
+    // CardSuit列挙型の定義
+    enum CardSuit
+    {
+        Club,
+        Spade,
+        Heart,
+        Diamond
+    }
+    // Card構造体の定義
+    struct Card
+    {
+        public CardSuit Suit { get; private set; }
+        public int Number { get; private set; }
+
+        public Card(CardSuit suit, int number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+
+        public void Print()
+        {
+            var s = "";
+            switch (Suit)
+            {
+                case CardSuit.Heart:
+                    s = "ハート";
+                    break;
+                case CardSuit.Diamond:
+                    s = "ダイヤ";
+                    break;
+                case CardSuit.Spade:
+                    s = "スペード";
+                    break;
+                case CardSuit.Club:
+                    s = "クラブ";
+                    break;
+            }
+            Console.WriteLine($"{s} {Number}");
         }
     }
 }
