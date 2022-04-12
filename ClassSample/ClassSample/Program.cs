1d83f21afe759772ffc9ca922c4b79ed613795b7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:56:42 +0900

    書式をより細かく指定して日付を文字列に変換する

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -11,14 +11,16 @@ namespace ClassSample
         static void Main(string[] args)
         {
             var date = new DateTime(2019, 4, 3, 19, 8, 53);
-            var s1 = date.ToString("d");
-            var s2 = date.ToString("D");
-            var s3 = date.ToString("f");
-            var s4 = date.ToString("F");
-            Console.WriteLine(s1);
-            Console.WriteLine(s2); 
-            Console.WriteLine(s3);
-            Console.WriteLine(s4);
+            var s5 = date.ToString("yyyy年MM月dd日(ddd) HH時mm分ss秒");
+            var s6 = date.ToString("yy年M月d日(ddd) H時m分s秒");
+            var s7 = date.ToString("yy年M月d日(ddd) tth時m分s秒");
+            var s8 = date.ToString("yyyy/MM/dd HH:mm");
+            var s9 = date.ToString("yy/M/d H:m");
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+            Console.WriteLine(s8);
+            Console.WriteLine(s9);
         }
     }
 }
