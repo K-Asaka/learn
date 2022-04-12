6a6f84827fe8aaa35277f952477941eee060141d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:24:40 +0900

    指定した部分文字列が存在するかどうかを調べる

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,9 +10,11 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var str = "インスタンスを生成せずに利用できるメソッドをstaticメソッドと言います。";
-            var str2 = str.Replace("staticメソッド", "静的メソッド");
-            Console.WriteLine(str2);
+            var str = "インスタンスを生成せずに利用できるメソッドを静的メソッドと言います。";
+            if (str.Contains("静的メソッド"))
+            {
+                Console.WriteLine("文字列の中に「静的メソッド」が含まれています。");
+            }
         }
     }
 }
