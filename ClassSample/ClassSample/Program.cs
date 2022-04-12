1071f1c218a1953515ac247cea75d344e6087cf8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 10:35:53 +0900

    メソッドの省略可能な引数

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,43 +10,26 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var scores = new int[] { 55, 60, 45, 70, 85, 93, 68 };
-            var total = ArrayUtils.Total(scores);
-            Console.WriteLine(total);
-
-            var scores2 = new double[] { 5.9, 9.6, 12.4, 8.0, 7.9 };
-            var total2 = ArrayUtils.Total(scores2);
-            Console.WriteLine(total2);
+            var person = new Person
+            {
+                FirstName = "隆之",
+                LastName = "森本"
+            };
+            var name1 = person.AddTitle("先生");
+            var name2 = person.AddTitle();
+            Console.WriteLine(name1);
+            Console.WriteLine(name2);
         }
     }
-    static class ArrayUtils
-    {
-        // 1⃣int型の配列内の数値の合計を求める
-        public static int Total(int[] numbers)
-        {
-            var total = 0;
-            foreach (var n in numbers)
-            {
-                total += n;
-            }
-            return total;
-        }
-        // 2⃣double型の配列内の数値の合計を求める
-        public static double Total(double[] numbers)
-        {
-            var total = 0.0;
-            foreach (var n in numbers)
-            {
-                total += n;
-            }
-            return total;
-        }
 
-        // 配列内の数値の平均を求める
-        public static double Average(int[] numbers)
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string AddTitle(string title = "様")
         {
-            var total = Total(numbers);
-            return (double)total / numbers.Length;
+            return $"{LastName}{FirstName} {title}";
         }
     }
 }
