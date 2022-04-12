445b7e9578fe76aa2ca67d481626101136b9e257
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 10:01:22 +0900

    テキストファイルを読み込む

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,14 +10,11 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var lines = new string[]
+            var lines = File.ReadAllLines(@"C:\temp\祇園精舎.txt");
+            foreach (var line in lines)
             {
-                "祇園精舎の鐘の声、諸行無常の響きあり。",
-                "沙羅双樹の花の色、盛者必衰の理をあらはす。",
-                "奢れる人も久しからず、ただ春の夜の夢のごとし。",
-                "猛き者もつひにはほろびぬ、ひとへに風の前の塵に同じ。"
-            };
-            File.WriteAllLines(@"C:\temp\祇園精舎.txt", lines);
+                Console.WriteLine(line);
+            }
         }
     }
 }
