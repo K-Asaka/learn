7ac8c755dfadad432972e719d9dd3f7e397c25e3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-12 09:20:39 +0900

    前後の空白文字を取り除く

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,12 +10,10 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var str = "オブジェクト指向";
-            var sub1 = str.Substring(0, 6);
-            var sub2 = str.Substring(6, 2);
-            Console.WriteLine(str);
-            Console.WriteLine(sub1);
-            Console.WriteLine(sub2);
+            var str = "　オブジェクト指向  ";
+            var str2 = str.Trim();
+            Console.WriteLine($"[{str}]");
+            Console.WriteLine($"[{str2}]");
         }
     }
 }
