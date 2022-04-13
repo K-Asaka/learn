4722345a433e14b3c9f3ec32cb3f58ec0bd4dd6e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 09:53:45 +0900

    クラスのコレクションに対してLINQを使う

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,15 +8,33 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var words = new List<string>
+            var books = new List<Book>();
+            books.Add(new Book("人間失格", "太宰治", 212, 5));
+            books.Add(new Book("女生徒", "太宰治", 279, 4));
+            books.Add(new Book("吾輩は猫である", "夏目漱石", 610, 4));
+            books.Add(new Book("こゝろ", "夏目漱石", 378, 5));
+            books.Add(new Book("銀河鉄道の夜", "宮沢賢治", 357, 3));
+            books.Add(new Book("伊豆の踊子", "川端康成", 201, 3));
+            var query = books.Where(x => x.Author == "夏目漱石");
+            foreach (var book in query)
             {
-                "effect", "access", "condition", "sign", "profit", "line", "result"
-            };
-            var query = words.Where(x => x.Length == 6);
-            foreach (var word in query)
-            {
-                Console.WriteLine(word);
+                Console.WriteLine($"{book.Title} {book.Author} {book.Rating}");
             }
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
+
+        public Book(string title, string author, int pages, int rating)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Rating = rating;
         }
     }
 }
