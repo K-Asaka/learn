0486dcf982563798abc5d24dd7775e9a164bf6fc
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 11:48:24 +0900

    Maxメソッドを使ったコード2

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,9 +8,30 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var max = nums.Max();
+            var books = new List<Book>();
+            books.Add(new Book("人間失格", "太宰治", 212, 5));
+            books.Add(new Book("女生徒", "太宰治", 279, 4));
+            books.Add(new Book("吾輩は猫である", "夏目漱石", 610, 4));
+            books.Add(new Book("こゝろ", "夏目漱石", 378, 5));
+            books.Add(new Book("銀河鉄道の夜", "宮沢賢治", 357, 3));
+            books.Add(new Book("伊豆の踊子", "川端康成", 201, 3));
+            var max = books.Max(x => x.Pages);
             Console.WriteLine(max);
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
+
+        public Book(string title, string author, int pages, int rating)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Rating = rating;
         }
     }
 } 
