2ea0a4daf7dd80b32a1d0ab7ad951545601dd0bb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 19:12:44 +0900

    例外をキャッチする

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,130 +8,19 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var pets = new List<IVirtualPet>();
-            var pet1 = new FoodiePet("エイミー");
-            var pet2 = new CheerfulPet("クー");
-            var pet3 = new SleepyPet("ライアン");
-            pets.Add(pet1);
-            pets.Add(pet2);
-            pets.Add(pet3);
-
-            foreach (var pet in pets)
+            try
             {
-                pet.Eat();
-                pet.Play();
-                Console.WriteLine($"{pet.Name} 機嫌:{pet.Mood} エネルギー:{pet.Energy}");
+                var total = 1000;
+                var line = Console.ReadLine();
+                var count = int.Parse(line);
+                var ans = total / count;
+                Console.WriteLine(ans);
+                Console.WriteLine("正常終了");
             }
-        }
-    }
-    interface IVirtualPet
-    {
-        string Name { get; }
-        int Mood { get; set; }
-        int Energy { get; set; }
-        void Eat();
-        void Play();
-        void Sleep();
-
-    }
-
-    class SleepyPet : IVirtualPet
-    {
-        public string Name { get; private set; }
-        public int Mood { get; set; }
-        public int Energy { get; set; }
-        public SleepyPet(string name)
-        {
-            Name = name;
-            Mood = 5;
-            Energy = 100;
-        }
-
-        public void Eat()
-        {
-            Mood -= 1;
-            Energy += 5;
-            Console.WriteLine("SleepyPet.Eatメソッドが実行されました");
-        }
-
-        public void Play()
-        {
-            Mood -= 1;
-            Energy -= 10;
-            Console.WriteLine("SleepyPet.Playメソッドが実行されました");
-        }
-
-        public void Sleep()
-        {
-            Mood += 2;
-            Energy += 2;
-            Console.WriteLine("SleepyPet.Sleepメソッドが実行されました");
-        }
-    }
-    class FoodiePet : IVirtualPet
-    {
-        public string Name { get; private set; }
-        public int Mood { get; set; }
-        public int Energy { get; set; }
-
-        public FoodiePet(string name)
-        {
-            Name = name;
-            Mood = 5;
-            Energy = 100;
-        }
-
-        public void Eat()
-        {
-            Mood += 3;
-            Energy += 5;
-            Console.WriteLine("FoodiePet.Eatメソッドが実行されました");
-        }
-
-        public void Play()
-        {
-            Mood -= 1;
-            Energy -= 10;
-            Console.WriteLine("FoodiePet.Playメソッドが実行されました");
-        }
-
-        public void Sleep()
-        {
-            Mood -= 1;
-            Energy += 2;
-            Console.WriteLine("FoodiePet.Sleepメソッドが実行されました");
-        }
-    }
-    class CheerfulPet : IVirtualPet
-    {
-        public string Name { get; private set; }
-        public int Mood { get; set; }
-        public int Energy { get; set; }
-
-        public CheerfulPet(string name)
-        {
-            Name = name;
-            Mood = 5;
-            Energy = 100;
-        }
-        public void Eat()
-        {
-            Mood += 0;
-            Energy += 5;
-            Console.WriteLine("CheerfulPet.Eatメソッドが実行されました");
-        }
-        public void Play()
-        {
-            Mood += 3;
-            Energy -= 10;
-            Console.WriteLine("CheerfulPet.Playメソッドが実行されました");
-        }
-
-        public void Sleep()
-        {
-            Mood -= 1;
-            Energy += 2;
-            Console.WriteLine("CheerfulPet.Sleepメソッドが実行されました");
+            catch
+            {
+                Console.WriteLine("入力した値が正しくありません");
+            }
         }
     }
 }
