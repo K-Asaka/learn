50737452f358c3bc44a87502b6e198218f6ce97e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 09:04:28 +0900

    値型をメソッドに渡した場合の動作

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,11 +10,16 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var cardA = new Card(CardSuit.Spade, 4);
-            var cardB = cardA;
-            cardA.Number = 12;
-            Console.WriteLine($"CardA: Suit:{cardA.Suit}, Number: {cardA.Number}");
-            Console.WriteLine($"CardB: Suit:{cardB.Suit}, Number: {cardB.Number}");
+            var king = new Card(CardSuit.Spade, 13);
+            Console.WriteLine($"Suit: {king.Suit}, Number: {king.Number}");
+            ChangeToAce(king);
+
+            Console.WriteLine($"Suit: {king.Suit}, Number: {king.Number}");
+        }
+
+        private static void ChangeToAce(Card card)
+        {
+            card.Number = 1;
         }
     }
     // CardSuit列挙型の定義
@@ -23,7 +28,7 @@ namespace ClassSample
         Club, Spade, Heart, Diamond
     }
     // Card構造体の定義
-    class Card
+    struct Card
     {
         public CardSuit Suit { get; set; }
         public int Number { get; set; }
