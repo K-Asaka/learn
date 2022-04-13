c87d8fdc1014b5c9f0a053cc0c2a70bbbbd0f242
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 15:06:30 +0900

    継承したクラスを利用する

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,6 +8,15 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
+            var employee = new Employee
+            {
+                Number = 352,
+                FirstName = "涼太",
+                LastName = "田中",
+                HireDate = new DateTime(2015, 10, 1)
+            };
+            Console.WriteLine("従業員番号{0}の{1}は、{2}年に入社しました。",
+                                employee.Number, employee.FullName, employee.HireDate.Year);
         }
     }
     class Person
