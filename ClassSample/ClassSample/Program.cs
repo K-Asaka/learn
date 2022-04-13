37dfaf0f58e36cfd939cfe1739f978ba8accfa75
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 15:04:04 +0900

    継承してクラスを定義する

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,10 +8,35 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var nums = new int[] { 12, 56, 75, 8, 12, 95, 32, 85, 24, 49 };
-            var list = nums.Where(x => x <= 10)
-                           .ToList();
-            Console.WriteLine(list[0]);
         }
     }
-} 
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get { return LastName + FirstName; }
+        }
+        public string Email { get; set; }
+        
+    }
+    // 従業員クラス
+    class Employee : Person
+    {
+        // 従業員番号
+        public int Number { get; set; }
+        // 入社年月日
+        public DateTime HireDate { get; set; }
+    }
+    // 顧客クラス
+    class Customer : Person
+    {
+        // 顧客ID
+        public string Id { set; get; }
+        // 顧客ランク 1～5　5が最上ランク
+        public int Rank { get; set; }
+        // クレジットカード番号
+        public string CreditCardNum { set; get; }
+    }
+}
