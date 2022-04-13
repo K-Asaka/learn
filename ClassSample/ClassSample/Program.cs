5130a1fc6e099f997157a6be1d01d03f30029feb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 09:19:20 +0900

    値型でnullを使うには？

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -10,38 +10,16 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var book = GetBook();
-            if (book == null)
+            int? number = 123;
+            if (number == null)
             {
-                Console.WriteLine("Bookオブジェクトは生成できませんでした");
+                Console.WriteLine("numberはnullです");
             }
             else
             {
-                Console.WriteLine($"{book.Title}  {book.Author}");
+                int n = number.Value;
+                Console.WriteLine($"number={n}");
             }
         }
-
-        private static Book GetBook()
-        {
-            var line = Console.ReadLine();
-            var items = line.Split(',');
-            if (items.Length != 2)
-            {
-                return null;
-            }
-            var book = new Book
-            {
-                Title = items[0],
-                Author = items[1],
-            };
-            return book;
-        }
-    }
-    class Book
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int Pages { get; set; }
-        public int Rating { get; set; }
     }
 }
