143a59a01974cb6686f0f0764fb4d380f353b4eb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 18:08:30 +0900

    継承元のクラスを定義する

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,61 +8,32 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var employee = new Employee
-            {
-                Number = 512,
-                FirstName = "理沙",
-                LastName = "石井",
-                HireDate = new DateTime(2016, 4, 1)
-            };
-            var s = employee.ToString();
-            Console.WriteLine(s);
         }
     }
-    class Person
+
+    class VirtualPet
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName
+        public string Name { get; set; }
+        public int Mood { get; set; }
+        public int Energy { get; set; }
+        // コンストラクター
+        public VirtualPet(string name)
         {
-            get { return LastName + FirstName; }
-        }
-        public string Email { get; set; }
-
-        public virtual void Print()
-        {
-            Console.WriteLine($"名前: {FullName} ({Email})");
+            Name = name;
+            Mood = 5;
+            Energy = 100;
         }
 
-    }
-    // 従業員クラス
-    class Employee : Person
-    {
-        // 従業員番号
-        public int Number { get; set; }
-        // 入社年月日
-        public DateTime HireDate { get; set; }
-
-        public override void Print()
+        public virtual void Eat()
         {
-            Console.WriteLine($"{Number}:{FullName} ({Email}) {HireDate.Year}年入社");
         }
 
-        public override string ToString()
+        public virtual void Play()
         {
-            var s = $"{Number} {FullName} " +
-                    $"{HireDate.Year}年{HireDate.Month}月{HireDate.Day}日入社";
-            return s;
         }
-    }
-    // 顧客クラス
-    class Customer : Person
-    {
-        // 顧客ID
-        public string Id { set; get; }
-        // 顧客ランク 1～5　5が最上ランク
-        public int Rank { get; set; }
-        // クレジットカード番号
-        public string CreditCardNum { set; get; }
+
+        public virtual void Sleep()
+        {
+        }
     }
 }
