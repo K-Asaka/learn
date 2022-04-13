46e8a75feaa6643980eba2a99aeed2fd8f0fed51
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 09:44:31 +0900

    配列に対してLINQのWhereメソッドが使える例

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -8,7 +8,7 @@ namespace ClassSample
     {
         static void Main(string[] args)
         {
-            var nums = new List<int> { 12, 56, 75, 8, 12, 95, 32, 85, 24, 49 };
+            var nums = new int[] { 12, 56, 75, 8, 12, 95, 32, 85, 24, 49 };
 
             var query = nums.Where(x => x >= 50);
             foreach (var n in query)
