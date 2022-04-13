96310ec8faaa129659a53583f484a487d602945c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-13 18:25:51 +0900

    CheerfulPetとSleepyPetの定義

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -38,6 +38,9 @@ namespace ClassSample
     }
     class FoodiePet : VirtualPet
     {
+        public FoodiePet(string name) : base(name)
+        {
+        }
         public override void Eat()
         {
             Mood += 3;
@@ -57,6 +60,58 @@ namespace ClassSample
             Mood -= 1;
             Energy += 2;
             Console.WriteLine("FoodiePet.Sleepメソッドが実行されました");
+        }
+    }
+
+    class CheerfulPet : VirtualPet
+    {
+        public CheerfulPet(string name) : base(name)
+        {
+        }
+        public override void Eat()
+        {
+            Mood += 0;
+            Energy += 5;
+            Console.WriteLine("CheerfulPet.Eatメソッドが実行されました");
+        }
+        public override void Play()
+        {
+            Mood += 3;
+            Energy -= 10;
+            Console.WriteLine("CheerfulPet.Playメソッドが実行されました");
+        }
+        public override void Sleep()
+        {
+            Mood -= 1;
+            Energy += 2;
+            Console.WriteLine("CheerfulPet.Sleepメソッドが実行されました");
+        }
+    }
+    class SleepyPet : VirtualPet
+    {
+        public SleepyPet(string name) : base(name)
+        {
+        }
+
+        public override void Eat()
+        {
+            Mood -= 1;
+            Energy += 5;
+            Console.WriteLine("SleepyPet.Eatメソッドが実行されました");
+        }
+
+        public override void Play()
+        {
+            Mood -= 1;
+            Energy -= 10;
+            Console.WriteLine("SleepyPet.Playメソッドが実行されました");
+        }
+
+        public override void Sleep()
+        {
+            Mood += 2;
+            Energy += 2;
+            Console.WriteLine("SleepyPet.Sleepメソッドが実行されました");
         }
     }
 }
