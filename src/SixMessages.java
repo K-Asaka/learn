0bd2d312c809c1ae03ca47f0d4debde7b8b7dcea
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 10:54:26 +0900

    メソッドを使った簡単なプログラム

---
+++ src/SixMessages.java
@@ -0,0 +1,13 @@
+class SixMessages {
+    public static void main(String[] args) {
+        System.out.println("*** top ***");
+        System.out.println("Hello.");
+        System.out.println("Hello.");
+        System.out.println("Hello.");
+        System.out.println("*** middle ***");
+        System.out.println("Hello.");
+        System.out.println("Hello.");
+        System.out.println("Hello.");
+        System.out.println("*** end ***");
+    }
+}
