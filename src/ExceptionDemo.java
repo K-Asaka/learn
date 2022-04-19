75afe17e881a46701022fe49e7f9c06d5d707f5a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 19:01:57 +0900

    例外補足(try catch)

--- src/ExceptionDemo.java
+++ src/ExceptionDemo.java
@@ -1,5 +1,5 @@
 class ExceptionDemo {
     public static void main(String[] args) {
-        java.io.FileReader fr = new java.io.FileReader("ExceptionDemo.java");
+        // java.io.FileReader fr = new java.io.FileReader("ExceptionDemo.java");
     }
 }
