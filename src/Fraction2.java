8ac26e781d04d64e35f570797486d2b1ba8113ed
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 18:21:51 +0900

    継承とコンストラクタ

--- src/Fraction2.java
+++ src/Fraction2.java
@@ -1,5 +1,15 @@
-class Fraction2 extends Fraction {
-    void add (Fraction f) {
+class Fraction2 extends FractionOrigin {
+    Fraction2() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    Fraction2(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    void add (Fraction2 f) {
         numerator = numerator * f.denominator + denominator * f.numerator;
         denominator = denominator * f.denominator;
     }
