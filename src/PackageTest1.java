3ce570cd2bd2b663f4479e77455d61237167adc5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 18:36:55 +0900

    importによるパッケージの利用

--- src/PackageTest1.java
+++ src/PackageTest1.java
@@ -1,8 +1,9 @@
+import java.text.DecimalFormat;
 class PackageTest1 {
     public static void main(String[] args) {
         int x = 1234567;
 
-        java.text.DecimalFormat df = new java.text.DecimalFormat(",###");
+        DecimalFormat df = new DecimalFormat(",###");
 
         System.out.println("x = " + x);
         System.out.println("x = " + df.format(x));
