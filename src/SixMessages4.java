8cd544b02b4607e122ee5841429f225e90869f9a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 10:57:38 +0900

    メソッドの書き方

---
+++ src/SixMessages4.java
@@ -0,0 +1,14 @@
+class SixMessages4 {
+    public static void main(String[] args) {
+        System.out.println("*** top ***");
+        void message() {
+            System.out.println("Hello.");
+            System.out.println("Hello.");
+            System.out.println("Hello.");
+        }
+        message();
+        System.out.println("*** middle ***");
+        message();
+        System.out.println("*** end ***");
+    }
+}
