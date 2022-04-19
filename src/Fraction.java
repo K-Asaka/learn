a8cad29cf3c90e598aadd25217d163b893eacfd3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-04-19 17:14:22 +0900

    設計図の改良

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,13 @@
 class Fraction {
     int numerator;
     int denominator;
+
+    void add(Fraction f) {
+        numerator = numerator * f.denominator + denominator * f.numerator;
+        denominator = denominator * f.denominator;
+    }
+
+    void add(int n) {
+        numerator = numerator + denominator * n;
+    }
 }
