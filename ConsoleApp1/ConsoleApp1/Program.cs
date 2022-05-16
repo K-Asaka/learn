ef0cd9dfc028fb22b3a7b04d55fe50489c12b69f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-16 19:10:10 +0900

    書式を指定して値を表示する

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            string s = "こんにちは";
-            Console.WriteLine("{0}、aの値は{1}です。", s, a);
-            Console.WriteLine($"{s}、aの値は{a}です。");
+            Console.WriteLine($"{1000:C}");             // 通貨表示
+            Console.WriteLine($"{250:X}");              // 16進表示
+            Console.WriteLine($"{2500000:N}");          // 数値表示
+            Console.WriteLine($"{123.45678:F3}");       // 小数点以下の桁数指定
+            Console.WriteLine($"{2.0 / 3.0:P}");        // %表示
         }
     }
 }
