29b05ac9b6f6103aebcd64ebd5c3a747307c9ea8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-16 13:31:20 +0900

    消費税を計算して変数と演算子を理解しよう

---
+++ AddTax/AddTax/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AddTax
+{
+    partial class FormAddTax
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMoney = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelAddTax = new System.Windows.Forms.Label();
+            this.buttonAddTax = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "金額";
+            // 
+            // textBoxMoney
+            // 
+            this.textBoxMoney.Location = new System.Drawing.Point(14, 25);
+            this.textBoxMoney.Name = "textBoxMoney";
+            this.textBoxMoney.Size = new System.Drawing.Size(100, 19);
+            this.textBoxMoney.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "税込金額";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "円";
+            // 
+            // labelAddTax
+            // 
+            this.labelAddTax.AutoSize = true;
+            this.labelAddTax.Location = new System.Drawing.Point(211, 32);
+            this.labelAddTax.Name = "labelAddTax";
+            this.labelAddTax.Size = new System.Drawing.Size(23, 12);
+            this.labelAddTax.TabIndex = 4;
+            this.labelAddTax.Text = "0円";
+            // 
+            // buttonAddTax
+            // 
+            this.buttonAddTax.Location = new System.Drawing.Point(153, 72);
+            this.buttonAddTax.Name = "buttonAddTax";
+            this.buttonAddTax.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTax.TabIndex = 5;
+            this.buttonAddTax.Text = "消費税追加";
+            this.buttonAddTax.UseVisualStyleBackColor = true;
+            this.buttonAddTax.Click += new System.EventHandler(this.buttonAddTax_Click);
+            // 
+            // FormAddTax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 117);
+            this.Controls.Add(this.buttonAddTax);
+            this.Controls.Add(this.labelAddTax);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMoney);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAddTax";
+            this.Text = "消費税追加";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxMoney;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelAddTax;
+        private System.Windows.Forms.Button buttonAddTax;
+    }
+}
+
