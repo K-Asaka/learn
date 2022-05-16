98d2d9805f00b0115c4ca61398e83bc8b50486c3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-16 19:17:16 +0900

    *を行の数だけ表示

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         {
             for (int i = 1; i <= 5; i++)
             {
-                for (int j = 1; j <= 7; j++)
+                for (int j = 1; j <= i; j++)
                 {
                     Console.Write("*");
                 }
