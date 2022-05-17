268c91dc4b712a2095211ff31de9f025c35d587d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-17 11:45:28 +0900

    Visual C#によるクラスの生成

---
+++ ClassSample/ClassSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Television tv = new Television();
+
+            Console.WriteLine("10チャンネルを設定します。");
+            tv.SetChannel(10);
+            Console.WriteLine($"TVは{tv.channel}チャンネルです。");
+            Console.WriteLine("チャンネルを+2します。");
+            tv.ChannelUP();
+            tv.ChannelUP();
+            Console.WriteLine($"TVは{tv.channel}チャンネルです。");
+
+            Console.WriteLine("チャンネルを-1します。");
+            tv.ChannelDown();
+            Console.WriteLine($"TVは{tv.channel}チャンネルです。");
+        }
+    }
+}
---
+++ ClassSample/ClassSample/Television.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample
+{
+    internal class Television
+    {
+        public bool power;                  // 電源の状態
+        public int channel;                 // 現在のチャンネル
+        public int volume;                  // 現在の音量
+        public const int ChannelMin = 1;    // チャンネル下限
+        public const int ChannelMax = 12;   // チャンネル上限
+        public const int VolumeMin = 0;     // 音量下限
+        public const int VolumeMax = 40;    // 音量上限
+
+        // メソッド
+        // テレビの電源をON/OFFする
+        public void OnOff()
+        {
+            if (power == true)
+                power = false;      // 電源OFF
+            else
+                power = true;       // 電源On
+        }
+
+        // チャンネルを設定する
+        public void SetChannel(int c)
+        {
+            if (c >= ChannelMin && c <= ChannelMax)
+                channel = c;
+        }
+
+        // チャンネルを+1する
+        public void ChannelUP()
+        {
+            if (channel < ChannelMax)
+                channel++;
+        }
+
+        // チャンネルを-1する
+        public void ChannelDown()
+        {
+            if (channel > ChannelMin)
+                channel--;
+        }
+
+        // 音量を上げる
+        public void VolumeUP()
+        {
+            if (volume < VolumeMax)
+                volume++;
+        }
+
+        // 音量を下げる
+        public void VolumeDown()
+        {
+            if (volume > VolumeMin)
+                volume--;
+        }
+    }
+}
