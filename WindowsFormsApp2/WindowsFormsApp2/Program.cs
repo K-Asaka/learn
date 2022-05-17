ae630fb9759112fd9652aa7ff5411a4a770b8aa7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-17 10:31:30 +0900

    DateTimeのプロパティ

--- WindowsFormsApp2/WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Program.cs
@@ -14,9 +14,27 @@ namespace WindowsFormsApp2
         [STAThread]
         static void Main()
         {
+            DateTimePropertyExample();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        static void DateTimePropertyExample()
+        {
+            Console.WriteLine("今日の日付：" + DateTime.Today);
+            Console.WriteLine("現在の日付と日時：" + DateTime.Now);
+
+            // 現在の日付と日時を取得する
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("年 = " + now.Year);
+            Console.WriteLine("月 = " + now.Month);
+            Console.WriteLine("日 = " + now.Day);
+            Console.WriteLine("時 = " + now.Hour);
+            Console.WriteLine("分 = " + now.Minute);
+            Console.WriteLine("秒 = " + now.Second);
+            Console.WriteLine("曜日 = " + now.DayOfWeek);
         }
     }
 }
