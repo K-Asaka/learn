d7a9d48449ae5ede1b1c155b32ae3c04a4558b73
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-17 10:27:12 +0900

    Windowsフォームアプリケーションとオブジェクト指向

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private Button button1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1 = new Button();
+
+            button1.Name = "button1";
+            button1.Text = "開く";
+
+            button1.Location = new Point(100, 100);
+            button1.Size = new Size(80, 20);
+
+            button1.Click += new EventHandler(Button1_Click);
+
+            Controls.Add(button1);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = form2.Feeling;
+            }
+            form2.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToString();
+        }
+    }
+}
