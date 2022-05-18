55e93654ff641aca379069c402e692529b91c57e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 19:16:35 +0900

    3次元以上の配列

--- ArraySample/ArraySample/Program.cs
+++ ArraySample/ArraySample/Program.cs
@@ -16,6 +16,20 @@ namespace ArraySample
             ArraySortExample();
             ArrayReverseExample();
             ArrayCopyExample();
+
+            int[,,] n3 = new int[2, 3, 4];
+            int m = 1;
+            for (int i = 0; i < n3.GetLength(0); i++)
+            {
+                for (int j = 0; j < n3.GetLength(1); j++)
+                {
+                    for (int k = 0; k < n3.GetLength(2); k++)
+                    {
+                        n3[i, j, k] = m++;
+                    }
+                }
+            }
+
         }
 
         static void ArrayLengthExample()
