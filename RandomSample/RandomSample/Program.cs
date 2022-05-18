a4d768be7198a1090aabf8a212f4d263db6321d6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 11:27:18 +0900

    インスタンスを複数同時に宣言した場合はseedが重なり同じパターンになる例

--- RandomSample/RandomSample/Program.cs
+++ RandomSample/RandomSample/Program.cs
@@ -10,21 +10,15 @@ namespace RandomSample
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();          // Randomインスタンスの生成
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
 
-            // 0以上でMaxValue(2,147,483,647)より小さい乱数を5個
             for (int i = 0; i < 5; i++)
-                Console.Write(rnd.Next() + " ");
+                Console.Write(rnd1.Next() + " ");
             Console.WriteLine();
 
-            // 0以上で100より小さい乱数を10個
-            for (int i = 0; i < 10; i++)
-                Console.Write(rnd.Next(100) + " ");
-            Console.WriteLine();
-
-            // 10以上で20より小さい乱数を10個
-            for (int i = 0; i < 10; i++)
-                Console.Write(rnd.Next(10, 20) + " ");
+            for (int i = 0; i < 5; i++)
+                Console.Write(rnd2.Next() + " ");
             Console.WriteLine();
         }
     }
