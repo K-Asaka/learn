c9fc550644ff1cb98d513cb115426641bf439f09
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 19:24:26 +0900

    Charsプロパティ

---
+++ StringSample/StringSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCharsExample();
+        }
+
+        static void StringCharsExample()
+        {
+            string str = "ABC";
+            char c = str[1];        // c = 'B'
+            Console.WriteLine(c);
+        }
+    }
+}
