0d0af88ee28f84d441552f6ca70075e5c8aa8d20
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 18:56:40 +0900

    Rankプロパティ

--- ArraySample/ArraySample/Program.cs
+++ ArraySample/ArraySample/Program.cs
@@ -11,6 +11,7 @@ namespace ArraySample
         static void Main(string[] args)
         {
             ArrayLengthExample();
+            ArrayRankExample();
         }
 
         static void ArrayLengthExample()
@@ -19,6 +20,16 @@ namespace ArraySample
             for (int i = 0; i < x1.Length; i++)
                 Console.Write(x1[i] + " ");
             Console.WriteLine();
+        }
+
+        static void ArrayRankExample()
+        {
+            int[] n1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };                   // 1次元配列
+            int[ , ] n2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };    // 2次元配列
+            int r1 = n1.Rank;           // r1 = 1
+            int r2 = n2.Rank;           // r2 = 2
+            Console.WriteLine("r1 = " + r1);
+            Console.WriteLine("r2 = " + r2);
         }
     }
 }
