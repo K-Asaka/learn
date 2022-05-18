597020861b0bef16275505f391ebde4cce857496
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 19:25:43 +0900

    Lengthプロパティ

--- StringSample/StringSample/Program.cs
+++ StringSample/StringSample/Program.cs
@@ -11,6 +11,7 @@ namespace StringSample
         static void Main(string[] args)
         {
             StringCharsExample();
+            StringLengthExample();
         }
 
         static void StringCharsExample()
@@ -18,6 +19,15 @@ namespace StringSample
             string str = "ABC";
             char c = str[1];        // c = 'B'
             Console.WriteLine(c);
+        }
+
+        static void StringLengthExample()
+        {
+            string str = "ABC", str2 = "あいうえお";
+            int length1 = str.Length;       // length1 = 3
+            int length2 = str2.Length;      // length2 = 5(全角文字も1字と数える)
+            Console.WriteLine("length1 = " + length1);
+            Console.WriteLine("length2 = " + length2);
         }
     }
 }
