584881b4d0284b1245c835b866178ae271271fb6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-18 11:25:16 +0900

    擬似乱数の発生

---
+++ RandomSample/RandomSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();          // Randomインスタンスの生成
+
+            // 0以上でMaxValue(2,147,483,647)より小さい乱数を5個
+            for (int i = 0; i < 5; i++)
+                Console.Write(rnd.Next() + " ");
+            Console.WriteLine();
+
+            // 0以上で100より小さい乱数を10個
+            for (int i = 0; i < 10; i++)
+                Console.Write(rnd.Next(100) + " ");
+            Console.WriteLine();
+
+            // 10以上で20より小さい乱数を10個
+            for (int i = 0; i < 10; i++)
+                Console.Write(rnd.Next(10, 20) + " ");
+            Console.WriteLine();
+        }
+    }
+}
