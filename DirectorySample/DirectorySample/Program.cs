0ea39ef5eb3206b23f4d883b972361df68e96e8b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-05-19 14:23:09 +0900

    Existsメソッド

---
+++ DirectorySample/DirectorySample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectorySample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExistsExample();
+        }
+
+        static void ExistsExample()
+        {
+            string directory = @"C:\Program Files\Microsoft Office";
+            if (Directory.Exists(directory) == true)
+                Console.WriteLine(directory + "ディレクトリは存在します。");
+            else
+                Console.WriteLine(directory + "ディレクトリは存在しません。");
+        }
+    }
+}
