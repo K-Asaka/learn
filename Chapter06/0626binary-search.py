5dd30139b3cc24c882df03a123c64b72a60fc472
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-06-22 11:40:12 +0900

    二分探索

---
+++ Chapter06/0626binary-search.py
@@ -0,0 +1,19 @@
+def search(sequence, number, lower, upper):
+    print(lower, upper)
+    if lower == upper:
+        assert number == sequence[upper]
+        return upper
+    else:
+        middle = (lower + upper) // 2
+    
+        if number > sequence[middle]:
+            return search(sequence, number, middle + 1, upper)
+        else:
+            return search(sequence, number, lower, middle)
+
+seq = [34, 67, 8, 123, 4, 100, 95]
+seq.sort()
+print(seq)
+
+print(search(seq, 34, 0, len(seq)))
+print(search(seq, 100, 0, len(seq)))
