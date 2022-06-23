c96d3b780aed988136bf9550de1a86de3d767c9e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-06-23 14:19:34 +0900

    コンストラクタとオーバーライド

---
+++ Chapter09/0903-1inherit.py
@@ -0,0 +1,11 @@
+class A:
+    def hello(self):
+        print("こんにちは、Aです。")
+
+class B(A):
+    pass
+
+a = A()
+b = B()
+a.hello()
+b.hello()
---
+++ Chapter09/0904-bird.py
@@ -0,0 +1,23 @@
+class Bird:
+    def __init__(self):
+        self.hungry = True
+    def eat(self):
+        if self.hungry:
+            print('いただきます！')
+            self.hungry = False
+        else:
+            print('お腹いっぱい！')
+
+class SongBird(Bird):
+    def __init__(self):
+        self.sound = 'ホー、ホケキョ！'
+    def sing(self):
+        print(self.sound)
+
+b = Bird()
+b.eat()
+b.eat()
+
+sb = SongBird()
+sb.sing()
+sb.eat()    # SongBirdにはhungryの初期化コードがないためエラー
