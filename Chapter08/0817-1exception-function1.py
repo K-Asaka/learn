8eb6fbcf99b5c9022957dc0a7cce5330efc09064
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-06-23 13:36:48 +0900

    例外と関数

---
+++ Chapter08/0817-1exception-function1.py
@@ -0,0 +1,13 @@
+def faulty():
+    raise Exception('例外が発生しました。')
+
+def ignore_exception():
+    faulty()
+
+def handle_exception():
+    try:
+        faulty()
+    except:
+        print('例外が処理されました。')
+
+handle_exception()
