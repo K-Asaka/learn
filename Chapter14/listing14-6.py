c21fe653c9b26abdc6183279e2a3278e8ffb5992
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-07-04 19:33:29 +0900

    selectとpollを使った非同期I/O

---
+++ Chapter14/listing14-6.py
@@ -0,0 +1,29 @@
+import socket, select
+
+s = socket.socket()
+
+host = socket.gethostname()
+port = 1234
+s.bind((host, port))
+
+s.listen(5)
+inputs = [s]
+while True:
+    rs, ws, es = select.select(inputs, [], [])
+    for r in rs:
+        if r is s:
+            c, addr = s.accept()
+            print('接続受付:', addr)
+            inputs.append(c)
+        else:
+            try:
+                data = r.recv(1024)
+                disconnected = not data
+            except socket.error:
+                disconnected = True
+            
+            if disconnected:
+                print(r.getpeername(), '接続解除')
+                inputs.remove(r)
+            else:
+                print(str(data, 'utf-8'))
