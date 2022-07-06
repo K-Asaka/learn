00af34bb6d7adbc88de2cfd0a8fe7e1aa869fa9b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-07-06 14:01:34 +0900

    テストの基本とツール

---
+++ Chapter16/listing16-1.py
@@ -0,0 +1,10 @@
+from area import rect_area
+
+height = 3
+width = 4
+correct_answer = 12
+answer = rect_area(height, width)
+if answer == correct_answer:
+    print('テストに合格しました')
+else:
+    print('テストに不合格でした')
