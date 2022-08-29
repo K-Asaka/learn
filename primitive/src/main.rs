f5f9a5ae41f7e63c3094d9ebf543085907965996
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-08-29 15:27:47 +0900

    生ポインタ

--- primitive/src/main.rs
+++ primitive/src/main.rs
@@ -11,7 +11,7 @@ fn hello() {
 
 // 関数f1は呼び出し元の値のコピーを引数nに束縛し、1に変更する
 fn f1(mut n: u32) {
-    n = 1;
+    //n = 1;
     println!("f1:       n = {}", n);
 }
 
@@ -84,4 +84,20 @@ fn main() {
     *n1_ptr = 1_000;
     assert_eq!(*n1_ptr, 1_000);
 
+    let c1 = 'A';   // char型
+    // '&'で参照を作り、型強制で生ポインタに変換する
+    let c1_ptr: *const char = &c1;  // *const char型。不変の生ポインタ
+    // 生ポインタの参照外しはunsafeな操作
+    assert_eq!(unsafe { *c1_ptr }, 'A');
+
+    let mut n1 = 0;     // i32型
+    let n1_ptr: *mut i32 = &mut n1; // *mut i32型。可変の生ポインタ
+    assert_eq!(unsafe { *n1_ptr }, 0);
+
+    // 可変の生ポインタでは参照先の値を変更できる
+    unsafe {
+        *n1_ptr = 1_000;
+        assert_eq!(*n1_ptr, 1_000);
+    }
+it
 }
