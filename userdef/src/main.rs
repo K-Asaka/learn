175daed692689bc8294689a6647c6b1dce280b7f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-07 11:18:43 +0900

    リザルト(std::result::Result<T, E>)

--- userdef/src/main.rs
+++ userdef/src/main.rs
@@ -16,6 +16,21 @@ fn add_elems(s: &[i32]) -> Option<i32> {
     Some(s0 + s3)
 }
 
+// 複数のResult値を扱うときは?演算子が便利
+// Ok(値)なら値を取り出し、Err(エラーを表す値)ならこの関数からリターンする
+fn add0(s0: &str, s1: &str) -> Result<i32, std::num::ParseIntError> {
+    let s0 = s0.parse::<i32>()?;
+    let s1 = s1.parse::<i32>()?;
+    Ok(s0 + s1)
+}
+
+// map_err()コンビネータを使うとErr(エラーを表す値)のときに別のエラーに変換できる
+fn add1(s0: &str, s1: &str) -> Result<i32, String> {
+    let s0 = s0.parse::<i32>().map_err(|_e| "s0が整数ではありません")?;
+    let s1 = s1.parse::<i32>().map_err(|_e| "s1が整数ではありません")?;
+    Ok(s0 + s1)
+}
+
 fn main() {
     let t1 = (3, "birds".to_string());      // (i32, String)型のタプル。スタックに置かれる
     let mut b1 = Box::new(t1);              // Boxポインタを作る。タプルがヒープに移動する
@@ -246,4 +261,17 @@ fn main() {
     assert_eq!(add_elems(&[7, 11]), None);
 
 
+    // str::parse()は文字列を指定した型(ここではi32型)に変換する
+    assert_eq!("10".parse::<i32>(), Ok(10));                        // 変換できたらOk(値)が返される
+    let res0 = "a".parse::<i32>();      // 変換できなかったら`Err(エラーを表す値)`が返される
+    assert!(res0.is_err());
+    println!("{:?}", res0);
+
+    assert_eq!(add0("3", "127"), Ok(3 + 127));
+    assert!(add0("3", "abc").is_err());
+
+
+    assert_eq!(add1("3", "abc"), Err("s1が整数ではありません".to_string()));
+
+    
 }
