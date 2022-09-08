b0390d138721f1c6cb4657e146fa4ebd081bb98c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-08 16:01:56 +0900

    関連関数

--- leap-year/src/main.rs
+++ leap-year/src/main.rs
@@ -7,15 +7,31 @@ use std::io;
 use std::io::Write;
 
 // 例5
+// struct Circle {
+//     radius: u32,
+// }
+// impl Circle {
+//     fn diameter(&self) -> u32 {
+//         self.radius * 2
+//     }
+// }
+
+// 例6
 struct Circle {
     radius: u32,
 }
+
 impl Circle {
+    // Circle::diameterメソッド
     fn diameter(&self) -> u32 {
         self.radius * 2
     }
-}
 
+    // small_circle関連関数
+    fn small_circle() -> Circle {
+        Circle { radius: 1 }
+    }
+}
 
 // エントリポイントとなる関数
 fn main() {
@@ -33,6 +49,13 @@ fn main() {
 
     let circle1 = Circle { radius: 10 };
     println!("Circle1's diameter: {}", circle1.diameter());
+
+
+    // Circleの関連関数small_circleの呼び出し
+    let circle1 = Circle::small_circle();
+    println!("Circle1's diameter: {}", circle1.diameter());
+
+
 }
 
 // うるう年の場合はtrue、平年の場合はfalseを返す関数
