8b9c07665d97b44a639c1daa779431c5e50c5d3d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-08 10:10:52 +0900

    メソッドレシーバの型強制

--- userdef/src/main.rs
+++ userdef/src/main.rs
@@ -716,4 +716,8 @@ fn main() {
     d = Box::new(Some(1));      // Box<Some<i32>> → Box<Debug>
 
 
+    let v1: Vec<u8> = vec![3, 4, 5];
+    assert_eq!(v1.first(), Some(&3u8));     // first()メソッドのレシーバはv1が束縛されたベクタ
+
+
 }
