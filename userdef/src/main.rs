23e303e8ee0f1af40a864755e09a683a9b361c97
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-08 10:01:18 +0900

    ポインタの弱体化

--- userdef/src/main.rs
+++ userdef/src/main.rs
@@ -581,9 +581,20 @@
 // }
 
 
-fn f1(n: &mut usize, str: &str, slice: &[i32]) {
-    *n = str.len() + slice.len()
+// fn f1(n: &mut usize, str: &str, slice: &[i32]) {
+//     *n = str.len() + slice.len()
+// }
+
+fn f1(slice: &[usize]) -> usize {
+    slice.len()
 }
+
+fn f2(slice: &mut [usize]) {
+    // ポインタの弱体化により&mut [usize]型から&[usize]型へ型強制される
+    let len = f1(slice);
+    slice[0] = len;
+}
+
 
 fn main() {
     let i1 = 42;    // i32型
@@ -682,8 +693,13 @@ fn main() {
     // - &mut Box<usize> → &mut usize
     // - &String → &str
     // - &Vec<i32> → &[i32]
-    f1(&mut b1, &s1, &v1);
-    assert_eq!(8, *b1);
+    // f1(&mut b1, &s1, &v1);
+    // assert_eq!(8, *b1);
 
 
+    let mut v = vec![0; 10];
+    f2(&mut v[..]);
+    assert_eq!(10, v[0]);
+
+    
 }
