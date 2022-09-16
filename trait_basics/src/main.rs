a1d0e6a08a427e59cbafd517646e3f8a0ba0059e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-16 19:31:10 +0900

    トレイト境界

--- trait_basics/src/main.rs
+++ trait_basics/src/main.rs
@@ -61,6 +61,54 @@ impl Coordinates for (f64, f64) {
     }
 }
 
+// トレイト境界の指定方法
+// fn print_point<P: Coordinates>(point: P) {   // 1
+// fn print_point<point: impl Coordinates) {    // 2
+fn print_point<P>(point: P)                     // 3
+where
+    P: Coordinates,
+{
+    let p = point.to_cartesian();
+    println!("({}, {})", p.x, p.y)
+}
+
+// PにCoordinatesとCloneの2つの境界を付ける
+fn as_cartesian<P: Coordinates + Clone>(point: &P) -> CartesianCoord {
+    point.clone().to_cartesian()
+}
+
+// Pを2回書くには型パラメータが必要
+fn double_point<P: Coordinates>(point: P) -> P {
+    let mut cart = point.to_cartesian();
+    cart.x *= 2.0;
+    cart.y *= 2.0;
+    P::from_cartesian(cart)
+}
+
+// (T, T)のようにTそのものでない型への制約はジェネリクスが必要
+fn make_point<T>(x: T, y: T) -> CartesianCoord
+where
+    (T, T): Coordinates
+{
+    (x, y).to_cartesian()
+}
+
+// ジェネリクストレイトを用意しておく
+// トレイトもジェネリクスにできる
+trait ConvertTo<Output> {
+    fn convert(&self) -> Output;
+}
+
+fn to<T>(i: i32) -> T
+where
+    // ConvertTo<T>と型パラメータがトレイト側にある
+    // where記法だとi32など具体的な型に制約がかける
+    i32: ConvertTo<T>,
+{
+    i.convert()
+}
+
+
 fn main() {
     // 値を用意する
     let point = (1.0, 1.0);
@@ -72,4 +120,16 @@ fn main() {
     // 同じくトレイとの関連関数を呼ぶ
     let p = PolarCoord::from_cartesian(c);
     println!("r = {}, 0 = {}", p.r, p.theta);
+
+    // Coordinatesを実装している型の値は渡せる
+    print_point((0.0, 1.0));        // (0, 1)
+    print_point(PolarCoord {
+        r: 1.0,
+        theta: std::f64::consts::PI / 2.0,
+    }); // (0.00000000000000006123233995736766, 1)
+
+    // しかしCoordinatesを実装していない型の値を引数に渡そうとするとコンパイルエラーになる
+    // print_point("string");
+
+
 }
