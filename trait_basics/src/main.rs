f232a410f46cff4c02293ae737172c6769f97a33
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-21 16:58:00 +0900

    存在impl Trait

--- trait_basics/src/main.rs
+++ trait_basics/src/main.rs
@@ -233,6 +233,41 @@ impl Overload1<char> for i32 {
     }
 }
 
+fn to_n(n: i32) -> impl Iterator {
+    0..n
+}
+// // impl Traitを使わない場合は以下のような型になる
+// use std::ops::Range;
+// fn to_n(n: i32) -> Range<i32> {
+//     0..n
+// }
+
+use std::iter::Filter;
+use std::ops::Range;
+fn to_n_even(n: i32) -> Filter<Range<i32>, fn(&i32) -> bool> {
+    (0..n).filter(|i| i % 2 == 0)
+}
+
+// dyn Traitでトレイトオブジェクトにしている
+// fn gen_counter(init: i32) -> Box<dyn FnMut() -> i32> {
+//     let mut n = init;
+//     // トレイとオブジェクトを作っている
+//     Box::new(move || {
+//         let ret = n;
+//         n += 1;
+//         ret
+//     })
+// }
+fn gen_counter(init: i32) -> impl FnMut() -> i32 {
+    let mut n = init;
+    // impl Traitのおかげでトレイトオブジェクトを作る必要がなくなった
+    move || {
+        let ret = n;
+        n += 1;
+        ret
+    }
+}
+
 
 fn main() {
     // 値を用意する
