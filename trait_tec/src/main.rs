d8eadf4556a064717f4be72128474bc28b267e58
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-22 09:29:24 +0900

    列挙型を使った型の混合

--- trait_tec/src/main.rs
+++ trait_tec/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // fn take_string(_s: String) {}
 fn take_string(s: impl Into<String>) {
     let _s = s.into();
@@ -5,6 +7,25 @@ fn take_string(s: impl Into<String>) {
 
 // minとmaxをオプショナルにする
 fn range(min: impl Into<Option<usize>>, max: impl Into<Option<usize>>) { }
+
+#[derive(Debug)]
+enum Either<A, B> {
+    A(A),
+    B(B),
+}
+
+impl<A, B> fmt::Display for Either<A, B>
+where
+    A: fmt::Display,
+    B: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Either::A(a) => a.fmt(f),
+            Either::B(b) => b.fmt(f),
+        }
+    }
+}
 
 fn main() {
     // take_string("some_string");      // 文字列リテラルは渡せない
@@ -17,4 +38,15 @@ fn main() {
 
     // minには値を渡し、maxに渡さない場合
     range(1, None);
+
+    // Vec<Either<bool, i32>>として宣言しておく
+    let mut v: Vec<Either<bool, i32>> = vec![];
+    // Eitherの値を入れる
+    v.push(Either::A(true));
+    v.push(Either::B(1i32));
+
+    // すると `{}` で表示できる
+    for e in v {
+        println!("{}", e);
+    }
 }
