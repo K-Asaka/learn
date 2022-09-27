4c9c2dac269d79fe699bed3d1b0d58d81866f6bc
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-27 18:51:47 +0900

    テストの追加

--- wordcount/src/lib.rs
+++ wordcount/src/lib.rs
@@ -4,6 +4,7 @@
 use regex::Regex;
 use std::collections::HashMap;
 use std::io::BufRead;
+use std::io;
 
 /// [`count`](fn.count.html)で使うオプション
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -29,6 +30,21 @@ impl Default for CountOption {
 /// * [`CountOption::Char`](enum.CountOption.html#variant.Char): Unicodeの1文字ごと
 /// * [`CountOption::Word`](enum.CountOption.html#variant.Word): 正規表現 \w+ にマッチする単語ごと
 /// * [`CountOption::Line`](enum.CountOption.html#variant.Line): \n または \r\n で区切られた1行ごと
+/// 
+/// # Examples
+/// 入力中の単語の出現頻度を数える例
+/// 
+/// ```
+/// use std::io::Cursor;
+/// use wordcount::{count, CountOption};
+/// 
+/// let mut input = Cursor::new("aa bb cc bb");
+/// let freq = count(input, CountOption::Word);
+/// 
+/// assert_eq!(freq["aa"], 1);
+/// assert_eq!(freq["bb"], 2);
+/// assert_eq!(freq["cc"], 1);
+/// ```
 /// 
 /// # Panics
 /// 
@@ -57,4 +73,108 @@ pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize>
         }
     }
     freqs
+}
+
+
+#[test]
+fn word_count_works() {
+    use std::io::Cursor;
+
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("bb".to_string(), 2);
+    exp.insert("cc".to_string(), 1);
+
+    assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
+}
+
+#[test]
+// fn word_count_fails() {
+//     use std::io::Cursor;
+//     let mut exp = HashMap::new();
+//     exp.insert("aa".to_string(), 1);
+//     assert_eq!(count(Cursor::new("aa  cc dd"), CountOption::Word), exp);
+// }
+fn word_count_works2() {
+    use std::io::Cursor;
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("cc".to_string(), 1);
+    exp.insert("dd".to_string(), 1);
+    assert_eq!(count(Cursor::new("aa  cc dd"), CountOption::Word), exp);
+}
+
+#[test]
+fn assert_test() {
+    let a = 3;
+    let b = 27;
+    // 第2引数以後にフォーマットを指定する
+    assert!(a + b == 30, "a = {}, b = {}", a, b);
+}
+
+
+#[test]
+fn result_test() -> io::Result<()> {
+    use std::fs::{read_to_string, remove_file, write};
+    // ? 演算子を使う
+    write("test.txt", "message")?;
+    let message = read_to_string("test.txt")?;
+    remove_file("test.txt")?;
+    assert_eq!(message, "message");
+    Ok(())
+}
+
+// #[test]
+// fn result_err() -> io::Result<()> {
+//     use std::fs::remove_file;
+//     remove_file("no_such_file")
+// }
+
+#[test]
+#[should_panic]
+fn word_count_do_not_contain_unknown_words() {
+    use std::io::Cursor;
+
+    count(
+        Cursor::new([
+            b'a', // a
+            0xf0, 0x90, 0x80,   // でたらめなバイト列
+            0xe3, 0x81, 0x82,   // あ
+        ]),
+        CountOption::Word,
+    );
+}
+
+#[test]
+#[ignore]
+fn large_test() {
+    // ...
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+
+    // Mapの複数のKey-Valueペアに対してassertをするマクロを定義しておく
+    macro_rules! assert_map {
+        ($expr: expr, {$($key: expr => $value:expr), *}) => {
+            $(assert_eq!($expr[$key], $value));*
+        };
+    }
+
+    #[test]
+    fn word_count_works3() {
+        let freqs = count(Cursor::new("aa  cc dd"), CountOption::Word);
+
+        assert_eq!(freqs.len(), 3);
+        // 定義したマクロを使ってテストを書く
+        assert_map!(freqs, {"aa" => 1, "cc" => 1, "dd" => 1});
+        // 展開すると概ね以下のようなコードになる
+        // ```
+        // assert_eq!(freqs["aa"], 1);
+        // assert_eq!(freqs["cc"], 1);
+        // assert_eq!(freqs["dd"], 1);
+        // ```
+    }
 }
