4adc3caf4830f9467ddea5708a9beab34bc91358
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-09-29 10:46:18 +0900

    マルチーパート/CSVファイルの扱い

--- log-collector/server/src/handlers.rs
+++ log-collector/server/src/handlers.rs
@@ -6,15 +6,65 @@ use log::debug;
 use crate::Server;
 use crate::db;
 
+use actix_web::FutureResponse;
+use actix_web_multipart_file::{Multiparts, FormData};
+use diesel::pg::PgConnection;
+use futures::prelude::*;
+use itertools::Itertools;
+use std::io::{BufReader, Read};
+
 
 /// POST /csvのハンドラ
-pub fn handle_post_csv(server: State<Server>) -> Result<HttpResponse, Error> {
-    // POSTされたファイルはActix Webでは簡単には扱えないのでここではまだコードなし
+pub fn handle_post_csv(
+    server: State<Server>,
+    // actix_web_multipart_fileを使ってマルチパートのリクエストをファイルに保存する
+    multiparts: Multiparts,
+) -> FutureResponse<HttpResponse> {
+    // multipartsはStreamになっているのでそのままメソッドを繋げる
+    // Streamは非同期版イテレータのような存在
+    let fut = multiparts
+        .from_err()
+        // text/csvでなければスキップ
+        .filter(|field| field.content_type == "text/csv")
+        // ファイルでなければスキップ
+        .filter_map(|field| match field.form_data {
+            FormData::File { file, .. } => Some(file),
+            FormData::Data { .. } => None,
+        })
+        // 1ファイルずつ処理する
+        .and_then(move |file| load_file(&*server.pool.get()?, file))
+        // usizeのStream(イテレータのようなもの) -> それらの和
+        .fold(0, |acc, x| Ok::<_, Error>(acc + x))
+        .map(|sum| HttpResponse::Ok().json(api::csv::post::Response(sum)))
+        .from_err();
+    
+    Box::new(fut)
+}
 
-    // レスポンスはDefaultでダミーデータを生成
-    let logs = Default::default();
+fn load_file(conn: &PgConnection, file: impl Read) -> Result<usize, Error> {
+    use crate::model::NewLog;
 
-    Ok(HttpResponse::Ok().json(api::csv::post::Response(logs)))
+    let mut ret = 0;
+
+    // CSVファイルが渡されるcsv::Readerを用いてapi::Logにデコードしていく
+    let in_csv = BufReader::new(file);
+    let in_log = csv::Reader::from_reader(in_csv).into_deserialize::<::api::Log>();
+    // Itertoolsのchunksを用いて1000件ずつ処理する
+    for logs in &in_log.chunks(1000) {
+        let logs = logs
+            // Logとしてパースできた行だけ集める
+            .filter_map(Result::ok)
+            .map(|log| NewLog {
+                user_agent: log.user_agent,
+                response_time: log.response_time,
+                timestamp: log.timestamp.naive_utc(),
+            })
+            .collect_vec();
+        
+        let inserted = db::insert_logs(conn, &logs)?;
+        ret += inserted.len();
+    }
+    Ok(ret)
 }
 
 /// POST /logsのハンドラ
@@ -67,10 +117,23 @@ pub fn handle_get_csv(
     server: State<Server>,
     range: Query<api::csv::get::Query>,
 ) -> Result<HttpResponse, Error> {
-    debug!("{:?}", range);
+    use chrono::{DateTime, Utc};
+    
+    let conn = server.pool.get()?;
+    let logs = db::logs(&conn, range.from, range.until)?;
+    let v = Vec::new();
+    let mut w = csv::Writer::from_writer(v);
 
-    // CSVファイルはバイナリデータにして返す
-    let csv: Vec<u8> = vec![];
+    for log in logs.into_iter().map(|log| ::api::Log {
+        user_agent: log.user_agent,
+        response_time: log.response_time,
+        timestamp: DateTime::from_utc(log.timestamp, Utc),
+    }) {
+        w.serialize(log)?;
+    }
+
+    let csv = w.into_inner()?;
+    
     Ok(HttpResponse::Ok()
         .header("Content-Type", "text/csv")
         .body(csv))
