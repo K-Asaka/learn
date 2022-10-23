ae5e385d3788dd977a4cbfae7c7b51099b75b985
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2022-10-23 18:14:03 +0900

    部分的用を施した型シノニム

--- type.hs
+++ type.hs
@@ -3,3 +3,10 @@ type Age = Integer -- typeに続き左辺に新しい型名、右辺にもとも
 legalDrink :: Age -> Bool
 legalDrink age | age >= 20 = True
                | otherwise = False
+
+data AppErr = NewAppErr deriving Show
+type AppResult a = Either AppErr a
+
+safeHead :: [a] -> AppResult a
+safeHead [] = Left  NewAppErr
+safeHead xs = Right (head xs)
