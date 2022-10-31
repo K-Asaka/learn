4c7ea731dd14d852178cb6f6a8c589d8a143312a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-10-31 11:12:25 +0900

    Scalaアプリケーション

--- Summer.scala
+++ Summer.scala
@@ -6,3 +6,11 @@ object Summer {
             println(arg + ": " + calculate(arg))
     }
 }
+
+// scalac ChecksumAccumulator.scala Summer.scala
+// または
+// fsc ChecksumAccumulator.scala Summer.scala
+// fscはWindows版では確認できず
+
+// 実行
+// scala Summer of love
