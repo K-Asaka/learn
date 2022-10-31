6001c1862a512a9e24a99cb722fd11d1c683963d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-10-31 10:55:07 +0900

    シングルトンオブジェクト

--- ChecksumAccumulator.scala
+++ ChecksumAccumulator.scala
@@ -1,5 +1,25 @@
+import scala.collection.mutable
+
+// コンパニオンクラス
 class CheckSumAccumulator {
     private var sum = 0
     def add(b: Byte): Unit = { sum += b }
     def checksum(): Int = ~(sum & 0xFF) + 1
+}
+
+// シングルトンオブジェクト
+// コンパニオンオブジェクト
+object ChecksumAccumulator {
+    private val cache = mutable.Map.empty[String, Int]
+    def calculate(s: String): Int =
+        if (cache.contains(s))
+            cache(s)
+        else {
+            val acc = new ChecksumAccumulator2
+            for (c <- s)
+                acc.add(c.toByte)
+            val cs = acc.checksum()
+            cache += (s -> cs)
+            cs
+        }
 }
