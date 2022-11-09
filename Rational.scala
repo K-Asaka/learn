d3249c304bef06600f936256f3cd7f9b2b2bf0e4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-09 18:17:24 +0900

    privateフィールドとメソッド

--- Rational.scala
+++ Rational.scala
@@ -1,8 +1,14 @@
 class Rational(n: Int, d: Int) {
     require(d != 0)
-    val numer: Int = n
-    val denom: Int = d
-    override def toString = s"$n/$d"
+
+    private val g = gcd(n.abs, d.abs)
+    val numer: Int = n / g
+    val denom: Int = d / g
+
+    def this(n: Int) = this(n, 1)   // 補助コンストラクター
+
+    override def toString = s"$numer/$denom"
+
     def add(that: Rational): Rational = 
         new Rational(
             numer * that.denom + that.numer * denom,
@@ -12,5 +18,6 @@ class Rational(n: Int, d: Int) {
         this.numer * that.denom < that.numer * this.denom
     def max(that: Rational) =
         if (this.lessThan(that)) that else this
-    
+    private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
 }
