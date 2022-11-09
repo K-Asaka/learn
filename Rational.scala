649d2d373e6124a084f8080639448643118323b5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-09 18:32:05 +0900

    演算子の定義

--- Rational.scala
+++ Rational.scala
@@ -9,15 +9,19 @@ class Rational(n: Int, d: Int) {
 
     override def toString = s"$numer/$denom"
 
-    def add(that: Rational): Rational = 
+    def + (that: Rational): Rational = 
         new Rational(
             numer * that.denom + that.numer * denom,
             denom * that.denom
         )
+    def * (that: Rational): Rational = 
+        new Rational(numer * that.numer, denom * that.denom)
+
+    private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
+    
     def lessThan(that: Rational) = 
         this.numer * that.denom < that.numer * this.denom
     def max(that: Rational) =
         if (this.lessThan(that)) that else this
-    private def gcd(a: Int, b: Int): Int =
-        if (b == 0) a else gcd(b, a % b)
 }
