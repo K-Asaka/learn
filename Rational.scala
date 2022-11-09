f794756a58e0c1555488fc1734d54e59ecbd1d2a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-09 16:56:02 +0900

    自己参照

--- Rational.scala
+++ Rational.scala
@@ -8,4 +8,9 @@ class Rational(n: Int, d: Int) {
             numer * that.denom + that.numer * denom,
             denom * that.denom
         )
+    def lessThan(that: Rational) = 
+        this.numer * that.denom < that.numer * this.denom
+    def max(that: Rational) =
+        if (this.lessThan(that)) that else this
+    
 }
