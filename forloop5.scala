d3353d497db47706c61577064a80d2feefede7e2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-14 11:17:06 +0900

    for式内での中間結果による変数の束縛

---
+++ forloop5.scala
@@ -0,0 +1,20 @@
+object ForLoop4 {
+
+    def main(args: Array[String]): Unit = {
+        grep(".*gcd.*")
+    }
+
+    def fileLines(file: java.io.File) =
+        scala.io.Source.fromFile(file, "UTF-8").getLines().toArray
+    
+    def grep(pattern: String) = {
+        val filesHere = (new java.io.File(".")).listFiles
+        for {
+            file <- filesHere
+            if file.getName.endsWith(".scala")
+            line <- fileLines(file)
+            trimmed = line.trim
+            if trimmed.matches(pattern)
+        } println(s"$file: $trimmed")
+    }
+}
