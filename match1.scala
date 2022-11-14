b27180f9f47ef0071ebeb2310c5b00df0990f117
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-14 11:50:59 +0900

    副作用のあるmatch式

---
+++ match1.scala
@@ -0,0 +1,12 @@
+object Match1 {
+    def main(args: Array[String]): Unit = {
+        val firstArg = if (args.length > 0) args(0) else ""
+
+        firstArg match {
+            case "salt" => println("pepper")
+            case "chips" => println("salsa")
+            case "eggs" => println("bacon")
+            case _ => println("huh?")
+        }
+    }
+}
