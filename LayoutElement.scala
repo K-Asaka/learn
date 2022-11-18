4fbe37a2ed979a03492ef24e89757c8f9fdb48fd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-18 11:39:20 +0900

    privateクラスにより実装を隠蔽する

--- LayoutElement.scala
+++ LayoutElement.scala
@@ -1,13 +1,15 @@
+import Element.elem
+
 abstract class Element {
     def contents: Array[String]
     def height: Int = contents.length
     def width: Int = if (height == 0) 0 else contents(0).length
 
     def above(that: Element): Element =
-        new ArrayElement(this.contents ++ that.contents)
+        elem(this.contents ++ that.contents)
     
     def beside(that: Element): Element =
-        new ArrayElement(
+        elem(
             for (
                 (line1, line2) <- this.contents zip that.contents
             ) yield line1 + line2
@@ -16,21 +18,32 @@ abstract class Element {
     override def toString = contents mkString "\n"
 }
 
-class ArrayElement(
-    val contents: Array[String]
-) extends Element
+object Element {
+    private class ArrayElement(
+        val contents: Array[String]
+    ) extends Element
 
-class LineElement(s: String) extends Element {
-    val contents = Array(s)
-    override def width = s.length
-    override def height = 1
-}
+    private class LineElement(s: String) extends Element {
+        val contents = Array(s)
+        override def width = s.length
+        override def height = 1
+    }
 
-class UniformElement (
-    ch: Char,
-    override val width: Int,
-    override val height: Int
-) extends Element {
-    private val line = ch.toString * width
-    def contents = Array.fill(height)(line)
+    private class UniformElement (
+        ch: Char,
+        override val width: Int,
+        override val height: Int
+    ) extends Element {
+        private val line = ch.toString * width
+        def contents = Array.fill(height)(line)
+    }
+
+    def elem(contents: Array[String]): Element =
+        new ArrayElement(contents)
+    
+    def elem(chr: Char, width: Int, height: Int): Element =
+        new UniformElement(chr, width, height)
+    
+    def elem(line: String): Element =
+        new LineElement(line)
 }
