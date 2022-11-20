de87dca31ae1ec925e23325b1fc6b4ff3de3acd8
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2022-11-20 15:28:15 +0900

    State sモナドから値を取り出す

--- cardgame.hs
+++ cardgame.hs
@@ -1,3 +1,6 @@
+-- State sモナドを使う場合transformersパッケージが必要
+-- $ stack ghci --package transformers
+import Control.Monad.Trans.State
 import Data.List
 
 type Card = Int         -- カード
@@ -6,23 +9,31 @@ type Hand = [Card]      -- 手札
 type Stock = [Card]     -- 山札
 type Player = String    -- プレイヤー
 
-deck :: [Card]
-deck = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-       , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-       , 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
-       ]
+-- 山札から指定した枚数のカードを引く
+drawCards :: Int
+  -> State Stock Hand   -- 状態は山札、返り値は手札
+drawCards n = do
+  -- 状態である山札を取得する
+  deck <- get
+  -- 引いた残りを新たに山札に設定
+  put $ drop n deck
+  -- 引いたカードを返す
+  return $ take n deck
 
 -- ランダムな並び順のカード一式 deck :: [Card] があるとする
 -- 太郎、花子、たかし、ゆみの四人に5枚づつカードを配り、合計点が多い人が勝ち
 -- 返却値は合計点の多い順でソートされた(合計得点, 手札, プレイヤー名)のリスト
-game :: [Card] -> [(Score, Hand, Player)]
-game deck = let
-    (taroHand, deck2) = (take 5 deck, drop 5 deck)
-    (hanakoHand, deck3) = (take 5 deck2, drop 5 deck2)
-    (takashiHand, deck4) = (take 5 deck3, drop 5 deck3)
-    (yumiHand, deck5) = (take 5 deck4, drop 5 deck4)
+gameWithState
+  -- 状態は山札、返り値は(得点、手札、プレイヤー名)のリスト
+  :: State Stock [(Score, Hand, Player)]
+gameWithState = do
+  -- 四人分のカードを配る
+  taroHand <- drawCards 5
+  hanakoHand <- drawCards 5
+  takashiHand <- drawCards 5
+  yumiHand <- drawCards 5
   -- 逆順にソートすれば合計得点が高い順に並ぶ、sortにはData.Listが必要
-  in reverse . sort $
+  return . reverse . sort $
     [ (sum taroHand, taroHand, "Taro")
     , (sum hanakoHand, hanakoHand, "Hanako")
     , (sum takashiHand, takashiHand, "Takashi")
@@ -30,7 +41,6 @@ game deck = let
     ]
   -- さらにデッキにカードを戻す処理も実装する必要がある...
 
-
 main :: IO ()
-main = do
-    print $ game deck
+main = 
+  print $ runState gameWithState [1..50]
