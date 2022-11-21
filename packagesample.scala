41be9432748324eb7f1ad01e4e0abb5707f68155
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-21 14:07:26 +0900

    同一ファイル内の複数パッケージ

---
+++ packagesample.scala
@@ -0,0 +1,16 @@
+package bobsrockets {
+    package navigation {
+
+        // パッケージbobsrockets.navigation
+        class Navigator
+
+        def main(args: Array[String]) = {
+            println("Hello")
+        }
+
+        package tests {
+            // パッケージbobsrockets.navigation.tests
+            class NavigatorSuite
+        }
+    }
+}
