a65ff7428dab422228135a349aa31f31e2d55859
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-21 14:41:27 +0900

    見えなくなっているパッケージ名へのアクセス

---
+++ bobsrockets.scala
@@ -0,0 +1,24 @@
+package bobsrockets {
+    package navigation {
+        package launch {
+            class Booster1
+        }
+        class MissionControl {
+            val booster1 = new launch.Booster1
+            val booster2 = new bobsrockets.launch.Booster2
+            val booster3 = new _root_.launch.Booster3
+        }
+    }
+    package launch {
+        class Booster2
+    }
+
+    def main(args: Array[String]) = {
+        val mc = new navigation.MissionControl
+        println(mc)
+    }
+}
+
+package launch {
+    class Booster3
+}
