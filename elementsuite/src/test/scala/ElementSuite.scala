95145b5760e56af946bbc50363424a41dd654f9a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-22 12:02:00 +0900

    情報が豊富なエラーレポート

--- elementsuite/src/test/scala/ElementSuite.scala
+++ elementsuite/src/test/scala/ElementSuite.scala
@@ -5,5 +5,16 @@ class ElementSuite extends AnyFunSuite {
     test("elem result should have passed width") {
         val ele = elem('x', 2, 3)
         assert(ele.width == 2)
+        assertResult(2) {
+            ele.width
+        }
+        // assertThrows[IllegalArgumentException] {
+        //     elem('x', -2, 3)
+        // }
+        val caught =
+            intercept[ArithmeticException] {
+                1 / 0
+            }
+        assert(caught.getMessage == "/ by zero")
     }
 }
