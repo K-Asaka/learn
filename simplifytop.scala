1cee25530fad8c6b709011bb8a6153ae9de3b413
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-22 19:38:40 +0900

    ワイルドカードパターンを使ったパターンマッチ

--- simplifytop.scala
+++ simplifytop.scala
@@ -4,3 +4,15 @@ def simplifyTop(expr: Expr): Expr = expr match {
     case BinOp("*", e, Number(1)) => e  // 1の乗算は元のまま
     case _ => expr
 }
+
+def m1(expr: Expr): Unit = expr match {
+    case BinOp(op, left, right) =>
+        println(s"$expr is a binary operation")
+    case _ =>
+}
+
+def m2(expr: Expr): Unit = expr match {
+    case BinOp(_, _, _) =>
+        println(s"$expr is a binary operation")
+    case _ => println("It's something else")
+}
