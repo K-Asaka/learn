28a5ce78604fe328e237f8b30488e6f9c152cd4d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-22 19:46:43 +0900

    変数パターンを使ったパターンマッチ

--- casepattern.scala
+++ casepattern.scala
@@ -5,3 +5,8 @@ def describe(x: Any) = x match {
     case Nil => "the empty list"
     case _ => "something else"
 }
+
+def varmatch(expr: Any) = expr match {
+    case 0 => "zero"
+    case somethingElse => "not zero: " + somethingElse
+}
