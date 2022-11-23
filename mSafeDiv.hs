303136a96a0f993f06a9a8c2e706b2680d38f62b
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2022-11-23 19:36:52 +0900

    Either Stringに書き換え

--- mSafeDiv.hs
+++ mSafeDiv.hs
@@ -1,8 +1,8 @@
-safeDiv :: Integer -> Integer -> Maybe Integer
-safeDiv k n | n == 0    = Nothing
-            | otherwise = Just (k `div` n)
+safeDiv :: Integer -> Integer -> Either String Integer
+safeDiv k n | n == 0    = Left $ "Illegal division by zero. k:" ++ show k
+            | otherwise = Right (k `div` n)
 
-calc :: Integer -> Maybe Integer
+calc :: Integer -> Either String Integer
 calc n = do
     x <- 100 `safeDiv` n        -- 100を引数で割る
     y <- 100 `safeDiv` (x - 1)  -- 100をx - 1で割る
