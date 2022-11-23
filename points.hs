7e5405d7275cdd522ea5648a5386178d11e8bc7a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-23 23:02:11 +0900

    Alternative型クラスとしての[]

--- points.hs
+++ points.hs
@@ -1,5 +1,14 @@
+import Control.Monad (guard)
+
 points :: [(Integer, Integer)]
 points = do
     x <- [1..3]
     y <- [1..3]
+    return (x, y)
+
+orderedPoints :: [(Integer, Integer)]
+orderedPoints = do
+    x <- [1..3]
+    y <- [1..3]
+    guard (x < y)   -- ここで絞り込む
     return (x, y)
