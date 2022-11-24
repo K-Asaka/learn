ef967db4060fad3df678aa32ac7a2f9f02c471f4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-24 19:37:37 +0900

    変数か定数か

--- casepattern.scala
+++ casepattern.scala
@@ -10,3 +10,14 @@ def varmatch(expr: Any) = expr match {
     case 0 => "zero"
     case somethingElse => "not zero: " + somethingElse
 }
+
+import math.{E, Pi}
+def constmatch(E: Any) = E match {
+    case Pi => "strange math? Pi = " + Pi
+    case _  => "OK"
+}
+
+def constvarmatch(E: Any) = E match {
+    case pi => "strange math? Pi = " + pi
+    //case _  => "OK"
+}
