66071a370a278c6a023d30a1ce770684d188bf07
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-11-25 09:21:16 +0900

    コンストラクタパターンを使ったパターンマッチ

--- casepattern.scala
+++ casepattern.scala
@@ -1,3 +1,4 @@
+
 def describe(x: Any) = x match {
     case 5 => "five"
     case true => "truth"
@@ -20,4 +21,15 @@ def constmatch(E: Any) = E match {
 def constvarmatch(E: Any) = E match {
     case pi => "strange math? Pi = " + pi
     //case _  => "OK"
+}
+
+abstract class Expr
+case class Var(name: String) extends Expr
+case class Number(num: Double) extends Expr
+case class UnOp(operator: String, arg: Expr) extends Expr
+case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+def constructormatch(expr: Any) = expr match {
+    case BinOp("+", e, Number(0)) => println("a deep match")
+    case _ => 
 }
