70879c08a134d26f68e19207f6fb9cb9bc6448b3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-05 11:39:16 +0900

    リストの構築

--- list.scala
+++ list.scala
@@ -1,9 +1,6 @@
-val fruit: List[String] = List("apples", "oranges", "pears")
-val nums: List[Int] = List(1, 2, 3, 4)
-val diag3: List[List[Int]] = 
-    List(
-        List(1, 0, 0),
-        List(0, 1, 0),
-        List(0, 0, 1)
-    )
-val empty: List[Nothing] = List()
+val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
+val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+val diag3 = (1 :: (0 :: (0 :: Nil))) ::
+            (0 :: (1 :: (0 :: Nil))) ::
+            (0 :: (0 :: (1 :: Nil))) :: Nil
+val empty = Nil
