5204f3a54e3a057312bc856939c7aafd6d3d9009
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-06 11:55:07 +0900

    リストバッファー

--- collection.scala
+++ collection.scala
@@ -5,3 +5,11 @@ val fiveInts = new Array[Int](5)
 val fiveToOne = Array(5, 4, 3, 2, 1)
 fiveInts(0) = fiveToOne(4)
 fiveInts
+
+import scala.collection.mutable.ListBuffer
+val buf = new ListBuffer[Int]
+buf += 1
+buf += 2
+buf
+3 +=: buf
+buf.toList
