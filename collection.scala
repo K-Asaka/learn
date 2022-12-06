87d3b5c64f415ca10712524029006d604a178157
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-06 12:16:48 +0900

    集合の使い方

--- collection.scala
+++ collection.scala
@@ -28,3 +28,10 @@ hasUpperCase("e e cummings")
 
 import scala.collection.mutable
 val mutaSet = mutable.Set(1, 2, 3)
+
+val text = "See Spot run. Run, Spot. Run!"
+val wordsArray = text.split("[ !,.]+")
+val words = mutable.Set.empty[String]
+for (word <- wordsArray)
+    words += word.toLowerCase
+words
