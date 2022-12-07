6f735fcacc47cd6435d87b6fbfa871dbb8bc9bde
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-07 19:39:34 +0900

    privateクラスというもう1つの方法

--- funcqueue.scala
+++ funcqueue.scala
@@ -11,35 +11,63 @@ class SlowHeadQueue[T](smele: List[T]) {        // 効率が悪い
     def enqueue(x: T) = new SlowHeadQueue(x :: smele)
 }
 
-class Queue[T] private (
-    private val leading: List[T],
-    private val trailing: List[T]
-) {
-    def this() = this(Nil, Nil)
-    def this(elems: T*) = this(elems.toList, Nil)
-
-
-    private def mirror = 
-        if (leading.isEmpty) 
-            new Queue(trailing.reverse, Nil)
-        else
-            this
-    def head = mirror.leading.head
-
-    def tail = {
-        val q = mirror
-        new Queue(q.leading.tail, q.trailing)
-    }
-
-    def enqueue(x: T) =
-        new Queue(leading, x :: trailing)
+trait Queue[T] {
+    def head: T
+    def tail: Queue[T]
+    def enqueue(x: T): Queue[T]
 }
-
 object Queue {
     // 初期要素'xs'を使ってキューを構築する
-    def apply[T](xs: T*) = new Queue[T](xs.toList, Nil)
+    def apply[T](xs: T*): Queue[T] =
+        new QueueImpl[T](xs.toList, Nil)
+    
+    private class QueueImpl[T](
+        private val leading: List[T],
+        private val trailing: List[T]
+    ) extends Queue[T] {
+
+        def mirror = 
+            if (leading.isEmpty)
+                new QueueImpl(trailing.reverse, Nil)
+            else
+                this
+            
+        def head: T = mirror.leading.head
+
+        def tail: QueueImpl[T] = {
+            val q = mirror
+            new QueueImpl(q.leading.tail, q.trailing)
+        }
+
+        def enqueue(x: T) = 
+            new QueueImpl(leading, x :: trailing)
+    }
 }
 
-val q = new Queue(1, 2, 3)
-val q1 = q enqueue 4
-q
+// class Queue[T] private (
+//     private val leading: List[T],
+//     private val trailing: List[T]
+// ) {
+//     def this() = this(Nil, Nil)
+//     def this(elems: T*) = this(elems.toList, Nil)
+
+
+//     private def mirror = 
+//         if (leading.isEmpty) 
+//             new Queue(trailing.reverse, Nil)
+//         else
+//             this
+//     def head = mirror.leading.head
+
+//     def tail = {
+//         val q = mirror
+//         new Queue(q.leading.tail, q.trailing)
+//     }
+
+//     def enqueue(x: T) =
+//         new Queue(leading, x :: trailing)
+// }
+
+// val q = new Queue(1, 2, 3)
+// val q1 = q enqueue 4
+// q
