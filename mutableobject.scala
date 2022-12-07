05b636eb40dc5c67d7dbb3d47e6244dc17ff478a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-07 11:00:21 +0900

    対応するフィールドを持たないゲッター・セッターの定義

--- mutableobject.scala
+++ mutableobject.scala
@@ -37,3 +37,19 @@ class Time {
         m = x
     }
 }
+
+class Thermometer {
+    var celsius: Float = _
+
+    def fahrenheit = celsius * 9 / 5 + 32
+    def fahrenheit_= (f: Float) = {
+        celsius = (f - 32) * 5 / 9
+    }
+    override def toString = s"${fahrenheit}F/${celsius}C"
+}
+
+val t = new Thermometer
+t.celsius = 100
+t
+t.fahrenheit = -40
+t
