021bf6379cd718b9b438f35ef4480b09da9484b2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-07 09:25:33 +0900

    ミュータブルな銀行口座クラス

---
+++ mutableobject.scala
@@ -0,0 +1,22 @@
+val cs = List('a', 'b', 'c')
+
+class BankAccount {
+    private var bal: Int = 0
+
+    def balance: Int = bal
+    def deposit(amount: Int) = {
+        require(amount > 0)
+        bal += amount
+    }
+    def withdraw(amount: Int): Boolean =
+        if (amount > bal) false
+        else {
+            bal -= amount
+            true
+        }
+}
+
+val account = new BankAccount
+account deposit 100
+account withdraw 80
+account withdraw 80
