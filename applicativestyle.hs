fecb1a26e6466f83c5c47fb99aad311ba18eb95c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-07 23:35:09 +0900

    /で区切ったケースとパースエラーの情報

--- applicativestyle.hs
+++ applicativestyle.hs
@@ -1,17 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main(main) where
 
+import qualified Data.Text as T
 import Data.Attoparsec.Text hiding (take)
+import Control.Applicative
 
 data YMD = YMD Int Int Int deriving Show
+data HMS = HMS Int Int Int deriving Show
+
+-- digitは0から9までの数値を表す
+ymdParser :: Parser YMD
+ymdParser = YMD
+    <$> countRead 4 digit <* char '/' -- 4桁の数字をパースして/は捨てる
+    <*> countRead 2 digit <* char '/'
+    <*> countRead 2 digit
+
+hmsParser :: Parser HMS
+hmsParser = HMS
+    <$> countRead 2 digit <* char ':'
+    <*> countRead 2 digit <* char ':'
+    <*> countRead 2 digit
+
+dateTimeParser :: Parser(YMD, HMS)
+dateTimeParser = (,) <$> ymdParser <* char ' ' <*> hmsParser
 
 countRead :: Read a => Int -> Parser Char -> Parser a
 countRead i = fmap read . count i
 
--- digitは0から9までの数値を表す
-ymdParser :: Parser YMD
-ymdParser = YMD <$> countRead 4 digit <*> countRead 2 digit <*> countRead 2 digit
-
 main :: IO ()
 main = do
-    print $ parse ymdParser "20221128" `feed` ""
+    print $ parse dateTimeParser "2022/11/28hoge 12:00:00" `feed` ""
+    print $ parse dateTimeParser "2018/08/21 12:00:00hoge" `feed` ""
+    print $ parse (dateTimeParser <* endOfInput) "2018/08/21 12:00:00hoge" `feed` ""
