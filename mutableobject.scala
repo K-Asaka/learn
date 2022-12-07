783c689210cbf33c7b9e736ad47607134eb1da50
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-07 09:25:53 +0900

    ゲッター・セッターメソッドの直接定義

--- mutableobject.scala
+++ mutableobject.scala
@@ -20,3 +20,20 @@ val account = new BankAccount
 account deposit 100
 account withdraw 80
 account withdraw 80
+
+class Time {
+    private[this] var h = 12
+    private[this] var m = 0
+
+    def hour: Int = h
+    def hour_= (x: Int) = {
+        require(0 <= x && x < 24)
+        h = x
+    }
+
+    def minute: Int = m
+    def minute_= (x: Int) = {
+        require(0 <= x && x < 60)
+        m = x
+    }
+}
