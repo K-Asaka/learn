d586316dd816a6e8066806b9dcfae9cc4f625b6e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-12 19:30:35 +0900

    遅延評価valによるトレイトの初期化

--- abstract.scala
+++ abstract.scala
@@ -46,22 +46,41 @@ trait RationalTrait {
 
 val x = 2
 
-new {
+// new {
+//     val numerArg = 1 * x
+//     val denomArg = 2 * x
+// } with RationalTrait 
+
+// object twoThirds extends {
+//     val numerArg = 2
+//     val denomArg = 3
+// } with RationalTrait
+
+// class RationalClass(n: Int, d: Int) extends {
+//     val numerArg = n
+//     val denomArg = d 
+// } with RationalTrait {
+//     def + (that: RationalClass) = new RationalClass(
+//         numer * that.denom + that.numer * denom,
+//         denom * that.denom
+//     )
+// }
+
+trait LazyRationalTrait {
+    val numerArg: Int
+    val denomArg: Int
+    lazy val numer = numerArg / g
+    lazy val denom = denomArg / g
+    override def toString = s"$numer/$denom"
+    private lazy val g = {
+        require(denomArg != 0)
+        gcd(numerArg, denomArg)
+    }
+    private def gcd(a: Int, b: Int): Int = 
+        if (b == 0) a else gcd(b, a % b)
+}
+
+new LazyRationalTrait {
     val numerArg = 1 * x
     val denomArg = 2 * x
-} with RationalTrait 
-
-object twoThirds extends {
-    val numerArg = 2
-    val denomArg = 3
-} with RationalTrait
-
-class RationalClass(n: Int, d: Int) extends {
-    val numerArg = n
-    val denomArg = d 
-} with RationalTrait {
-    def + (that: RationalClass) = new RationalClass(
-        numer * that.denom + that.numer * denom,
-        denom * that.denom
-    )
 }
