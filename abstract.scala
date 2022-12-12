476cb63f616abd495ce1a69c0e773392c618a0a4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-12 14:09:56 +0900

    抽象valの初期化

--- abstract.scala
+++ abstract.scala
@@ -32,3 +32,12 @@ trait AbstractTime {
     var minute: Int
 }
 
+trait RationalTrait {
+    val numerArg: Int
+    val denomArg: Int
+}
+
+new RationalTrait {
+    val numerArg = 1
+    val denomArg = 2
+}
