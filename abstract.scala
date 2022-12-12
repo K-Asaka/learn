2dcde0284ff41b39ee7aa7080ce3483026c8475d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-12 14:04:01 +0900

    抽象valとパラメーターなしメソッドのオーバーライド

--- abstract.scala
+++ abstract.scala
@@ -11,3 +11,18 @@ class Concrete extends Abstract {
     val initial = "hi"
     var current = initial
 }
+
+abstract class Fruit {
+    val v: String       // valueの'v'
+    def m: String       // methodの'm'
+}
+
+abstract class Apple extends Fruit {
+    val v: String
+    val m: String       // OK: 'def'は'val'でオーバーライドしてよい
+}
+
+abstract class BadApple extends Fruit {
+    //def v: String       // エラー: 'val'は'def'でオーバーライドできない
+    def m: String
+}
