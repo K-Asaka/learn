f13af8793ba6236d91ae075435a0ff181f04b8ca
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-12 09:54:44 +0900

    下限境界が設定された型パラメーター

--- funcqueue.scala
+++ funcqueue.scala
@@ -11,39 +11,40 @@ class SlowHeadQueue[T](smele: List[T]) {        // 効率が悪い
     def enqueue(x: T) = new SlowHeadQueue(x :: smele)
 }
 
-trait Queue[+T] {
-    // [T]: 不変　[+T]: 共変　[-T]: 反変
-    def head: T
-    def tail: Queue[T]
-//    def enqueue(x: T): Queue[T]
-}
-object Queue {
-    // 初期要素'xs'を使ってキューを構築する
-    def apply[T](xs: T*): Queue[T] =
-        new QueueImpl[T](xs.toList, Nil)
+// trait Queue[+T] {
+//     // [T]: 不変　[+T]: 共変　[-T]: 反変
+//     def head: T
+//     def tail: Queue[T]
+//     def enqueue[U >: T](x: U): Queue[U]
+// }
+
+// object Queue {
+//     // 初期要素'xs'を使ってキューを構築する
+//     def apply[T](xs: T*): Queue[T] =
+//         new QueueImpl[T](xs.toList, Nil)
     
-    private class QueueImpl[T](
-        private val leading: List[T],
-        private val trailing: List[T]
-    ) extends Queue[T] {
+//     private class QueueImpl[T](
+//         private val leading: List[T],
+//         private val trailing: List[T]
+//     ) extends Queue[T] {
 
-        def mirror = 
-            if (leading.isEmpty)
-                new QueueImpl(trailing.reverse, Nil)
-            else
-                this
+//         def mirror = 
+//             if (leading.isEmpty)
+//                 new QueueImpl(trailing.reverse, Nil)
+//             else
+//                 this
             
-        def head: T = mirror.leading.head
+//         def head: T = mirror.leading.head
 
-        def tail: QueueImpl[T] = {
-            val q = mirror
-            new QueueImpl(q.leading.tail, q.trailing)
-        }
+//         def tail: QueueImpl[T] = {
+//             val q = mirror
+//             new QueueImpl(q.leading.tail, q.trailing)
+//         }
 
-        def enqueue(x: T) = 
-            new QueueImpl(leading, x :: trailing)
-    }
-}
+//         def enqueue(x: T) = 
+//             new QueueImpl(leading, x :: trailing)
+//     }
+// }
 
 // class Queue[T] private (
 //     private val leading: List[T],
@@ -73,12 +74,26 @@ object Queue {
 // val q1 = q enqueue 4
 // q
 
-class Cell[T](init: T) {
-    private[this] var current = init
-    def get = current
-    def set(x: T) = { current = x }
-}
+// class Cell[T](init: T) {
+//     private[this] var current = init
+//     def get = current
+//     def set(x: T) = { current = x }
+// }
 
-val a1 = Array("abc")
-//val a2: Array[Any] = a1
-val a2: Array[Object] = a1.asInstanceOf[Array[Object]]
+// val a1 = Array("abc")
+// //val a2: Array[Any] = a1
+// val a2: Array[Object] = a1.asInstanceOf[Array[Object]]
+
+// class StrangeIntQueue extends Queue[Int] {
+//     override def enqueue(x: Int) = {
+//         println(math.sqrt(x))
+//         super.enqueue(x)
+//     }
+// }
+
+class Queue[+T] (
+        private val leading: List[T],
+        private val trailing: List[T] ) {
+    def enqueue[U >: T](x: U) =
+        new Queue[U](leading, x :: trailing)
+}
