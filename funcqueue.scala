b6201aea98e6ab2754001aa6056da1ad0a1a3608
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-12 11:16:13 +0900

    最適化された関数型キュー

--- funcqueue.scala
+++ funcqueue.scala
@@ -91,13 +91,36 @@ class SlowHeadQueue[T](smele: List[T]) {        // 効率が悪い
 //     }
 // }
 
-class Queue[+T] (
-        private val leading: List[T],
-        private val trailing: List[T] ) {
-    def enqueue[U >: T](x: U) =
-        new Queue[U](leading, x :: trailing)
-}
+// class Queue[+T] (
+//         private val leading: List[T],
+//         private val trailing: List[T] ) {
+//     def enqueue[U >: T](x: U) =
+//         new Queue[U](leading, x :: trailing)
+// }
 
 trait OutputChannel[-T] {
     def write(x: T): Unit
+}
+
+class Queue[+T] private (
+    private[this] var leading: List[T],
+    private[this] var trailing: List[T]
+) {
+    private def mirror() =
+        if (leading.isEmpty) {
+            while (!trailing.isEmpty) {
+                leading = trailing.head :: leading
+                trailing = trailing.tail
+            }
+        }
+    def head: T = {
+        mirror()
+        leading.head
+    }
+    def tail: Queue[T] = {
+        mirror()
+        new Queue(leading.tail, trailing)
+    }
+    def enqueue[U >: T](x: U) =
+        new Queue[U](leading, x :: trailing)
 }
