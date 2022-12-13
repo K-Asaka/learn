3bbd32f7ffcaa047e35f46571c83b72710574f4c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-13 19:26:00 +0900

    パス依存型

--- BuggyAnimals.scala
+++ BuggyAnimals.scala
@@ -10,6 +10,18 @@ class Cow extends Animal {
     override def eat(food: Grass) = {}  // コンパイルできない
 }
 
-class Fish extends Food
-val bessy: Animal = new Cow
-bessy eat(new Fish)
+// class Fish extends Food
+// val bessy: Animal = new Cow
+// bessy eat(new Fish)
+
+class DogFood extends Food
+class Dog extends Animal {
+    type SuitableFood = DogFood
+    override def eat(food: DogFood) = {}
+}
+
+val bessy = new Cow
+val lassie = new Dog
+// lassie eat (new bessy.SuitableFood)   // 互換性がないためエラー
+val bootsie = new Dog
+lassie eat (new bootsie.SuitableFood)
