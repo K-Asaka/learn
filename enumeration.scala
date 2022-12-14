f6af5faa8a508770b30c53196b0a5a5d61a3d85d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-14 09:21:05 +0900

    列挙

---
+++ enumeration.scala
@@ -0,0 +1,11 @@
+object Color extends Enumeration {
+    // val Red = Value
+    // val Green = Value
+    // val Blue = Value
+    // 以下のように書いても上記と同じ
+    val Red, Gree, Blue = Value
+}
+
+object Direction extends Enumeration {
+    val North, East, South, West = Value
+}
