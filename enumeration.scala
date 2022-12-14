c7e47778a41cba4d9206b8ef9e22c5056be218bd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-14 09:27:10 +0900

    列挙を使った例

--- enumeration.scala
+++ enumeration.scala
@@ -7,5 +7,13 @@ object Color extends Enumeration {
 }
 
 object Direction extends Enumeration {
-    val North, East, South, West = Value
+    // val North, East, South, West = Value
+    val North = Value("North")
+    val East = Value("East")
+    val South = Value("South")
+    val West = Value("West")
 }
+
+for (d <- Direction.values) println(d)
+Direction.East.id
+Direction(1)
