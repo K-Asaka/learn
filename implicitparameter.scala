4dd04025088d1dc2ac5fee945a791a948d2ccb49
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-14 19:18:04 +0900

    複数のパラメータを持つ暗黙のパラメーターリスト

--- implicitparameter.scala
+++ implicitparameter.scala
@@ -1,8 +1,12 @@
 class PreferredPrompt(val preference: String)
+class PreferredDrink(val preference: String)
 
 object Greeter {
-    def greet(name: String)(implicit prompt: PreferredPrompt) = {
+    def greet(name: String)(implicit prompt: PreferredPrompt,
+            drink: PreferredDrink) = {
         println("Welcome, " + name + ". The system is ready.")
+        print("But while you work, ")
+        println("why not enjoy a cup of " + drink.preference + "?")
         println(prompt.preference)
     }
 }
@@ -12,7 +16,8 @@ Greeter.greet("Bob")(bobsPrompt)
 
 object JoesPrefs {
     implicit val prompt = new PreferredPrompt("Yes, master> ")
+    implicit val drink = new PreferredDrink("tea")
 }
 
 import JoesPrefs._
-Greeter.greet("Joe")
+Greeter.greet("Joe")(prompt, drink)
