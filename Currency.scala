15a70143a6530c8a4e0edb2fd46d8f56578e3d0c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-14 10:07:12 +0900

    通貨圏オブジェクトの生成

--- Currency.scala
+++ Currency.scala
@@ -24,12 +24,18 @@ abstract class CurrencyZone {
     abstract class AbstractCurrency {
         val amount: Long
         def designation: String
-        override def toString = s"$amount $designation"
+        override def toString =
+            ((amount.toDouble / CurrencyUnit.amount.toDouble)
+            formatted ("%." + decimals(CurrencyUnit.amount) + "f")
+            + " " + designation)
         def + (that: Currency): Currency = 
             make(this.amount + that.amount)
         def * (x: Double): Currency =
             make((this.amount * x).toLong)
+        private def decimals(n: Long): Int =
+            if (n == 1) 0 else 1 + decimals(n / 10)
     }
+    val CurrencyUnit: Currency
 }
 
 // new Currency {
@@ -51,5 +57,35 @@ object US extends CurrencyZone {
         def designation = "USD"
     }
     type Currency = Dollar
-    def make(x: Long) = new Dollar { val amount = x }
+    def make(cents: Long) = new Dollar {
+        val amount = cents
+    }
+    val Cent = make(1)
+    val Dollar = make(100)
+    val CurrencyUnit = Dollar
+}
+
+object Europe extends CurrencyZone {
+    abstract class Euro extends AbstractCurrency {
+        def designation = "EUR"
+    }
+    type Currency = Euro
+    def make(cents: Long) = new Euro {
+        val amount = cents
+    }
+    val Cent = make(1)
+    val Euro = make(100)
+    val CurrencyUnit = Euro
+}
+
+object Japan extends CurrencyZone {
+    abstract class Yen extends AbstractCurrency {
+        def designation = "JPY"
+    }
+    type Currency = Yen
+    def make(yen: Long) = new Yen {
+        val amount = yen
+    }
+    val Yen = make(1)
+    val CurrencyUnit = Yen
 }
