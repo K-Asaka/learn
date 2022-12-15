a52ec452ea95d78fe7fbad36e70fba3ef1c2a4b5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-15 09:33:43 +0900

    スーパー型要素のサブ型リストへの挿入

--- listclass.scala
+++ listclass.scala
@@ -1,2 +1,9 @@
 val xs = List(1, 2, 3)
 var ys: List[Any] = List(1, 2, 3)
+
+abstract class Fruit
+class Apple extends Fruit
+class Orange extends Fruit
+
+val apples = new Apple :: Nil
+val fruits = new Orange :: apples
