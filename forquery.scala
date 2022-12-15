6835f665999a04489b7c95634f8d76986339c621
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-16 08:56:09 +0900

    逆方向への変換

--- forquery.scala
+++ forquery.scala
@@ -42,3 +42,15 @@ def removeDuplicates[A](xs: List[A]): List[A] = {
         )
 }
 removeDuplicates(res)
+
+
+object Demo {
+    def map[A, B](xs: List[A], f: A => B): List[B] =
+        for (x <- xs) yield f(x)
+    
+    def flatMap[A, B](xs: List[A], f: A => List[B]): List[B] =
+        for (x <- xs; y <- f(x)) yield y
+    
+    def filter[A](xs: List[A], p: A => Boolean): List[A] =
+        for (x <- xs if p(x)) yield x
+}
