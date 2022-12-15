d3b2910532d9dacd660706bd9796d461d3cab7bd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-15 10:39:29 +0900

    for式

--- forexpre.scala
+++ forexpre.scala
@@ -13,3 +13,14 @@ persons withFilter (p => !p.isMale) flatMap (p =>
 
 for (p <- persons; if !p.isMale; c <- p.children)
     yield (p.name, c.name)
+
+
+for (p <- persons; n = p.name; if (n startsWith "To"))
+yield n
+for {
+    p <- persons            // ジェネレーター
+    n = p.name              // 定義
+    if (n startsWith "To")  // フィルター
+} yield n
+for (x <- List(1, 2); y <- List("one", "two"))
+    yield (x, y)
