bbad952dedf95f8e46347d895611a75d35f5dabd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-15 09:40:41 +0900

    ListBufferクラス

--- listclass.scala
+++ listclass.scala
@@ -7,3 +7,17 @@ class Orange extends Fruit
 
 val apples = new Apple :: Nil
 val fruits = new Orange :: apples
+
+def incAll(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case x :: xs1 => x + 1 :: incAll(xs1)
+}
+
+var result = List[Int]()    // 非常に効率の悪いアプローチ
+for (x <- xs) result = result ::: List(x + 1)
+result
+
+import scala.collection.mutable.ListBuffer
+val buf = new ListBuffer[Int]
+for (x <- xs) buf += x + 1
+buf.toList
