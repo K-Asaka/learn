a6ba55a8042e0c75dde3b0fa337ced6f16e60684
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-19 09:45:06 +0900

    ベクトル

--- collection3.scala
+++ collection3.scala
@@ -6,3 +6,12 @@ def fibFrom(a: Int, b: Int): LazyList[Int] =
 val fibs = fibFrom(1, 1).take(7)
 fibs.toList
 
+val vec = scala.collection.immutable.Vector.empty
+val vec2 = vec :+ 1 :+ 2
+val vec3 = 100 +: vec2
+vec3(0)
+val vec4 = Vector(1, 2, 3)
+vec4 updated (2, 4)
+vec4
+
+collection.immutable.IndexedSeq(1, 2, 3)
