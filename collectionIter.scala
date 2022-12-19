ec81f12f9222fa142751969599f6e44464d22cbd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-19 18:47:11 +0900

    バッファー付きイテレーター

--- collectionIter.scala
+++ collectionIter.scala
@@ -9,3 +9,17 @@ it2 dropWhile (_.length < 2)
 it2.next()
 
 val (itt1, itt2) = it.duplicate
+
+// これでは動作しない
+// def skipEmptyWordsNOT(it: Iterator[String]) = {
+//     while (it.next().isEmpty) {}
+// }
+def skipEmptyWords(it: BufferedIterator[String]) = {
+    while (it.head.isEmpty) { it.next() }
+}
+
+val it3 = Iterator(1, 2, 3, 4)
+val bit = it3.buffered
+bit.head
+bit.next()
+bit.next()
