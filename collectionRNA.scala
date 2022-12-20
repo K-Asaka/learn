b59c58266921905063c72b26a59f4de6d0d27881
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-20 11:14:38 +0900

    RNA鎖クラス:Ver.2

--- collectionRNA.scala
+++ collectionRNA.scala
@@ -1,4 +1,3 @@
-import java.util.Base64
 abstract class Base
 case object A extends Base
 case object U extends Base
@@ -10,14 +9,14 @@ object Base {
     val toInt: Base => Int = Map(A -> 0, U -> 1, G -> 2, C -> 3)
 }
 
-import collection.mutable
-import collection.immutable.{IndexedSeq, IndexedSeqOps}
+import scala.collection.{View, mutable}
+import scala.collection.immutable.{IndexedSeq, IndexedSeqOps}
 
-final class RNA1 private (val groups: Array[Int], val length: Int)
+final class RNA2 private (val groups: Array[Int], val length: Int)
         extends IndexedSeq[Base]
-        with IndexedSeqOps[Base, IndexedSeq, RNA1] {
+        with IndexedSeqOps[Base, IndexedSeq, RNA2] {
 
-    import RNA1._
+    import RNA2._
 
     def apply(idx: Int): Base = {
         if (idx < 0 || length <= idx)
@@ -25,15 +24,42 @@ final class RNA1 private (val groups: Array[Int], val length: Int)
         Base.fromInt(groups(idx / N) >> (idx % N * S) & M)
     }
 
-    override def className = "RNA1"
+    override def className = "RNA2"
 
-    override protected def fromSpecific(source: IterableOnce[Base]): RNA1 =
+    override protected def fromSpecific(source: IterableOnce[Base]): RNA2 =
         fromSeq(source.iterator.toSeq)
-    override protected def newSpecificBuilder: mutable.Builder[Base, RNA1] =
+    override protected def newSpecificBuilder: mutable.Builder[Base, RNA2] =
         iterableFactory.newBuilder[Base].mapResult(fromSeq)
-    override def empty: RNA1 = fromSeq(Seq.empty)
+    override def empty: RNA2 = fromSeq(Seq.empty)
+
+    // RNA2を返すための多重定義メソッド
+    def appended(base: Base): RNA2 = fromSpecific(
+        new View.Appended(this, base))
+    
+    def appendedAll(suffix: IterableOnce[Base]): RNA2 =
+        concat(suffix)
+    
+    def concat(suffix: IterableOnce[Base]): RNA2 =
+        fromSpecific(this.iterator ++ suffix.iterator)
+    
+    def flatMap(f: Base => IterableOnce[Base]): RNA2 =
+        fromSpecific(new View.FlatMap(this, f))
+    
+    def map(f: Base => Base): RNA2 =
+        fromSpecific(new View.Map(this, f))
+
+    def prepended(base: Base): RNA2 = fromSpecific(
+        new View.Prepended(base, this))
+    
+    def prependedAll(prefix: IterableOnce[Base]): RNA2 =
+        fromSpecific(prefix.iterator ++ this.iterator)
+    
+    // concatの記号による別名
+    @inline final def ++ (suffix: IterableOnce[Base]): RNA2 =
+        concat(suffix)
 }
-object RNA1 {
+
+object RNA2 {
     // 塩基識別情報を表現するために必要なビット数
     private val S = 2
     // Intに収まる塩基識別情報の数
@@ -41,18 +67,25 @@ object RNA1 {
     // 塩基識別情報を分離するビットマスク
     private val M = (1 << S) - 1
 
-    def fromSeq(buf: collection.Seq[Base]): RNA1 = {
+    def fromSeq(buf: collection.Seq[Base]): RNA2 = {
         val groups = new Array[Int]((buf.length + N - 1) / N)
         for (i <- 0 until buf.length)
             groups(i / N) |= Base.toInt(buf(i)) << (i % N * S)
-        new RNA1(groups, buf.length)
+        new RNA2(groups, buf.length)
     }
     def apply(bases: Base*) = fromSeq(bases)
 }
 
-RNA1.fromSeq(List(A, G, U, A))
-val rna1 = RNA1(A, U, G, G, C)
+RNA2.fromSeq(List(A, G, U, A))
+val rna1 = RNA2(A, U, G, G, C)
 rna1.take(3)
 rna1.filter(_ != U)
 rna1.map(base => base)
 rna1 ++ rna1
+
+val rna2 = RNA2(A, U, G, G, C)
+rna1.map(base => base)
+rna1 ++ rna1
+
+val bases: Iterable[Base] = List(A, U, C, C)
+// bases.to(RNA2)  // エラー
