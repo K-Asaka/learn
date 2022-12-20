237ed9a4a46fb339ed9a1ba806da1893463999d3
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-20 10:23:03 +0900

    サイズ上限付きコレクション:Ver.2

--- collectionCapped.scala
+++ collectionCapped.scala
@@ -1,11 +1,12 @@
 import scala.collection._
 
-class Capped1[A] private (val capacity: Int, val length: Int, offset: Int, elems: Array[Any])
-        extends immutable.Iterable[A] { self =>
+class Capped2[A] private (val capacity: Int, val length: Int, offset: Int, elems: Array[Any])
+        extends immutable.Iterable[A]
+        with IterableOps[A, Capped2, Capped2[A]] { self =>
 
     def this(capacity: Int) = this(capacity, length = 0, offset = 0, elems = Array.ofDim(capacity))
 
-    def appended[B >: A](elem: B): Capped1[B] = {
+    def appended[B >: A](elem: B): Capped2[B] = {
         val newElems = Array.ofDim[Any](capacity)
         Array.copy(elems, 0, newElems, 0, capacity)
         val (newOffset, newLength) =
@@ -16,9 +17,9 @@ class Capped1[A] private (val capacity: Int, val length: Int, offset: Int, elems
                 newElems(length) = elem
                 (offset, length + 1)
             }
-        new Capped1[B](capacity, newLength, newOffset, newElems)
+        new Capped2[B](capacity, newLength, newOffset, newElems)
     }
-    @inline def :+ [B >: A](elem: B): Capped1[B] = appended(elem)
+    @inline def :+ [B >: A](elem: B): Capped2[B] = appended(elem)
 
     def apply(i: Int): A =
         elems((i + offset) % capacity).asInstanceOf[A]
@@ -32,12 +33,39 @@ class Capped1[A] private (val capacity: Int, val length: Int, offset: Int, elems
             elem
         }
     }
-    override def className = "Capped1"
+    override def className = "Capped2"
+
+    override val iterableFactory: IterableFactory[Capped2] =
+        new Capped2Factory(capacity)
+    
+    override protected def fromSpecific(coll: IterableOnce[A]): Capped2[A] =
+        iterableFactory.from(coll)
+    
+    override protected def newSpecificBuilder: mutable.Builder[A, Capped2[A]] =
+        iterableFactory.newBuilder
+    
+    override def empty: Capped2[A] = iterableFactory.empty
+}
+class Capped2Factory(capacity: Int)
+        extends IterableFactory[Capped2] {
+    
+    def from[A](source: IterableOnce[A]): Capped2[A] =
+        (newBuilder[A] ++= source).result()
+    
+    def empty[A]: Capped2[A] = new Capped2[A](capacity)
+
+    def newBuilder[A]: mutable.Builder[A, Capped2[A]] =
+        new mutable.ImmutableBuilder[A, Capped2[A]](empty) {
+            def addOne(elem: A): this.type = {
+            elems = elems :+ elem
+            this
+        }
+    }
 }
 
-val x1 = new Capped1(capacity = 4)
-val x2 = x1 :+ 1 :+ 2 :+ 3
-x2.length
-x2.lastOption
-val x3 = x2 :+ 4 :+ 5 :+ 6
-x3.take(3)
+object Capped extends Capped2Factory(capacity = 4)
+val x = Capped(1, 2, 3)
+x.take(2)
+x.filter(x => x % 2 == 1)
+x.map(x => x * x)
+List(1, 2, 3, 4, 5).to(Capped)
