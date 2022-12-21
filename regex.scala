6639106ac1e49214d977ad6a06d0fa0b59c40a22
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 11:48:09 +0900

    正規表現による抽出

--- regex.scala
+++ regex.scala
@@ -9,3 +9,8 @@ val input = "for -1.0 to 99 by 3"
 for (s <- Decimal findAllIn input)
 Decimal findFirstIn input
 Decimal findPrefixOf input
+
+val Decimal(sign, integerpart, decimalpart) = "-1.23"
+val Decimal(sign, integerpart, decimalpart) = "1.0"
+for (Decimal(s, i, d) <- Decimal findAllIn input)
+    println("sign: " + s + ", integer: " + i + ", decimal: " + d)
