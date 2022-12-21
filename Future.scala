9869e1311d3ea4db368b8d54b3582e6a8a9e83a0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 19:28:44 +0900

    非同期実行とTry。実行確認

--- Future.scala
+++ Future.scala
@@ -4,3 +4,14 @@ import scala.concurrent.Future
 
 import scala.concurrent.ExecutionContext.Implicits.global
 val fut = Future { Thread.sleep(10000); 21 + 21 }
+
+fut.isCompleted
+fut.value
+// 10秒経ってから実行する
+fut.isCompleted
+fut.value
+
+val fut2 = Future { Thread.sleep(10000); 21 / 0 }
+fut2.value
+// 10秒経ってから実行する
+fut2.value
