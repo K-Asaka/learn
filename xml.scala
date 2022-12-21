df992b9f7d47092ea17a56126a9dafbebac47526
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 13:24:25 +0900

    for式との組み合わせ(空白を考慮しない場合)

--- xml.scala
+++ xml.scala
@@ -42,3 +42,29 @@ proc(<c>cherry</c>)
 // @パターンを使って_*の結果値でcontents変数を束縛するとマッチする
 proc(<a>a <em>red</em> apple</a>)
 proc(<a/>)
+
+val catalog =
+    <catalog>
+      <cctherm>
+        <description>hot dog #5</description>
+        <yearMade>1952</yearMade>
+        <dateObtained>March 14, 2006</dateObtained>
+        <bookPrice>2199</bookPrice>
+        <purchasePrice>500</purchasePrice>
+        <condition>9</condition>
+      </cctherm>
+      <cctherm>
+        <description>Sprite Boy</description>
+        <yearMade>1964</yearMade>
+        <dateObtained>April 28, 2003</dateObtained>
+        <bookPrice>1695</bookPrice>
+        <purchasePrice>595</purchasePrice>
+        <condition>5</condition>
+      </cctherm>
+    </catalog>
+
+catalog match {
+    case <catalog>{therms @ _*}</catalog> =>
+        for (therm <- therms)
+            println("processing: " + (therm \ "description").text)
+}
