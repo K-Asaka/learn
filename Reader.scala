6e7f75dff48a2b0a9128d12b7888a780e4bec6d7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 18:36:27 +0900

    投げられた例外

---
+++ Reader.scala
@@ -0,0 +1,11 @@
+import java.io._
+class Reader(fname: String) {
+    private val in = 
+        new BufferedReader(new FileReader(fname))
+    
+    @throws(classOf[IOException])
+    def read() = in.read()
+}
+
+// $ scalac Reader.scala
+// $ javap Reader
