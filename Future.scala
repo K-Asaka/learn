3671b7094ce68cdd286d4ca8b84bbbdbdafdc5b2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-22 08:45:57 +0900

    for式によるFutureの変換

--- Future.scala
+++ Future.scala
@@ -11,12 +11,21 @@ fut.value
 fut.isCompleted
 fut.value
 
-val fut2 = Future { Thread.sleep(10000); 21 / 0 }
-fut2.value
+val fut_try = Future { Thread.sleep(10000); 21 / 0 }
+fut_try.value
 // 10秒経ってから実行する
-fut2.value
+fut_try.value
 
 val result = fut.map(x => x + 1)
 result.value
 // 10秒経ってから実行する
 result.value
+
+
+val fut1 = Future { Thread.sleep(10000); 21 + 21 }
+val fut2 = Future { Thread.sleep(10000); 23 + 23 }
+val fut_result = for {
+    x <- fut1
+    y <- fut2
+} yield x + y
+fut_result.value
