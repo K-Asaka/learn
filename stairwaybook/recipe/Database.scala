a8ab52803c25cee868687c297a3f2842569c26c1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 14:00:21 +0900

    抽象メソッドを持つDatabaseクラス

---
+++ stairwaybook/recipe/Database.scala
@@ -0,0 +1,12 @@
+package org.stairwaybook.recipe
+
+abstract class Database {
+    def allFoods: List[Food]
+    def allRecipes: List[Recipe]
+
+    def foodNamed(name: String) =
+        allFoods.find(f => f.name == name)
+    
+    case class FoodCategory(name: String, foods: List[Food])
+    def allCategories = List[FoodCategory]
+}
