eb13b3cefdb6a59cd72c34573abc233cd6fdd185
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 17:27:01 +0900

    ミュータブルなフィールドを使ってequalsを定義するとおかしな挙動になる

--- equals.scala
+++ equals.scala
@@ -1,4 +1,4 @@
-class Point(val x: Int, val y: Int) {
+class Point(var x: Int, var y: Int) {   // 問題あり
     override def hashCode = (x, y).##
     override def equals(other: Any) = other match {
         case that: Point => this.x == that.x && this.y == that.y
@@ -19,3 +19,13 @@ val p2a: Any = p2
 p1 equals p2a
 
 collection.mutable.HashSet(p1) contains p2
+
+val p = new Point(1, 2)
+val coll2 = collection.mutable.HashSet(p)
+coll2 contains p
+
+p.x += 1
+
+coll2 contains p
+
+coll2.iterator contains p
