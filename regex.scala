65c543f0244f2b05b095b75ecfba99f82374153b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 11:45:08 +0900

    正規表現による探索

--- regex.scala
+++ regex.scala
@@ -1,4 +1,11 @@
 import scala.util.matching.Regex
 
-val Decimal = new Regex("(-)?(\\d+)(\\.\\d*)?")
-val Decimal2 = """(-)?(\d+)(\.\d*)?""".r
+// val Decimal = new Regex("(-)?(\\d+)(\\.\\d*)?")
+// val Decimal2 = """(-)?(\d+)(\.\d*)?""".r
+
+val Decimal = """(-)?(\d+)(\.\d*)?""".r
+
+val input = "for -1.0 to 99 by 3"
+for (s <- Decimal findAllIn input)
+Decimal findFirstIn input
+Decimal findPrefixOf input
