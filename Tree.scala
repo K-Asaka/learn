c6611fac73d6cc5eccfd06639ea23c876eb2714c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 18:17:42 +0900

    equalsとhashCodeが定義されたパラメータ化された型クラス

--- Tree.scala
+++ Tree.scala
@@ -13,17 +13,24 @@ object EmptyTree extends Tree[Nothing] {
         throw new NoSuchElementException("EmptyTree.right")
 }
 
-class Branch[+T] (
+class Branch[T] (
     val elem: T,
     val left: Tree[T],
     val right: Tree[T]
 ) extends Tree[T] {
     override def equals(other: Any) = other match {
-        case that: Branch[T] => this.elem == that.elem &&
+        case that: Branch[_] => (that canEqual this) &&
+                                this.elem == that.elem &&
                                 this.left == that.left &&
                                 this.right == that.right
         case _ => false
     }
+    // def canEqual(other: Any) = other match {
+    //     case that: Branch[_] => true
+    //     case _ => false
+    // }
+    def canEqual(other: Any) = other.isInstanceOf[Branch[_]]
+    override def hashCode: Int = (elem, left, right).##
 }
 
 val b1 = new Branch[List[String]](Nil, EmptyTree, EmptyTree)
