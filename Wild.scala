6ea7dc1a9dd495a24b269290bcd39c805a020882
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 19:06:12 +0900

    ワイルドカード型

--- Wild.scala
+++ Wild.scala
@@ -2,7 +2,24 @@ val contents = (new Wild).contents
 contents.size()
 
 import scala.collection.mutable
-val iter = (new Wild).contents.iterator
-val set = mutable.Set.empty[???]        // ここにどの型を指定すればよいのか？
-while (iter.hasMore)
-    set += iter.next()
+import java.util.Collection
+
+abstract class SetAndType {
+    type Elem
+    val set: mutable.Set[Elem]
+}
+
+def javaSet2ScalaSet[T](jset: Collection[T]): SetAndType = {
+    val sset = mutable.Set.empty[T]  // Tという名前で呼べるようになった
+
+    val iter = jset.iterator
+    while (iter.hasNext)
+        sset += iter.next()
+    
+    new SetAndType {
+        type Elem = T
+        val set = sset
+    }
+}
+
+
