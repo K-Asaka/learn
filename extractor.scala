73c8ffa56e63f035f4eaa29be9219357c7099e84
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 09:18:53 +0900

    抽出子。メールアドレスの抽出

---
+++ extractor.scala
@@ -0,0 +1,10 @@
+object EMail extends ((String, String) => String) {
+    // 注入メソッド(オプション)
+    def apply (user: String, domain: String) = user + "@" + domain
+
+    // 抽出メソッド(必須)
+    def unapply(str: String): Option[(String, String)] = {
+        val parts = str split "@"
+        if (parts.length == 2) Some(parts(0), parts(1)) else None
+    }
+}
