bb53231a80909f35f0e5754f6ac51b03d6ddb80b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 14:25:41 +0900

    自分型を指定したSimpleRecipes

--- stairwaybook/recipe/SimpleRecipes.scala
+++ stairwaybook/recipe/SimpleRecipes.scala
@@ -1,9 +1,11 @@
 package org.stairwaybook.recipe
 
-trait SimpleRecipes {           // コンパイルできない
+trait SimpleRecipes {
+    this: SimpleFoods => 
+
     object FruitSalad extends Recipe(
         "fruit salad",
-        List(Apple, Pear),      // スコープが…
+        List(Apple, Pear),      // これでPearはスコープに入る
         "Mix it all together."
     )
     def allRecipes = List(FruitSalad)
