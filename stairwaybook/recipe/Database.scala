7be3dd366d19cd8d260166e62fb2dd7fd0b5090e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 14:17:59 +0900

    FoodCategoriesトレイトをミックスインするDatabaseクラス

--- stairwaybook/recipe/Database.scala
+++ stairwaybook/recipe/Database.scala
@@ -1,12 +1,9 @@
 package org.stairwaybook.recipe
 
-abstract class Database {
+abstract class Database extends FoodCategories {
     def allFoods: List[Food]
     def allRecipes: List[Recipe]
 
     def foodNamed(name: String) =
         allFoods.find(f => f.name == name)
-    
-    case class FoodCategory(name: String, foods: List[Food])
-    def allCategories: List[FoodCategory]
 }
