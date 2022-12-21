3c545bbeab0e431825bd2f557a9478ee68182d79
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 11:56:31 +0900

    非推奨機能

--- annotation.scala
+++ annotation.scala
@@ -5,3 +5,14 @@ class delayed extends Annotation
 
 // @strategy(@delayed) def f () = {}
 @strategy(new delayed) def f () = {}
+
+@deprecated def bigMistake() = {}
+@deprecated("代わりにnewShinyMethod()を使ってください")
+def bigMistake2() = {}
+
+object Deprecation2 {
+    def main(args: Array[String]) = {
+        bigMistake2()
+    }
+}
+// scalac -deprecation annotation.scala
