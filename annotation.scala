46995627673c9b7d2f435e87746dd4d814b81836
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-21 11:52:49 +0900

    アノテーションの構文

---
+++ annotation.scala
@@ -0,0 +1,7 @@
+import annotation._
+
+class strategy(arg: Annotation) extends Annotation
+class delayed extends Annotation
+
+// @strategy(@delayed) def f () = {}
+@strategy(new delayed) def f () = {}
