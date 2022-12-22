e71f8cfe4dd3593d9e32871584958b98f00bdab5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-22 09:18:10 +0900

    Futureの作成。failed、successful、fromTry、Promise

--- Future.scala
+++ Future.scala
@@ -29,3 +29,28 @@ val fut_result = for {
     y <- fut2
 } yield x + y
 fut_result.value
+// 10秒後に終了
+fut_result.value
+
+// 並列化されない例
+val fut_result2 = for {
+    x <- Future { Thread.sleep(10000); 21 + 21 }
+    y <- Future { Thread.sleep(10000); 23 + 23 }
+} yield x + y
+fut_result2.value
+// 20秒後に終了する
+
+Future.successful { 21 + 21 }
+Future.failed(new Exception("bummer!"))
+
+import scala.util.{Success, Failure}
+Future.fromTry(Success { 21 + 21 })
+Future.fromTry(Failure(new Exception("bummer!")))
+
+import scala.concurrent.Promise
+val pro = Promise[Int]()
+val pfut = pro.future
+pfut.value
+
+pro.success(42)
+pfut.value
