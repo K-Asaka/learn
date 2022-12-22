f71c09058793502bd724d44fd16a6f3954d7d32a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-22 10:46:24 +0900

    意味のある結果値を返すJSONパーサー

---
+++ JSON1Test.scala
@@ -0,0 +1,19 @@
+class JSON1 extends JavaTokenParsers {
+    def obj: Parser[Map[String, Any]] = 
+        "{"~> repsep(member, ",") <~"}" ^^ (Map() ++ _)
+    def arr: Parser[List[Any]] =
+        "["~> repsep(value, ",") <~"]"
+    def member: Parser[(String, Any)] =
+        stringLiteral~":"~value ^^
+            { case name~":"~value => (name, value) }
+    def value: Parser[Any] = (
+        obj
+      | arr
+      | stringLiteral
+      | floatingPointNumber ^^ (_.toDouble)
+      | "null" ^^ (x => null)
+      | "true" ^^ (x => true)
+      | "false" ^^ (x => false)
+    )
+}
+// $ scala JSON1Test address-book.json
