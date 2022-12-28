bac60bd412804169c0350cf79111fc5aec43dbf8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-28 22:45:34 +0900

    JSONのデータ構造を直接操作する

--- json.hs
+++ json.hs
@@ -1,59 +1,26 @@
 {-# LANGUAGE TemplateHaskell, OverloadedStrings, OverloadedLists #-}
 
 import Data.Aeson -- aeson
-import Data.Aeson.TH -- aeson
 import qualified Data.ByteString.Lazy.Char8 as B -- bytestring
 
--- JSONにしたいデータ
-data Human = Human
-    { name :: String
-    , age :: Int
-    } deriving Show
-
-data Department = Department
-    { departmentName :: String
-    , coworkers :: [Human]
-    } deriving Show
-
-deriveJSON defaultOptions ''Human   -- ''Humanという記法がTemplate Haskell由来
-deriveJSON defaultOptions ''Department
-
-taro :: Human
-taro = Human { name = "Taro", age = 30 }
-
-saburo :: Human
-saburo = Human { name = "Saburo", age = 31 }
-
-shiro :: Human
-shiro = Human { name = "Shiro", age = 31 }
-
-matsuko :: Human
-matsuko = Human { name = "Matsuko", age = 26 }
-
-nameList :: [Department]
-nameList =
-    [ Department
-        { departmentName = "General Affairs"
-        , coworkers =
-            [ taro
-            , matsuko
+nameListValue :: Value
+nameListValue = Array
+    [ object
+        [ "coworkers" .= Array
+            [ object
+                [ "age" .= Number 20
+                , "name" .= String "Satoshi"
+                ]
+            , object
+                [ "age" .= Number 23
+                , "name" .= String "Takeshi"
+                ]
             ]
-        }
-      , Department
-        { departmentName = "Development"
-        , coworkers =
-            [ saburo
-            , shiro
-            ]
-        }
+          , "departmentName" .= String "Planning"
+        ]
     ]
-
-data IntStr = IntData Int | StrData String
-deriveJSON defaultOptions ''IntStr
 
 main :: IO ()
 main = do
-    B.putStrLn $ encode $ nameList
-
-    B.putStrLn $ encode $ IntData 999
-    B.putStrLn $ encode $ StrData "World!"
+    B.putStrLn $ encode $ nameListValue
+    
