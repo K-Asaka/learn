2db1483cf795200f1427b5b1ac7626890d03c108
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-29 01:16:11 +0900

    lens関数によるアクセサの提供

--- lens.hs
+++ lens.hs
@@ -11,6 +11,8 @@ data User = User
 
 makeLenses ''User     -- ''はTemplateHaskellを使う箇所
 
+userPass :: Lens User User String String
+userPass = lens userPassword (\user password -> user { userPassword = password })
 
 main :: IO ()
 main = do
@@ -20,8 +22,7 @@ main = do
             , userPassword = "12345"
             }
     -- USer型に対しアクセサが生成される
-    -- userName :: Functor f => Lens User User String String
+    -- userName :: Functor f = Lens User User String String
     -- userAge :: Functor f => Lens User User Int Int
-    print (user^.userName)      -- "Taro"
-    print (user^.userAge)       -- 25
-    print (user&userName.~"Jiro")   -- User {_userName = "Jiro", _userAge = 25, userPassword = "12345"}
+    print (user^.userPass)      -- "12345"
+    print (user & userPass .~ "new-password") -- User {_userName = "Taro", _userAge = 25, userPassword = "new-password"}
