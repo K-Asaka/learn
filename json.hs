047553e523c14af92c145f844e2db6daa0c0aee4
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2022-12-28 22:51:29 +0900

    Optionsによる制限の回避

--- json.hs
+++ json.hs
@@ -1,26 +1,64 @@
 {-# LANGUAGE TemplateHaskell, OverloadedStrings, OverloadedLists #-}
 
 import Data.Aeson -- aeson
+import Data.Aeson.TH -- aeson
 import qualified Data.ByteString.Lazy.Char8 as B -- bytestring
 
-nameListValue :: Value
-nameListValue = Array
-    [ object
-        [ "coworkers" .= Array
-            [ object
-                [ "age" .= Number 20
-                , "name" .= String "Satoshi"
-                ]
-            , object
-                [ "age" .= Number 23
-                , "name" .= String "Takeshi"
-                ]
+-- JSONにしたいデータ
+data Human = Human
+    { name :: String
+    , age :: Int
+    } deriving Show
+
+data Department = Department
+    { departmentName :: String
+    , coworkers :: [Human]
+    } deriving Show
+
+deriveJSON defaultOptions ''Human   -- ''Humanという記法がTemplate Haskell由来
+deriveJSON (defaultOptions
+    { fieldLabelModifier = \s -> case s of
+        "departmentName" -> "name"
+        t -> t
+    } ) ''Department    -- TemplateHaskell由来
+
+taro :: Human
+taro = Human { name = "Taro", age = 30 }
+
+saburo :: Human
+saburo = Human { name = "Saburo", age = 31 }
+
+shiro :: Human
+shiro = Human { name = "Shiro", age = 31 }
+
+matsuko :: Human
+matsuko = Human { name = "Matsuko", age = 26 }
+
+nameList :: [Department]
+nameList =
+    [ Department
+        { departmentName = "General Affairs"
+        , coworkers =
+            [ taro
+            , matsuko
             ]
-          , "departmentName" .= String "Planning"
-        ]
+        }
+      , Department
+        { departmentName = "Development"
+        , coworkers =
+            [ saburo
+            , shiro
+            ]
+        }
     ]
+
+data IntStr = IntData Int | StrData String
+deriveJSON defaultOptions ''IntStr
 
 main :: IO ()
 main = do
-    B.putStrLn $ encode $ nameListValue
+    B.putStrLn $ encode $ nameList
+
+    B.putStrLn $ encode $ IntData 999
+    B.putStrLn $ encode $ StrData "World!"
     
