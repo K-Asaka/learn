f386f4cf9338b2671e658b36a8f0110e71fc9282
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-02 14:24:04 +0900

    Consumerの自作

--- myproject/app/Main.hs
+++ myproject/app/Main.hs
@@ -8,7 +8,7 @@ hello = do
         return $ "Hello, " ++ s
 
 main :: IO ()
-main = sample1
+main = sample2
 
 
 sample1 :: IO ()
@@ -19,3 +19,28 @@ sampleProducer = do
     yield "Hoge"
     yield "Piyo"
     yield "Fuga"
+
+sample2 :: IO ()
+sample2 = runEffect $ range 1 20 >-> sampleConsumer
+
+-- nからmまでの範囲を表す簡単なProducerを
+-- Control.Monadで提供されているmapM_関数を利用して作成する
+range :: Int -> Int -> Producer Int IO ()
+range n m = mapM_ yield [n..m]
+
+sampleConsumer :: Consumer Int IO ()
+sampleConsumer = do
+    -- await関数でストリームの上流からデータを取得する
+    x <- await
+    -- fizzBuzzして出力
+    lift . putStrLn $ fizzBuzz x
+    -- ストリームデータの最後まで自分自身を呼び出す
+    sampleConsumer
+
+-- 簡単なFizzBuzzの実装
+fizzBuzz :: Int -> String
+fizzBuzz n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 3 == 0 = "Fizz"
+    | n `mod` 5 == 0 = "Buzz"
+    | otherwise = show n
