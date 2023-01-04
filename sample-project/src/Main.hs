c693085328ee28b1bcd6e3f0eae31b46c845be6b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-04 19:30:31 +0900

    withAsyncを使った例

--- sample-project/src/Main.hs
+++ sample-project/src/Main.hs
@@ -2,16 +2,18 @@ module Main where
 
 import Control.Monad (forever)
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.Async (async)
+import Control.Concurrent.Async (withAsync, waitEither_)
 
 main :: IO ()
 main = do
-    putStrLn "Using async"
-    _ <- async $ do
-        _ <- async $ forever $ do
-            putStrLn "Thread2: Can you hear me?"
+    putStrLn "Using withAsync"
+    withAsync thread1 $ \a1 ->
+        withAsync thread2 $ \a2 ->
+            waitEither_ a1 a2
+    where
+        thread1 = do
+            threadDelay 2000000 -- 2sec
+            putStrLn "Thread1: end"
+        thread2 = forever $ do
+            putStrLn "Thread2: Can you her me?"
             threadDelay 500000 -- 0.5sec
-        threadDelay 1000000 -- 1sec
-        putStrLn "Thread1: end"
-    
-    threadDelay 3000000 -- 3sec
