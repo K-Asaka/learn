d82498d3e0eb47fa1f5e4ce2a462c499dc99b8ab
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-04 19:10:15 +0900

    基本の利用

---
+++ sample-project/src/Http.hs
@@ -0,0 +1,22 @@
+-- build時にchcp 65001でコードページを変更する必要がある
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Concurrent.Async
+import qualified Data.ByteString.Lazy.Char8 as C8
+import Network.HTTP.Simple -- HTTPリクエストを投げる
+
+main :: IO ()
+main = do
+    a1 <- async $ getUrl "http://httpbin.org/get"
+    a2 <- async $ getUrl "http://httpbin.org/get"
+    result1 <- wait a1
+    result2 <- wait a2
+
+    C8.putStrLn result1
+    C8.putStrLn result2
+
+getUrl :: Request -> IO C8.ByteString
+getUrl url = do
+    response <- httpLBS url
+    return $ getResponseBody response
