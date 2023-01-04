33a651b412e632e3f9ecbd582c79badeb0edcfa1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-04 19:35:20 +0900

    race_を使った例

--- sample-project/src/Main.hs
+++ sample-project/src/Main.hs
@@ -2,18 +2,16 @@ module Main where
 
 import Control.Monad (forever)
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.Async (withAsync, waitEither_)
+import Control.Concurrent.Async (race_)
 
 main :: IO ()
 main = do
-    putStrLn "Using withAsync"
-    withAsync thread1 $ \a1 ->
-        withAsync thread2 $ \a2 ->
-            waitEither_ a1 a2
-    where
-        thread1 = do
-            threadDelay 2000000 -- 2sec
-            putStrLn "Thread1: end"
-        thread2 = forever $ do
-            putStrLn "Thread2: Can you her me?"
-            threadDelay 500000 -- 0.5sec
+    putStrLn "Using race_"
+    race_ thread1 thread2
+  where
+    thread1 = do
+        threadDelay 2000000 -- 2sec
+        putStrLn "Thread1: end"
+    thread2 = forever $ do
+        putStrLn "Thread2: Can you her me?"
+        threadDelay 500000 -- 0.5sec
