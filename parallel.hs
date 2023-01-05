21b79a490765dd9294bae53f2c927a8a4cd94929
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-05 11:04:51 +0900

    戦略に基づく高速化

--- parallel.hs
+++ parallel.hs
@@ -1,7 +1,9 @@
+import Control.Parallel.Strategies
+
 main :: IO ()
 main = do
     let list = take 5000 (cycle [20..30])
-    print $ sum (map fib list)
+    print $ sum (map fib list `using` parListChunk 30 rseq)
 
 fib :: Int -> Int
 fib 0 = 0
