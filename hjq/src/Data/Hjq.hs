7d2f5468cf9550f5291dba3187d2c19e32a0e90c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2023-01-05 17:49:04 +0900

    コマンドラインツールの作成

--- hjq/src/Data/Hjq.hs
+++ hjq/src/Data/Hjq.hs
@@ -1,7 +1,15 @@
-module Data.Hjq where
+{-# LANGUAGE OverloadedStrings #-}
+module Data.Hjq ( hjq ) where
+import Control.Error.Util
+import Data.Aeson
 import Data.Text as T
+import Data.ByteString.Lazy as B
 import Data.Hjq.Parser
+import Data.Hjq.Query
+import Data.Aeson.Encode.Pretty
 
--- とりあえずパターンマッチで処理
-parseJqFilter :: Text -> Either String JqFilter
-parseJqFilter _ = Right JqNil
+hjq :: ByteString -> T.Text -> Either T.Text ByteString
+hjq jsonString queryString = do
+  value <- note "Invalid json format." $ decode jsonString
+  query <- parseJqQuery queryString
+  executeQuery query value >>= return . encodePretty
