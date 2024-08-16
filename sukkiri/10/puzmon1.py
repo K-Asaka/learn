33e652d4fd32fb449b4635f04ee66c10de077fb5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-08-16 23:48:55 +0900

    go_dungeon()関数を追加

--- sukkiri/10/puzmon1.py
+++ sukkiri/10/puzmon1.py
@@ -7,8 +7,19 @@
 # グローバル変数の宣言
 
 # 関数宣言
+def go_dungeon(player):
+    print(player + "はダンジョンに到達した")
+
+    print(player + "はダンジョンを制覇した")
+    return 5
+
 def main():
     print('*** Puzzle & Monsters ***')
+    player = "あいうえお"
+
+    result = go_dungeon(player)
+    print('*** GAME CLEARED!! ***')
+    print(f'倒したモンスター数={result}')
 
 # main関数の呼び出し
 main()
