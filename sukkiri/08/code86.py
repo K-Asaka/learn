7d9ffa650175fc5223319676284cf7312481f1fb
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-08-16 23:32:47 +0900

    OpenAIのサンプル

--- sukkiri/08/code86.py
+++ sukkiri/08/code86.py
@@ -1,7 +1,15 @@
-# カレールウの月別の支出額をグラフ化する
-import pandas as pd
-df = pd.read_csv('curry.csv', encoding='Shift_JIS')
-df['month'] = pd.to_datetime(df['時間軸(月次)'], format='%Y年%m月').dt.month
-df = df.groupby('month').mean()
-df.mean(axis=1)
-df.mean(axis=1).plot()
+import openai
+
+# APIキーの設定
+openai.api_key = "xxxxxxxxxx"
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user",
+         "content":
+         "インプレスのすっきりわかる入門シリーズって、どんな本？"}
+    ],
+)
+
+print(response.choices[0]["message"]["content"].strip())
