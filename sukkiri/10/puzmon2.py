80e899d46cff8c765c0dca2c4f5cef01dcbf431d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-08-18 00:53:37 +0900

    敵モンスターの準備

--- sukkiri/10/puzmon2.py
+++ sukkiri/10/puzmon2.py
@@ -7,16 +7,29 @@
 # グローバル変数の宣言
 
 # 関数宣言
+def print_monster_name(monster):
+    # monsterはディクショナリで受け取る
+    # (1)モンスターの名前をキーnameで取得する
+    # (2)
+    # (3)
+    
+    monster_name = monster['name']
+
+    # モンスター名を表示する
+    print(f'{monster_name}', end='')
+
+
 def do_battle(monster):
     is_win = 1
-    print(monster + 'が現れた！')
+    print_monster_name(monster)
+    print('が現れた！')
 
-    print(monster + 'を倒した！')
+    print_monster_name(monster)
+    print('を倒した！')
     return is_win
 
-def go_dungeon(player):
+def go_dungeon(player, monsters):
     print(player + "はダンジョンに到達した")
-    monsters = ['スライム', 'ゴブリン', 'オオコウモリ', 'ウェアフレア', 'ドラゴン']
     is_win = 0
 
     for monster in monsters:
@@ -27,6 +40,48 @@ def go_dungeon(player):
 
 def main():
     print('*** Puzzle & Monsters ***')
+    slime = {
+        'name': 'スライム',
+        'hp': 100,
+        'max_hp': 100,
+        'element': '水',
+        'ap': 10,
+        'dp': 1
+    }
+    goblin = {
+        'name': 'ゴブリン',
+        'hp': 200,
+        'max_hp': 200,
+        'element': '土',
+        'ap': 20,
+        'dp': 5
+    }
+    giantbat = {
+        'name': 'オオコウモリ',
+        'hp': 300,
+        'max_hp': 300,
+        'element': '風',
+        'ap': 30,
+        'dp': 10
+    }
+    werewolf = {
+        'name': 'ウェアウルフ',
+        'hp': 400,
+        'max_hp': 400,
+        'element': '風',
+        'ap': 40,
+        'dp': 15
+    }
+    dragon = {
+        'name': 'ドラゴン',
+        'hp': 600,
+        'max_hp': 600,
+        'element': '火',
+        'ap': 50,
+        'dp': 20
+    }
+    monsters = [slime, goblin, giantbat, werewolf, dragon]
+
     while True:
         player = input('プレイヤー名を入力してください> ')
         if player != '':
@@ -34,7 +89,7 @@ def main():
         else:
             print('エラー：プレイヤー名を入力してください')            
 
-    is_win = go_dungeon(player)
+    is_win = go_dungeon(player, monsters)
 
     if is_win < 5:
         print('*** GAME OVER!! ***')
