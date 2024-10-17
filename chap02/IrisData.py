cdccfaf01a1c18e533e59762a2751d7684020cf0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-10-18 00:31:59 +0900

    確率的勾配降下法

--- chap02/IrisData.py
+++ chap02/IrisData.py
@@ -132,3 +132,29 @@ plt.xlabel('Epochs')
 plt.ylabel('Sum-squared-error')
 # 図の表示
 plt.show()
+
+
+from adalinegd import AdalineSGD
+# 確率的勾配降下法によるADALINEの学習
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+# モデルへの適合
+ada.fit(X_std, y)
+# 境界領域のプロット
+plot_decision_regions(X_std, y, classifier=ada)
+# タイトルの設定
+plt.title('Adaline - Stochastic Gradient Descent')
+# 軸のラベルの設定
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+# 凡例の設定(左上に配置)
+plt.legend(loc='upper left')
+plt.tight_layout()
+# プロットの表示
+plt.show()
+# エポックとコストの折れ線グラフのプロット
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+# 軸のラベルの設定
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+# プロットの表示
+plt.show()
