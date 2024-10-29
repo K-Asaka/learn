b48070b93c1c573685fdd8ddeefb12c6bfa5e716
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-10-29 23:39:18 +0900

    scikit-learnでの代替実装

--- chap03/IrisData.py
+++ chap03/IrisData.py
@@ -177,3 +177,11 @@ plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
 
+
+from sklearn.linear_model import SGDClassifier
+# 確率的勾配降下法バージョンのパーセプトロンを生成
+ppn = SGDClassifier(loss='perceptron')
+# 確率的勾配降下法バージョンのロジスティック回帰を生成
+lr = SGDClassifier(loss='log')
+# 確率的勾配降下法バージョンのSVM(損失関数=ヒンジ関数)を生成
+svm = SGDClassifier(loss='hinge')
