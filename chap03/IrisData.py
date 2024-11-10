fe33adf200f42498d149f64866f4c99945b83e41
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-11-11 00:01:13 +0900

    ランダムフォレストを使って弱い学習アルゴリズムと強い学習アルゴリズムを結合する

--- chap03/IrisData.py
+++ chap03/IrisData.py
@@ -253,3 +253,17 @@ plt.show()
 from sklearn.tree import export_graphviz
 export_graphviz(tree, out_file='tree.dot',
                 feature_names=['petal length', 'petal width'])
+
+
+from sklearn.ensemble import RandomForestClassifier
+# エントロピーを指標とするランダムフォレストのインスタンスを生成
+forest = RandomForestClassifier(criterion='entropy', 
+                                n_estimators=10, random_state=1, n_jobs=2)
+# ランダムフォレストのモデルにトレーニングデータを適合させる
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
