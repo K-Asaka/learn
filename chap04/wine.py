ea03c39c4045332418b719cf78c713006946dc27
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-11-17 20:30:34 +0900

    L1正則化による疎な解

--- chap04/wine.py
+++ chap04/wine.py
@@ -39,3 +39,60 @@ stdsc = StandardScaler()
 X_train_std = stdsc.fit_transform(X_train)
 X_test_std = stdsc.transform(X_test)
 
+
+from sklearn.linear_model import LogisticRegression
+# L1正則化ロジスティック回帰のインスタンスを生成
+print(LogisticRegression(penalty='l1'))
+
+# L1正則化ロジスティック回帰のインスタンスを生成(逆正則化パラメータ C=0.1)
+# lr = LogisticRegression(penalty='l1', C=0.1)
+lr = LogisticRegression(penalty='l1', C=0.1, solver='liblinear')
+# トレーニングデータに適合
+lr.fit(X_train_std, y_train)
+# トレーニングデータに対する正解率の表示
+print('Training accuracy:', lr.score(X_train_std, y_train))
+# テストデータに対する正解率の表示
+print('Test accuracu:', lr.score(X_test_std, y_test))
+
+# 切片の表示
+print(lr.intercept_)
+
+# 重み係数の表示
+print(lr.coef_)
+
+
+import matplotlib.pyplot as plt
+# 描画の準備
+fig = plt.figure()
+ax = plt.subplot(111)
+# 各係数の色のリスト
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue', 'gray', 'indigo', 'orange']
+# 空のリストを生成(重み係数、逆正則化パラメータ)
+weights, params = [], []
+# 逆正則化パラメータの値ごとに処理
+for c in np.arange(-4, 6):
+    lr = LogisticRegression(penalty='l1', C=10.**c, random_state=0, solver='liblinear')
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10.**c)
+
+# 重み係数をNumPy配列に変換
+weights = np.array(weights)
+# 各重み係数をプロット　
+for column, color in zip(range(weights.shape[1]), colors):
+    # 横軸を逆正則化パラメータ、縦軸を重み係数とした折れ線グラフ
+    plt.plot(params, weights[:, column], label=df_wine.columns[column+1],
+             color=color)
+# y = 0に黒い波線を引く
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+# 横軸の範囲の設定
+plt.xlim([10**(-5), 10**5])
+# 軸のラベル設定
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+# 横軸を対数スケールに設定
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', bbox_to_anchor=(1.38, 1.03), ncol=1, fancybox=True)
+plt.show()
