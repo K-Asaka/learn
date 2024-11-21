633a9553ce786a88fc6fb1f4961f9dd7b583bffd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-11-21 22:58:41 +0900

    特徴変換

--- chap04/wine2.py
+++ chap04/wine2.py
@@ -39,3 +39,29 @@ plt.ylabel('Explained variance ratio')
 plt.xlabel('Principal components')
 plt.legend(loc='best')
 plt.show()
+
+
+# (固有値, 固有ベクトル)のタプルのリストを作成
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+# (固有値, 固有ベクトル)のタプルを大きいものから順に並べ替え
+eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+print('Matrix W:\n', w)
+
+print(X_train_std[0].dot(w))
+
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+# 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを生成してプロット
+for l, c, m in zip(np.unique(y_train), colors, markers):
+        plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1],
+                c=c, label=l, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
+
