dc9cfff0401e7971d95ffa8b41a95e0465efeef1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-12-24 17:42:10 +0900

    複数のビットがオンであるか確認する場合

--- chap03/scripts/bit_flag.js
+++ chap03/scripts/bit_flag.js
@@ -6,3 +6,6 @@ const INDIA = 1 << 3;
 let flags = AMERICA | CHINA;
 // flagsにINDIAが含まれるか確認
 console.log(flags & INDIA);
+
+// 複数のビットがオンであるか確認する場合
+console.log((flags & (AMERICA | CHINA)) === (AMERICA | CHINA));
