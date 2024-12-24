6dea9630e128ce81d4a40169a2cfc2dc04b84340
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2024-12-24 17:34:35 +0900

    ||=演算子、??=演算子

--- chap03/scripts/condition_falsy.js
+++ chap03/scripts/condition_falsy.js
@@ -1,4 +1,9 @@
 let value = '';
-value = value ? value : '既定値';
+//value = value ? value : '既定値';
+//value ||= '既定値';
+// ゼロ、空文字列などを除外できない場合は??=を使う
+//value ??= '既定値';
 
 console.log(value);
+console.log(value || '既定値');
+console.log(value ?? '既定値');
