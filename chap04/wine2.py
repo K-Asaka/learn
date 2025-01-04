5af99b7e64eb441fb1ab4d130565045d025538ac
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-04 14:36:26 +0900

    scikit-learnによるLDA

--- chap04/wine2.py
+++ chap04/wine2.py
@@ -198,3 +198,26 @@ plt.xlabel('LD 1')
 plt.ylabel('LD 2')
 plt.legend(loc='lower right')
 plt.show()
+
+# from sklearn.lda import LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+# 次元数を指定して、LDAのインスタンスを生成
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.show()
+
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.show()
