0cf7c0e1f592623cf195c22e42d2a2c1725cfe0b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-04 10:42:53 +0900

    scikit-learnの主成分分析

--- chap04/wine2.py
+++ chap04/wine2.py
@@ -65,3 +65,48 @@ plt.ylabel('PC 2')
 plt.legend(loc='lower left')
 plt.show()
 
+from matplotlib.colors import ListedColormap
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+        # マーカーとカラーマップの準備
+        markers = ('s', 'x', 'o', '^', 'v')
+        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+        cmap = ListedColormap(colors[:len(np.unique(y))])
+        
+        # 決定領域のプロット
+        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        # グリッドポイントの生成
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                np.arange(x2_min, x2_max, resolution))
+        # 各特徴量を1次元配列に変換して予測を実行
+        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        # 予測結果を元のグリッドポイントのデータサイズに変換
+        Z = Z.reshape(xx1.shape)
+        # グリッドポイントの等高線のプロット
+        plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+        # 軸の範囲の設定
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
+
+        # クラスごとにサンプルをプロット
+        for idx, cl in enumerate(np.unique(y)):
+                plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx),
+                        marker=markers[idx], label=cl)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+# 主成分数を指定して、PCAのインスタンスを生成
+pca = PCA(n_components=2)
+# ロジスティック回帰のインスタンスを生成
+lr = LogisticRegression()
+# トレーニングデータとテストデータをPCAに適合させる
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+# トレーニングデータをロジスティック回帰に適合させる
+lr.fit(X_train_pca, y_train)
+# 決定境界をプロット
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
