c2960406ce74bad2c0d971f6292a536b18bd5f9f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-06 00:21:21 +0900

    Breast Cancer Wisconsinデータセットを読み込む

---
+++ chap05/bcw.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+
+from sklearn.preprocessing import LabelEncoder
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(le.transform(['M', 'B']))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.20, random_state=1)
