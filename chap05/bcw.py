ded7fd11ec668cf06b6ce3fdeb39151d237ce432
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-06 00:25:37 +0900

    パイプラインで変換器と推定器を結合する

--- chap05/bcw.py
+++ chap05/bcw.py
@@ -12,3 +12,13 @@ print(le.transform(['M', 'B']))
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = \
     train_test_split(X, y, test_size=0.20, random_state=1)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+# 連結する処理としてスケーリング、主成分分析、ロジスティック回帰を指定
+pipe_lr = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=2)),
+                    ('clf', LogisticRegression(random_state=1))])
+pipe_lr.fit(X_train, y_train)
+print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
