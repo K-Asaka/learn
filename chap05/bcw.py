07a40017d87da91c20701c4f927aa2a58784d0d0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-06 14:41:21 +0900

    混同行列を解釈する

--- chap05/bcw.py
+++ chap05/bcw.py
@@ -186,3 +186,23 @@ scores = cross_val_score(gs,
                          scoring='accuracy',
                          cv=5)
 print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+
+from sklearn.metrics import confusion_matrix
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+# テストと予測のデータから混同行列を生成
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+# 図のサイズを指定
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+# matshow関数で行列からヒートマップを描画
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):   # クラス0の繰り返し処理
+    for j in range(confmat.shape[1]):   # クラス1の繰り返し処理
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')    # 件数を表示
+
+plt.xlabel('predicted label')
+plt.ylabel('true label')
+plt.show()
