6e28d4fa9497ad73f8f262efcf04fea13336f5c6
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-06 12:11:16 +0900

    グリッドサーチを使ってハイパーパラメータをチューニングする

--- chap05/bcw.py
+++ chap05/bcw.py
@@ -137,3 +137,30 @@ plt.xlabel('Parameter C')
 plt.ylabel('Accuracy')
 plt.ylim([0.8, 1.0])
 plt.show()
+
+
+# from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+pipe_svc = Pipeline([('scl', StandardScaler()),
+                     ('clf', SVC(random_state=1))])
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [{'clf__C': param_range, 'clf__kernel': ['linear']},
+              {'clf__C': param_range, 'clf__gamma': param_range,
+               'clf__kernel': ['rbf']}]
+# ハイパーパラメータ値のリストparam_gridを指定し、
+# グリッドサーチを行うGridSearchCVクラスのインスタンス化
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring='accuracy',
+                  cv=10,
+                  n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+# モデルの最良スコアを出力
+print(gs.best_score_)
+# 最良スコアとなるパラメータ値を出力
+print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test accuracy: %.3f' % clf.score(X_test, y_test))
