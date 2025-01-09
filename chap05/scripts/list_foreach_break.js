877d0c68536866d82695dd1e27a04d57d6799a6a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-09 18:31:03 +0900

    forEachループを中断する方法

---
+++ chap05/scripts/list_foreach_break.js
@@ -0,0 +1,13 @@
+let list = [ 1, 2, null, 4, 5 ];
+
+try {
+	list.forEach(function(value) {
+		// null値の場合に例外をスロー
+		if (value === null) {
+			throw new Error('null値を検出しました。');
+		}
+		console.log(value);
+	});
+} catch (e) {
+	console.log(e.message);
+}
