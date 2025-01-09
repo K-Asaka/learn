58601992d55056f0020b5bdaedd540f3caa4c644
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-09 19:25:27 +0900

    マップの値を設定／取得する

---
+++ chap05/scripts/map_set.js
@@ -0,0 +1,12 @@
+let data = new Map();
+data.set('壱', '1')
+	.set('弐', '2')
+	.set('参', '3')
+	.set('壱', '一');
+
+console.log(data.get('参'));
+console.log(data.get('壱'));
+console.log(data.get('肆'));
+
+// キーが存在するかを判定する
+console.log(data.has('肆') ? data.get('肆') : '4');
