156addb52eb3d39c414fa19412b287ed87b5bf72
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-09 19:36:19 +0900

    マップから既存のキーを削除する

---
+++ chap05/scripts/map_delete.js
@@ -0,0 +1,9 @@
+let data = new Map();
+data.set('1st', 'ファースト');
+data.set('2nd', 'セカンド');
+console.log(data.delete('1st'));
+console.log(data.delete('3rd'));
+
+// マップに存在するすべてのキーを破棄
+data.clear();
+console.log(data);
