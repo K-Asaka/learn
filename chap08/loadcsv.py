d8b7cee187114b74c79791b8d8b01af1c8232a64
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 12:03:25 +0900

    TF-IDFを使って単語の関連性を評価する

--- chap08/loadcsv.py
+++ chap08/loadcsv.py
@@ -12,3 +12,8 @@ docs = np.array(['The sun is shining',
 bag = count.fit_transform(docs)
 print(count.vocabulary_)
 print(bag.toarray())
+
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
+np.set_printoptions(precision=2)
+print(tfidf.fit_transform(count.fit_transform(docs)).toarray())
