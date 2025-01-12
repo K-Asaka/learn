e52c8bbbeeacf8aaa8fe2996a70bd1c045dd0f6b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 13:18:06 +0900

    ドキュメントをトークン化する

--- chap08/loadcsv.py
+++ chap08/loadcsv.py
@@ -31,3 +31,23 @@ print(preprocessor(df.loc[0, 'review'][-50:]))
 print(preprocessor("</a>This :) is :( a test :-)!"))
 
 df['review'] = df['review'].apply(preprocessor)
+
+def tokenizer(text):
+    return text.split()
+
+print(tokenizer('runners like running and thus they run'))
+
+from nltk.stem.porter import PorterStemmer
+porter = PorterStemmer()
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+tokenizer_porter('runners like running and thus they run')
+
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+print([w for w in tokenizer_porter('a runner likes running and runs a lot')[-10:]
+        if w not in stop])
