1a5728ecd89a529a9040fbc394240d61c66cd330
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 11:58:56 +0900

    単語を特徴ベクトルに変換する

--- chap08/loadcsv.py
+++ chap08/loadcsv.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('./movie_data.csv')
 print(df.head(3))
+
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+count = CountVectorizer()
+docs = np.array(['The sun is shining',
+                 'The weather is sweet',
+                 'The sun is shining, the weather is sweet, and one and one is two'])
+bag = count.fit_transform(docs)
+print(count.vocabulary_)
+print(bag.toarray())
