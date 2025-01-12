8e2530a5d4cfd332b6a4d37408e80cac74fea744
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 13:10:07 +0900

    テキストデータのクレンジング

--- chap08/loadcsv.py
+++ chap08/loadcsv.py
@@ -17,3 +17,17 @@ from sklearn.feature_extraction.text import TfidfTransformer
 tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
 np.set_printoptions(precision=2)
 print(tfidf.fit_transform(count.fit_transform(docs)).toarray())
+
+print(df.loc[0, 'review'][-50:])
+
+import re
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ''.join(emoticons).replace('-', '')
+    return text
+
+print(preprocessor(df.loc[0, 'review'][-50:]))
+print(preprocessor("</a>This :) is :( a test :-)!"))
+
+df['review'] = df['review'].apply(preprocessor)
