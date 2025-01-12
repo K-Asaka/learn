4648149f560706b4a17943d098e55e55c94e6402
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 23:37:07 +0900

    データストレージとしてSQLiteデータベースをセットアップする

---
+++ chap09/movieclassifier/select_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+import os
+
+conn = sqlite3.connect('movieclassifier/reviews.sqlite')
+c = conn.cursor()
+c.execute("SELECT * FROM review_db WHERE date"\
+          " BETWEEN '2016-01-01 00:00:00' AND DATETIME('now')")
+results = c.fetchall()
+conn.close()
+print(results)
