ded4680462a4052858e596b438f1ca26fb931371
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-12 15:18:29 +0900

    ドキュメントを分類するロジスティック回帰モデルのトレーニング

--- chap08/loadcsv.py
+++ chap08/loadcsv.py
@@ -51,3 +51,42 @@ from nltk.corpus import stopwords
 stop = stopwords.words('english')
 print([w for w in tokenizer_porter('a runner likes running and runs a lot')[-10:]
         if w not in stop])
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+# from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf': [False],
+               'vect__norm': [None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]}
+]
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5, verbose=1,
+                           n_jobs=-1)
+gs_lr_tfidf.fit(X_train, y_train)
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
