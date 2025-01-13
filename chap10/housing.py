325d2adc860dd290795391ad36a8309cfde5ef93
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-14 00:22:19 +0900

    データセットの重要な特性を可視化する

--- chap10/housing.py
+++ chap10/housing.py
@@ -4,3 +4,14 @@ df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/hous
 df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
               'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 print(df.head())
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+# グラフのスタイルを指定(whitegridは白背景グリッド、notebookはデフォルトサイズ)
+sns.set(style='whitegrid', context='notebook')
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+# 変数のペアの関係をプロット：dfはDataFrameオブジェクト、sizeは1面のインチサイズ
+# sns.pairplot(df[cols], size=2.5)
+sns.pairplot(df[cols], height=2.5)
+plt.show()
