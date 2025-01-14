e97f0991e97f1fca50c94dff5f22461f9dc40adf
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-15 00:51:41 +0900

    相関行列をヒートマップとしてプロットする

--- chap10/housing.py
+++ chap10/housing.py
@@ -17,3 +17,17 @@ cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
 # sns.pairplot(df[cols], size=2.5)
 sns.pairplot(df[cols], height=2.5)
 plt.show()
+
+
+import numpy as np
+cm = np.corrcoef(df[cols].values.T)     # ピアソンの積率相関係数を計算
+sns.set(font_scale=1.5)                 # 全体のスケールとは別にフォントサイズを指定
+hm = sns.heatmap(cm,                    # 第1引数の相関係数をもとにヒートマップを作製
+                 cbar=True,             # カラーバーの表示
+                 annot=True,            # データ値の表示
+                 square=True,           # 各矩形の正方形化(縦と横のサイズを一致させる)
+                 fmt='.2f',             # 数値などの表示形式
+                 annot_kws={'size': 15},    # データ値のサイズの設定
+                 yticklabels=cols,      # 行の目盛のラベル名
+                 xticklabels=cols)      # 列の目盛のラベル名
+plt.show()
