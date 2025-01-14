890114c540627acedaa1db3a28dcd1599a7425d5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-14 19:34:32 +0900

    戻り値がない関数

--- chap06/scripts/func_basic.js
+++ chap06/scripts/func_basic.js
@@ -3,5 +3,12 @@ function getTriangleArea(base, height) {
 	return base * height / 2;
 }
 
+// 戻り値がない関数
+function showTriangleArea(base, height) {
+	console.log(base * height / 2);
+}
+
 let area = getTriangleArea(10, 4);
 console.log(area);
+
+showTriangleArea(10, 4);
