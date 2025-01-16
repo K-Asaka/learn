4ac87be712b2c5ee816c6480488c68065f849fd8
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 11:54:57 +0900

    名前付き引数の実装

---
+++ chap06/scripts/args_named.js
@@ -0,0 +1,21 @@
+function showDialog({
+	content = '',
+	title = 'My Dialog',
+	width = 100,
+	height = 100,
+	position = 'center',
+	modal = false
+}) {
+	// 引数の内容を表示
+	console.log(`content: ${content}`);
+	console.log(`title: ${title}`);
+	console.log(`width: ${width}`);
+	console.log(`height: ${height}`);
+	console.log(`position: ${position}`);
+	console.log(`modal: ${modal}`);
+}
+
+showDialog({
+	content: 'ダイアログです',
+	modal: true,
+});
