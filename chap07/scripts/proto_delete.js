7cf6cbd1b9d9fdff3a542dc9a2f7d9ec1192d2dd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 19:38:32 +0900

    プロトタイプ経由のプロパティを強制的に削除する方法

--- chap07/scripts/proto_delete.js
+++ chap07/scripts/proto_delete.js
@@ -23,3 +23,21 @@ console.log(parent.x);
 console.log(delete obj.x);
 console.log(obj.x);
 console.log(parent.x);
+
+// プロトタイプ経由のプロパティを強制的に削除する方法
+delete Object.getPrototypeOf(obj).x;
+console.log(parent.x);
+
+// プロトタイプで提供されるプロパティを現在のオブジェクト上で削除する
+obj.x = undefined
+console.log(obj.x);
+// プロパティの存在はそのままに、値を強制的に未定義にしている
+
+// プロトタイプそのものを入れ替える
+let new_parent = {
+	a: 'Hello'
+};
+Object.setPrototypeOf(obj, new_parent);
+console.log(obj.a);
+Object.setPrototypeOf(obj, null);
+console.log(obj.a);
