b2da86528a14c45bd62a8fcfd875b52e019e007c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 11:12:15 +0900

    既定値として式の結果を使う

--- chap06/scripts/args_default.js
+++ chap06/scripts/args_default.js
@@ -2,6 +2,11 @@ function getTriangleArea(base=10, height=base) {
 	return base * height / 2;
 }
 
+function formatTime(date = Date.now()) {
+	console.log(date);
+}
+
 console.log(getTriangleArea());
 console.log(getTriangleArea(4));
 console.log(getTriangleArea(1, 2));
+formatTime();
