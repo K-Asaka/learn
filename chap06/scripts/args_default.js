e34094f4004db85de0d2075516a75737f5e8061a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 11:14:47 +0900

    既定値が適用されるのは、値が渡されなかった場合

--- chap06/scripts/args_default.js
+++ chap06/scripts/args_default.js
@@ -1,4 +1,4 @@
-function getTriangleArea(base=10, height=base) {
+function getTriangleArea(base=10, height=5) {
 	return base * height / 2;
 }
 
@@ -10,3 +10,7 @@ console.log(getTriangleArea());
 console.log(getTriangleArea(4));
 console.log(getTriangleArea(1, 2));
 formatTime();
+
+// 既定値が適用されるのは、値が渡されなかった場合
+console.log(getTriangleArea(5, null));	// 明示的に渡された場合、既定値は適用されない
+console.log(getTriangleArea(5, undefined));
