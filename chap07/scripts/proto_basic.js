ffccb01f60662d361fdfb0d02f5754b6b69fb34a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 15:05:26 +0900

    プロトタイプチェーンの挙動を確認する

---
+++ chap07/scripts/proto_basic.js
@@ -0,0 +1,22 @@
+// objの元となるオブジェクト
+let parent = {
+	x: 10,
+	y: 20,
+};
+
+// parentをプロトタイプにして作成したオブジェクト
+let obj = Object.create(parent, {
+	z: {
+		value: 30,
+		writable: true,
+		configurable: true,
+		enumerable: true
+	}
+});
+
+console.log(obj);
+console.log(Object.getPrototypeOf(obj));
+
+for (let prop in obj) {
+	console.log(`${prop}: ${obj[prop]}`);
+}
