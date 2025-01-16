06a749b16286a4984463bd3f7412b53c21a22a97
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 14:09:22 +0900

    余分な)を削除

--- chap07/scripts/new_object.js
+++ chap07/scripts/new_object.js
@@ -2,6 +2,6 @@ let member = new Object();
 member.name = '佐藤リオ';
 member.age = 21;
 member.show = function() {
-	console.log(`私は${this.name})、${this.age}歳です。`);
+	console.log(`私は${this.name}、${this.age}歳です。`);
 };
 member.show();
