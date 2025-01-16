06c89057087522ce41e7d917709c9442eed11542
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-17 00:08:16 +0900

    多項式回帰：線形回帰モデルから曲線を見い出す

--- chap10/housing.py
+++ chap10/housing.py
@@ -204,3 +204,40 @@ lasso = Lasso(alpha=1.0)
 
 from sklearn.linear_model import ElasticNet
 elasticnet = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+
+from sklearn.preprocessing import PolynomialFeatures
+X = np.array([258.0, 270.0, 294.0, 320.0, 342.0, 368.0, 396.0, 446.0, 480.0, 586.0])[:, np.newaxis]
+y = np.array([236.4, 234.4, 252.8, 298.6, 314.2, 342.2, 360.8, 368.0, 391.2, 390.8])
+# 線形回帰(最小二乗法)モデルのクラスをインスタンス化
+lr = LinearRegression()
+pr = LinearRegression()
+# 2次の多項式特徴量のクラスをインスタンス化
+quadratic = PolynomialFeatures(degree=2)
+# データに適合させ、データを変換
+X_quad = quadratic.fit_transform(X)
+
+# データに適合させる
+lr.fit(X, y)
+# np.newaxisで列ベクトルにする
+X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+# 予測値を計算
+y_lin_fit = lr.predict(X_fit)
+
+# データに適合させる
+pr.fit(X_quad, y)
+# 2次式でyの値を計算
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+# 散布図、線形回帰モデル、多項式回帰モデルの結果をプロット
+plt.scatter(X, y, label='training points')
+plt.plot(X_fit, y_lin_fit, label='linear fit', linestyle='--')
+plt.plot(X_fit, y_quad_fit, label='quadratic fit')
+plt.legend(loc='upper left')
+plt.show()
+
+y_lin_pred = lr.predict(X)
+y_quad_pred = pr.predict(X_quad)
+print('Training MSE linear: %.3f, quadratic: %.3f' %
+        (mean_squared_error(y, y_lin_pred), mean_squared_error(y, y_quad_pred)))
+print('Training R^2 linear: %.3f, quadratic: %.3f' %
+        (r2_score(y, y_lin_pred), r2_score(y, y_quad_pred)))
