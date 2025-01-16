0a6145a157251d3321622974093c29dd219e7297
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-17 00:21:30 +0900

    Housingデータセットで非線形関係をモデリングする

--- chap10/housing.py
+++ chap10/housing.py
@@ -241,3 +241,62 @@ print('Training MSE linear: %.3f, quadratic: %.3f' %
         (mean_squared_error(y, y_lin_pred), mean_squared_error(y, y_quad_pred)))
 print('Training R^2 linear: %.3f, quadratic: %.3f' %
         (r2_score(y, y_lin_pred), r2_score(y, y_quad_pred)))
+
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+regr = LinearRegression()
+
+# 多項式の特徴量を作成
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
+
+# 線形回帰モデルの学習
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
+
+# 2次の多項式回帰モデルの学習
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+# 3次の多項式回帰モデルの学習
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+# 各モデルの結果をプロット
+plt.scatter(X, y, label='training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' %
+         linear_r2, color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_quad_fit, label='quadratic (d=2), $R^2=%.2f$' %
+         quadratic_r2, color='red', lw=2, linestyle='-')
+plt.plot(X_fit, y_cubic_fit, label='cubic (d=3), $R^2=%.2f$' %
+         cubic_r2, color='green', lw=2, linestyle='--')
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.legend(loc='upper right')
+plt.show()
+
+# 特徴量を変換
+X_log = np.log(X)
+y_sqrt = np.sqrt(y)
+
+# 特徴量への適合
+X_fit = np.arange(X_log.min() - 1, X_log.max() + 1, 1)[:, np.newaxis]
+regr = regr.fit(X_log, y_sqrt)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+# 射影したデータを使った学習結果をプロット
+plt.scatter(X_log, y_sqrt, label='training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' %
+         linear_r2, color='blue', lw=2)
+plt.xlabel('log(% lower status of the population [LSTAT])')
+plt.ylabel('$\sqrt{Price \; in \; \$1000\'s [MEDV]}$')
+plt.legend(loc='lower left')
+plt.show()
