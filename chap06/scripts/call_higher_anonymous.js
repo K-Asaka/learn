2db3b1a55828196c3813dc159dadb548f16bb3ea
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 12:26:20 +0900

    「使い捨ての関数」は匿名関数で

---
+++ chap06/scripts/call_higher_anonymous.js
@@ -0,0 +1,15 @@
+// 高階関数arrayWalkを定義
+function arrayWalk(data, callback) {
+	for (let [key, value] of data.entries()) {
+		// 引数callbackで指定された関数を呼び出し
+		callback(value, key);
+	}
+}
+
+let list = [1, 2, 4, 8, 16];
+arrayWalk(
+	list,
+	function (value, key) {
+		console.log(`${key}：${value}`);
+	}
+	);
