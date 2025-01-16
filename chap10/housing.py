206c97fe32bfdf273c3b3a8d643f1837b32c691f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-16 23:55:59 +0900

    線形回帰モデルの性能評価

--- chap10/housing.py
+++ chap10/housing.py
@@ -164,3 +164,33 @@ plt.show()
 
 print('Slope: %.3f' % ransac.estimator_.coef_[0].item())
 print('Intercept: %.3f' % ransac.estimator_.intercept_.item())
+
+# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+plt.scatter(y_train_pred, y_train_pred - y_train,
+            c='blue', marker='o', label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test,
+            c='lightgreen', marker='s', label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
+plt.show()
+
+from sklearn.metrics import mean_squared_error
+# 平均二乗誤差を出力
+print('MSE train: %.3f, test: %.3f' % (mean_squared_error(y_train, y_train_pred),
+                                       mean_squared_error(y_test, y_test_pred)))
+# R^2(決定係数のスコアを出力)
+from sklearn.metrics import r2_score
+print('R^2 train: %.3f, test: %.3f' % (r2_score(y_train, y_train_pred),
+                                       r2_score(y_test, y_test_pred)))
