4a3f55ecab908538db4c527ebc883303730bf7df
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-18 00:38:52 +0900

    k-means++法

--- chap11/kmeans.py
+++ chap11/kmeans.py
@@ -19,3 +19,32 @@ km = KMeans(n_clusters=3,           # クラスタの個数
             tol=1e-04,              # 収束と判定するための相対的な許容誤差
             random_state=0)         # セントロイドの初期化に用いる乱数生成器の状態
 y_km = km.fit_predict(X)
+
+
+plt.scatter(X[y_km==0,0],       # グラフのxの値
+            X[y_km==0,1],       # グラフのyの値
+            s=50,               # プロットのサイズ
+            c='lightgreen',     # プロットの色
+            marker='s',         # マーカーの形
+            label='cluster 1')  # ラベル名
+plt.scatter(X[y_km==1,0],
+            X[y_km==1,1],
+            s=50,
+            c='orange',
+            marker='o',
+            label='cluster 2')
+plt.scatter(X[y_km==2,0],
+            X[y_km==2,1],
+            s=50,
+            c='lightblue',
+            marker='v',
+            label='cluster 3')
+plt.scatter(km.cluster_centers_[:,0],
+            km.cluster_centers_[:,1],
+            s=250,
+            marker='*',
+            c='red',
+            label='centroids')
+plt.legend()
+plt.grid()
+plt.show()
