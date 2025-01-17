ea64b85f4f95f824548bf1a2a3b50edd94d3a1f5
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-17 23:55:03 +0900

    ランダムフォレスト回帰

--- chap10/housing.py
+++ chap10/housing.py
@@ -315,3 +315,44 @@ lin_regplot(X[sort_idx], y[sort_idx], tree)
 plt.xlabel('% lower status of the population [LSTAT]')
 plt.ylabel('Price in $1000\'s [MEDV]')
 plt.show()
+
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+from sklearn.ensemble import RandomForestRegressor
+# ランダムフォレスト回帰のクラスをインスタンス化
+# forest = RandomForestRegressor(n_estimators=1000, criterion='mse',
+forest = RandomForestRegressor(n_estimators=1000, criterion='squared_error',
+                                random_state = 1, n_jobs=-1)
+forest.fit(X_train, y_train)
+y_train_pred = forest.predict(X_train)
+y_test_pred = forest.predict(X_test)
+# MSE(平均以上誤差)を出力
+print('MSE train: %.3f, test: %.3f' % (mean_squared_error(y_train, y_train_pred),
+                                       mean_squared_error(y_test, y_test_pred)))
+# R^2(決定係数)を出力
+print('R^2 train: %.3f, test: %.3f' % (r2_score(y_train, y_train_pred),
+                                       r2_score(y_test, y_test_pred)))
+
+# 予測値と残差をプロット
+plt.scatter(y_train_pred,               # グラフのx値(予測値)
+            y_train_pred - y_train,     # グラフのy値(予測値とトレーニング値の差)
+            c='black',                  # プロットの色
+            marker='o',                 # マーカーの種類
+            s=35,                       # マーカーのサイズ
+            alpha=0.5,                  # 透明度
+            label='Training data')      # ラベルの文字
+plt.scatter(y_test_pred,
+            y_test_pred - y_test,
+            c='lightgreen',
+            marker='s',
+            s=35,
+            alpha=0.7,
+            label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
+plt.show()
