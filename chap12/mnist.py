2dfb4968bad1e92c669540e37401e1c3329e2c99
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-20 01:16:27 +0900

    勾配チェックを使ったニューラルネットワークのデバッグ

--- chap12/mnist.py
+++ chap12/mnist.py
@@ -118,3 +118,17 @@ ax[0].set_xticks([])
 ax[0].set_yticks([])
 plt.tight_layout()
 plt.show()
+
+from MLPGradientCheck import MLPGradientCheck
+nn_check = MLPGradientCheck(n_output=10,
+                            n_features=X_train.shape[1],
+                            n_hidden=10,
+                            l2=0.0,
+                            l1=0.0,
+                            epochs=10,
+                            eta=0.001,
+                            alpha=0.0,
+                            decrease_const=0.0,
+                            minibatches=1,
+                            random_state=1)
+nn_check.fit(X_train[:5], y_train[:5], print_progress=False)
