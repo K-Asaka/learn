b732ec662b0a391b11f0800731e4cca55597673e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-20 09:08:58 +0900

    静的プロパティの使いどころ

---
+++ chap08/scripts/class_static_prop.js
@@ -0,0 +1,24 @@
+class Member {
+	// インスタンスをキャッシュするための静的プロパティ
+	static cache = new Map();
+
+	constructor(name) {
+		this.name = name;
+	}
+
+	// インスタンスを生成するための静的メソッド
+	static of(name) {
+		// キャッシュが存在する場合は、そのまま返す
+		if (this.cache.has(name)) {
+			return this.cache.get(name);
+		}
+		// さもなければ、新規インスタンスを生成＆キャッシュに登録
+		let m = new Member(name);
+		this.cache.set(name, m);
+		return m;
+	}
+}
+
+let m1 = Member.of('田中譲二');
+let m2 = Member.of('田中譲二');
+console.log(m1 === m2);
