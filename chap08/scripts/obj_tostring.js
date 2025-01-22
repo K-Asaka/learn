ab662bb7c1f9460c97543ad950c501b3894687ba
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-22 19:12:27 +0900

    ユーザー定義型でのtoString／valueOfメソッド

---
+++ chap08/scripts/obj_tostring.js
@@ -0,0 +1,21 @@
+class Coordinate {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// 座標の文字列表現を返す
+	toString() {
+		return `(${this.x}, ${this.y})`;
+	}
+
+	// 座標の数値表現を返す
+	valueOf() {
+		return Math.sqrt(this.x ** 2 + this.y ** 2);
+	}
+}
+
+let c = new Coordinate(5, 2);
+console.log(`${c}`);
+console.log(+c);
+console.log(c + '');
