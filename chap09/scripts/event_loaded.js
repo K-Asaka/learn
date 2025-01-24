ff49f010e0b1a23abaa0d7f4d468212a9f48d93f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-24 09:08:37 +0900

    コード全体をDOMContentLoadedイベントリスナーでくくる

---
+++ chap09/scripts/event_loaded.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let pic = document.querySelector('#cover');
+	pic.addEventListener('mouseenter', function() {
+		this.src = 'images/haribo.jpg';
+	}, false);
+
+	pic.addEventListener('mouseleave', function() {
+		this.src = 'images/sauce.jpg';
+	}, false);
+}, false);
