c44c04c7389f33ab2f1140cf25d775f1e240aa92
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-29 23:52:00 +0900

    ページクリックで次の章へ移動する

--- chap10/scripts/scroll_basic.js
+++ chap10/scripts/scroll_basic.js
@@ -1,4 +1,14 @@
-document.querySelector('#main').addEventListener('click', function() {
-    // 表示サイズ分だけスクロール
-    window.scrollBy(0, window.innerHeight);
+// document.querySelector('#main').addEventListener('click', function() {
+//     // 表示サイズ分だけスクロール
+//     window.scrollBy(0, window.innerHeight);
+// }, false);
+
+document.querySelector('#main').addEventListener('click', function(e) {
+    // ターゲット要素の祖先でid="chap_XX">であるものを取得
+    let p = e.target.closest('[id^="chap"]');
+    if (p) {
+        // 移動先の章番号を生成＆移動
+        let next = `#chap_${Number(p.id.split('_')[1]) + 1}`;
+        document.querySelector(next)?.scrollIntoView(true);
+    }
 }, false);
