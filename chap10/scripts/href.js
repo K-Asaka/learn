6687cb4b9822c6c59b9ce74b5b24d74fe0455d2b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-29 23:59:26 +0900

    assignメソッド

--- chap10/scripts/href.js
+++ chap10/scripts/href.js
@@ -1,3 +1,3 @@
 document.querySelector('#isbn').addEventListener('change', function(e) {
-    location.replace(`https://wings.msn.to/index.php/-/A-03/${e.target.value}`);
+    location.assign(`https://wings.msn.to/index.php/-/A-03/${e.target.value}`);
 }, false);
