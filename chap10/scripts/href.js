7e373decfe93a6c1ae6ac7a4693f00055fdcb1f1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-29 23:58:41 +0900

    replaceメソッド

--- chap10/scripts/href.js
+++ chap10/scripts/href.js
@@ -1,3 +1,3 @@
 document.querySelector('#isbn').addEventListener('change', function(e) {
-    location.href = `https://wings.msn.to/index.php/-/A-03/${e.target.value}`;
+    location.replace(`https://wings.msn.to/index.php/-/A-03/${e.target.value}`);
 }, false);
