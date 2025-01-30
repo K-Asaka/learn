8cd8ae7fe6ada99fa4486c5ba4a9b8b052854d9b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-01-31 00:27:54 +0900

    異なるオリジンのドキュメントを操作した場合 - assignメソッド

--- chap10/scripts/location_assign.js
+++ chap10/scripts/location_assign.js
@@ -1,3 +1,3 @@
 document.querySelector('#btn').addEventListener('click', function() {
-    window.frames[0].location.href = 'https://example.com/';
+    window.frames[0].location.assign('https://example.com/');
 }, false);
