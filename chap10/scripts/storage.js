df01eb7235fed7a380a4698b8fdad1eb63c24291
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-02-12 00:13:03 +0900

    既存のデータを削除する

--- chap10/scripts/storage.js
+++ chap10/scripts/storage.js
@@ -7,3 +7,12 @@ storage['fruit3'] = 'ぶどう';
 console.log(storage.getItem('fruit1'));
 console.log(storage.fruit2);
 console.log(storage['fruit3']);
+
+
+storage.removeItem('fruit1');
+delete storage.fruit2;
+delete storage['fruit3']
+
+console.log(storage.getItem('fruit1'));
+console.log(storage.fruit2);
+console.log(storage['fruit3']);
