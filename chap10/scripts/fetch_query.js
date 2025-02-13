89c59e9ea3e43250001e54c580ecdb6325122faf
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-02-13 14:49:45 +0900

    クエリ情報でデータを送信する

---
+++ chap10/scripts/fetch_query.js
@@ -0,0 +1,11 @@
+let result = document.querySelector('#result');
+// [送信]ボタンクリック時に入力値を送信
+document.querySelector('#btn').addEventListener('click', function() {
+    // クエリ情報の組み立て
+    let params = new URLSearchParams();
+    params.set('name', document.querySelector('#name').value);
+    // クエリ情報を付与してリクエストを発信
+    fetch(`fetch_query.php?${params.toString()}`)
+        .then(res => res.text())
+        .then(text => result.textContent = text);
+}, false);
