2dc33884030f98ef3d2c61bed64ebb5828a3fc89
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-02-16 22:23:47 +0900

    Promiseの処理を同期的に記述する

---
+++ chap10/scripts/promise_async.js
@@ -0,0 +1,24 @@
+// 与えられた数値を自乗する非同期関数
+function asyncProcess(value) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // number型であればresolve、さもなくばreject
+            if (typeof value === 'number') {
+                resolve(value ** 2);
+            } else {
+                reject('引数valueは数値でなければいけません。');
+            }
+        }, 500);
+    });
+}
+
+async function main() {
+    let result1 = await asyncProcess(2);
+    let result2 = await asyncProcess(result1);
+    let result3 = await asyncProcess(result2);
+    return result3;
+}
+
+main()
+    .then(response => console.log(response))
+    .catch(error => console.log(`エラー： ${error}`));
