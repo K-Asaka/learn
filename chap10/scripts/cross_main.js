2c96f4a6f2577591f2b8ec161429dea1f7bd518f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-02-16 13:52:45 +0900

    メッセージを応答する

--- chap10/scripts/cross_main.js
+++ chap10/scripts/cross_main.js
@@ -6,3 +6,8 @@ let message = document.querySelector('#message');
 document.querySelector('#btn').addEventListener('click', function() {
     frame.contentWindow.postMessage(message.value, target);
 }, false);
+
+window.addEventListener('message', function(e) {
+    if (e.origin !== target) { return; }
+    console.log(e.data);
+}, false);
--- chap10/scripts/cross_other.js
+++ chap10/scripts/cross_other.js
@@ -4,4 +4,6 @@ let result = document.querySelector('#result');
 window.addEventListener('message', function(e) {
     if (e.origin !== origin) { return; }
     result.textContent = e.data;
+    // 現在の日時を返送
+    e.source.postMessage(new Date(), origin);
 }, false);
