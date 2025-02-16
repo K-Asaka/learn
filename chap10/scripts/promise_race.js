3799baba1410b85769ea7208ad62e2a609bb6397
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-02-16 22:10:28 +0900

    複数の非同期処理のどれかが完了したところでコールバックする

---
+++ chap10/scripts/promise_race.js
@@ -0,0 +1,21 @@
+function asyncProcess(value) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // 引数valueが未定義であるかどうかによって成否を判定
+            if (value) {
+                resolve(`入力値： ${value}`);
+            } else {
+                reject('入力は空です');
+            }
+        }, 500);
+    });
+}
+
+Promise.race([
+    asyncProcess('トクジロウ'),
+    asyncProcess('ニンザブロウ'),
+    asyncProcess('リンリン')
+])
+    .then(response => console.log(response))
+    .catch(error => console.log(`エラー： ${error}`));
+    
