359d43d92d2f5713840abd81c462895c9615660a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-04-27 15:58:27 +0900

    配列構造を操作する

--- chap13/theano_sample.py
+++ chap13/theano_sample.py
@@ -30,3 +30,58 @@ print(theano.config.device)
 # .theanorc
 # echo -e "\n[global]\nfloatX=float32\ndevice=gpu\n" >> ~/.theanorc
 
+
+import numpy as np
+
+# 初期化
+# Theanoを64ビットモードで実行している場合は、
+# fmatrixの代わりにdmatrixを使用する必要がある
+x = T.fmatrix(name='x')
+#x = T.dmatrix(name='x')
+x_sum = T.sum(x, axis=0)
+
+# コンパイル
+calc_sum = theano.function(inputs=[x], outputs=x_sum)
+
+# 実行(Pythonリスト)
+ary = [[1, 2, 3], [1, 2, 3]]
+print('Column sum:', calc_sum(ary))
+
+# 実行(NumPy配列)
+ary = np.array([[1, 2, 3], [1, 2, 3]], dtype=theano.config.floatX)
+print('Column sum:', calc_sum(ary))
+
+
+# 初期化
+x = T.fmatrix('x')
+w = theano.shared(np.asarray([[0.0, 0.0, 0.0]], dtype=theano.config.floatX))
+z = x.dot(w.T)
+update = [[w, w + 1.0]]
+
+# コンパイル
+net_input = theano.function(inputs=[x],
+								updates=update,
+								outputs=z)
+
+# 実行
+data = np.array([[1, 2, 3]], dtype=theano.config.floatX)
+for i in range(5):
+	print('z%d:' % i, net_input(data))
+
+
+# 初期化
+data = np.array([[1, 2, 3]], dtype=theano.config.floatX)
+x = T.fmatrix('x')
+w = theano.shared(np.asarray([[0.0, 0.0, 0.0]], dtype=theano.config.floatX))
+z = x.dot(w.T)
+update = [[w, w + 1.0]]
+
+# コンパイル
+net_input = theano.function(inputs=[],
+								updates=update,
+								givens={x: data},
+								outputs=z)
+
+# 実行
+for i in range(5):
+	print('z%d:' % i, net_input())
