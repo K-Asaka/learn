88a29c2ebe181d4e46ca852ac1cc48d77dbcd725
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-04-27 15:43:02 +0900

    Theanoを設定する

--- chap13/theano_sample.py
+++ chap13/theano_sample.py
@@ -13,3 +13,20 @@ net_input = theano.function(inputs=[w1, x1, w0], outputs=z1)
 
 # 実行
 print('Net input: %.2f' % net_input(2.0, 1.0, 0.5))
+
+
+print(theano.config.floatX)
+
+theano.config.floatX = 'float32'
+print(theano.config.floatX)
+# bash
+# export THEANO_FLAGS=floatX=float32
+# THEANO_FLAGS=floatX=float32 python <Pythonスクリプト>
+
+print(theano.config.device)
+# THEANO_FLAGS=device=cpu,floatX=float64 python <Pythonスクリプト>
+# THEANO_FLAGS=device=gpu,floatX=float32 python <Pythonスクリプト>
+
+# .theanorc
+# echo -e "\n[global]\nfloatX=float32\ndevice=gpu\n" >> ~/.theanorc
+
