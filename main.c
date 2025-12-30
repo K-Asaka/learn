fa54a5b1e3c9b2733a7405fb369d4291bdd11a7f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2025-12-30 23:51:34 +0900

    プログラムのソースコードを挿入する - 簡単に使えるverbatim環境

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  int n;
+
+  printf("回数を入力 > ");
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    printf("Hello LaTeX!!\n");
+  }
+
+  return 0;
+}
+
