7ba45bedae295e8e03454c8438ba33c1388c45bd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-01 23:36:51 +0900

    constの性質

---
+++ my-modern/const.js
@@ -0,0 +1,9 @@
+const author = 'YAMADA.Yoshihiro';
+//author = 'WINGS Project';     // 定数に再代入はできない
+
+const list = ['React', 'Vue', 'Angular']
+console.log(list);
+list[0] = 'Riot';               // 中身の要素だけを入れ替えている
+console.log(list);
+
+//list = ['Java', 'C#', 'Python'];  // 定数に再代入している
