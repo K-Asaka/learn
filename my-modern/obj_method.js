1829568fda3514b4e97b99c7138b3d3d03e1582e
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-01 23:47:39 +0900

    オブジェクトリテラルの簡易構文：メソッドの簡易構文

---
+++ my-modern/obj_method.js
@@ -0,0 +1,11 @@
+const member = {
+    name: '佐藤理央',
+    // greet: function() {
+    //     console.log(`こんにちは、${this.name}さん！`);
+    // }
+    greet() {
+        console.log(`こんにちは、${this.name}さん！`);
+    }
+}
+
+member.greet();
