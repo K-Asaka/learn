c73529f4ace7c12cc0143d37f58ea461307d3fcc
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-04 00:24:00 +0900

    宣言とエクスポートを分離

--- my-modern/App.js
+++ my-modern/App.js
@@ -1,11 +1,13 @@
 const APP_TITLE = 'Reactアプリ';
 
-export function getTriangle(base, height) {
+function getTriangle(base, height) {
     return base * height / 2;
 }
 
-export class Article {
+class Article {
     getAppTitle() {
         return APP_TITLE;
     }
 }
+
+export { getTriangle, Article };
