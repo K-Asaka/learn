58db8039674ba09ee79243efe2a1aa67e6b2eeb9
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-05 15:29:54 +0900

    Stateの基本

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import EventBasic from './EventBasic';
+import StateBasic from './StateBasic'
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <EventBasic type="time" />
+  <StateBasic init={0} />
 );
 
 // If you want to start measuring performance in your app, pass a function
