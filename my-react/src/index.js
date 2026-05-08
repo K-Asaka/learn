0ace8c66a44dc60ecadf9060ab53e79906ac16d2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 07:33:12 +0900

    イベントハンドラーを初回のみ実行する

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import EventPropagation from './EventPropagation';
+import EventOnce from './EventOnce';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <EventPropagation />
+  <EventOnce />
 );
 
 // If you want to start measuring performance in your app, pass a function
