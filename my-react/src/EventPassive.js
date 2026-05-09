1dbbf4c7b0ae52c1d062d547dd95b0461b9cc917
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 11:36:06 +0900

    非Passiveモードでイベントハンドラーを設置する(エラー無し)

--- my-react/src/EventPassive.js
+++ my-react/src/EventPassive.js
@@ -1,10 +1,23 @@
+import { useRef, useEffect } from 'react';
 import './EventPassive.css';
 
 export default function EventPassive() {
     const handleWheel = e => e.preventDefault();
+    // <div>要素への参照を取得
+    const divRef = useRef(null);
+    useEffect(() => {
+        // コンポーネント起動時にリスナーを設定
+        const div = divRef.current;
+        div.addEventListener('wheel', handleWheel, { passive: false });
+        return (() => {
+            // コンポーネント破棄時にリスナーも破棄
+            div.removeEventListener('wheel', handleWheel);
+        });
+    });
+
     return (
-        <div className="box"
-            onWheel={handleWheel}>たとえばWheelイベントをハンドラーで…
+        <div ref={divRef} className="box">
+            たとえばWheelイベントをハンドラーで…
         </div>
     );
 }
