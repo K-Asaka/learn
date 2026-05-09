4b970b9b7123a65449ee334bcde5a04020956197
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 19:22:54 +0900

    配列の並べ替え - Todo項目の昇順／降順ソート

--- my-react/src/StateTodo.js
+++ my-react/src/StateTodo.js
@@ -4,6 +4,8 @@ import './StateTodo.css';
 // Todo項目idの最大値(登録都度にインクリメント)
 let maxId = 0;
 export default function StateTodo() {
+    // 次のソート方向(降順であればtrue)
+    const [desc, setDesc] = useState(true);
     // 入力値(title)、Todoリスト(todo)をStateで管理
     const [title, setTitle] = useState('');
     const [todo, setTodo] = useState([]);
@@ -48,6 +50,23 @@ export default function StateTodo() {
         ));
     };
 
+    const handleSort = e => {
+        // 既存のTodoリストを複製の上でソート
+        const sorted = [...todo];
+        sorted.sort((m, n) => {
+            // desc値に応じて昇順／降順を決定
+            if (desc) {
+                return n.created.getTime() - m.created.getTime();
+            } else {
+                return m.created.getTime() - n.created.getTime();
+            }
+        });
+        // desc値を反転
+        setDesc(d => !d);
+        // ソート済みのリストを再セット
+        setTodo(sorted);
+    };
+
     return (
         <div>
             <label>
@@ -57,6 +76,10 @@ export default function StateTodo() {
             </label>
             <button type="button"
                 onClick={handleClick}>追加</button>
+            {/* desc値に応じてキャプションを変更 */}
+            <button type="button"
+                onClick={handleSort}>
+                ソート({desc ? '↑' : '↓'})</button>
             <hr />
             {/* Todoをリストに整形 */}
             <ul>
