aff05e40f46f05ab33aaec3045f3f9c86f1221e0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 19:42:03 +0900

    React Hook Formの基本

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import StateTodo from './StateTodo';
+import FormBasic from './FormBasic';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateTodo />
+  <FormBasic />
 );
 
 // If you want to start measuring performance in your app, pass a function
