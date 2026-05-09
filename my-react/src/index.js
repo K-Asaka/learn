cd3b826c0b20e901dcec4b13f25b8874ea6c9114
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 15:56:59 +0900

    チェックボックス(単一選択)

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import FormRadio from './FormRadio';
+import FormCheck from './FormCheck';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <FormRadio />
+  <FormCheck />
 );
 
 // If you want to start measuring performance in your app, pass a function
