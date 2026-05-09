9225b2bd1a99881b2232db433ab6c217ad8b975b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-10 00:56:13 +0900

    汎用的な検証ルールを追加する

--- my-react/src/FormYup.js
+++ my-react/src/FormYup.js
@@ -2,6 +2,22 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+// ngルールを追加
+yup.addMethod(yup.string, 'ng', function() {
+    return this.test('ng',
+        ({ label }) => `${label}にNGワードが含まれています`,
+        value => {
+            const ngs = ['暴力', '死', 'グロ'];
+            for (const ng of ngs) {
+                if (value.includes(ng)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    );
+});
+
 // 検証ルールを準備
 const schema = yup.object({
     name: yup
@@ -23,19 +39,7 @@ const schema = yup.object({
         .label('備考')
         .required('${label}は必須入力です。')
         .min(10, '${label}は${min}文字以上で入力してください。')
-        .test('ng',
-            ({ label }) => `${label}にNGワードが含まれています`,
-            value => {
-                // 不適切ワードを準備
-                const ngs = ['暴力', '死', 'グロ'];
-                // 入力文字列に不適切ワードが含まれているかを判定
-                for (const ng of ngs) {
-                    if (value.includes(ng)) {
-                        return false;
-                    }
-                }
-                return true;
-        })
+        .ng()
 });
 
 export default function FormYup() {
