dd6b73223525970801bba227473508b94b562367
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 14:18:50 +0900

    非制御コンポーネントによるフォーム管理

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import StateForm from './StateForm';
+import StateFormUC from './StateFormUC';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateForm />
+  <StateFormUC />
 );
 
 // If you want to start measuring performance in your app, pass a function
