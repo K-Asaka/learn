f922bc0e5874996e9345b0e52d7c4e7cdeff43ac
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-09 17:07:27 +0900

    Immerライブラリによる改善

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import StateNest from './StateNest';
+import StateNestImmer from './StateNestImmer';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateNest />
+  <StateNestImmer />
 );
 
 // If you want to start measuring performance in your app, pass a function
