ec1760ab2c95128890dfe23b5e99d38465f6592d
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-10 12:26:42 +0900

    Promiseによる非同期処理を受け取る

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import LazyMulti from './LazyMulti';
+import SuspenseSimple from './SuspenseSimple';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <LazyMulti />
+  <SuspenseSimple />
 );
 
 // If you want to start measuring performance in your app, pass a function
