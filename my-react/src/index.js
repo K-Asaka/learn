acd665e89c961bb19bfeea474558f2f79572a5bd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-10 17:06:48 +0900

    既存のコンポーネントにスタイル付けする

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import StyledComp from './StyledComp';
+import { MyButton, MyStyledButton } from './StyledComp2';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,10 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StyledComp />
+  <>
+    <MyButton>ボタン</MyButton>
+    <MyStyledButton>ボタン</MyStyledButton>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
