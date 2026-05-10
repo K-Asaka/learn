d83dda248765be772bd0fc8fcda4b11ea05a200c
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-11 00:10:21 +0900

    大文字／小文字を修正する

--- my-react/src/MaterialBasic.js
+++ my-react/src/MaterialBasic.js
@@ -1,11 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { Button } from '@mui/material';
 
 export default function MaterialBasic() {
+    // テキストの大文字／小文字変換を無効化
+    const font = css`
+        text-transform: none;
+    `;
     return (
         <>
-            <Button variant="text" color="secondary">Text</Button>
-            <Button variant="contained" color="secondary">Contained</Button>
-            <Button variant="outlined" color="secondary">Outlined</Button>
+            <Button variant="text" css={font}>Text</Button>
+            <Button variant="contained" css={font}>Contained</Button>
+            <Button variant="outlined" css={font}>Outlined</Button>
         </>
     );
 }
