33f9ce106bfa6be480798a695cee3bb2f0411597
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-10 17:12:11 +0900

    スタイル定義を外部化する

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { MyButton, MyStyledButton } from './StyledComp2';
+import StyledCommon from './StyledCommon';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,10 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <MyButton>ボタン</MyButton>
-    <MyStyledButton>ボタン</MyStyledButton>
-  </>
+  <StyledCommon />
 );
 
 // If you want to start measuring performance in your app, pass a function
