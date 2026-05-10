5948a621714a99d450215a7a5ab774cb710e63f1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-10 14:17:40 +0900

    Styled JSXの基本

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import ProfilerBasic from './ProfilerBasic';
+import StyledBasic from './StyledBasic';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,10 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProfilerBasic />
+  <>
+    <h3>Styled JSXの基本</h3>
+    <StyledBasic />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
