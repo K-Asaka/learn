63de8325e7465a21d4aca368abeb5a6740cba612
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-13 00:15:00 +0900

    [Controls]タブの制御

--- my-react/src/MyButton.js
+++ my-react/src/MyButton.js
@@ -1,5 +1,9 @@
+import PropTypes from 'prop-types';
 import './stories/button.css';
 
+/**
+ * 属性の設定に応じて種々のボタンを生成
+ */
 export default function MyButton({
     primary = false,
     backgroundColor = null,
@@ -20,4 +24,28 @@ export default function MyButton({
             {label}
         </button>
     );
+};
+
+// Propsの型情報を宣言
+MyButton.propTypes = {
+    /**
+     * Primaryカラーを有効にするか
+     */
+    primary: PropTypes.bool,
+    /**
+     * 背景色
+     */
+    backgroundColor: PropTypes.string,
+    /**
+     * ボタンの大きさ
+     */
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    /**
+     * ボタンのキャプション
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     * clickハンドラー
+     */
+    onClick: PropTypes.func,
 };
