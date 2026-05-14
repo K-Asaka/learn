de83f119a3b8ae5ecbd4f52ee0e96417c8755d4f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-14 23:37:51 +0900

    アプリ全体でhandleXxxx属性を有効にする

--- my-react/.storybook/preview.js
+++ my-react/.storybook/preview.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/react-webpack5').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: "^(on|handle)[A-Z].*" },
     controls: {
       matchers: {
        color: /(background|color)$/i,
--- my-react/src/MyButton.js
+++ my-react/src/MyButton.js
@@ -9,6 +9,7 @@ export default function MyButton({
     backgroundColor = null,
     size = 'medium',
     label = 'Button',
+    handleClick,
     ...props
 }) {
     // primary属性に応じてスタイルクラスを決定
@@ -19,6 +20,7 @@ export default function MyButton({
             type="button"
             className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
             style={backgroundColor && { backgroundColor }}
+            onClick={handleClick}
             {...props}
         >
             {label}
@@ -47,5 +49,5 @@ MyButton.propTypes = {
     /**
      * clickハンドラー
      */
-    onClick: PropTypes.func,
+    handleClick: PropTypes.func,
 };
