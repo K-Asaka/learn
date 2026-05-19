1dbdf56db2b5206df6a1b7631d2bb98f2325af45
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-19 23:18:32 +0900

    コンポーネント単位で設定する

--- my-react/.storybook/preview.js
+++ my-react/.storybook/preview.js
@@ -8,13 +8,6 @@ const preview = {
         date: /Date$/i,
       },
     },
-    backgrounds: {
-      values: [
-        { name: 'ghotwhite', value: '#f8f8ff' },
-        { name: 'aquamarine', value: '#7fffd4' },
-        { name: 'coral', value: '#ff7f50' },
-      ],
-    },
   },
 };
 
--- my-react/src/MyButton.stories.js
+++ my-react/src/MyButton.stories.js
@@ -33,6 +33,15 @@ export default {
         },
         handleClick: { action: 'clicked' },
     },
+    parameters: {
+        backgrounds: {
+            values: [
+                { name: 'ghostwhite', value: '#f8f8ff' },
+                { name: 'aquamarine', value: '#7fffd4' },
+                { name: 'coral', value: '#ff7f50' },
+            ],
+        },
+    }
 };
 
 // Index、Whiteストーリーを追加
