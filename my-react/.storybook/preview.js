82bdd203c231d242f4a797e418115f87530fd951
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-19 23:16:07 +0900

    ストーリーの背景色を設定する

--- my-react/.storybook/preview.js
+++ my-react/.storybook/preview.js
@@ -8,6 +8,13 @@ const preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      values: [
+        { name: 'ghotwhite', value: '#f8f8ff' },
+        { name: 'aquamarine', value: '#7fffd4' },
+        { name: 'coral', value: '#ff7f50' },
+      ],
+    },
   },
 };
 
