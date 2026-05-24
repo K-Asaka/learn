0d075e304ca528a54d84abc9785e16dc94193009
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-24 19:22:52 +0900

    コールバック関数をref属性に引き渡す：コールバックRefを使わない例

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import HookRefForward from './HookRefForward';
+import HookCallbackRef from './HookCallbackRef';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HookRefForward />
+  <HookCallbackRef />
 );
 
 // If you want to start measuring performance in your app, pass a function
