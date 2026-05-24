5ad34356b218a061e18b0957b7df05bc17fb175f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-24 19:45:50 +0900

    Stateの初期値を生成する場合の注意点：意味のない関数呼び出し

---
+++ my-react/src/HookReducerInit.js
@@ -0,0 +1,34 @@
+import { useReducer } from 'react';
+
+// 引数に基づいて0～initの間の値を生成
+function initCounter(init) {
+    console.log('initCounter is called!!');
+    return {
+        count: Math.floor(Math.random() * (init + 1))
+    };
+}
+
+export default function HookReducerInit({ init }) {
+    const [state, dispatch] = useReducer(
+        (state, action) => {
+            switch (action.type) {
+                case 'update':
+                    return { count: state.count + 1 };
+                default:
+                    return state;
+            }
+        },
+        initCounter(init),
+    );
+
+    // Reducer経由でStateを更新
+    const handleClick = () => {
+        dispatch({ type: 'update' });
+    };
+    return (
+        <>
+            <button onClick={handleClick}>カウント</button>
+            <p>{state.count}回、クリックされました。</p>
+        </>
+    );
+}
