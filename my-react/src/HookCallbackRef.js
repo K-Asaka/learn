42a0a198e0cc185f9c57c46313134687f6b11106
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-24 19:25:05 +0900

    コールバック関数をref属性に引き渡す

--- my-react/src/HookCallbackRef.js
+++ my-react/src/HookCallbackRef.js
@@ -2,16 +2,9 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function HookCallbackRef() {
     const [show, setShow] = useState(false);
-    // ボタンクリックで表示／非表示を反転
     const handleClick = () => setShow(!show);
-    // [住所]欄への参照
-    const address = useRef(null);
-    // [住所]項目が空でなければフォーカスを移動
-    useEffect(() => {
-        if (address.current) {
-            address.current.focus();
-        }
-    }, [show]);
+    // コールバックRefを準備
+    const callbackRef = elem => elem?.focus();
 
     return (
         <>
@@ -24,11 +17,10 @@ export default function HookCallbackRef() {
                 <input id="email" type="text" />
                 <button onClick={handleClick}>拡張表示</button>
             </div>
-            {/* State(show)値に応じて[住所]欄を表示 */}
             {show &&
                 <div>
                     <label htmlFor="address">住所：</label>
-                    <input id="address" type="text" ref={address} />
+                    <input id="address" type="text" ref={callbackRef} />
                 </div>
             }
        </>
