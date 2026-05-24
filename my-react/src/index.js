a29f476e50c426f8f5b7458abd8276e3565c2cd1
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-24 18:21:21 +0900

    useEffect関数の基本

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,8 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import React, { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import QuerySuspense from './QuerySuspense';
+import StateEffect from './StateEffect';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -11,23 +8,8 @@ import reportWebVitals from './reportWebVitals';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// Suspenseモードを有効化
-const cli = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
-
 root.render(
-  <Suspense fallback={<p>Loading...</p>}>
-    <ErrorBoundary fallback={<div>エラーが発生しました</div>}>
-      <QueryClientProvider client={cli}>
-        <QuerySuspense />
-      </QueryClientProvider>
-    </ErrorBoundary>
-  </Suspense>
+  <StateEffect init={0} />
 );
 
 // If you want to start measuring performance in your app, pass a function
