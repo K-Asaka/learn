07e6049d37b9eb459f0f02f0d08defd93a386ec2
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-24 19:46:52 +0900

    Stateの初期値を生成する場合の注意点

--- my-react/src/HookReducerInit.js
+++ my-react/src/HookReducerInit.js
@@ -18,7 +18,8 @@ export default function HookReducerInit({ init }) {
                     return state;
             }
         },
-        initCounter(init),
+        init,
+        initCounter
     );
 
     // Reducer経由でStateを更新
