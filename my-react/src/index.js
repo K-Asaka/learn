f671b0f73273c41f686049adeb8e4543177acf13
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-30 18:51:34 +0900

    パスの一部をパラメーターとして引き渡す

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import routesLink from './routesLink';
+import routesParam from './routesParam';
 // アプリ固有のコードをインポート
 import './index.css';
 
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // React Routerを有効化
 root.render(
-  <RouterProvider router={routesLink} />
+  <RouterProvider router={routesParam} />
 );
 
 // If you want to start measuring performance in your app, pass a function
