bf00a4eefcac9763fe881b0d3342ef152cfd5e3f
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-30 13:05:00 +0900

    レイアウトルート

--- my-react/src/routesLink.js
+++ my-react/src/routesLink.js
@@ -4,14 +4,14 @@ import TopPage from './TopPage';
 import ArticlePage from './ArticlePage';
 import AboutPage from './AboutPage';
 
-const routesLink = createBrowserRouter([
-    { path: '/', element: <RouterApp />,
-        children: [
-            { path: '', element: <TopPage /> },
-            { path: "article", element: <ArticlePage /> },
-            { path: "about", element: <AboutPage /> },
-        ]
-    }
-]);
+const routesLink = createBrowserRouter(
+    createRoutesFromElements(
+        <Route element={<RouterApp />}>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/about" element={<AboutPage />} />
+        </Route>
+    )
+);
 
 export default routesLink;
