517c4fa71a4b727f511b1733446be2a4ea666342
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-30 18:55:50 +0900

    省略可能なパラメーター

--- my-react/src/routesParam.js
+++ my-react/src/routesParam.js
@@ -8,7 +8,7 @@ const routesParam = createBrowserRouter (
     createRoutesFromElements(
         <Route element={<RouterParam />}>
             <Route path="/" element={<TopPage />} />
-            <Route path="/book/:isbn" element={<BookPage />} />
+            <Route path="/book/:isbn?" element={<BookPage />} />
         </Route>
     )
 );
