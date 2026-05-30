00125504cc668343bb867b8bf593f766a3b222bd
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-05-30 13:58:30 +0900

    相対パスを生成する

--- my-react/src/routesLink.js
+++ my-react/src/routesLink.js
@@ -1,14 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import RouterNav from './RouterNav';
+import RouterApp from './RouterApp';
 import TopPage from './TopPage';
 import ArticlePage from './ArticlePage';
 import AboutPage from './AboutPage';
+import InfoPage from './InfoPage';
 
 const routesLink = createBrowserRouter(
     createRoutesFromElements(
-        <Route element={<RouterNav />}>
+        <Route element={<RouterApp />}>
             <Route path="/" element={<TopPage />} />
             <Route path="/article" element={<ArticlePage />} />
+            <Route path="/article/info" element={<InfoPage />} />
             <Route path="/about" element={<AboutPage />} />
         </Route>
     )
