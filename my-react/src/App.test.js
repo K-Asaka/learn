fc127416417e0ec60946bc0f91b0967e2421d804
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 07:39:42 +0900

    不要な部分を削除

--- my-react/src/App.test.js
+++ my-react/src/App.test.js
@@ -8,8 +8,6 @@ test('renders learn react link', () => {
     container: document.body.appendChild(para)
   });
   debug(baseElement);
-  // コンポーネントを描画
-  render(<App />);
   // テスト対象の要素を検索＆取得
   const linkElement = screen.getByText(/learn react/i);
   // 描画結果が正しいかを検証
