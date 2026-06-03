c3142d211d73a7e3a8a8664e6d9baeed73cb276a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 19:22:47 +0900

    子要素群(children)の型

--- my-react-ts/src/index.tsx
+++ my-react-ts/src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import StateBasic from './StateBasic';
+import StyledPanel from './StyledPanel';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <StateBasic init={0} />
+    <StyledPanel children={undefined} />
   </React.StrictMode>
 );
 
