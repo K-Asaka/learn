e09e500ab9d03d4ffd64c01e502e53ce9ee7e5f0
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 19:32:27 +0900

    子要素として関数を引き渡す場合

--- my-react-ts/src/index.tsx
+++ my-react-ts/src/index.tsx
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import StyledPanel from './StyledPanel';
+import ListTemplate from './ListTemplate';
+import books from './books';
+import type { Book } from './Book';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <StyledPanel children={undefined} />
-  </React.StrictMode>
+  <ListTemplate src={books}>
+    {(elem: Book) => (
+      <>
+        <dt>
+          <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+            {elem.title} ({elem.price}円)
+          </a>
+        </dt>
+        <dd>{elem.summary}</dd>
+      </>
+    )}
+  </ListTemplate>
 );
 
 // If you want to start measuring performance in your app, pass a function
