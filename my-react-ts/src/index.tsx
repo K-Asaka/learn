ddb3d5f7c41418d57311bc6195c02ffa7bbd0f5b
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 20:51:58 +0900

    fetchデータへの型定義

--- my-react-ts/src/index.tsx
+++ my-react-ts/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import StateFormUC from './StateFormUC';
+import QueryPre from './QueryPre';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StateFormUC />
+  <QueryPre />
 );
 
 // If you want to start measuring performance in your app, pass a function
