2754feb31d96e64894117ff9e8242dc9dfd84e76
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 20:41:12 +0900

    useRefの型定義

--- my-react-ts/src/index.tsx
+++ my-react-ts/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import HookReducerUp from './HookReducerUp';
+import StateFormUC from './StateFormUC';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HookReducerUp init={0} />
+  <StateFormUC />
 );
 
 // If you want to start measuring performance in your app, pass a function
