2592450923b4f1a544d14bfb76d132d848e88fc7
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 19:18:26 +0900

    Props型の基本

---
+++ my-react-ts/src/StateBasic.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+// Propsの型宣言
+type StateBasicProps = {
+    init: number
+};
+
+export default function StateBasic({ init }: StateBasicProps) {
+    const [count, setCount] = useState(init);
+    console.log(`count is ${count}`);
+    const handleClick = () => {
+        setCount(c => c + 1);
+    }
+    return (
+        <>
+            <button onClick={handleClick}>カウント</button>
+            <p>{count}回、クリックされました。</p>
+        </>
+    );
+}
--- my-react-ts/src/index.tsx
+++ my-react-ts/src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import StateBasic from './StateBasic';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StateBasic init={0} />
   </React.StrictMode>
 );
 
