a4cc190583ac283545def4462fc9da7bc91f8c0a
KatsuhiroAsaka <asaka@chiba-fjb.ac.jp>
2026-06-03 18:25:40 +0900

    E2Eテストの実行

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import routesScroll from './routesScroll';
+import routesParam from './routesParam';
 
 // アプリ固有のコードをインポート
 import './index.css';
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={routesScroll} />
+  <RouterProvider router={routesParam} />
 );
 
 // If you want to start measuring performance in your app, pass a function
